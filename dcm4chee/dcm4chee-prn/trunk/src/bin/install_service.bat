79eb6b552256bad985da9d98e994a053b3195338
gunter <gunter@a605150a-d416-0410-a114-93ca8beb8bc8>
2003-01-14 00:27:08 +0000

    add dasho config for Linux

--- dcm4chee/dcm4chee-prn/trunk/src/bin/install_service.bat
+++ dcm4chee/dcm4chee-prn/trunk/src/bin/install_service.bat
@@ -3,22 +3,21 @@ if "%1" == "uninstall" goto uninstall
 if "%1" == "-uninstall" goto uninstall
 if "%1" == "" goto usage
 if "%2" == "" goto usage
-if "%3" == "" goto usage
 if "%1" == "-help" goto usage
 if "%1" == "-?" goto usage
 if "%1" == "/?" goto usage
 :install
-JavaService.exe -install prnscp-1.0.0 %1\jre\bin\%3\jvm.dll -Xmx128m -Djava.class.path=%1\lib\tools.jar;%2\bin\run.jar -start org.jboss.Main -stop org.jboss.Main -method systemExit -out %2\bin\out.txt -current %2\bin
+JavaService.exe -install prnscp-1.0.0 %1\jre\bin\server\jvm.dll -Xmx128m -Djava.class.path=%1\lib\tools.jar;%2\bin\run.jar -start org.jboss.Main -stop org.jboss.Main -method systemExit -out %2\bin\out.txt -current %2\bin
 goto eof
 :uninstall
 JavaService.exe -uninstall prnscp-1.0.0
 goto eof
 :usage
 echo -------- To Install prnscp 1.0.0 do
-echo Usage: %0 jdk_home prnscp_home (client/server)
+echo Usage: %0 jdk_home prnscp_home
 echo NOTE: You MAY NOT use spaces in the path names. If you know how
 echo to fix this, please tell me.
-echo Example: %0 c:\progra~1\jdk c:\progra~1\prnscp-1.0.0 server
+echo Example: %0 c:\progra~1\j2sdk1.4.1_01 c:\progra~1\prnscp-1.0.0
 echo --------
 echo -------- To Uninstall prnscp 1.0.0 do
 echo Usage: %0 uninstall
