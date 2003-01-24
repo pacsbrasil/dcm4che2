a1c4634a9db9fc3b0bf7868f8f9f59c2d24d8b6f
gunter <gunter@a605150a-d416-0410-a114-93ca8beb8bc8>
2003-01-24 22:01:29 +0000

    direct PrinterServiceLookup instead access PrinterServiceMBean via RMIConnector

--- dcm4chee/dcm4chee-prn/trunk/src/java/com/tiani/prnscp/client/GetAvailablePrinters.java
+++ dcm4chee/dcm4chee-prn/trunk/src/java/com/tiani/prnscp/client/GetAvailablePrinters.java
@@ -1,43 +1,54 @@
-/*
- * JMXBrowser.java
- *
- * Created on January 16, 2003, 3:13 PM
- */
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright (c) 2002 by TIANI MEDGRAPH AG                                  *
+ *                                                                           *
+ *  This file is part of dcm4che.                                            *
+ *                                                                           *
+ *  This library is free software; you can redistribute it and/or modify it  *
+ *  under the terms of the GNU Lesser General Public License as published    *
+ *  by the Free Software Foundation; either version 2 of the License, or     *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This library is distributed in the hope that it will be useful, but      *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
+ *  Lesser General Public License for more details.                          *
+ *                                                                           *
+ *  You should have received a copy of the GNU Lesser General Public         *
+ *  License along with this library; if not, write to the Free Software      *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
+ *                                                                           *
+ *****************************************************************************/
 
 /**
+ * <description>
  *
- * @author  jforaci
+ * @see <related>
+ * @author  <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
+ * @version $Revision$
+ * @since January 10, 2003
+ *
+ * <p><b>Revisions:</b>
+ *
+ * <p><b>yyyymmdd author:</b>
+ * <ul>
+ * <li> explicit fix description (no line numbers but methods) go
+ *            beyond the cvs commit message
+ * </ul>
  */
 package com.tiani.prnscp.client;
 
-import java.net.InetAddress;
-import java.util.Hashtable;
-import javax.management.MBeanInfo;
-import javax.management.MBeanOperationInfo;
-import javax.management.MBeanParameterInfo;
-import javax.management.ObjectName;
-import javax.naming.InitialContext;
-
-import org.jboss.jmx.adaptor.rmi.RMIAdaptor;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.DocFlavor;
 
 public class GetAvailablePrinters {
    public static void main(String[] args) throws Exception {
-      String serviceName = "dcm4chex:service=Printer,aet=TIANI_PRINT";
-      if (args.length > 0) {
-         serviceName = "dcm4chex:service=Printer,aet=" + args[0];
-      }
-      String serviceHost = InetAddress.getLocalHost().getHostName();
-      Hashtable env = new Hashtable();
-      env.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
-      env.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
-      env.put("java.naming.provider.url", "localhost");
-      InitialContext ic = new InitialContext(env);
-      RMIAdaptor server = (RMIAdaptor) ic.lookup("jmx:" + serviceHost + ":rmi");
-      String[] printers = (String[]) server.getAttribute(
-         new ObjectName(serviceName), "AvailablePrinters");
+      PrintService[] services =  PrintServiceLookup.lookupPrintServices(
+         DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
       System.out.println("Available Printers:");
-      for (int i = 0; i < printers.length; ++i) {
-         System.out.println(printers[i]);
+      for (int i = 0; i < services.length; ++i) {
+         System.out.println(services[i].getName());
       }
    }
 }
