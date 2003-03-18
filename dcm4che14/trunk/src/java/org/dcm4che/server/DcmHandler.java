026c145c9b20903ddbe13b09f964de04d7bcf04e
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2003-03-18 08:53:09 +0000

    add property PackPDVs

--- dcm4che14/trunk/src/java/org/dcm4che/server/DcmHandler.java
+++ dcm4che14/trunk/src/java/org/dcm4che/server/DcmHandler.java
@@ -1,5 +1,4 @@
-/*****************************************************************************
- *                                                                           *
+/*                                                                           *
  *  Copyright (c) 2002 by TIANI MEDGRAPH AG                                  *
  *                                                                           *
  *  This file is part of dcm4che.                                            *
@@ -17,9 +16,7 @@
  *  You should have received a copy of the GNU Lesser General Public         *
  *  License along with this library; if not, write to the Free Software      *
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
- *                                                                           *
- *****************************************************************************/
-
+ */
 package org.dcm4che.server;
 
 import org.dcm4che.net.AcceptorPolicy;
@@ -27,62 +24,122 @@ import org.dcm4che.net.AssociationListener;
 import org.dcm4che.net.DcmServiceRegistry;
 
 /**
- * <description> 
- *
- * @see <related>
- * @author  <a href="mailto:{email}">{full name}</a>.
- * @author  <a href="mailto:gunter@tiani.com">Gunter Zeilinger</a>
- * @version $Revision$ $Date$
- *   
- * <p><b>Revisions:</b>
- *
- * <p><b>20020810 gunter:</b>
- * <ul>
- * <li> add properties rqTimeout, dimseTimeout, soCloseDelay
- * </ul>
+ *@author     <a href="mailto:gunter@tiani.com">Gunter Zeilinger</a>
+ *@created    June, 2002
+ *@version    $Revision$ $Date$
  */
 public interface DcmHandler extends Server.Handler
 {
-   void addAssociationListener(AssociationListener l);
-   
-   void removeAssociationListener(AssociationListener l);
+    /**
+     *  Adds a feature to the AssociationListener attribute of the DcmHandler
+     *  object
+     *
+     *@param  l  The feature to be added to the AssociationListener attribute
+     */
+    void addAssociationListener(AssociationListener l);
 
-   void setAcceptorPolicy(AcceptorPolicy policy);
-   
-   AcceptorPolicy getAcceptorPolicy();
 
-   void setDcmServiceRegistry(DcmServiceRegistry services);
-   
-   DcmServiceRegistry getDcmServiceRegistry();
-   
-   /** Getter for property rqTimeout.
-    * @return Value of property rqTimeout.
-    */
-   int getRqTimeout();
-   
-   /** Setter for property rqTimeout.
-    * @param rqTimeout New value of property rqTimeout.
-    */
-   void setRqTimeout(int timeout);
-   
-   /** Getter for property dimseTimeout.
-    * @return Value of property dimseTimeout.
-    */
-   int getDimseTimeout();
-   
-   /** Setter for property dimseTimeout.
-    * @param dimseTimeout New value of property dimseTimeout.
-    */
-   void setDimseTimeout(int dimseTimeout);
-   
-   /** Getter for property soCloseDelay.
-    * @return Value of property soCloseDelay.
-    */
-   int getSoCloseDelay();
-   
-   /** Setter for property soCloseDelay.
-    * @param soCloseDelay New value of property soCloseDelay.
-    */
-   void setSoCloseDelay(int soCloseDelay);
-   
+    /**
+     *  Description of the Method
+     *
+     *@param  l  Description of the Parameter
+     */
+    void removeAssociationListener(AssociationListener l);
+
+
+    /**
+     *  Sets the acceptorPolicy attribute of the DcmHandler object
+     *
+     *@param  policy  The new acceptorPolicy value
+     */
+    void setAcceptorPolicy(AcceptorPolicy policy);
+
+
+    /**
+     *  Gets the acceptorPolicy attribute of the DcmHandler object
+     *
+     *@return    The acceptorPolicy value
+     */
+    AcceptorPolicy getAcceptorPolicy();
+
+
+    /**
+     *  Sets the dcmServiceRegistry attribute of the DcmHandler object
+     *
+     *@param  services  The new dcmServiceRegistry value
+     */
+    void setDcmServiceRegistry(DcmServiceRegistry services);
+
+
+    /**
+     *  Gets the dcmServiceRegistry attribute of the DcmHandler object
+     *
+     *@return    The dcmServiceRegistry value
+     */
+    DcmServiceRegistry getDcmServiceRegistry();
+
+
+    /**
+     *  Getter for property rqTimeout.
+     *
+     *@return    Value of property rqTimeout.
+     */
+    int getRqTimeout();
+
+
+    /**
+     *  Setter for property rqTimeout.
+     *
+     *@param  timeout    The new rqTimeout value
+     */
+    void setRqTimeout(int timeout);
+
+
+    /**
+     *  Getter for property dimseTimeout.
+     *
+     *@return    Value of property dimseTimeout.
+     */
+    int getDimseTimeout();
+
+
+    /**
+     *  Setter for property dimseTimeout.
+     *
+     *@param  dimseTimeout  New value of property dimseTimeout.
+     */
+    void setDimseTimeout(int dimseTimeout);
+
+
+    /**
+     *  Getter for property soCloseDelay.
+     *
+     *@return    Value of property soCloseDelay.
+     */
+    int getSoCloseDelay();
+
+
+    /**
+     *  Setter for property soCloseDelay.
+     *
+     *@param  soCloseDelay  New value of property soCloseDelay.
+     */
+    void setSoCloseDelay(int soCloseDelay);
+
+
+    /**
+     *  Getter for property packPDVs.
+     *
+     *@return    Value of property packPDVs.
+     */
+    boolean isPackPDVs();
+
+
+    /**
+     *  Setter for property packPDVs.
+     *
+     *@param  packPDVs  New value of property packPDVs.
+     */
+    void setPackPDVs(boolean packPDVs);
 }
+
--- dcm4che14/trunk/src/java/org/dcm4cheri/server/DcmHandlerImpl.java
+++ dcm4che14/trunk/src/java/org/dcm4cheri/server/DcmHandlerImpl.java
@@ -1,5 +1,4 @@
-/*****************************************************************************
- *                                                                           *
+/*                                                                           *
  *  Copyright (c) 2002 by TIANI MEDGRAPH AG                                  *
  *                                                                           *
  *  This file is part of dcm4che.                                            *
@@ -17,9 +16,7 @@
  *  You should have received a copy of the GNU Lesser General Public         *
  *  License along with this library; if not, write to the Free Software      *
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
- *                                                                           *
- *****************************************************************************/
-
+ */
 package org.dcm4cheri.server;
 
 import org.dcm4che.server.DcmHandler;
@@ -36,134 +33,248 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- * <description>
- *
- * @see <related>
- * @author  <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
- * @version $Revision$ $Date$
- *
- * <p><b>Revisions:</b>
- *
- * <p><b>yyyymmdd author:</b>
- * <ul>
- * <li> explicit fix description (no line numbers but methods) go
- *            beyond the cvs commit message
- * </ul>
+ *@author     <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
+ *@created    June, 2002
+ *@version    $Revision$ $Date$
  */
-class DcmHandlerImpl implements DcmHandler {
-    
+class DcmHandlerImpl implements DcmHandler
+{
+
     // Constants -----------------------------------------------------
-    
+
     // Attributes ----------------------------------------------------
     private final static AssociationFactory fact =
-        AssociationFactory.getInstance();
-    
+            AssociationFactory.getInstance();
+
     private final LinkedList listeners = new LinkedList();
     private AcceptorPolicy policy;
     private DcmServiceRegistry services;
-    
+
     private int rqTimeout = 5000;
     private int dimseTimeout = 0;
     private int soCloseDelay = 500;
-    
+    private boolean packPDVs = false;
+
     // Static --------------------------------------------------------
-    
+
     // Constructors --------------------------------------------------
-    public DcmHandlerImpl(AcceptorPolicy policy, DcmServiceRegistry services) {
+    /**
+     *  Constructor for the DcmHandlerImpl object
+     *
+     *@param  policy    Description of the Parameter
+     *@param  services  Description of the Parameter
+     */
+    public DcmHandlerImpl(AcceptorPolicy policy, DcmServiceRegistry services)
+    {
         setAcceptorPolicy(policy);
         setDcmServiceRegistry(services);
     }
-    
+
     // Public --------------------------------------------------------
-    public final void setAcceptorPolicy(AcceptorPolicy policy) {
+    /**
+     *  Sets the acceptorPolicy attribute of the DcmHandlerImpl object
+     *
+     *@param  policy  The new acceptorPolicy value
+     */
+    public final void setAcceptorPolicy(AcceptorPolicy policy)
+    {
         if (policy == null) {
             throw new NullPointerException();
         }
         this.policy = policy;
     }
 
-    public final AcceptorPolicy getAcceptorPolicy() {
+
+    /**
+     *  Gets the acceptorPolicy attribute of the DcmHandlerImpl object
+     *
+     *@return    The acceptorPolicy value
+     */
+    public final AcceptorPolicy getAcceptorPolicy()
+    {
         return policy;
     }
-    
-    public final void setDcmServiceRegistry(DcmServiceRegistry services) {
+
+
+    /**
+     *  Sets the dcmServiceRegistry attribute of the DcmHandlerImpl object
+     *
+     *@param  services  The new dcmServiceRegistry value
+     */
+    public final void setDcmServiceRegistry(DcmServiceRegistry services)
+    {
         if (services == null) {
             throw new NullPointerException();
         }
         this.services = services;
     }
 
-    public final DcmServiceRegistry getDcmServiceRegistry() {
+
+    /**
+     *  Gets the dcmServiceRegistry attribute of the DcmHandlerImpl object
+     *
+     *@return    The dcmServiceRegistry value
+     */
+    public final DcmServiceRegistry getDcmServiceRegistry()
+    {
         return services;
     }
-    
-    public void setRqTimeout(int timeout) {
+
+
+    /**
+     *  Sets the rqTimeout attribute of the DcmHandlerImpl object
+     *
+     *@param  timeout  The new rqTimeout value
+     */
+    public void setRqTimeout(int timeout)
+    {
         if (timeout < 0) {
             throw new IllegalArgumentException("timeout: " + timeout);
         }
         this.rqTimeout = timeout;
     }
-    
-    public int getRqTimeout() {
+
+
+    /**
+     *  Gets the rqTimeout attribute of the DcmHandlerImpl object
+     *
+     *@return    The rqTimeout value
+     */
+    public int getRqTimeout()
+    {
         return rqTimeout;
     }
 
-    public int getDimseTimeout() {
+
+    /**
+     *  Gets the dimseTimeout attribute of the DcmHandlerImpl object
+     *
+     *@return    The dimseTimeout value
+     */
+    public int getDimseTimeout()
+    {
         return dimseTimeout;
     }
-    
-    public void setDimseTimeout(int timeout) {
+
+
+    /**
+     *  Sets the dimseTimeout attribute of the DcmHandlerImpl object
+     *
+     *@param  timeout  The new dimseTimeout value
+     */
+    public void setDimseTimeout(int timeout)
+    {
         if (timeout < 0) {
             throw new IllegalArgumentException("timeout: " + timeout);
         }
         this.dimseTimeout = dimseTimeout;
     }
-    
-    public int getSoCloseDelay() {
+
+
+    /**
+     *  Gets the soCloseDelay attribute of the DcmHandlerImpl object
+     *
+     *@return    The soCloseDelay value
+     */
+    public int getSoCloseDelay()
+    {
         return soCloseDelay;
     }
-    
-    public void setSoCloseDelay(int delay) {
+
+
+    /**
+     *  Sets the soCloseDelay attribute of the DcmHandlerImpl object
+     *
+     *@param  delay  The new soCloseDelay value
+     */
+    public void setSoCloseDelay(int delay)
+    {
         if (delay < 0) {
             throw new IllegalArgumentException("delay: " + delay);
         }
         this.soCloseDelay = delay;
     }
+
+    /** Getter for property packPDVs.
+     * @return Value of property packPDVs.
+     */
+    public boolean isPackPDVs() {
+        return packPDVs;
+    }
     
+    /** Setter for property packPDVs.
+     * @param packPDVs New value of property packPDVs.
+     */
+    public void setPackPDVs(boolean packPDVs) {
+        this.packPDVs = packPDVs;
+    }
     // DcmHandler implementation -------------------------------------
-    public void handle(Socket s) throws IOException {
+    /**
+     *  Description of the Method
+     *
+     *@param  s                Description of the Parameter
+     *@exception  IOException  Description of the Exception
+     */
+    public void handle(Socket s)
+        throws IOException
+    {
         Association assoc = fact.newAcceptor(s);
         assoc.setRqTimeout(rqTimeout);
         assoc.setDimseTimeout(dimseTimeout);
         assoc.setSoCloseDelay(soCloseDelay);
-        for (Iterator it = listeners.iterator(); it.hasNext();) {
-            assoc.addAssociationListener((AssociationListener)it.next());
+        assoc.setPackPDVs(packPDVs);
+        for (Iterator it = listeners.iterator(); it.hasNext(); ) {
+            assoc.addAssociationListener((AssociationListener) it.next());
         }
-        if (assoc.accept(policy) instanceof AAssociateAC)
+        if (assoc.accept(policy) instanceof AAssociateAC) {
             fact.newActiveAssociation(assoc, services).run();
+        }
     }
-    
-    public void addAssociationListener(AssociationListener l) {
+
+
+    /**
+     *  Adds a feature to the AssociationListener attribute of the
+     *  DcmHandlerImpl object
+     *
+     *@param  l  The feature to be added to the AssociationListener attribute
+     */
+    public void addAssociationListener(AssociationListener l)
+    {
         synchronized (listeners) {
             listeners.add(l);
         }
     }
-    
-    public void removeAssociationListener(AssociationListener l) {
+
+
+    /**
+     *  Description of the Method
+     *
+     *@param  l  Description of the Parameter
+     */
+    public void removeAssociationListener(AssociationListener l)
+    {
         synchronized (listeners) {
             listeners.remove(l);
         }
     }
-    
-    public boolean isSockedClosedByHandler() {
+
+
+    /**
+     *  Gets the sockedClosedByHandler attribute of the DcmHandlerImpl object
+     *
+     *@return    The sockedClosedByHandler value
+     */
+    public boolean isSockedClosedByHandler()
+    {
         return true;
     }
-    
+
     // Package protected ---------------------------------------------
-    
+
     // Protected -----------------------------------------------------
-    
+
     // Private -------------------------------------------------------
-    
+
     // Inner classes -------------------------------------------------
 }
+
