bf093037bc75482434a5a23d13818cf25e581b71
jforaci <jforaci@a605150a-d416-0410-a114-93ca8beb8bc8>
2003-07-23 15:00:23 +0000

    - added support to scale to 8/8 or 12/16 - added code to check for pixel cell overlays - misc bug fixes, refactoring

--- dcm4che14/trunk/src/java/org/dcm4che/client/PrintSCUDataSource.java
+++ dcm4che14/trunk/src/java/org/dcm4che/client/PrintSCUDataSource.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Vector;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
 
 import org.dcm4che.data.Dataset;
 import org.dcm4che.data.DcmDecodeParam;
@@ -41,13 +40,11 @@ import org.dcm4che.data.DcmParser;
 import org.dcm4che.data.DcmParserFactory;
 import org.dcm4che.dict.Tags;
 import org.dcm4che.dict.VRs;
+import org.dcm4che.image.PixelDataDescription;
 import org.dcm4che.image.PixelDataFactory;
 import org.dcm4che.image.PixelDataReader;
 import org.dcm4che.image.PixelDataWriter;
 import org.dcm4che.net.DataSource;
-
-/**
- * @author <a href="mailto:gunter@tia
 
 /**
  * @author <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
@@ -103,49 +100,24 @@ class PrintSCUDataSource implements DataSource {
 		InputStream in = new BufferedInputStream(
 			new FileInputStream(file));
 		final Dataset ds = PrintSCU.dcmFact.newDataset();
+        final int toBitsStored, toBitsAllocated;
+        
 		try {
 			DcmParser parser = parserFact.newDcmParser(in);
 			parser.setDcmHandler(ds.getDcmHandler());
 			parser.parseDcmFile(null, Tags.PixelData);
+            //err check for PixelData
 			if (parser.getReadTag() != Tags.PixelData) {
 				throw new IOException("No Pixel Data in file - " + file);
 			}
-			String pmi = ds.getString(Tags.PhotometricInterpretation);			
-			if (printSCU.isColorPrint()) {
-				if (!"RGB".equals(pmi) && !"MONOCHROME1".equals(pmi)) {
-					throw new IOException("Conversion from " + pmi + " to RGB not yet supported");
-				}
-				if (ds.getInt(Tags.PlanarConfiguration, 0) != 0) {
-					throw new IOException("Conversion from Planar Configuration color-by-pixel to color-by-plane not yet supported");
-				}
-			} else {
-				if (!"MONOCHROME2".equals(pmi) && !"MONOCHROME1".equals(pmi)) {
-					throw new IOException("Conversion from " + pmi + " to MONOCHROME not yet supported");
-				}
-				int bitsStored = ds.getInt(Tags.BitsStored, -1);
-				if (bitsStored != 8 && bitsStored != 12) {
-					throw new IOException("Conversion from " + bitsStored + " bits stored to 8 or 12 bits not yet supported");			
-				}
-				if (ds.getInt(Tags.PixelPresentation, 0) != 0) {
-					throw new IOException("Conversion from unsigned to signed pixel values not yet supported");						
-				}
-			}
-			DcmEncodeParam encodeParam = DcmDecodeParam.valueOf(tsUID);
-			imageBox.writeDataset(out, encodeParam);
-			ds.writeHeader(out, encodeParam,
-				printSCU.isColorPrint() 
-				? Tags.BasicColorImageSeq
-				: Tags.BasicGrayscaleImageSeq,
-				VRs.SQ, -1);
-			ds.writeHeader(out, encodeParam,
-				Tags.Item, VRs.NONE, -1);
-			ds.subSet(IMAGE_MODULE).writeDataset(out, encodeParam);
-			// TODO change this (length) if pixel data is rescaled to a different bit depth
-			ds.writeHeader(out, encodeParam,
-				parser.getReadTag(),
-				parser.getReadVR(),
-				parser.getReadLength());
-                
+            DcmDecodeParam decodeParam = parser.getDcmDecodeParam();
+            DcmEncodeParam encodeParam = DcmDecodeParam.valueOf(tsUID);
+            PixelDataDescription toDesc = null;
+            PixelDataDescription fromDesc = new PixelDataDescription(ds,
+                decodeParam.byteOrder, parser.getReadVR());
+            if (fromDesc.getNumberOfFrames() > 1)
+                throw new IOException("multi-frame images are not currently supported");
+            //grab some common pixel module attributes
             final int bitsAlloc = ds.getInt(Tags.BitsAllocated, 8);
             final int bitsStored = ds.getInt(Tags.BitsStored, 8);
             final int highBit = ds.getInt(Tags.HighBit, bitsStored - 1);
@@ -154,28 +126,64 @@ class PrintSCUDataSource implements DataSource {
             final int spp = ds.getInt(Tags.SamplesPerPixel, 1);
             final boolean signed = (ds.getInt(Tags.PixelRepresentation, 0) == 1);
             final float rs = ds.getFloat(Tags.RescaleSlope, 1F);
-			final float ri = ds.getFloat(Tags.RescaleIntercept, 0F);
+            final float ri = ds.getFloat(Tags.RescaleIntercept, 0F);
+            //err check on bit depth
+            if (bitsStored == 32 && !signed) {
+                throw new IOException("conversion from " + bitsStored
+                    + " bits stored, unsigned is not currently supported");
+            }
+            //err check PMI
+            String pmi = ds.getString(Tags.PhotometricInterpretation);          
+			if (printSCU.isColorPrint()) {
+				if (!"RGB".equals(pmi) && !"MONOCHROME1".equals(pmi)) {
+					throw new IOException("Conversion from " + pmi + " to RGB not currently supported");
+				}
+			}
+            else {
+				if (!"MONOCHROME2".equals(pmi) && !"MONOCHROME1".equals(pmi)) {
+					throw new IOException("Conversion from " + pmi + " to MONOCHROME not currently supported");
+				}
+			}
+            //
+            if (bitsStored > 8) {
+                toBitsStored = 12;
+                toBitsAllocated = 16;
+            }
+            else {
+                toBitsStored = 8;
+                toBitsAllocated = 8;
+            }
+            if (bitsStored != toBitsStored || highBit != bitsStored - 1 || signed
+                || !fromDesc.isByPlane()) {
+                toDesc = new PixelDataDescription(fromDesc, encodeParam,
+                    toBitsAllocated, toBitsStored, false, true);
+                ds.putUS(Tags.BitsStored, toBitsStored);
+                ds.putUS(Tags.BitsAllocated, toBitsAllocated);
+                ds.putUS(Tags.HighBit, toBitsStored - 1);
+                ds.putUS(Tags.PixelRepresentation, 0);
+                ds.putUS(Tags.PlanarConfiguration, 1);
+            }
+            else
+                toDesc = fromDesc;
+            //write image box
+			imageBox.writeDataset(out, encodeParam);
+			ds.writeHeader(out, encodeParam,
+				printSCU.isColorPrint() ? Tags.BasicColorImageSeq
+				                        : Tags.BasicGrayscaleImageSeq,
+				VRs.SQ, -1);
+			ds.writeHeader(out, encodeParam,
+				Tags.Item, VRs.NONE, -1);
+            //write image box pixel representation attributes (only header for PixelData)
+			ds.subSet(IMAGE_MODULE).writeDataset(out, encodeParam);
+			ds.writeHeader(out, encodeParam,
+				parser.getReadTag(),
+				toDesc.getPixelDataVr(),
+				(int)toDesc.calcPixelDataLength());
+                
 			int[][] buff = null;
-            ImageInputStream iis = null;
 			PixelDataReader pd = null;
 			
-            //if a window exists, scale pixel data to window range
-            if (ds.contains(Tags.WindowCenter) && ds.contains(Tags.WindowWidth)) {
-            	final int winTop = (int)Math.ceil(((ds.getFloat(Tags.WindowCenter, 0F)
-            		+ ds.getFloat(Tags.WindowWidth, 0F) / 2) - ri) / rs);
-				final int winBot = (int)Math.floor(((ds.getFloat(Tags.WindowCenter, 0F)
-                    - ds.getFloat(Tags.WindowWidth, 0F) / 2) - ri) / rs);
-				if (buff == null) {
-					iis = ImageIO.createImageInputStream(in);
-					pd = pdFact.newReader(ds,
-						iis, parser.getDcmDecodeParam().byteOrder,
-						parser.getReadVR());
-					pd.readPixelData(false);
-					buff = pd.getPixelDataArray(0);
-				}
-				scaleToRange(buff, signed, bitsStored, bitsStored, winBot, winTop);
-            }
-            
+            //overlays
             final class Overlay
             {
                 public int cols, rows;
@@ -198,62 +206,93 @@ class PrintSCUDataSource implements DataSource {
                 }
             };
             
-            DcmElement el;
-            int group = 0x60000000, cntr = 16;
-            List list = new Vector(16);
-            while (cntr > 0) {
-                if ((el = ds.get(group | 0x3000)) != null)
-                    list.add(new Overlay(group));
-                cntr--;
-                group += 0x20000;
-            }
-            Overlay[] overlays = (Overlay[])list.toArray(new Overlay[0]);
-            list = null;
-            
             //check if any overlays exist and if they are to be burned into the image
-            if (burnInOverlays && overlays.length > 0) {
-                final int burnValue = (signed) ? (1 << highBit) - 1 : -1;
-                if (buff == null) {
-	                iis = ImageIO.createImageInputStream(in);
-	                pd = pdFact.newReader(ds,
-	                	iis, parser.getDcmDecodeParam().byteOrder,
-	                	parser.getReadVR());
-					pd.readPixelData(false);
-	                buff = pd.getPixelDataArray(0);
+            if (burnInOverlays) {
+                DcmElement el;
+                int group = 0x60000000, cntr = 16;
+                List list = new Vector(16);
+                while (cntr > 0) {
+                    if ((el = ds.get(group | 0x3000)) != null)
+                        list.add(new Overlay(group));
+                    cntr--;
+                    group += 0x20000;
                 }
-                Overlay ovl;
+                Overlay[] overlays = (Overlay[])list.toArray(new Overlay[0]);
+                list = null;
+                final int burnValue = (signed) ? (1 << highBit) - 1 : -1;
                 
-                for (int i = 0; i < overlays.length; i++) {
-                    ovl = overlays[i];
-                    final int colstart = Math.max(0, ovl.x);
-                    final int colend = Math.min(ovl.x + ovl.cols, cols);
-                    final int rowstart = Math.max(0, ovl.y);
-                    final int rowend = Math.min(ovl.y + ovl.rows, rows);
-                    final int x = (ovl.x >= 0) ? 0 : -ovl.x;
-                    final int y = (ovl.y >= 0) ? 0 : -ovl.y;
-                    int mask = 1;
-                    int ind = x + y * ovl.cols;
-                    for (int j = rowstart; j < rowend; j++) {
-                        for (int k = colstart; k < colend; k++) {
-                            if ((ovl.data[ind >> 3] & mask) > 0) {
-                                for (int s = 0; s < spp; s++)
-                                	buff[s][(j * cols) + k] = burnValue;
+                if (overlays.length > 0) {
+                    if (buff == null)
+                        buff = (pd = readPixelData(fromDesc, in)).getPixelDataArray(0);
+                    
+                    Overlay ovl;
+                    
+                    //iterate over each overlay
+                    for (int i = 0; i < overlays.length; i++) {
+                        ovl = overlays[i];
+                        final int colstart = Math.max(0, ovl.x);
+                        final int colend = Math.min(ovl.x + ovl.cols, cols);
+                        final int rowstart = Math.max(0, ovl.y);
+                        final int rowend = Math.min(ovl.y + ovl.rows, rows);
+                        final int x = (ovl.x >= 0) ? 0 : -ovl.x;
+                        final int y = (ovl.y >= 0) ? 0 : -ovl.y;
+                        int mask = 1;
+                        int ind = x + y * ovl.cols;
+                        for (int j = rowstart; j < rowend; j++) {
+                            for (int k = colstart; k < colend; k++) {
+                                if ((ovl.data[ind >> 3] & mask) > 0) {
+                                    for (int s = 0; s < spp; s++)
+                                    	buff[s][(j * cols) + k] = burnValue;
+                                }
+                                ind++;
+                                if (mask == 0x80)
+                                    mask = 1;
+                                else
+                                    mask = mask << 1;
                             }
-                            ind++;
-                            if (mask == 0x80)
-                                mask = 1;
-                            else
-                                mask = mask << 1;
+                            ind += ovl.cols;
                         }
-                        ind += ovl.cols;
                     }
                 }
+                
+                //iterate over pixel data for overlay data in pixel data itself
+                if (bitsAlloc > bitsStored) {
+                    if (buff == null)
+                        buff = (pd = readPixelData(fromDesc, in)).getPixelDataArray(0);
+                    
+                    final int mask = ~(((1 << bitsStored) - 1) << (highBit - bitsStored - 1));
+                    for (int s = 0; s < spp; s++)
+                        for (int i = 0; i < buff[s].length; i++)
+                            if ((buff[s][i] & mask) != 0)
+                                buff[s][i] = burnValue;
+                }
+            }
+            
+            //if a window exists, scale pixel data to window range
+            int winTop = 0, winBot = 0;
+            final boolean windowPresent = ds.contains(Tags.WindowCenter) && ds.contains(Tags.WindowWidth);
+            if (windowPresent) {
+                winTop = (int)Math.ceil(((ds.getFloat(Tags.WindowCenter, 0F)
+                    + ds.getFloat(Tags.WindowWidth, 0F) / 2) - ri) / rs);
+                winBot = (int)Math.floor(((ds.getFloat(Tags.WindowCenter, 0F)
+                    - ds.getFloat(Tags.WindowWidth, 0F) / 2) - ri) / rs);
+            }
+            
+            //scale
+            if (toDesc != fromDesc || windowPresent) {
+                if (buff == null)
+                    buff = (pd = readPixelData(fromDesc, in)).getPixelDataArray(0);
+                scaleToRange(buff, fromDesc, bitsStored, false, winBot, winTop);
             }
             
             if (pd != null) {
-                PixelDataWriter pdWriter = pdFact.newWriter(pd.getPixelDataArray(), false,
-                    pd.getPixelDataDescription(), new DataSourceImageOutputStream(out));
-				pdWriter.writePixelData(false);
+                //for the writing we're going on the assumption that the print server
+                // will ignore the overlay bits in pixel cell (in the case of bs=12)
+                int[][][] tmp = new int[1][0][0];
+                tmp[0] = buff;
+                PixelDataWriter pdWriter = pdFact.newWriter(tmp, false, toDesc,
+                    new DataSourceImageOutputStream(out));
+				pdWriter.writePixelData();
             }
             else {
     			copy(in, out, parser.getReadLength());
@@ -271,35 +310,65 @@ class PrintSCUDataSource implements DataSource {
             catch (IOException ignore) {}
 		}
 	}
-    
-    private void scaleToRange(int[][] pixelData, boolean signed, int fromBitDepth,
-        int toBitDepth, int start, int end)
-    {
-        final int min = (signed) ? -(1 << (fromBitDepth - 1)) : 0;
-        final int max = (signed) ? (1 << (fromBitDepth - 1)) - 1
+
+    private PixelDataReader readPixelData(PixelDataDescription desc,
+        InputStream in)
+        throws IOException {
+        PixelDataReader reader = pdFact.newReader(desc, ImageIO.createImageInputStream(in));
+        reader.readPixelData(true);
+        return reader;
+    }
+
+    /**
+     * Scales the range [<code>start..end</code>] in pixelData described by
+     * <code>from</code> to the specified bit depth and sign representation.
+     * The pixel data is assumed to contain the <i>entire cell</i>, and
+     * loses any overlay bits that may be present.
+     */
+    private void scaleToRange(int[][] pixelData, PixelDataDescription from,
+        int toBitDepth, boolean toSigned, int start, int end) {
+        final int fromBitDepth = from.getBitsStored();
+        final int min = (from.isSigned()) ? -(1 << (fromBitDepth - 1)) : 0;
+        final int max = (from.isSigned()) ? (1 << (fromBitDepth - 1)) - 1
                                  : (1 << fromBitDepth) - 1;
-        if (start == end) {
+        if (start >= end) {
             start = min;
             end = max;
         }
         final int rngOrig = end - start;
-        final int newMin = (signed) ? -(1 << (toBitDepth - 1)) : 0;
-        final int newMax = (signed) ? (1 << (toBitDepth - 1)) - 1
-                                    : (1 << toBitDepth) - 1;
+        final int newMin = (toSigned) ? -(1 << (toBitDepth - 1)) : 0;
+        final int newMax = (toSigned) ? (1 << (toBitDepth - 1)) - 1
+                                      : (1 << toBitDepth) - 1;
         final int rngNew = newMax - newMin;
         final float f = rngNew / rngOrig;
+        final int leftShift = 32 - from.getHighBit() - 1;
+        final int rightShift = 32 - fromBitDepth;
         float tmp;
-        
-        for (int s = 0; s < pixelData.length; s++)
-            for (int i = 0; i < pixelData[s].length; i++) {
-                tmp = (pixelData[s][i] - start) * f;
-                if (tmp < 0)
-                    pixelData[s][i] = newMin;
-                else if (tmp > rngNew)
-                    pixelData[s][i] = newMax;
-                else
-                    pixelData[s][i] = (int)(tmp + 0.5F) - newMin;
-            }
+
+        if (from.isSigned()) {
+            for (int s = 0; s < pixelData.length; s++)
+                for (int i = 0; i < pixelData[s].length; i++) {
+                    tmp = (((pixelData[s][i] << leftShift) >> rightShift) - start) * f;
+                    if (tmp < 0)
+                        pixelData[s][i] = newMin;
+                    else if (tmp > rngNew)
+                        pixelData[s][i] = newMax;
+                    else
+                        pixelData[s][i] = (int)(tmp + 0.5F) - newMin;
+                }
+        }
+        else {
+            for (int s = 0; s < pixelData.length; s++)
+                for (int i = 0; i < pixelData[s].length; i++) {
+                    tmp = (((pixelData[s][i] << leftShift) >>> rightShift) - start) * f;
+                    if (tmp < 0)
+                        pixelData[s][i] = newMin;
+                    else if (tmp > rngNew)
+                        pixelData[s][i] = newMax;
+                    else
+                        pixelData[s][i] = (int)(tmp + 0.5F) - newMin;
+                }
+        }
     }
     
 	private void copy(InputStream in, OutputStream out, int len) throws IOException {
