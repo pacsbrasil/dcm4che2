e83d0f8f0ed8d9a8f718c42e4188557fb01bb7e4
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2003-12-19 11:56:39 +0000

    build Jdbc.properties from jbosscmp-jdbc.xml instead parsing it at runtime to avoid network connection by XMLParser for jboss DTD

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/JdbcProperties.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/JdbcProperties.java
@@ -21,96 +21,15 @@
 package org.dcm4chex.archive.ejb.jdbc;
 
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * @author Gunter.Zeilinger@tiani.com
- * @version $Revision$
+ * @version $Revision$ $Date$
  * @since 25.08.2003
  */
 public class JdbcProperties extends Properties {
 
-    private static final int NONE = -1;
-    private static final int EJB_NAME = 0;
-    private static final int TABLE_NAME = 1;
-    private static final int FIELD_NAME = 2;
-    private static final int COLUMN_NAME = 3;
-    private static final String ENTITY = "entity";
-
-    private class SAXHandler extends DefaultHandler {
-        private final String[] QNAMES = {
-            "ejb-name",        
-            "table-name",
-            "field-name",
-            "column-name",
-        };
-        private List QNAMES_AS_LIST = Arrays.asList(QNAMES);
-        private int curEl = NONE;
-        private StringBuffer text = new StringBuffer();
-        private String ejb;
-        private String field;
-        private boolean inEntity = false;
-        private String table;
-
-        public void characters(char[] ch, int start, int length)
-            throws SAXException {
-            if (curEl != NONE) {
-                for (int i = 0; i < length; i++) {
-                    text.append(ch[start + i]);
-                }
-            }
-        }
-
-        public void endElement(String uri, String localName, String qName)
-            throws SAXException {
-            if (inEntity) {
-                switch (curEl) {
-                    case NONE:
-                        if (ENTITY.equals(qName)) {
-                            inEntity = false;
-                        }
-                        break;
-                    case EJB_NAME :
-                        ejb = text.toString();
-                        break;
-                    case TABLE_NAME :
-                        put(ejb, table = text.toString());
-                        break;
-                    case FIELD_NAME :
-                        field = text.toString();
-                        break;
-                    case COLUMN_NAME :
-                        put(ejb + '.' + field, table + '.' + text.toString());
-                        break;
-                }
-            }
-            text.setLength(0);
-            curEl = NONE;
-        }
-
-        public void startElement(
-            String uri,
-            String localName,
-            String qName,
-            Attributes attributes)
-            throws SAXException {
-            if (inEntity) {
-                curEl = QNAMES_AS_LIST.indexOf(qName);
-            } else {
-                inEntity = ENTITY.equals(qName);
-            }
-        }
-
-    }
     private static final String[] FK_FIELDS =
         {
             "Study",
@@ -125,8 +44,6 @@ public class JdbcProperties extends Properties {
             "instance_fk",
             };
     private static final JdbcProperties instance = new JdbcProperties();
-    private static final String JBOSSCMP_JDBC_XML =
-        "META-INF/jbosscmp-jdbc.xml";
 
     public static JdbcProperties getInstance() {
         return instance;
@@ -148,19 +65,16 @@ public class JdbcProperties extends Properties {
 
     private JdbcProperties() {
         try {
-            SAXParserFactory fact = SAXParserFactory.newInstance();
-            SAXParser parser = fact.newSAXParser();
-            ClassLoader ccl = Thread.currentThread().getContextClassLoader();
-            InputStream in = ccl.getResourceAsStream(JBOSSCMP_JDBC_XML);
-            parser.parse(in, new SAXHandler());
+            InputStream in =
+                JdbcProperties.class.getResourceAsStream("Jdbc.properties");
+            load(in);
+            in.close();
             for (int i = 0; i < FK_FIELDS.length; ++i, ++i)
                 put(
                     FK_FIELDS[i] + '.' + FK_FIELDS[i + 1],
                     getProperty(FK_FIELDS[i]) + '.' + FK_FIELDS[i + 1]);
         } catch (Exception e) {
-            throw new RuntimeException(
-                "Failed to load jdbc properties from " + JBOSSCMP_JDBC_XML,
-                e);
+            throw new RuntimeException("Failed to load jdbc properties", e);
         }
     }
 
