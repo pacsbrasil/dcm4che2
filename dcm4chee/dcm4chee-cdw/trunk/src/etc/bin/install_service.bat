972e788d689be8ea202adad39820a33353cb4d33
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2004-07-12 16:49:39 +0000

    FIX install_service.bat

--- dcm4chee/dcm4chee-cdw/trunk/src/etc/bin/install_service.bat
+++ dcm4chee/dcm4chee-cdw/trunk/src/etc/bin/install_service.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 rem Set maximum size of the memory allocation pool
-rem default value of 64MB may not be sufficient for dcm4jboss
+rem default value of 64MB may not be sufficient for dcmcdw
 set JAVA_OPTS=-Xmx100m
 set DIRNAME=%~dp0%
-set RUNJAR=%DIRNAME%\run.jar
+set RUNJAR=%DIRNAME%run.jar
 if exist "%RUNJAR%" goto found_runjar
 echo Could not locate %RUNJAR%. Please check that you are in the
 echo bin directory when running this script.
@@ -16,9 +16,9 @@ if "%1" == "server" goto install
 if "%1" == "client" goto install
 echo "Usage: %0 server|client|uninstall"
 echo Options:
-echo   client    install "DICOM CD Writer" service, using client hotspot vm
-echo   server    install "DICOM CD Writer, using server hotspot vm
-echo   uninstall uninstall "DICOM CD Writer" service
+echo   client    install "DICOM Media Creation Mgt." service, using client hotspot vm
+echo   server    install "DICOM Media Creation Mgt." service, using server hotspot vm
+echo   uninstall uninstall "DICOM Media Creation Mgt." service
 goto eof
 
 :install
@@ -42,11 +42,11 @@ echo Could not locate %TOOLS_JAR%. Unexpected results may occur.
 echo Make sure that JAVA_HOME points to a JDK and not a JRE.
 
 :install
-JavaService.exe -install "DICOM CD Writer" "%VM%" %JAVA_OPTS% -Djava.class.path=%TOOLS_JAR%;%RUNJAR%  -start org.jboss.Main -params -c dcmcdw -stop org.jboss.Main -method systemExit  -out %DIRNAME%\out.txt -current %DIRNAME%
+JavaService.exe -install "DICOM Media Creation Mgt." "%VM%" %JAVA_OPTS% -Djava.class.path=%TOOLS_JAR%;%RUNJAR%  -start org.jboss.Main -params -c dcmcdw -stop org.jboss.Main -method systemExit  -out %DIRNAME%..\server\dcmcdw\log\install_service.log -current %DIRNAME%
 goto eof
 
 :uninstall
-JavaService.exe -uninstall "DICOM CD Writer"
+JavaService.exe -uninstall "DICOM Media Creation Mgt."
 goto eof
 
 :eof
