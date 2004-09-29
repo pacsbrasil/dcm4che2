ec21d43645aeed396b8fcdf8debe5e7b3604d67c
gunter <gunter@a605150a-d416-0410-a114-93ca8beb8bc8>
2004-09-29 08:03:36 +0000

    query supported attributes

--- dcm4chee/dcm4chee-prn/trunk/src/java/com/tiani/prnscp/client/GetAvailablePrinters.java
+++ dcm4chee/dcm4chee-prn/trunk/src/java/com/tiani/prnscp/client/GetAvailablePrinters.java
@@ -19,34 +19,66 @@
  */
 package com.tiani.prnscp.client;
 
+import javax.print.DocFlavor;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
-import javax.print.DocFlavor;
 
 /**
- *  Description of the Class
- *
- * @author  <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
- * @created  January 22, 2003
- * @version  $Revision$
+ * Description of the Class
+ * 
+ * @author <a href="mailto:gunter@tiani.com">gunter zeilinger </a>
+ * @created January 22, 2003
+ * @version $Revision$
  */
-public class GetAvailablePrinters
-{
-    /**
-     *  The main program for the GetAvailablePrinters class
-     *
-     * @param  args The command line arguments
-     * @exception  Exception Description of the Exception
-     */
-    public static void main(String[] args)
-        throws Exception
-    {
-        PrintService[] services = PrintServiceLookup.lookupPrintServices(
-                DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
-        System.out.println("Available Printers:");
-        for (int i = 0; i < services.length; ++i) {
-            System.out.println(services[i].getName());
-        }
-    }
+public class GetAvailablePrinters {
+	final PrintService service;
+
+	public static void main(String[] args) throws Exception {
+		PrintService[] services = PrintServiceLookup.lookupPrintServices(
+				DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
+		if (args.length == 0)
+			System.out.println("Available Printers:");
+		for (int i = 0; i < services.length; ++i) {
+			String name = services[i].getName();
+			if (args.length == 0)
+				System.out.println(name);
+			else if (name.equals(args[0]))
+				System.out.println(new GetAvailablePrinters(services[i]));
+		}
+	}
+
+	public GetAvailablePrinters(final PrintService service) {
+		this.service = service;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer("Printer: ");
+		sb.append(service.getName());
+		Class[] cats = service.getSupportedAttributeCategories();
+		for (int i = 0; i < cats.length; i++) {
+			appendSupportedValues(sb, cats[i]);
+		}
+		return sb.toString();
+	}
+
+	private void appendSupportedValues(StringBuffer sb, Class category) {
+		sb.append("\r\n\t").append(category.getName()).append(": ");
+		Object values = service.getSupportedAttributeValues(
+				category, null, null);
+		
+		try {
+			Object d = service.getDefaultAttributeValue(category);
+			Object[] a = (Object[]) values;
+			for (int i = 0; i < a.length; i++) {
+				sb.append("\r\n\t\t");
+				if (a[i].equals(d))
+					sb.append('[').append(a[i]).append(']');
+				else
+					sb.append(a[i]);
+			}
+		} catch (Exception e) {
+			sb.append(values);
+		}
+	}
 }
 
