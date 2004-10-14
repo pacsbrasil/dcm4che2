8344c63061f1f549dbc2404f09dd0b3c3bcb667d
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2004-10-14 23:06:00 +0000

    + support for emitting Patient/Procedure Record Audit Message

--- dcm4che14/trunk/src/java/org/dcm4che/auditlog/AuditLogger.java
+++ dcm4che14/trunk/src/java/org/dcm4che/auditlog/AuditLogger.java
@@ -1,23 +1,13 @@
-/*                                                                           *
- *  Copyright (c) 2002, 2003 by TIANI MEDGRAPH AG                            *
- *                                                                           *
- *  This file is part of dcm4che.                                            *
- *                                                                           *
- *  This library is free software; you can redistribute it and/or modify it  *
- *  under the terms of the GNU Lesser General Public License as published    *
- *  by the Free Software Foundation; either version 2 of the License, or     *
- *  (at your option) any later version.                                      *
- *                                                                           *
- *  This library is distributed in the hope that it will be useful, but      *
- *  WITHOUT ANY WARRANTY; without even the implied warranty of               *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
- *  Lesser General Public License for more details.                          *
- *                                                                           *
- *  You should have received a copy of the GNU Lesser General Public         *
- *  License along with this library; if not, write to the Free Software      *
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
- */
+/******************************************
+ *                                        *
+ *  dcm4che: A OpenSource DICOM Toolkit   *
+ *                                        *
+ *  Distributable under LGPL license.     *
+ *  See terms of license at gnu.org.      *
+ *                                        *
+ ******************************************/
 package org.dcm4che.auditlog;
+
 import java.net.UnknownHostException;
 
 import org.dcm4che.data.Dataset;
@@ -27,296 +17,100 @@ import org.dcm4che.data.Dataset;
  * @since      September 1, 2002
  * @version    $Revision$ $Date$
  */
-public interface AuditLogger
-{
+public interface AuditLogger {
 
-    // Constants -----------------------------------------------------
-    /**  Description of the Field */
     public final static String START = "Start";
-    /**  Description of the Field */
+
     public final static String STOP = "Stop";
-    /**  Description of the Field */
+
     public final static String SECURITY = "Security";
-    /**  Description of the Field */
+
     public final static String NETWORKING = "Networking";
 
-    // Methods -------------------------------------------------------
-    /**
-     *  Sets the syslogHost attribute of the AuditLogger object
-     *
-     * @param  syslogHost                The new syslogHost value
-     * @exception  UnknownHostException  Description of the Exception
-     */
-    public void setSyslogHost(String syslogHost)
-        throws UnknownHostException;
+    public void setSyslogHost(String syslogHost) throws UnknownHostException;
 
-
-    /**
-     *  Gets the syslogHost attribute of the AuditLogger object
-     *
-     * @return    The syslogHost value
-     */
     public String getSyslogHost();
 
-
-    /**
-     *  Sets the syslogPort attribute of the AuditLogger object
-     *
-     * @param  syslogPort  The new syslogPort value
-     */
     public void setSyslogPort(int syslogPort);
 
-
-    /**
-     *  Gets the syslogPort attribute of the AuditLogger object
-     *
-     * @return    The syslogPort value
-     */
     public int getSyslogPort();
 
-
-    /**
-     *  Gets the facility attribute of the AuditLogger object
-     *
-     * @return    The facility value
-     */
     public String getFacility();
 
-
-    /**
-     *  Sets the facility attribute of the AuditLogger object
-     *
-     * @param  facility  The new facility value
-     */
     public void setFacility(String facility);
 
-
-    /**
-     *  Gets the logActorStartStop attribute of the AuditLogger object
-     *
-     * @return    The logActorStartStop value
-     */
     public boolean isLogActorStartStop();
 
-
-    /**
-     *  Sets the logActorStartStop attribute of the AuditLogger object
-     *
-     * @param  enable  The new logActorStartStop value
-     */
     public void setLogActorStartStop(boolean enable);
 
-
-    /**
-     *  Gets the logInstancesStored attribute of the AuditLogger object
-     *
-     * @return    The logInstancesStored value
-     */
     public boolean isLogInstancesStored();
 
-
-    /**
-     *  Sets the logInstancesStored attribute of the AuditLogger object
-     *
-     * @param  enable  The new logInstancesStored value
-     */
     public void setLogInstancesStored(boolean enable);
 
-
-    /**
-     *  Gets the logBeginStoringInstances attribute of the AuditLogger object
-     *
-     * @return    The logBeginStoringInstances value
-     */
     public boolean isLogBeginStoringInstances();
 
-
-    /**
-     *  Sets the logBeginStoringInstances attribute of the AuditLogger object
-     *
-     * @param  enable  The new logBeginStoringInstances value
-     */
     public void setLogBeginStoringInstances(boolean enable);
 
-
-    /**
-     *  Gets the logInstancesSent attribute of the AuditLogger object
-     *
-     * @return    The logInstancesSent value
-     */
     public boolean isLogInstancesSent();
 
-
-    /**
-     *  Sets the logInstancesSent attribute of the AuditLogger object
-     *
-     * @param  enable  The new logInstancesSent value
-     */
     public void setLogInstancesSent(boolean enable);
 
-
-    /**
-     *  Gets the logDicomQuery attribute of the AuditLogger object
-     *
-     * @return    The logDicomQuery value
-     */
     public boolean isLogDicomQuery();
 
-
-    /**
-     *  Sets the logDicomQuery attribute of the AuditLogger object
-     *
-     * @param  enable  The new logDicomQuery value
-     */
     public void setLogDicomQuery(boolean enable);
 
-
-    /**
-     *  Gets the logSecurityAlert attribute of the AuditLogger object
-     *
-     * @return    The logSecurityAlert value
-     */
     public boolean isLogSecurityAlert();
 
-
-    /**
-     *  Sets the logSecurityAlert attribute of the AuditLogger object
-     *
-     * @param  enable  The new logSecurityAlert value
-     */
     public void setLogSecurityAlert(boolean enable);
 
-
-    /**
-     *  Gets the logUserAuthenticated attribute of the AuditLogger object
-     *
-     * @return    The logUserAuthenticated value
-     */
     public boolean isLogUserAuthenticated();
 
-
-    /**
-     *  Sets the logUserAuthenticated attribute of the AuditLogger object
-     *
-     * @param  enable  The new logUserAuthenticated value
-     */
     public void setLogUserAuthenticated(boolean enable);
 
-
-    /**
-     *  Gets the logActorConfig attribute of the AuditLogger object
-     *
-     * @return    The logActorConfig value
-     */
     public boolean isLogActorConfig();
 
-
-    /**
-     *  Sets the logActorConfig attribute of the AuditLogger object
-     *
-     * @param  enable  The new logActorConfig value
-     */
     public void setLogActorConfig(boolean enable);
 
-
-    /**
-     *  Gets the logExport attribute of the AuditLogger object
-     *
-     * @return    The logExport value
-     */
     public boolean isLogExport();
 
-
-    /**
-     *  Sets the logExport attribute of the AuditLogger object
-     *
-     * @param  enable  The new logExport value
-     */
     public void setLogExport(boolean enable);
 
+    public boolean isLogPatientRecord();
 
-    /**
-     *  Description of the Method
-     *
-     * @param  actorName  Description of the Parameter
-     * @param  action     Description of the Parameter
-     * @param  user       Description of the Parameter
-     */
+    public void setLogPatientRecord(boolean enable);
+
+    public boolean isLogProcedureRecord();
+
+    public void setLogProcedureRecord(boolean enable);
+
+    public boolean isLogStudyDeleted();
+
+    public void setLogStudyDeleted(boolean enable);
+
     public void logActorStartStop(String actorName, String action, User user);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  rNode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     */
     public void logInstancesStored(RemoteNode rNode, InstancesAction action);
 
+    public void logBeginStoringInstances(RemoteNode rNode,
+            InstancesAction action);
 
-    /**
-     *  Description of the Method
-     *
-     * @param  rNode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     */
-    public void logBeginStoringInstances(RemoteNode rNode, InstancesAction action);
-
-
-    /**
-     *  Description of the Method
-     *
-     * @param  rNode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     */
     public void logInstancesSent(RemoteNode rNode, InstancesAction action);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  keys       Description of the Parameter
-     * @param  requestor  Description of the Parameter
-     * @param  cuid       Description of the Parameter
-     */
     public void logDicomQuery(Dataset keys, RemoteNode requestor, String cuid);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  alertType    Description of the Parameter
-     * @param  user         Description of the Parameter
-     * @param  description  Description of the Parameter
-     */
     public void logSecurityAlert(String alertType, User user, String description);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  localUserName  Description of the Parameter
-     * @param  action         Description of the Parameter
-     */
     public void logUserAuthenticated(String localUserName, String action);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  description  Description of the Parameter
-     * @param  user         Description of the Parameter
-     * @param  configType   Description of the Parameter
-     */
     public void logActorConfig(String description, User user, String configType);
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  media  Description of the Parameter
-     * @param  user   Description of the Parameter
-     */
     public void logExport(MediaDescription media, User user);
-}
 
+    public void logPatientRecord(String action, Patient patient, User user);
+
+    public void logProcedureRecord(String action, Patient patient,
+            String placerOrderNumber, String fillerOrderNumber, String suid,
+            String accessionNumber, User user);
+
+    public void logStudyDeleted(InstancesAction action);
+}
--- dcm4che14/trunk/src/java/org/dcm4cheri/auditlog/IHEYr4.java
+++ dcm4che14/trunk/src/java/org/dcm4cheri/auditlog/IHEYr4.java
@@ -1,26 +1,17 @@
-/*  Copyright (c) 2002,2003 by TIANI MEDGRAPH AG
- *
- *  This file is part of dcm4che.
- *
- *  This library is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+/******************************************
+ *                                        *
+ *  dcm4che: A OpenSource DICOM Toolkit   *
+ *                                        *
+ *  Distributable under LGPL license.     *
+ *  See terms of license at gnu.org.      *
+ *                                        *
+ ******************************************/
 package org.dcm4cheri.auditlog;
 
 import java.util.Date;
 import org.dcm4che.auditlog.InstancesAction;
 import org.dcm4che.auditlog.MediaDescription;
+import org.dcm4che.auditlog.Patient;
 import org.dcm4che.auditlog.RemoteNode;
 import org.dcm4che.auditlog.User;
 import org.dcm4che.data.Dataset;
@@ -31,18 +22,10 @@ import org.dcm4che.util.ISO8601DateFormat;
  * @since      August 27, 2002
  * @version    $Revision$ $Date$
  */
-class IHEYr4
-{
+class IHEYr4 {
 
-    /**
-     *  Description of the Interface
-     *
-     * @author    <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
-     * @version $Revision$ $Date$
-     * @since     August 27, 2002
-     */
-    interface Message
-    {
+    interface Message {
+
         void writeTo(StringBuffer sb);
     }
 
@@ -51,209 +34,94 @@ class IHEYr4
     private static String localHostName;
 
     private Message msg;
+
     private String host;
+
     private long millis;
 
     // Constructors --------------------------------------------------
-    private IHEYr4(Message msg, String host, long millis)
-    {
+    private IHEYr4(Message msg, String host, long millis) {
         this.msg = msg;
         this.host = host;
         this.millis = millis;
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  rnode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     * @param  host    Description of the Parameter
-     * @param  millis  Description of the Parameter
-     * @return         Description of the Return Value
-     */
     public static IHEYr4 newInstancesStored(RemoteNode rnode,
-            InstancesAction action,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new RnodeWithInstanceActionDescription("InstancesStored",
-                rnode, action, "RemoteNode"), host, millis);
+            InstancesAction action, String host, long millis) {
+        return new IHEYr4(new RnodeWithInstanceActionDescription(
+                "InstancesStored", rnode, action, "RemoteNode"), host, millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  rnode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     * @param  host    Description of the Parameter
-     * @param  millis  Description of the Parameter
-     * @return         Description of the Return Value
-     */
     public static IHEYr4 newBeginStoringInstances(RemoteNode rnode,
-            InstancesAction action,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new RnodeWithInstanceActionDescription("BeginStoringInstances",
-                rnode, action, "Rnode"), host, millis);
+            InstancesAction action, String host, long millis) {
+        return new IHEYr4(new RnodeWithInstanceActionDescription(
+                "BeginStoringInstances", rnode, action, "Rnode"), host, millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  rnode   Description of the Parameter
-     * @param  action  Description of the Parameter
-     * @param  host    Description of the Parameter
-     * @param  millis  Description of the Parameter
-     * @return         Description of the Return Value
-     */
     public static IHEYr4 newInstancesSent(RemoteNode rnode,
-            InstancesAction action,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new RnodeWithInstanceActionDescription("InstancesSent",
-                rnode, action, "RNode"), host, millis);
+            InstancesAction action, String host, long millis) {
+        return new IHEYr4(new RnodeWithInstanceActionDescription(
+                "InstancesSent", rnode, action, "RNode"), host, millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  actorName          Description of the Parameter
-     * @param  applicationAction  Description of the Parameter
-     * @param  user               Description of the Parameter
-     * @param  host               Description of the Parameter
-     * @param  millis             Description of the Parameter
-     * @return                    Description of the Return Value
-     */
-    public static IHEYr4 newActorStartStop(
-            String actorName, String applicationAction, User user,
-            String host, long millis)
-    {
+    public static IHEYr4 newActorStartStop(String actorName,
+            String applicationAction, User user, String host, long millis) {
         return new IHEYr4(
-                new ActorStartStop(actorName, applicationAction, user),
-                host, millis);
+                new ActorStartStop(actorName, applicationAction, user), host,
+                millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  description  Description of the Parameter
-     * @param  user         Description of the Parameter
-     * @param  configType   Description of the Parameter
-     * @param  host         Description of the Parameter
-     * @param  millis       Description of the Parameter
-     * @return              Description of the Return Value
-     */
-    public static IHEYr4 newActorConfig(
-            String description, User user, String configType,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new ActorConfig(description, user, configType), host, millis);
+    public static IHEYr4 newActorConfig(String description, User user,
+            String configType, String host, long millis) {
+        return new IHEYr4(new ActorConfig(description, user, configType), host,
+                millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  action  Description of the Parameter
-     * @param  host    Description of the Parameter
-     * @param  millis  Description of the Parameter
-     * @return         Description of the Return Value
-     */
-    public static IHEYr4 newStudyDeleted(InstancesAction action,
-            String host, long millis)
-    {
+    public static IHEYr4 newStudyDeleted(InstancesAction action, String host,
+            long millis) {
         return new IHEYr4(new StudyDeleted(action), host, millis);
     }
 
+    public static IHEYr4 newPatientRecord(String action, Patient patient,
+            User user, String host, long millis) {
+        return new IHEYr4(new PatientRecord(action, patient, user), host,
+                millis);
 
-    /**
-     *  Description of the Method
-     *
-     * @param  keys       Description of the Parameter
-     * @param  requestor  Description of the Parameter
-     * @param  cuid       Description of the Parameter
-     * @param  host       Description of the Parameter
-     * @param  millis     Description of the Parameter
-     * @return            Description of the Return Value
-     */
-    public static IHEYr4 newDicomQuery(
-            Dataset keys, RemoteNode requestor, String cuid,
-            String host, long millis)
-    {
+    }
+
+    public static IHEYr4 newProcedureRecord(String action,
+            String placerOrderNumber, String fillerOrderNumber, String suid,
+            String accessionNumber, Patient patient, User user, String host,
+            long millis) {
+        return new IHEYr4(new ProcedureRecord(action, placerOrderNumber,
+                fillerOrderNumber, suid, accessionNumber, patient, user), host,
+                millis);
+
+    }
+
+    public static IHEYr4 newDicomQuery(Dataset keys, RemoteNode requestor,
+            String cuid, String host, long millis) {
         return new IHEYr4(new DicomQuery(keys, requestor, cuid), host, millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  alertType    Description of the Parameter
-     * @param  user         Description of the Parameter
-     * @param  description  Description of the Parameter
-     * @param  host         Description of the Parameter
-     * @param  millis       Description of the Parameter
-     * @return              Description of the Return Value
-     */
-    public static IHEYr4 newSecurityAlert(
-            String alertType, User user, String description,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new SecurityAlert(alertType, user, description), host, millis);
+    public static IHEYr4 newSecurityAlert(String alertType, User user,
+            String description, String host, long millis) {
+        return new IHEYr4(new SecurityAlert(alertType, user, description),
+                host, millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  localUserName  Description of the Parameter
-     * @param  action         Description of the Parameter
-     * @param  host           Description of the Parameter
-     * @param  millis         Description of the Parameter
-     * @return                Description of the Return Value
-     */
-    public static IHEYr4 newUserAuthenticated(
-            String localUserName, String action,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new UserAuthenticated(localUserName, action), host, millis);
+    public static IHEYr4 newUserAuthenticated(String localUserName,
+            String action, String host, long millis) {
+        return new IHEYr4(new UserAuthenticated(localUserName, action), host,
+                millis);
     }
 
-
-    /**
-     *  Description of the Method
-     *
-     * @param  media   Description of the Parameter
-     * @param  user    Description of the Parameter
-     * @param  host    Description of the Parameter
-     * @param  millis  Description of the Parameter
-     * @return         Description of the Return Value
-     */
-    public static IHEYr4 newExport(
-            MediaDescription media, User user,
-            String host, long millis)
-    {
-        return new IHEYr4(
-                new Export(media, user), host, millis);
+    public static IHEYr4 newExport(MediaDescription media, User user,
+            String host, long millis) {
+        return new IHEYr4(new Export(media, user), host, millis);
     }
 
-    // Methods -------------------------------------------------------
-    /**
-     *  Description of the Method
-     *
-     * @return    Description of the Return Value
-     */
-    public String toString()
-    {
+    public String toString() {
         StringBuffer sb = new StringBuffer(512);
         sb.append("<IHEYr4>");
         msg.writeTo(sb);
