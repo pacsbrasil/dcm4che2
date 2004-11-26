dc1d7e1ba3565a7ed87b936ed65377505e5d5b3b
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2004-11-26 18:07:05 +0000

    added: move function

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/FolderSubmitCtrl.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/FolderSubmitCtrl.java
@@ -10,6 +10,7 @@ package org.dcm4chex.archive.web.maverick;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -27,6 +28,7 @@ import org.dcm4chex.archive.ejb.interfaces.ContentManager;
 import org.dcm4chex.archive.ejb.interfaces.ContentManagerHome;
 import org.dcm4chex.archive.util.EJBHomeFactory;
 import org.dcm4chex.archive.util.JMSDelegate;
+import org.dcm4chex.archive.web.maverick.model.AbstractModel;
 import org.dcm4chex.archive.web.maverick.model.InstanceModel;
 import org.dcm4chex.archive.web.maverick.model.PatientModel;
 import org.dcm4chex.archive.web.maverick.model.SeriesModel;
@@ -41,6 +43,11 @@ import org.dcm4chex.archive.web.maverick.model.StudyModel;
  */
 public class FolderSubmitCtrl extends FolderCtrl {
     
+	public static final int MOVE_ERROR = -1;
+	public static final int MOVE_STUDIES = 0;
+	public static final int MOVE_SERIES = 1;
+	public static final int MOVE_INSTANCES = 2;
+	
     private static final int MOVE_PRIOR = 0;
 
     protected String perform() throws Exception {
@@ -64,7 +71,7 @@ public class FolderSubmitCtrl extends FolderCtrl {
             if (rq.getParameter("merge") != null
                     || rq.getParameter("merge.x") != null) { return MERGE; }
             if (rq.getParameter("move") != null
-                    || rq.getParameter("move.x") != null) { return FOLDER; }
+                    || rq.getParameter("move.x") != null) { return move(); }
             return FOLDER;
         } catch (Exception e) {
             e.printStackTrace();
@@ -328,4 +335,322 @@ public class FolderSubmitCtrl extends FolderCtrl {
             }
         }
     }
+ 
+    private List listSeriesOfStudy(int studyPk) throws Exception {
+        ContentManagerHome home = (ContentManagerHome) EJBHomeFactory
+                .getFactory().lookup(ContentManagerHome.class,
+                        ContentManagerHome.JNDI_NAME);
+        ContentManager cm = home.create();
+        try {
+            return cm.listSeriesOfStudy(studyPk);
+        } finally {
+            try {
+                cm.remove();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private List listInstancesOfSeries(int seriesPk) throws Exception {
+        ContentManagerHome home = (ContentManagerHome) EJBHomeFactory
+                .getFactory().lookup(ContentManagerHome.class,
+                        ContentManagerHome.JNDI_NAME);
+        ContentManager cm = home.create();
+        try {
+            return cm.listInstancesOfSeries(seriesPk);
+        } finally {
+            try {
+                cm.remove();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    /**
+     * Move one ore more model instances to another parent.
+     * <p>
+     * The move is restricted with following rules:<br>
+     * 1) the destinations model type must be the same as the source parents model type.<br>
+     * 2) the destination must be different to the source parent.<br>
+     * 3) all source models must have the same parent.<br>
+     * 4) the destination and the source parent must have the same parent.<br>
+     * 
+     * @return the name of the next view.
+     */
+    private String move() {
+    	int moveType = checkStickyForMove();
+    	String ret = ERROR;
+		FolderForm folderForm = (FolderForm) getForm();
+    	if ( moveType != MOVE_ERROR) {
+    		folderForm.setErrorCode( FolderForm.NO_ERROR );
+			System.out.println("*********************************");
+    		int[] iaSrc;
+    		int iDest;
+    		ContentManager cm = null;
+    		ContentEdit ce = null;
+    		try {
+    			ce = lookupContentEdit();
+    			ContentManagerHome home = (ContentManagerHome) EJBHomeFactory
+		        .getFactory().lookup(ContentManagerHome.class,
+		                ContentManagerHome.JNDI_NAME);
+		        cm = home.create();
+    			
+	    		if ( moveType == MOVE_STUDIES ) {
+	    			System.out.println("move_STUDIES");
+	    			_move_studies( ce, cm );
+	    		} else if ( moveType == MOVE_SERIES ) {
+	    			System.out.println("move_SERIES");
+	    			_move_series( ce, cm );
+	    		} else if ( moveType == MOVE_INSTANCES ) {
+	    			System.out.println("move_INSTANCES");
+	    			_move_instances( ce, cm );
+	    		}
+	    		ret = FOLDER;
+    		} catch ( Exception x ) {
+    			System.out.println("Exception:"+x);
+    			x.printStackTrace(System.out);
+    			
+    			//ret is ERROR
+    		}	finally {
+    	            try {
+    	            	if ( ce != null )
+    	            		ce.remove();
+    	            } catch (Exception e) {
+    	            }
+    	            try {
+    	            	if ( cm != null )
+    	            		cm.remove();
+    	            } catch (Exception e) {
+    	            }
+    		}
+    	} else {
+    		ret = FOLDER;
+    		folderForm.setErrorCode( FolderForm.ERROR_MOVE );
+    	}
+		return ret;
+    }
+    
+    /**
+     * Move selected studies to a patient.
+     * 
+	 * @param ce ContentEditBean to make the move.
+	 * @param cm ContentManagerBean to update the model.
+	 */
+	private void _move_studies( ContentEdit ce, ContentManager cm ) throws Exception {
+		FolderForm folderForm = (FolderForm) getForm();
+		int iDest = Integer.parseInt( (String) folderForm.getStickyPatients().iterator().next().toString() );
+		int[] iaSrc = getIntArrayFromSet( folderForm.getStickyStudies() );
+		ce.moveStudies( iaSrc, iDest );
+		
+		PatientModel destModel = folderForm.getPatientByPk( iDest );
+		List l = findModelHistory( folderForm.getPatients(), iaSrc[0], 1 );
+		_updatePatientWithStudies( destModel, cm );
+		_updatePatientWithStudies( (PatientModel) l.get(0), cm );
+	}
+	
+    /**
+     * update given patient with studies.
+     * 
+	 * @param patient Patient.
+	 * @param cm ContentManagerBean to get current list of studies.
+	 */
+	private void _updatePatientWithStudies( PatientModel patient, ContentManager cm ) throws Exception {
+		List studies = cm.listStudiesOfPatient( patient.getPk() );
+        for (int i = 0, n = studies.size(); i < n; i++)
+            studies.set(i, new StudyModel((Dataset) studies.get(i)));
+        patient.setStudies( studies );
+	}
+
+	private void _move_series( ContentEdit ce, ContentManager cm ) throws Exception {
+		FolderForm folderForm = (FolderForm) getForm();
+		int iDest = Integer.parseInt( (String) folderForm.getStickyStudies().iterator().next().toString() );
+		int[] iaSrc = getIntArrayFromSet( folderForm.getStickySeries() );
+		ce.moveSeries( iaSrc, iDest );
+
+		List l = findModelHistory( folderForm.getPatients(), iDest, 1 );
+
+		StudyModel destModel = (StudyModel) l.get(1);
+		l = findModelHistory( folderForm.getPatients(), iaSrc[0], 2 );
+		_updateStudyWithSeries( destModel, cm );
+		_updateStudyWithSeries( (StudyModel) l.get(1), cm );//0..patient,1..study,2..series
+	}
+	
+	private void _updateStudyWithSeries(StudyModel study, ContentManager cm) throws Exception {
+		List series = cm.listSeriesOfStudy( study.getPk() );
+        for (int i = 0, n = series.size(); i < n; i++)
+        	series.set(i, new SeriesModel((Dataset) series.get(i)));
+        study.setSeries( series );
+        study.update( cm.getStudy( study.getPk() ) );
+	}
+
+	private void _move_instances( ContentEdit ce, ContentManager cm ) throws Exception {
+		FolderForm folderForm = (FolderForm) getForm();
+		int iDest = Integer.parseInt( (String) folderForm.getStickySeries().iterator().next().toString() );
+		int[] iaSrc = getIntArrayFromSet( folderForm.getStickyInstances() );
+		ce.moveInstances( iaSrc, iDest );
+
+		List l = findModelHistory( folderForm.getPatients(), iDest, 2 );
+
+		SeriesModel destModel = (SeriesModel) l.get(2);//0..patient,1..study,2..series
+		l = findModelHistory( folderForm.getPatients(), iaSrc[0], 3 );
+		_updateSeriesWithInstances( destModel, cm );
+		_updateSeriesWithInstances( (SeriesModel) l.get(2), cm );//0..patient,1..study,2..series,3..instances
+	}
+	
+	private void _updateSeriesWithInstances(SeriesModel series, ContentManager cm) throws Exception {
+		List inst = cm.listInstancesOfSeries( series.getPk() );
+        for (int i = 0, n = inst.size(); i < n; i++)
+        	inst.set(i, InstanceModel.valueOf((Dataset) inst.get(i)));
+        series.setInstances( inst );
+        series.update( cm.getSeries( series.getPk() ) );
+	}
+	
+	/**
+     * Converts a Set of int values as String to an int array.
+     * 
+	 * @param set Set with String objects of int
+	 * @return int Array
+	 */
+	public static int[] getIntArrayFromSet(Set set) {
+		if ( set == null ) return null;
+		int[] ia = new int[ set.size() ];
+		int i = 0;
+		Iterator iter = set.iterator();
+		while ( iter.hasNext() ) {
+			ia[i] = Integer.parseInt( (String) iter.next() );
+			i++;
+		}
+		return ia;
+	}
+
+	/**
+	 * Checks the current sticky settings for move request.
+	 * <p>
+	 * Checks if the Sticky Informations are correct and obtain the type of move.
+	 * 
+	 * @return Type of move ( MOVE_STUDIES, MOVE_SERIES or MOVE_INSTANCES) or MOVE_ERROR if move is not allowed.
+	 */
+	private int checkStickyForMove() {
+    	FolderForm folderForm = (FolderForm) getForm();
+       	int iStickyPat = folderForm.getStickyPatients().size();
+       	int iStickyStu = folderForm.getStickyStudies().size();
+       	int iStickySer = folderForm.getStickySeries().size();
+       	int iStickyInst = folderForm.getStickyInstances().size();
+       	if ( iStickyPat > 0 ) {
+       		if ( iStickyPat > 1 || iStickyStu < 1 || iStickySer > 0 || iStickyInst > 0 ) {
+       			return MOVE_ERROR;
+       		}
+        	if ( checkStickyPlacement( folderForm.getStickyPatients(), folderForm.getStickyStudies(), 0 ) ) {
+        		return MOVE_STUDIES;
+       		}
+       	} else if ( iStickyStu > 0 ) {
+       		if ( iStickyStu > 1 || iStickySer < 1 || iStickyInst > 0 ) {
+       			return MOVE_ERROR;
+       		}
+        	if ( this.checkStickyPlacement( folderForm.getStickyStudies(), folderForm.getStickySeries(), 1 ) ) {
+        		return MOVE_SERIES;	
+        	}
+       	} else if ( iStickySer > 0 ) {
+       		if ( iStickySer > 1 || iStickyInst < 1 ) {
+       			return MOVE_ERROR;
+       		}
+        	if ( checkStickyPlacement( folderForm.getStickySeries(), folderForm.getStickyInstances(), 2 ) ) {
+        		return MOVE_INSTANCES;
+        	}
+       	}
+   		return MOVE_ERROR; //nothing selected
+    }
+    
+	/**
+	 * Returns the tree nodes for a given pk.
+	 * <p>
+	 * Because the pk's are only unique within a model type, it is necessary to use the correct
+	 * depth value!
+	 * <p>
+	 * 
+	 * @param parent 	This is the root model of the tree to search.
+	 * @param pk		The pk as String to search
+	 * @param depth		The tree depth where the pk should be found.
+	 * @return A list with all nodes to get the model.
+	 */
+    private List findModelHistory( List parent, String pk, int depth ) {
+       	int iPk = Integer.parseInt(pk);
+   	    return findModelHistory( parent, iPk, depth );
+    }
+ 
+	/**
+	 * Returns the tree nodes for a given pk.
+	 * <p>
+	 * Because the pk's are only unique within a model type, it is necessary to use the correct
+	 * depth value!
+	 * <p>
+	 * 
+	 * @param parent 	This is the root model of the tree to search.
+	 * @param pk		The pk as int to search
+	 * @param depth		The tree depth where the pk should be found.
+	 * @return A list with all nodes to get the model.
+	 */
+    private List findModelHistory( List parent, int iPk, int depth ) {
+    	Iterator iter = parent.iterator();
+    	AbstractModel model = null;
+    	List l = new ArrayList();
+    	while ( iter.hasNext() ) {
+    		model = (AbstractModel) iter.next();
+    		if ( depth == 0 ) {//should pk in this parent?
+	    		if ( model.getPk() == iPk ) {
+		    			l.add( model );
+		    			return l;
+	    		}
+    		} else { //search in next tree segment.
+ 				List l1 = findModelHistory( model.getChilds(), iPk, depth-1 );
+				if ( l1 != null ) {
+					l.add( model );
+					l.addAll( l1 );
+					return l;
+				}
+     		}
+    	}
+    	return null;
+    }
+    
+    /**
+     * Checks if the sticky informations are at correct positions.
+     * <p>
+     * This method checks following restrictions:<br>
+     * 1) the destination must be different to the source parent.<br>
+     * 2) all source models must have the same parent.<br>
+     * 3) the destination and the source parent must have the same parent.<br>
+     * 
+     * @param stickyParent Sticky information of the parent (the destination).
+     * @param stickyChilds Sticky information of the childs (the source).
+     * @param depth Tree depth (where is the destination) (use 0 for Patient,1 for Study,2 for Series)
+     * @return
+     */
+    private boolean checkStickyPlacement( Set stickyParent, Set stickyChilds, int depth ) {
+		FolderForm folderForm = (FolderForm) getForm();
+  		List listParent, listChilds;
+		listParent = findModelHistory( folderForm.getPatients(), (String)stickyParent.iterator().next(), depth );
+		listChilds = findModelHistory( folderForm.getPatients(), (String)stickyChilds.iterator().next(), depth+1 );
+		int listParentSize = listParent.size();
+		if ( listParent.get(listParentSize-1).equals( listChilds.get(listChilds.size()-2) ) ) {
+			return false; //same parent;
+		}
+		listParentSize -= 1;
+		for ( int i = 0 ; i < listParentSize ; i++ ) {
+			if ( !listParent.get(i).equals( listChilds.get(i) ) ) {
+				return false; //different parent(s) from parent;
+			}
+			
+		}
+		AbstractModel model = (AbstractModel) listChilds.get(listChilds.size()-2);//parent model of first child
+		Iterator iter = stickyChilds.iterator();
+		iter.next(); //skip first element (is used for listChilds)
+		while ( iter.hasNext() ) {
+			if ( !model.containsPK( Integer.parseInt( iter.next().toString() ) ) ) {
+				return false; //this child has not the same parent as first child element
+			}
+		}
+ 		return true;
+    }
+
 }
