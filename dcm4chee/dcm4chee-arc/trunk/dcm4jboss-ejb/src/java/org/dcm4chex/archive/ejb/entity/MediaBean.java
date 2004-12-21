24394ccd215664dd2638f3c00fa183fa33e49a75
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2004-12-21 11:07:44 +0000

    MediaBeanLocalHome: add listByCreatedTime add listByUpdatedTime

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/entity/MediaBean.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/entity/MediaBean.java
@@ -8,8 +8,13 @@
  ******************************************/
 package org.dcm4chex.archive.ejb.entity;
 
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Set;
+
 import javax.ejb.CreateException;
 import javax.ejb.EntityBean;
+import javax.ejb.FinderException;
 
 import org.apache.log4j.Logger;
 
@@ -51,6 +56,12 @@ import org.apache.log4j.Logger;
  *  strategy="on-find"
  *  eager-load-group="*"
  *
+ * @jboss.query 
+ * 	signature="java.util.Collection ejbSelectGeneric(java.lang.String jbossQl, java.lang.Object[] args)"
+ *  dynamic="true"
+ *  strategy="on-find"
+ *  eager-load-group="*"
+ *
  * @author gunter.zeilinger@tiani.com
  * @version $Revision$ $Date$
  * @since 26.11.2004
@@ -89,7 +100,7 @@ public abstract class MediaBean implements EntityBean {
     public abstract java.sql.Timestamp getUpdatedTime();
 
     public abstract void setUpdatedTime(java.sql.Timestamp time);
-    
+
     /**
      * @ejb.interface-method
      * @ejb.persistence column-name="fileset_id"
@@ -130,7 +141,7 @@ public abstract class MediaBean implements EntityBean {
      * @ejb.interface-method
      */
     public abstract void setMediaUsage(long mediaUsage);
-    
+
     /**
      * @ejb.interface-method
      * @ejb.persistence column-name="media_status"
@@ -164,14 +175,100 @@ public abstract class MediaBean implements EntityBean {
 
     public abstract void setInstances(java.util.Collection insts);
 
-   /**
-    * @ejb.create-method
-    */
-   public Integer ejbCreate(String fsIuid) throws CreateException {
-       setFilesetIuid(fsIuid);
-       return null;
-   }
+    /**
+     * @ejb.create-method
+     */
+    public Integer ejbCreate(String fsIuid) throws CreateException {
+        setFilesetIuid(fsIuid);
+        return null;
+    }
 
-   public void ejbPostCreate(String fsIuid) throws CreateException {
-   }
+    public void ejbPostCreate(String fsIuid) throws CreateException {
+    }
+
+    /**    
+     * @ejb.home-method
+     */
+    public java.util.Collection ejbHomeListByCreatedTime(Timestamp after,
+            Timestamp before, Set stati, Integer offset, Integer limit,
+            boolean desc) throws FinderException {
+        return findBy("m.createdTime", after, before, stati, offset, limit,
+                desc);
+    }
+
+    /**    
+     * @ejb.home-method
+     */
+    public java.util.Collection ejbHomeListByUpdatedTime(Timestamp after,
+            Timestamp before, Set stati, Integer offset, Integer limit,
+            boolean desc) throws FinderException {
+        return findBy("m.updatedTime", after, before, stati, offset, limit,
+                desc);
+    }
+
+    private Collection findBy(String attrName, Timestamp after,
+            Timestamp before, Set stati, Integer offset, Integer limit,
+            boolean desc) throws FinderException {
+        // generate JBossQL query
+        StringBuffer jbossQl = new StringBuffer();
+        boolean filterStati = stati != null && !stati.isEmpty();
+        int argsCount = filterStati ? stati.size() : 0;
+        if (after != null)
+            ++argsCount;
+        if (before != null)
+            ++argsCount;
+        if (offset != null)
+            ++argsCount;
+        if (limit != null)
+            ++argsCount;
+        jbossQl.append("SELECT OBJECT(m) FROM media m");
+        Object[] args = new Object[argsCount];
+        int i = 0;
+        if (filterStati) {
+            stati.toArray(args);
+            jbossQl.append(" WHERE m.mediaStatus IN (");
+            for (int n = stati.size(); i < n; i++) {
+                jbossQl.append(i > 0 ? ", ?" : "?").append(i + 1);
+            }
+            jbossQl.append(")");
+        }
+        if (after != null || before != null) {
+            jbossQl.append(filterStati ? " AND " : " WHERE ");
+            jbossQl.append(attrName);
+            if (after != null) {
+                args[i++] = after;
+                if (before == null) {
+                    jbossQl.append(" > ?").append(i);
+                } else {
+                    jbossQl.append(" BETWEEN ?").append(i);
+                    args[i++] = before;
+                    jbossQl.append(" AND ?").append(i);
+                }
+            } else if (before != null) {
+                args[i++] = before;
+                jbossQl.append(" < ?").append(i);
+            }
+        }
+        jbossQl.append(" ORDER BY ");
+        jbossQl.append(attrName);
+        jbossQl.append(desc ? " DESC" : " ASC");
+        if (offset != null) {
+            args[i++] = offset;
+            jbossQl.append(" OFFSET ?").append(i);
+        }
+        if (limit != null) {
+            args[i++] = limit;
+            jbossQl.append(" LIMIT ?").append(i);
+        }
+
+        // call dynamic-ql query
+        return ejbSelectGeneric(jbossQl.toString(), args);
+    }
+
+    /**
+     * @ejb.select query=""
+     *  transaction-type="Supports"
+     */ 
+    public abstract Collection ejbSelectGeneric(String jbossQl, Object[] args)
+    		throws FinderException;
 }
