dccae00a1884c3ffc76066995639c659189169f9
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-01-07 00:51:49 +0000

    Remove last modification (implements Serializable)

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/FileInfo.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/FileInfo.java
@@ -8,16 +8,13 @@
  ******************************************/
 package org.dcm4chex.archive.ejb.jdbc;
 
-import java.io.Serializable;
 import java.util.Comparator;
 
 /**
  * @author <a href="mailto:gunter@tiani.com">Gunter Zeilinger </a>
  *  
  */
-public class FileInfo implements Serializable {
-
-    private static final long serialVersionUID = 4051044159831685173L;
+public class FileInfo {
 
     public static final Comparator ASC_ORDER = new Comparator() {
 
