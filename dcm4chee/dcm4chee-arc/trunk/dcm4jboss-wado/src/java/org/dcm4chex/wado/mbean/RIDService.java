afe107a32d847fa1ed187450899e9e60dbce9174
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-02-09 14:29:35 +0000

    scheduler for freeDiskSpace. Setter/getter for 'space' changed to use FileUtils methods. clearCache and freeDiskSpace (old:cleanCache) changed to get status response.

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/RIDService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/RIDService.java
@@ -16,13 +16,10 @@ import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.xml.transform.TransformerConfigurationException;
 
-import org.apache.log4j.Logger;
 import org.dcm4cheri.util.StringUtils;
 import org.dcm4chex.wado.common.RIDRequestObject;
 import org.dcm4chex.wado.common.RIDResponseObject;
-import org.dcm4chex.wado.mbean.cache.WADOCache;
 import org.dcm4chex.wado.mbean.cache.WADOCacheImpl;
-import org.jboss.system.ServiceMBeanSupport;
 import org.xml.sax.SAXException;
 
 /**
@@ -32,67 +29,13 @@ import org.xml.sax.SAXException;
  * <p>
  * This class use RIDSupport for the Retrieve Information for Display methods and WADOCache for caching documents.
  */
-public class RIDService extends ServiceMBeanSupport  {
+public class RIDService extends AbstractCacheService  {
 
-	private Logger log = Logger.getLogger( getClass().getName() );
-	private WADOCache cache = WADOCacheImpl.getRIDCache();
 	private RIDSupport support = new RIDSupport( this.server );
 	
-	private static final long GIGA = 1000000000L;
-	private static final long MEGA = 1000000L;
-	
-	public void setCacheRoot( String newRoot ) {
-		cache.setCacheRoot( newRoot );
+	public RIDService() {
+		cache = WADOCacheImpl.getRIDCache();
 	}
-	
-	public String getCacheRoot() {
-		return cache.getCacheRoot();
-	}
-	
-	public void setMinFreeSpace( int minFree ) {
-		cache.setMinFreeSpace( minFree * MEGA );
-	}
-	
-	public int getMinFreeSpace() {
-		return (int) (cache.getMinFreeSpace() / MEGA);
-	}
-
-	public void setPreferredFreeSpace( int minFree ) {
-		cache.setPreferredFreeSpace( minFree * MEGA );
-	}
-	
-	public int getPreferredFreeSpace() {
-		return (int) (cache.getPreferredFreeSpace() / MEGA);
-	}
-	
-	/**
-	 * Clears the cache.
-	 * <p>
-	 * Remove all files in the cache.
-	 *
-	 */
-	public void clearCache() {
-		cache.clearCache();
-	}
-	
-	/**
-	 * Cleans the cache if necessary.
-	 *
-	 */
-	public void cleanCache() {
-		cache.cleanCache( false ); //cleans the cache and wait until clean process is done. (not in background)
-	}
-	
-	/**
-	 * Returns the available space of the cache drive.
-	 * <p>
-	 * 
-	 * @return
-	 */
-	public String getFreeSpace() {
-		return getSizeWithUnit( cache.getFreeSpace() );
-	}
-	
 	public String getRIDSummaryXsl() {
 		String s = support.getRIDSummaryXsl();
 		if ( s == null ) s = "";
@@ -212,31 +155,5 @@ public class RIDService extends ServiceMBeanSupport  {
 		if ( log.isDebugEnabled() ) log.debug("getRIDDocument:"+reqVO );
 		return support.getRIDDocument( reqVO );
 	}
-
 	
-	/**
-	 * Returns a String representation with trailing (G|M|K)B.
-	 * <p>
-	 * Rounds the value to a more readable form.
-	 * <p>
-	 * Use 1000 instead of 1024 for one KB.
-	 * 
-	 * @param size the size in bytes.
-	 * 
-	 * @return The size with unit. e.g. 2GB
-	 */
-	private String getSizeWithUnit(long size) {
-		if ( size >= GIGA ){
-				return (size/GIGA)+" GB";
-		} else if ( size > MEGA ) {
-			if ( (size % MEGA) == 0 ) {
-				return (size/MEGA)+" MB";
-			} else {
-				return (size/1000L)+" KB";
-			}
-		} else {
-			return (size/1000L)+" KB";
-		}
-	}
-
 }
