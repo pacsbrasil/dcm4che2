9ed47d0d4f255c78b7db3e4a04cdbc6d306d0ab6
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-02-21 13:27:01 +0000

    added

---
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/session/CodeToDeviceMappingBean.java
@@ -0,0 +1,233 @@
+/******************************************
+ *                                        *
+ *  dcm4che: A OpenSource DICOM Toolkit   *
+ *                                        *
+ *  Distributable under LGPL license.     *
+ *  See terms of license at gnu.org.      *
+ *                                        *
+ ******************************************/
+package org.dcm4chex.archive.ejb.session;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.ejb.FinderException;
+import javax.ejb.RemoveException;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
+import org.dcm4che.data.Dataset;
+import org.dcm4che.data.DcmElement;
+import org.dcm4che.dict.Tags;
+import org.dcm4chex.archive.ejb.entity.CodeBean;
+import org.dcm4chex.archive.ejb.interfaces.CodeLocal;
+import org.dcm4chex.archive.ejb.interfaces.CodeLocalHome;
+import org.dcm4chex.archive.ejb.interfaces.DeviceLocal;
+import org.dcm4chex.archive.ejb.interfaces.DeviceLocalHome;
+
+/**
+ * 
+ * @author franz.willer@tiani.com
+ * @version $Revision$ $Date$
+ * @since 17.02.2005
+ * 
+ * @ejb.bean
+ *  name="CodeToDeviceMapping"
+ *  type="Stateless"
+ *  view-type="remote"
+ *  jndi-name="ejb/CodeToDeviceMapping"
+ * 
+ * @ejb.transaction-type 
+ *  type="Container"
+ * 
+ * @ejb.transaction 
+ *  type="Required"
+ * 
+ * @ejb.ejb-ref
+ *  ejb-name="Code" 
+ *  view-type="local"
+ *  ref-name="ejb/Code" 
+ * 
+ * @ejb.ejb-ref
+ *  ejb-name="Device" 
+ *  view-type="local"
+ *  ref-name="ejb/Device" 
+ * 
+ */
+public abstract class CodeToDeviceMappingBean implements SessionBean {
+
+    private CodeLocalHome codeHome;
+
+    private DeviceLocalHome devHome;
+
+    private static final Logger log = Logger.getLogger(CodeToDeviceMappingBean.class);
+
+    public void setSessionContext(SessionContext arg0) throws EJBException,
+            RemoteException {
+        Context jndiCtx = null;
+        try {
+            jndiCtx = new InitialContext();
+            codeHome = (CodeLocalHome) jndiCtx
+                    .lookup("java:comp/env/ejb/Code");
+            devHome = (DeviceLocalHome) jndiCtx
+                    .lookup("java:comp/env/ejb/Device");
+        } catch (NamingException e) {
+            throw new EJBException(e);
+        } finally {
+            if (jndiCtx != null) {
+                try {
+                    jndiCtx.close();
+                } catch (NamingException ignore) {
+                }
+            }
+        }
+    }
+
+    public void unsetSessionContext() {
+        codeHome = null;
+        devHome = null;
+    }
+
+    /**
+     * @throws CreateException
+     * @throws FinderException
+     * @ejb.interface-method
+     */
+    public void createMapping( Dataset ds  ) throws CreateException, FinderException {
+    	DcmElement spsSeq = ds.get( Tags.SPSSeq );
+    	DcmElement codeSeq;
+    	int len = spsSeq.vm();
+    	int lenCS;
+    	Dataset ds1, dsCode;
+    	for ( int i = 0 ; i < len ; i++ ) {
+    		ds1 = spsSeq.getItem( i );
+    		DeviceLocal dl = getDeviceLocal( ds1 );
+    		List codes = new ArrayList();
+    		codeSeq = ds1.get( Tags.ScheduledProtocolCodeSeq );
+    		lenCS = codeSeq.vm();
+    		for ( int j = 0 ; j < lenCS ; j++ ) {
+    			dsCode = codeSeq.getItem( j );
+    	    	codes.add( CodeBean.valueOf( codeHome, dsCode ) );
+    		}
+    		dl.setProtocolCodes( codes );
+    		log.info("Device "+dl.getStationName()+" mapped to protocols:"+codes);
+     	}
+    }
+
+    /**
+	 * @param ds1
+	 * @return
+     * @throws FinderException
+     * @throws CreateException
+	 */
+	private DeviceLocal getDeviceLocal(Dataset ds1) throws FinderException, CreateException {
+		String name = ds1.getString( Tags.ScheduledStationName );
+		String aet = ds1.getString( Tags.ScheduledStationAET );
+		String modality = ds1.getString( Tags.Modality );
+		DeviceLocal dl = null;
+		try {
+			dl = devHome.findByStationName( name );
+		} catch ( Exception x ) { //IGNORE
+		}
+		if ( dl == null ) {
+			dl = devHome.create( name, aet, modality );
+		} else {
+			//delete mapping with this device ?
+			//update aet and/or modality ?
+		}
+		return dl;
+	}
+
+	
+	/**
+     * @throws FinderException
+	 * @throws CreateException
+	 * @ejb.interface-method
+     */
+    public Dataset addDeviceInfo( Dataset ds ) throws FinderException {
+    	DcmElement spsSeq = ds.get( Tags.SPSSeq );
+    	DcmElement codeSeq;
+    	int len = spsSeq.vm();
+    	int lenCS;
+    	Dataset ds1, dsCode;
+    	for ( int i = 0 ; i < len ; i++ ) {
+    		ds1 = spsSeq.getItem( i );
+    		codeSeq = ds1.get( Tags.ScheduledProtocolCodeSeq );
+   			dsCode = codeSeq.getItem();
+    		Collection col = codeHome.findByValueAndDesignator( dsCode.getString( Tags.CodeValue ),
+    															dsCode.getString( Tags.CodingSchemeDesignator ) );
+    		if ( ! col.isEmpty() ) {
+    			Collection col1 = devHome.findByProtocolCode( (CodeLocal) col.iterator().next() );
+    			if ( ! col1.isEmpty() ) {
+    				DeviceLocal dl = (DeviceLocal) col1.iterator().next();
+    				ds1.putAE( Tags.ScheduledStationAET, dl.getStationAET() );
+    				ds1.putSH( Tags.ScheduledStationName, dl.getStationName() );
+    				String reqModality = ds1.getString( Tags.Modality );
+    				if ( reqModality == null ) {
+    					ds1.putCS( Tags.Modality, dl.getModality() );
+    				} else if ( ! reqModality.equalsIgnoreCase( dl.getModality() ) ) {
+    					log.warn( "Different Modality ("+reqModality+") in request and device ("+dl.getModality()+") !!!");
+    				}
+    			}
+    		}
+     	}
+    	return ds;
+    }
+    
+	/**
+     * @throws FinderException
+	 * @throws EJBException
+	 * @throws RemoveException
+	 * @ejb.interface-method
+     */
+    public boolean deleteDevice( String stationName ) throws FinderException, EJBException, RemoveException {
+    	DeviceLocal dl = devHome.findByStationName( stationName );
+    	if ( dl == null ) return false;
+    	deleteDevice( dl );
+    	return true;
+    }
+    
+    private void deleteDevice( DeviceLocal dl ) throws EJBException, RemoveException {
+    	Collection col = new ArrayList();
+    	col.addAll( dl.getProtocolCodes() ); //need independent collection!
+    	String name = dl.getStationName();
+    	log.info("Delete device:"+name);
+    	dl.remove();
+    	Iterator iter = col.iterator();
+    	while ( iter.hasNext() ) {
+    		CodeLocal code = (CodeLocal) iter.next();
+    		log.info("Delete protocol code for device ("+name+"):"+
+    					code.getCodeValue()+"("+code.getCodingSchemeDesignator()+")" );
+        	try {
+        		( code ).remove();
+        	} catch ( Exception x ) { log.error("Cant delete code ('"+code.getCodeValue()+"')!!!"); }
+    	}
+    	
+    	
+    }
+    
+	/**
+     * @throws FinderException
+	 * @throws EJBException
+	 * @throws RemoveException
+	 * @ejb.interface-method
+     */
+    public boolean deleteMapping() throws FinderException, EJBException, RemoveException {
+    	Collection col = devHome.findAll();
+    	Iterator iter = col.iterator();
+    	while ( iter.hasNext() ) {
+    		deleteDevice( (DeviceLocal) iter.next() );
+    	}
+    	return true;
+    }
+
+}
---
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/mbean/DeviceService.java
@@ -0,0 +1,141 @@
+/******************************************
+ *                                        *
+ *  dcm4che: A OpenSource DICOM Toolkit   *
+ *                                        *
+ *  Distributable under LGPL license.     *
+ *  See terms of license at gnu.org.      *
+ *                                        *
+ ******************************************/
+package org.dcm4chex.archive.mbean;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+import javax.ejb.RemoveException;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.dcm4che.data.Dataset;
+import org.dcm4che.data.DcmElement;
+import org.dcm4che.data.DcmObjectFactory;
+import org.dcm4che.dict.Tags;
+import org.dcm4cheri.util.StringUtils;
+import org.dcm4chex.archive.ejb.interfaces.CodeToDeviceMapping;
+import org.dcm4chex.archive.ejb.interfaces.CodeToDeviceMappingHome;
+import org.dcm4chex.archive.util.EJBHomeFactory;
+import org.dcm4chex.archive.util.HomeFactoryException;
+import org.jboss.system.ServiceMBeanSupport;
+
+/**
+ * @author franz.willer@tiani.com
+ * @version $Revision$ $Date$
+ * @since 17.02.2005
+ */
+public class DeviceService extends ServiceMBeanSupport {
+
+	private static final String STANDARD_XSL_URL = "resource:xsl/device/import.xsl";
+	
+    private CodeToDeviceMapping mapper;
+	private Map templates = new HashMap();
+
+	private static final TransformerFactory tf = TransformerFactory.newInstance();
+    private static final DcmObjectFactory dof = DcmObjectFactory.getInstance();
+
+	public DeviceService() {
+    }
+    
+    protected void startService() throws Exception {
+    }
+
+    protected void stopService() throws Exception {
+    }
+
+    
+    public String importDeviceMapping(String importURL)  {
+        Dataset ds = dof.newDataset();
+        try {
+	        Transformer t = getTemplates(STANDARD_XSL_URL).newTransformer();
+	        t.transform(new StreamSource(importURL),
+	        		new SAXResult(ds.getSAXHandler2(null)));
+	        lookupMapper().createMapping( ds );
+        } catch ( Exception x ) {
+        	x.printStackTrace();
+        	return "Exception:"+x.getMessage();
+        }
+     	return "Device mapping from url '"+importURL+"' imported!";
+    }
+
+    public String deleteMapping() throws RemoteException, FinderException, RemoveException, HomeFactoryException, CreateException {
+    	return "Status:"+lookupMapper().deleteMapping();
+    }
+
+    public String deleteDevice(String device) throws RemoteException, FinderException, RemoveException, HomeFactoryException, CreateException {
+    	return "Status:"+lookupMapper().deleteDevice( device );
+    }
+    
+    public Collection getDeviceList( String protocol ) throws RemoteException, HomeFactoryException, CreateException, FinderException {
+        String[] sa = StringUtils.split( protocol, '^' );
+        if ( sa.length < 3 ) {
+        	throw new IllegalArgumentException( "Wrong protocol format! Use <code>^<meaning>^<designator> !");
+        }
+        Dataset ds = dof.newDataset(); 
+        DcmElement sq = ds.putSQ( Tags.SPSSeq );
+        Dataset ds1 = sq.addNewItem();
+        DcmElement codeSq = ds1.putSQ( Tags.ScheduledProtocolCodeSeq );
+        Dataset ds2 = codeSq.addNewItem();
+        ds2.putSH( Tags.CodeValue, sa[0] );
+    	ds2.putSH( Tags.CodeMeaning, sa[1]);
+    	ds2.putSH( Tags.CodingSchemeDesignator, sa[2] );
+    	Dataset ds3 = addDeviceInfo( ds );
+    	sq = ds3.get( Tags.SPSSeq );
+    	ds1 = sq.getItem();
+    	String dev = ds1.getString( Tags.ScheduledStationName );
+    	List l = new ArrayList(); l.add( dev );
+    	return l;
+    }
+    
+    public Dataset addDeviceInfo( Dataset ds ) throws RemoteException, FinderException, HomeFactoryException, CreateException {
+    	return lookupMapper().addDeviceInfo( ds );
+    }
+    
+    public Templates getTemplates(String uri) throws TransformerConfigurationException {
+		Templates tpl = (Templates) templates.get(uri);
+		if (tpl == null) {
+		    tpl = tf.newTemplates(new StreamSource(uri));
+		    templates.put(uri, tpl);
+		}
+		return tpl;
+    }
+
+
+   
+	/**
+	 * Returns the CodeToDeviceMapping session bean.
+	 * 
+	 * @return The CodeToDeviceMapping.
+	 * 
+	 * @throws HomeFactoryException
+	 * @throws RemoteException
+	 * @throws CreateException
+	 */
+    private CodeToDeviceMapping lookupMapper() throws HomeFactoryException, RemoteException, CreateException {
+    	if ( mapper == null ) {
+    		CodeToDeviceMappingHome home = (CodeToDeviceMappingHome) EJBHomeFactory
+	        .getFactory().lookup(CodeToDeviceMappingHome.class,
+	        		CodeToDeviceMappingHome.JNDI_NAME);
+    		mapper = home.create();
+    	}
+    	return mapper;
+    }
+    
+}
