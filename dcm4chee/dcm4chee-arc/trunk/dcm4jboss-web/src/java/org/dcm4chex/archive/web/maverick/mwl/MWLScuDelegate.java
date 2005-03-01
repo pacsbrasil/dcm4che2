ddf79ddee9b81f357b5588092006bd1f640d5a08
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-03-01 16:28:44 +0000

    move check for aet="LOCAL" to MWLScuService

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/mwl/MWLScuDelegate.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/mwl/MWLScuDelegate.java
@@ -18,105 +18,110 @@ import org.jboss.mx.util.MBeanServerLocator;
 
 /**
  * @author franz.willer
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class MWLScuDelegate {
-	   private static ObjectName mwlScuServiceName = null;
-		private static MBeanServer server;
-		
-	    private static Logger log = Logger.getLogger( MWLScuDelegate.class.getName() );
+	private static ObjectName mwlScuServiceName = null;
 
-	    /** 
-	     * Iinitialize the MWLScu service delegator.
-	     * <p>
-	     * Set the name of the MwlScuService MBean with the servlet config param 'mwlScuServiceName'.
-	     * 
-	     * @param config The ServletConfig object.
-	     */
-		public void init( ServletConfig config ) {
-	        if (server != null) return;
-	        server = MBeanServerLocator.locate();
-	        String s = config.getInitParameter("mwlScuServiceName");
-	        try {
-	        	mwlScuServiceName = new ObjectName(s);
-				
-			} catch (Exception e) {
-				log.error( "Exception in init! ",e );
-			}
-	       
-	    }
+	private static MBeanServer server;
 
-		public Logger getLogger() {
-			return log;
+	private static Logger log = Logger
+			.getLogger(MWLScuDelegate.class.getName());
+
+	/**
+	 * Iinitialize the MWLScu service delegator.
+	 * <p>
+	 * Set the name of the MwlScuService MBean with the servlet config param
+	 * 'mwlScuServiceName'.
+	 * 
+	 * @param config
+	 *            The ServletConfig object.
+	 */
+	public void init(ServletConfig config) {
+		if (server != null)
+			return;
+		server = MBeanServerLocator.locate();
+		String s = config.getInitParameter("mwlScuServiceName");
+		try {
+			mwlScuServiceName = new ObjectName(s);
+
+		} catch (Exception e) {
+			log.error("Exception in init! ", e);
 		}
-		
-		/**
-		 * Makes the MBean call to get the list of worklist entries for given filter (ds).
-		 * 
-		 * @param ds	The WADO request.
-		 * 
-		 * @return The list of worklist entries ( Each item in the list is a Dataset of one scheduled procedure step).
-		 */
-		public List findMWLEntries( Dataset ds ) {
-			List resp = null;
-			try {
-		        Object o = server.invoke(mwlScuServiceName,
-		                "findMWLEntries",
-	                    new Object[]{ ds },
-						new String[]{ Dataset.class.getName() });
-		        resp = (List) o;
-			} catch ( Exception x ) {
-				log.error( "Exception occured in getMWLEntries: "+x.getMessage(), x );
-			}
-	        return resp;
+
+	}
+
+	public Logger getLogger() {
+		return log;
+	}
+
+	/**
+	 * Makes the MBean call to get the list of worklist entries for given filter
+	 * (ds).
+	 * 
+	 * @param ds
+	 *            The WADO request.
+	 * 
+	 * @return The list of worklist entries ( Each item in the list is a Dataset
+	 *         of one scheduled procedure step).
+	 */
+	public List findMWLEntries(Dataset ds) {
+		List resp = null;
+		try {
+			Object o = server.invoke(mwlScuServiceName, "findMWLEntries",
+					new Object[] { ds },
+					new String[] { Dataset.class.getName() });
+			resp = (List) o;
+		} catch (Exception x) {
+			log.error("Exception occured in findMWLEntries: " + x.getMessage(),
+					x);
 		}
-		
-		/**
-		 * Checks if the MwlScpAET is local.
-		 * <p>
-		 * This means, that the MWLSCP is in the same container.
-		 * <p>
-		 * If it runs in the same container, the query can be done directly without a CFIND.
-		 * Also we can allow deletion of MWLEntries.
-		 * 
-		 * @return true if the MWLSCP runs in the same container.
-		 */
-		public boolean isLocal() {
-			try {
-		        Object o = server.invoke(mwlScuServiceName,
-		                "getMwlScpAET",
-	                    null,
-						null );
-		        String resp = (String) o;
-		        if ( resp == null ) return false;
-		        return ( "local".equalsIgnoreCase( resp ) );
-			} catch ( Exception x ) {
-				log.error( "Exception occured in isLocal: "+x.getMessage(), x );
-			}
-			return false;
+		return resp;
+	}
+
+	/**
+	 * Checks if the MwlScpAET is local.
+	 * <p>
+	 * This means, that the MWLSCP is in the same container.
+	 * <p>
+	 * If it runs in the same container, the query can be done directly without
+	 * a CFIND. Also we can allow deletion of MWLEntries.
+	 * 
+	 * @return true if the MWLSCP runs in the same container.
+	 */
+	public boolean isLocal() {
+		try {
+			Boolean b = (Boolean) server.getAttribute(mwlScuServiceName,
+					"Local");
+			return b.booleanValue();
+		} catch (Exception x) {
+			log.error("Exception occured in isLocal: " + x.getMessage(), x);
 		}
-		
-		/**
-		 * Deletes an MWL entry with given id.
-		 * <p>
-		 * This method should only be called if isLocal() returns true!
-		 * 
-		 * @param spsID The ID of the MWLEntry (Scheduled Procedure Step ID)
-		 * @return
-		 */
-		public boolean deleteMWLEntry( String spsID ) {
-			try {
-		        Object o = server.invoke(mwlScuServiceName,
-		                "deleteMWLEntry",
-	                    new Object[]{ spsID },
-						new String[]{ String.class.getName() });
-		        return ((Boolean) o).booleanValue();
-			} catch ( Exception x ) {
-				log.error( "Exception occured in deleteMWLEntry: "+x.getMessage(), x );
-			}
-			return false;
+		return false;
+	}
+
+	/**
+	 * Deletes an MWL entry with given id.
+	 * <p>
+	 * This method should only be called if isLocal() returns true!
+	 * 
+	 * @param spsID
+	 *            The ID of the MWLEntry (Scheduled Procedure Step ID)
+	 * @return
+	 */
+	public boolean deleteMWLEntry(String spsID) {
+		try {
+			Object o = server.invoke(mwlScuServiceName, "deleteMWLEntry",
+					new Object[] { spsID }, new String[] { String.class
+							.getName() });
+			return ((Boolean) o).booleanValue();
+		} catch (Exception x) {
+			log.error("Exception occured in deleteMWLEntry: " + x.getMessage(),
+					x);
 		}
+		return false;
+	}
 
 }
