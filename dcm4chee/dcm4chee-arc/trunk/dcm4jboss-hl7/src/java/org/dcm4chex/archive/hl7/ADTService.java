55f46d2fde1c1065b99e848af517ebebbee07a76
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-06-23 11:27:00 +0000

    Support for ADT^23,ADT^A01,ADT^A04,ADT^A05 messages.

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-hl7/src/java/org/dcm4chex/archive/hl7/ADTService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-hl7/src/java/org/dcm4chex/archive/hl7/ADTService.java
@@ -29,9 +29,26 @@ import org.xml.sax.ContentHandler;
 
 public class ADTService extends AbstractHL7Service {
 
-    private String pidStylesheetURL = "resource:xsl/hl7/pid2dcm.xsl";
+    private static final String ADT_A08 = "A08";
+	private String pidStylesheetURL = "resource:xsl/hl7/pid2dcm.xsl";
 
     private String mrgStylesheetURL = "resource:xsl/hl7/mrg2dcm.xsl";
+    
+    private String patientUpdateMessages = ADT_A08+",A01,A04,A05";//A01,A04,A05 'create' patient (via update)
+    
+    public String getPatientUpdateMessages() {
+    	return patientUpdateMessages;
+    }
+    
+    public void setPatientUpdateMessages( String msgs ) {
+    	if (msgs == null || msgs.trim().length() < 1) {
+    		patientUpdateMessages = ADT_A08;
+    	} else {
+    		if ( msgs.indexOf( ADT_A08 ) == -1 ) {
+    			patientUpdateMessages = ADT_A08+","+msgs;
+    		}
+    	}
+    }
 
     public final String getMrgStylesheetURL() {
 		return mrgStylesheetURL;
@@ -78,6 +95,9 @@ public class ADTService extends AbstractHL7Service {
                             + " with "+ pat.getString(Tags.PatientName)
                             + ", PID:" + pat.getString(Tags.PatientID));
                     update.mergePatient(pat, mrg);
+                } else if ( isDelete(msh) ) {
+                	log.info("Delete Patient "+pat.getString( Tags.PatientName )+", PID:"+pat.getString( Tags.PatientID ));
+                	update.deletePatient(pat);
                 }
             } finally {
                 try {
@@ -92,11 +112,15 @@ public class ADTService extends AbstractHL7Service {
     }
 
     private boolean isUpdate(MSH msh) {
-        return "A08".equals(msh.triggerEvent);
+        return patientUpdateMessages.indexOf(msh.triggerEvent) != -1;
     }
 
     private boolean isMerge(MSH msh) {
         return "A40".equals(msh.triggerEvent);
+    }
+
+    private boolean isDelete(MSH msh) {
+        return "A23".equals(msh.triggerEvent);
     }
 
     private PatientUpdateHome getPatientUpdateHome()
