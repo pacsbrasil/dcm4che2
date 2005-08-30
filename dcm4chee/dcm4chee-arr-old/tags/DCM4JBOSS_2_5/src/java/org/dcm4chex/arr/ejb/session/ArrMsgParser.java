8684ddab366cbcc13b4917bba5f751a147c97a15
nobody <nobody@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-08-30 10:35:15 +0000

    This commit was manufactured by cvs2svn to create tag 'DCM4JBOSS_2_5'.

---
+++ dcm4chee/dcm4chee-arr-old/tags/DCM4JBOSS_2_5/src/java/org/dcm4chex/arr/ejb/session/ArrMsgParser.java
@@ -0,0 +1,54 @@
+package org.dcm4chex.arr.ejb.session;
+
+import java.util.Date;
+
+import org.xml.sax.InputSource;
+
+/**
+ * @author joseph foraci
+ *
+ * This is the interface for a Audit Record Repository service that may be used
+ * to parse a IHE audit message.
+ */
+public interface ArrMsgParser {
+    public static final int INVALID_XML = 1;        //not well-formed xml data (or complete garbage)
+    public static final int INVALID_SCHEMA = 2;     //xml data is well-formed, but data does not validate
+    public static final int INVALID_INCOMPLETE = 4; //could not interpret some/all information such as Hostname, TimeStamp, or Type
+    
+	int parse(String xmlData)
+		throws ArrInputException;
+    
+	int parse(InputSource is)
+		throws ArrInputException;
+    
+	/**
+	 * Get the audit type from parsed data (ie the name of the audit event
+	 * element that was generated for the triggering event).
+	 *
+	 * @return A <code>String</code> representing the audit type or
+	 *   <code>null</code> if it was not included.
+	 */
+	String getType();
+    
+	/** Get parsed Hostname from Host element
+	 *
+	 * @return A <code>String</code> representing the host mentioned or
+	 *   <code>null</code> if it was not included.
+	 */
+	String getHost();
+    
+	/** Get parsed TimeStamp from Timestamp element
+	 *
+	 * @return A <code>Date</code> representing the time stamp mentioned or
+	 *   <code>null</code> if it was not included.
+	 */
+	Date getTimeStamp();
+    
+    String getAet();
+    
+    String getUserName();
+    
+    String getPatientName();
+    
+    String getPatientId();
+}
