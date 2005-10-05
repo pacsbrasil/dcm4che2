188b81cb237d59cb273b29a784aa7c9fe7610aee
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2005-10-05 16:26:16 +0000

    Change License from LGPL to MPL/LGPL/GPL triple License

--- dcm4che14/trunk/src/java/org/dcm4che/data/DcmDecodeParam.java
+++ dcm4che14/trunk/src/java/org/dcm4che/data/DcmDecodeParam.java
@@ -1,7 +1,5 @@
-/*$Id$*/
-/*****************************************************************************
- *                                                                           *
- *  Copyright (c) 2002 by TIANI MEDGRAPH AG <gunter.zeilinger@tiani.com>     *
+/*                                                                           *
+ *  Copyright (c) 2002-2005 by TIANI MEDGRAPH AG                             *
  *                                                                           *
  *  This file is part of dcm4che.                                            *
  *                                                                           *
@@ -18,8 +16,7 @@
  *  You should have received a copy of the GNU Lesser General Public         *
  *  License along with this library; if not, write to the Free Software      *
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
- *                                                                           *
- *****************************************************************************/
+ */
 
 package org.dcm4che.data;
 
