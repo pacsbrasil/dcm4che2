f6a10ecc9a0185eb2d3bd42c96d208e7ec9718af
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-01-09 14:48:23 +0000

    change JBoss configuration directory from server/dcmcdw to server/default (=JBoss default)

--- dcm4chee/dcm4chee-cdw/trunk/src/etc/bin/init_redhat.sh
+++ dcm4chee/dcm4chee-cdw/trunk/src/etc/bin/init_redhat.sh
@@ -32,7 +32,7 @@ JAVAPTH=${JAVAPTH:-"/usr/local/jdk/bin"}
 JBOSSCP=${JBOSSCP:-"$JBOSS_HOME/bin/shutdown.jar"}
 
 #define the script to use to start jboss
-JBOSSSH=${JBOSSSH:-"$JBOSS_HOME/bin/run.sh -c dcmcdw"}
+JBOSSSH=${JBOSSSH:-"$JBOSS_HOME/bin/run.sh"}
 
 if [ -n "$JBOSS_CONSOLE" -a ! -d "$JBOSS_CONSOLE" ]; then
   # ensure the file exists
