697459e43fcacb6ebba5020fa0a310ab42c0c9b9
nobody <nobody@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-06-22 15:58:38 +0000

    This commit was manufactured by cvs2svn to create tag 'DCM4CHEE_2_8_2'.

---
+++ dcm4chee/dcm4chee-cdw/tags/DCM4CHEE_2_8_2/src/etc/bin/install.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+rem Batch file to install dcm4che/cdw Media Creation Server
+
+rem %~dp0 is the expanded pathname of the current script under NT
+set BINDIR=.
+if "%OS%"=="Windows_NT" set BINDIR=%~dp0
+
+set DCMCDW=%BINDIR%\..
+set DEFAULT=%DCMCDW%\..\default
+
+copy %DEFAULT%\deploy\hsqldb-ds.xml %DCMCDW%\deploy
+copy %DEFAULT%\deploy\jboss-jca.sar %DCMCDW%\deploy
+copy %DEFAULT%\deploy\jboss-local-jdbc.rar %DCMCDW%\deploy
+copy %DEFAULT%\deploy\transaction-service.xml %DCMCDW%\deploy
+xcopy /E %DEFAULT%\deploy\jbossweb-tomcat50.sar %DCMCDW%\deploy\jbossweb-tomcat50.sar\
+xcopy /E %DEFAULT%\deploy\jmx-invoker-adaptor-server.sar %DCMCDW%\deploy\jmx-invoker-adaptor-server.sar\
+xcopy /E %DEFAULT%\deploy\jmx-console.war %DCMCDW%\deploy\jmx-console.war\
+
+copy %DEFAULT%\deploy\jms\hsqldb-jdbc2-service.xml %DCMCDW%\deploy\jms
+copy %DEFAULT%\deploy\jms\jvm-il-service.xml %DCMCDW%\deploy\jms
+  
+copy %DEFAULT%\conf\jbossmq-state.xml %DCMCDW%\conf
+copy %DEFAULT%\conf\jndi.properties %DCMCDW%\conf
+copy %DEFAULT%\conf\login-config.xml %DCMCDW%\conf
+copy %DEFAULT%\conf\server.policy %DCMCDW%\conf
+xcopy /E %DEFAULT%\conf\xmdesc %DCMCDW%\conf\xmdesc\
+xcopy /E %DEFAULT%\lib %DCMCDW%\lib\
