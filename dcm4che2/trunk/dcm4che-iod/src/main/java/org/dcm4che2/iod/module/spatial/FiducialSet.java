b5280e0c360186ae28e27c6b190708ce7af3f9e8
tinton <tinton@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-07-04 15:24:38 +0000

    The spatial fiducials IOD and its modules

---
+++ dcm4che2/trunk/dcm4che-iod/src/main/java/org/dcm4che2/iod/module/spatial/FiducialSet.java
@@ -0,0 +1,108 @@
+package org.dcm4che2.iod.module.spatial;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.dcm4che2.data.VR;
+import org.dcm4che2.iod.module.macro.SOPInstanceReference;
+
+/**
+ * This class represents a set of Fiducials.
+ * <p>
+ * Since there is no Fiducial Set without Fiducials, this class extends
+ * {@link org.dcm4che2.iod.module.spatial.Fiducial}.
+ * 
+ * @author Antonio Magni <dcm4ceph@antoniomagni.org>
+ * 
+ */
+public class FiducialSet extends Fiducial {
+
+    public FiducialSet(DicomObject dcmobj) {
+        super(dcmobj);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * Identifies a Frame of Reference that may or may not be an image set.
+     * 
+     * (e.g. an atlas or physical space).
+     * 
+     * See C.7.4.1.1.1 for further explanation. Required if Referenced Image
+     * Sequence (0008,1140) is absMay be present otherwise.
+     * <p>
+     * Type 1C
+     * 
+     * @return
+     */
+    public String getFrameofRerenceUID() {
+        return dcmobj.getString(Tag.FrameofReferenceUID);
+    }
+
+    /**
+     * Identifies a Frame of Reference that may or may not be an image set.
+     * 
+     * (e.g. an atlas or physical space).
+     * 
+     * See C.7.4.1.1.1 for further explanation. Required if Referenced Image
+     * Sequence (0008,1140) is absMay be present otherwise.
+     * <p>
+     * Type 1C
+     * 
+     * @param ui
+     */
+    public void setFrameofReferenceUID(String ui) {
+        dcmobj.putString(Tag.FrameofReferenceUID, VR.UI, ui);
+    }
+
+    /**
+     * Identifies the set of images in which the fiducials are located.
+     * 
+     * Required if Frame of Reference UID (0020,0052) is absent. May be present
+     * otherwise. One or more Items shall be present. All referenced images
+     * shall have the same Frame of Reference UID if present in the images.
+     * <p>
+     * Type 1C
+     * 
+     * @return
+     */
+    public SOPInstanceReference[] getReferencedImages() {
+        return SOPInstanceReference.toSOPInstanceReferences(dcmobj
+                .get(Tag.ReferencedImageSequence));
+    }
+
+    /**
+     * Identifies the set of images in which the fiducials are located.
+     * 
+     * Required if Frame of Reference UID (0020,0052) is absent. May be present
+     * otherwise. One or more Items shall be present. All referenced images
+     * shall have the same Frame of Reference UID if present in the images.
+     * <p>
+     * Type 1C
+     * 
+     * @param sops
+     */
+    public void setReferencedImages(SOPInstanceReference[] sops) {
+        updateSequence(Tag.ReferencedImageSequence, sops);
+    }
+
+    /**
+     * A sequence that specifies one or more fiducials, one item per fiducial.
+     * <p>
+     * Type 1
+     * 
+     * @return
+     */
+    public Fiducial[] getFiducials() {
+        return (Fiducial[]) toModules(dcmobj.get(Tag.FiducialSequence));
+    }
+
+    /**
+     * A sequence that specifies one or more fiducials, one item per fiducial.
+     * <p>
+     * Type 1
+     * 
+     * @param fids
+     */
+    public void setFiducials(Fiducial[] fids) {
+        updateSequence(Tag.FiducialSequence, fids);
+    }
+}
---
+++ dcm4che2/trunk/dcm4che-iod/src/main/java/org/dcm4che2/iod/module/spatial/GraphicCoordinatesData.java
@@ -0,0 +1,142 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Gunter Zeilinger, Huetteldorferstr. 24/10, 1150 Vienna/Austria/Europe.
+ * Portions created by the Initial Developer are Copyright (C) 2002-2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Gunter Zeilinger <gunterze@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4che2.iod.module.spatial;
+
+import org.dcm4che2.data.DicomElement;
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.dcm4che2.data.VR;
+import org.dcm4che2.iod.module.Module;
+import org.dcm4che2.iod.module.macro.SOPInstanceReference;
+
+/**
+ * 
+ * @author Antonio Magni <dcm4ceph@antoniomagni.org>
+ * 
+ */
+public class GraphicCoordinatesData extends Module {
+
+    public GraphicCoordinatesData(DicomObject dcmobj) {
+        super(dcmobj);
+        // TODO Auto-generated constructor stub
+    }
+
+    public static Module[] toModules(DicomElement sq) {
+        if (sq == null || !sq.hasItems()) {
+            return null;
+        }
+        Module[] a = new Module[sq.countItems()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Module(sq.getDicomObject(i));
+        }
+        return a;
+
+    }
+
+    /**
+     * Graphic point coordinates of the fiducial.
+     * 
+     * Graphic point coordinates of the fiducial points in the image of the
+     * Referenced Image Sequence. If Fiducial’s Contour Data (3006,0050) is
+     * present, these points correlate to the points in the Contour Data, one
+     * row-column pair for each point and in the same order. See C.10.5.1.2 for
+     * further explanation.
+     * <p>
+     * Tpe 1
+     * 
+     * @return
+     */
+    public float getGraphicData() {
+        return dcmobj.getFloat(Tag.GraphicData);
+    }
+
+    /**
+     * Graphic point coordinates of the fiducial.
+     * 
+     * Graphic point coordinates of the fiducial points in the image of the
+     * Referenced Image Sequence. If Fiducial’s Contour Data (3006,0050) is
+     * present, these points correlate to the points in the Contour Data, one
+     * row-column pair for each point and in the same order. See C.10.5.1.2 for
+     * further explanation.
+     * <p>
+     * Type 1
+     * 
+     * @param ds
+     */
+    public void setGraphicData(float ds) {
+        dcmobj.putFloat(Tag.GraphicData, VR.DS, ds);
+    }
+
+    /**
+     * Image containing the fiducial’s graphic coordinates.
+     * 
+     * A sequence that specifies the image containing the fiducial’s graphic
+     * coordinates. Only one item shall be present. Shall be an image within the
+     * set of the images in the Referenced Image Sequence (0008,1140) of the
+     * encapsulating Fiducial Set Sequence (0070,031C) item.
+     * 
+     * <p>
+     * Type 1
+     * 
+     * @param sop
+     */
+    public void setReferencedImage(SOPInstanceReference sop) {
+        updateSequence(Tag.ReferencedImageSequence, sop);
+    }
+
+    /**
+     * Image containing the fiducial’s graphic coordinates.
+     * 
+     * A sequence that specifies the image containing the fiducial’s graphic
+     * coordinates. Only one item shall be present. Shall be an image within the
+     * set of the images in the Referenced Image Sequence (0008,1140) of the
+     * encapsulating Fiducial Set Sequence (0070,031C) item.
+     * 
+     * <p>
+     * Type 1
+     * 
+     * @return
+     */
+    public SOPInstanceReference getReferencedImage() {
+        DicomObject item = dcmobj
+                .getNestedDicomObject(Tag.ReferencedImageSequence);
+        return item != null ? new SOPInstanceReference(item) : null;
+    }
+
+}
