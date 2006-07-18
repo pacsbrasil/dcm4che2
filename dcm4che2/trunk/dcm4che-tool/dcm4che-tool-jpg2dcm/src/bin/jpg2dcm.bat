01c4a08a2d651b49a6cf3eab59cdd52b51bc5318
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-07-18 23:49:27 +0000

    increment version to 2.0.7-SNAPSHOT

--- dcm4che2/trunk/dcm4che-tool/dcm4che-tool-jpg2dcm/src/bin/jpg2dcm.bat
+++ dcm4che2/trunk/dcm4che-tool/dcm4che-tool-jpg2dcm/src/bin/jpg2dcm.bat
@@ -9,7 +9,7 @@ if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
 set MAIN_CLASS=org.dcm4che2.tool.jpg2dcm.Jpg2Dcm
-set MAIN_JAR=dcm4che-tool-Jpg2dcm-2.0.6.jar
+set MAIN_JAR=dcm4che-tool-Jpg2dcm-2.0.7-SNAPSHOT.jar
 
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
@@ -44,7 +44,7 @@ set JAVA=%JAVA_HOME%\bin\java
 
 set CP=%DCM4CHE_HOME%\etc\
 set CP=%CP%;%DCM4CHE_HOME%\lib\%MAIN_JAR%
-set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-2.0.6.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-2.0.7-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\nlog4j-1.2.19.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.0.jar
 
