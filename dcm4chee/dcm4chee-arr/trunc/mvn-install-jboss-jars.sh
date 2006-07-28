61eddfa4b633d92afe9940a9ce40cc1b4aa2bf2c
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-07-28 10:42:17 +0000

    generate POMs for imported jboss jars configure dcm4che maven2 repostory [http://www.dcm4che.org/maven2]

--- dcm4chee/dcm4chee-arr/trunc/mvn-install-jboss-jars.sh
+++ dcm4chee/dcm4chee-arr/trunc/mvn-install-jboss-jars.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
+JBOSS_HOME=${JBOSS_HOME:-$HOME/jboss-4.0.4.GA}
 JBOSS_LIB=$JBOSS_HOME/server/default/lib
 JBOSS_EJB3=$JBOSS_HOME/server/default/deploy/ejb3.deployer
-mvn install:install-file -Dfile=$JBOSS_LIB/jboss-j2ee.jar -DgroupId=jboss \
-    -DartifactId=jboss-j2ee -Dversion=4.0.4.GA -Dpackaging=jar
-mvn install:install-file -Dfile=$JBOSS_LIB/ejb3-persistence.jar -DgroupId=jboss \
-    -DartifactId=ejb3-persistence -Dversion=4.0.4.GA -Dpackaging=jar
-mvn install:install-file -Dfile=$JBOSS_EJB3/jboss-ejb3.jar -DgroupId=jboss \
-    -DartifactId=jboss-ejb3 -Dversion=4.0.4.GA -Dpackaging=jar
-mvn install:install-file -Dfile=$JBOSS_EJB3/jboss-ejb3x.jar -DgroupId=jboss \
-    -DartifactId=jboss-ejb3x -Dversion=4.0.4.GA -Dpackaging=jar
-mvn install:install-file -Dfile=$JBOSS_EJB3/jboss-annotations-ejb3.jar -DgroupId=jboss \
-    -DartifactId=jboss-annotations-ejb3 -Dversion=4.0.4.GA -Dpackaging=jar
+MVN="mvn install:install-file -DgeneratePom=true -Dpackaging=jar "
+$MVN -Dfile=$JBOSS_LIB/jboss-j2ee.jar \
+     -DgroupId=jboss \
+     -DartifactId=jboss-j2ee \
+     -Dversion=4.0.4.GA
+$MVN -Dfile=$JBOSS_LIB/ejb3-persistence.jar \
+     -DgroupId=jboss \
+     -DartifactId=ejb3-persistence \
+     -Dversion=4.0.4.GA  
+$MVN -Dfile=$JBOSS_EJB3/jboss-ejb3.jar \
+     -DgroupId=jboss \
+     -DartifactId=jboss-ejb3 \
+     -Dversion=4.0.4.GA
+$MVN -Dfile=$JBOSS_EJB3/jboss-ejb3x.jar \
+     -DgroupId=jboss \
+     -DartifactId=jboss-ejb3x \
+     -Dversion=4.0.4.GA
+$MVN -Dfile=$JBOSS_EJB3/jboss-annotations-ejb3.jar \
+     -DgroupId=jboss \
+     -DartifactId=jboss-annotations-ejb3 \
+     -Dversion=4.0.4.GA
