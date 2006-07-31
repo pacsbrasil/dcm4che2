42f1f1b6edae80849c68ca834e4f238c6ef04294
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-07-31 01:36:29 +0000

    Add initial web app

--- dcm4chee/dcm4chee-arr/trunc/mvn-install-jboss-jars.sh
+++ dcm4chee/dcm4chee-arr/trunc/mvn-install-jboss-jars.sh
@@ -23,3 +23,16 @@ $MVN -Dfile=$JBOSS_EJB3/jboss-annotations-ejb3.jar \
      -DgroupId=jboss \
      -DartifactId=jboss-annotations-ejb3 \
      -Dversion=4.0.4.GA
+$MVN -Dfile=$JBOSS_LIB/hibernate-annotations.jar \
+     -DgroupId=hibernate \
+     -DartifactId=hibernate-annotations \
+     -Dversion=3.2.0.CR1
+$MVN -Dfile=$JBOSS_LIB/hibernate-entitymanager.jar \
+     -DgroupId=hibernate \
+     -DartifactId=hibernate-entitymanager \
+     -Dversion=3.2.0.CR1
+$MVN -Dfile=$JBOSS_LIB/hibernate.jar \
+     -DgroupId=hibernate \
+     -DartifactId=hibernate \
+     -Dversion=3.2.0.cr2
+	       
