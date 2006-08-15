7e4a6617bbe7acb7c3cf13d713db8b6814f53710
damien_evans <damien_evans@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-08-15 04:30:25 +0000

    Add javadoc.

--- dcm4che2/trunk/dcm4che-net/src/main/java/org/dcm4che2/net/service/DicomService.java
+++ dcm4che2/trunk/dcm4che-net/src/main/java/org/dcm4che2/net/service/DicomService.java
@@ -21,6 +21,7 @@
  *
  * Contributor(s):
  * Gunter Zeilinger <gunterze@gmail.com>
+ * Damien Evans <damien.daddy@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -39,45 +40,96 @@
 package org.dcm4che2.net.service;
 
 /**
+ * Base class for DICOM services.
+ * 
  * @author gunter zeilinger(gunterze@gmail.com)
  * @version $Reversion$ $Date$
  * @since Oct 3, 2005
- *
  */
 public abstract class DicomService
 {
     private final String[] sopClasses;
     private final String serviceClass;
-    
+
+    /**
+     * Constructor. The parameters of this constructor define the manner in
+     * which the service will be looked up from the
+     * <code>DicomServiceRegistry</code>. The <code>sopClasses</code> array
+     * defines the specific SOP class UIDs that this <code>DicomService</code>
+     * will respond to, while the <code>serviceClass</code> can define a
+     * higher level SOP class UID that the service will respond to. The
+     * <code>serviceClass</code> may be null.
+     * 
+     * @param sopClasses A String array containing the SOP class UIDs that this
+     *            service responds to.
+     * @param serviceClass The service class of the DicomDevice. For example,
+     *            <code>UID.StorageServiceClass</code>. May be null.
+     */
     protected DicomService(String[] sopClasses, String serviceClass)
     {
         this.sopClasses = (String[]) sopClasses.clone();
         this.serviceClass = serviceClass;
     }
 
+    /**
+     * Constructor. The parameter of this constructor defines the manner in
+     * which the service will be looked up from the
+     * <code>DicomServiceRegistry</code>. The <code>sopClasses</code> array
+     * defines the specific SOP class UIDs that this <code>DicomService</code>
+     * will respond to.
+     * 
+     * @param sopClasses A String array containing the SOP class UIDs that this
+     *            service responds to.
+     */
     protected DicomService(String[] sopClasses)
     {
         this(sopClasses, null);
     }
-    
+
+    /**
+     * Constructor. The parameter of this constructor defines the manner in
+     * which the service will be looked up from the
+     * <code>DicomServiceRegistry</code>. The <code>sopClass</code> defines
+     * the specific SOP class UID that this <code>DicomService</code> will
+     * respond to.
+     * 
+     * @param sopClasses A String array containing the SOP class UIDs that this
+     *            service responds to.
+     */
     protected DicomService(String sopClass)
     {
         if (sopClass == null)
             throw new NullPointerException("sopClass");
-        this.sopClasses = new String[]{ sopClass };
+        this.sopClasses = new String[] { sopClass };
         this.serviceClass = null;
     }
-    
+
+    /**
+     * Get the SOP classes that this service responds to.
+     * 
+     * @return String array containing the SOP Class UIDs.
+     */
     public final String[] getSopClasses()
     {
         return (String[]) sopClasses.clone();
     }
 
+    /**
+     * Get the SOP class that this service responds to. If there are multiple
+     * SOP classes, the first one is returned.
+     * 
+     * @return String containing the SOP Class UID.
+     */
     public final String getSopClass()
     {
         return sopClasses[0];
     }
-    
+
+    /**
+     * Get the service class that this service belongs to.
+     * 
+     * @return String containing the SOP Class UID.
+     */
     public final String getServiceClass()
     {
         return serviceClass;
