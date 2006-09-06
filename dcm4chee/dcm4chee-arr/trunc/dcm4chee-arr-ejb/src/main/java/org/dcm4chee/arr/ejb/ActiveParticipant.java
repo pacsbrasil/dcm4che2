2d63b5e488109705082e6a8f0c3ae28e7c02d52c
fangatagfa <fangatagfa@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-09-06 13:59:14 +0000

    added debug logging

--- dcm4chee/dcm4chee-arr/trunc/dcm4chee-arr-ejb/src/main/java/org/dcm4chee/arr/ejb/ActiveParticipant.java
+++ dcm4chee/dcm4chee-arr/trunc/dcm4chee-arr-ejb/src/main/java/org/dcm4chee/arr/ejb/ActiveParticipant.java
@@ -163,20 +163,19 @@ public class ActiveParticipant implements Serializable {
     }
     
     @PostPersist
-	public void postPersit() {
-		if(log.isDebugEnabled())
-			log.debug( "Created " + this.toString() );
-	}
-    
+    public void postPersit() {
+        if (log.isDebugEnabled())
+            log.debug("Created " + this.toString());
+    }
+
     @PostRemove
-	public void postRemove() {
-		if(log.isDebugEnabled())
-			log.debug( "Removed " + this.toString() );
-	}
-    
+    public void postRemove() {
+        if (log.isDebugEnabled())
+            log.debug("Removed " + this.toString());
+    }
+
     @Override
-	public String toString() {
-		// Default implementation. Sub class should provide meaningful overriding
-		return "[" + this.getClass().getSimpleName() + "[pk=" + pk + "]";
-	}
+    public String toString() {
+        return "[" + this.getClass().getSimpleName() + "[pk=" + pk + "]";
+    }
 }
