9d5301b41efc7af0da90df1470b0a1ea215cebb8
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-11-05 15:36:28 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/movescu/MoveScuService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/movescu/MoveScuService.java
@@ -75,81 +75,81 @@ import org.jboss.system.ServiceMBeanSupport;
  * @since 17.12.2003
  */
 public class MoveScuService extends ServiceMBeanSupport implements
-		MessageListener {
+        MessageListener {
 
-	private static final String[] NATIVE_TS = { UIDs.ExplicitVRLittleEndian,
-			UIDs.ImplicitVRLittleEndian };
+    private static final String[] NATIVE_TS = { UIDs.ExplicitVRLittleEndian,
+            UIDs.ImplicitVRLittleEndian };
 
-	private static final int ERR_MOVE_RJ = -2;
+    private static final int ERR_MOVE_RJ = -2;
 
-	private static final int ERR_ASSOC_RJ = -1;
+    private static final int ERR_ASSOC_RJ = -1;
 
-	private static final int PCID_MOVE = 1;
+    private static final int PCID_MOVE = 1;
 
-	private static final String DEF_CALLING_AET = "MOVE_SCU";
+    private static final String DEF_CALLING_AET = "MOVE_SCU";
 
-	private static final String DEF_CALLED_AET = "QR_SCP";
+    private static final String DEF_CALLED_AET = "QR_SCP";
 
-	private TLSConfigDelegate tlsConfig = new TLSConfigDelegate(this);
+    private TLSConfigDelegate tlsConfig = new TLSConfigDelegate(this);
 
-	private String callingAET = DEF_CALLING_AET;
+    private String callingAET = DEF_CALLING_AET;
 
-	private String calledAET = DEF_CALLED_AET;
+    private String calledAET = DEF_CALLED_AET;
 
-	private int acTimeout;
+    private int acTimeout;
 
-	private int dimseTimeout;
+    private int dimseTimeout;
 
-	private int soCloseDelay;
+    private int soCloseDelay;
 
-	private RetryIntervalls retryIntervalls = new RetryIntervalls();
+    private RetryIntervalls retryIntervalls = new RetryIntervalls();
 
-	private int concurrency = 1;
-        
-        private String queueName;
+    private int concurrency = 1;
 
-        private JMSDelegate jmsDelegate = new JMSDelegate(this);
+    private String queueName;
 
-        public final ObjectName getJmsServiceName() {
-            return jmsDelegate.getJmsServiceName();
-        }
+    private JMSDelegate jmsDelegate = new JMSDelegate(this);
 
-        public final void setJmsServiceName(ObjectName jmsServiceName) {
-            jmsDelegate.setJmsServiceName(jmsServiceName);
-        }
-        
-        public final String getQueueName() {
-            return queueName;
-        }
-        
-        public final void setQueueName(String queueName) {
-            this.queueName = queueName;
-        }
+    public final ObjectName getJmsServiceName() {
+        return jmsDelegate.getJmsServiceName();
+    }
 
-        public final ObjectName getTLSConfigName() {
-		return tlsConfig.getTLSConfigName();
-	}
+    public final void setJmsServiceName(ObjectName jmsServiceName) {
+        jmsDelegate.setJmsServiceName(jmsServiceName);
+    }
 
-	public final void setTLSConfigName(ObjectName tlsConfigName) {
-		tlsConfig.setTLSConfigName(tlsConfigName);
-	}
+    public final String getQueueName() {
+        return queueName;
+    }
+
+    public final void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
+
+    public final ObjectName getTLSConfigName() {
+        return tlsConfig.getTLSConfigName();
+    }
+
+    public final void setTLSConfigName(ObjectName tlsConfigName) {
+        tlsConfig.setTLSConfigName(tlsConfigName);
+    }
 
     public final int getReceiveBufferSize() {
-        return tlsConfig.getReceiveBufferSize();        
+        return tlsConfig.getReceiveBufferSize();
     }
-    
+
     public final void setReceiveBufferSize(int size) {
         tlsConfig.setReceiveBufferSize(size);
     }
 
     public final int getSendBufferSize() {
-        return tlsConfig.getSendBufferSize();        
+        return tlsConfig.getSendBufferSize();
     }
-    
+
     public final void setSendBufferSize(int size) {
         tlsConfig.setSendBufferSize(size);
     }
-        
+
     public final boolean isTcpNoDelay() {
         return tlsConfig.isTcpNoDelay();
     }
@@ -157,224 +157,220 @@ public class MoveScuService extends ServiceMBeanSupport implements
     public final void setTcpNoDelay(boolean on) {
         tlsConfig.setTcpNoDelay(on);
     }
-        
-	public final String getCalledAET() {
-		return calledAET;
-	}
 
-	public final void setCalledAET(String retrieveAET) {
-		this.calledAET = retrieveAET;
-	}
+    public final String getCalledAET() {
+        return calledAET;
+    }
 
-	public final int getAcTimeout() {
-		return acTimeout;
-	}
+    public final void setCalledAET(String retrieveAET) {
+        this.calledAET = retrieveAET;
+    }
 
-	public final void setAcTimeout(int acTimeout) {
-		this.acTimeout = acTimeout;
-	}
+    public final int getAcTimeout() {
+        return acTimeout;
+    }
 
-	public final int getDimseTimeout() {
-		return dimseTimeout;
-	}
+    public final void setAcTimeout(int acTimeout) {
+        this.acTimeout = acTimeout;
+    }
 
-	public final void setDimseTimeout(int dimseTimeout) {
-		this.dimseTimeout = dimseTimeout;
-	}
+    public final int getDimseTimeout() {
+        return dimseTimeout;
+    }
 
-	public final int getSoCloseDelay() {
-		return soCloseDelay;
-	}
+    public final void setDimseTimeout(int dimseTimeout) {
+        this.dimseTimeout = dimseTimeout;
+    }
 
-	public final void setSoCloseDelay(int soCloseDelay) {
-		this.soCloseDelay = soCloseDelay;
-	}
+    public final int getSoCloseDelay() {
+        return soCloseDelay;
+    }
 
-	public final int getConcurrency() {
-		return concurrency;
-	}
+    public final void setSoCloseDelay(int soCloseDelay) {
+        this.soCloseDelay = soCloseDelay;
+    }
 
-	public final void setConcurrency(int concurrency) throws Exception {
-		if (concurrency <= 0)
-			throw new IllegalArgumentException("Concurrency: " + concurrency);
-		if (this.concurrency != concurrency) {
-			final boolean restart = getState() == STARTED;
-			if (restart)
-				stop();
-			this.concurrency = concurrency;
-			if (restart)
-				start();
-		}
-	}
+    public final int getConcurrency() {
+        return concurrency;
+    }
 
-	public String getRetryIntervalls() {
-		return retryIntervalls.toString();
-	}
-
-	public void setRetryIntervalls(String text) {
-		retryIntervalls = new RetryIntervalls(text);
-	}
-
-	public String getCallingAET() {
-		return callingAET;
-	}
-
-	public void setCallingAET(String aet) {
-		this.callingAET = aet;
-	}
-	
-	public void scheduleMoveSeries(String retrieveAET, String destAET,
-			int priority, String pid, String studyIUID, String seriesIUID,
-			long scheduledTime) {
-		scheduleMoveOrder(new MoveOrder(retrieveAET, destAET, priority, pid,
-				studyIUID, seriesIUID), scheduledTime);
-	}
-
-        public void scheduleMove(String retrieveAET, String destAET,
-                int priority, String pid, String[] studyIUIDs,
-                String[] seriesIUIDs, String[] sopIUIDs, long scheduledTime) {
-            scheduleMoveOrder(new MoveOrder(retrieveAET, destAET, priority, pid,
-                        studyIUIDs, seriesIUIDs, sopIUIDs), scheduledTime);
+    public final void setConcurrency(int concurrency) throws Exception {
+        if (concurrency <= 0)
+            throw new IllegalArgumentException("Concurrency: " + concurrency);
+        if (this.concurrency != concurrency) {
+            final boolean restart = getState() == STARTED;
+            if (restart)
+                stop();
+            this.concurrency = concurrency;
+            if (restart)
+                start();
         }
-        
-	private void scheduleMoveOrder(MoveOrder order, long scheduledTime) {
-		try {
-			jmsDelegate.queue(queueName, order,
-					JMSDelegate.toJMSPriority(order.getPriority()),
-					scheduledTime);
-		} catch (Exception e) {
-			log.error("Failed to schedule order: " + order);
-		}
-	}
-	
-	protected void startService() throws Exception {
-		jmsDelegate.startListening(queueName, this, concurrency);
-	}
+    }
 
-	protected void stopService() throws Exception {
-		jmsDelegate.stopListening(queueName);
-	}
-	
+    public String getRetryIntervalls() {
+        return retryIntervalls.toString();
+    }
 
-	public void onMessage(Message message) {
-		ObjectMessage om = (ObjectMessage) message;
-		try {
-			MoveOrder order = (MoveOrder) om.getObject();
-			log.info("Start processing " + order);
-			try {
-				process(order);
-				log.info("Finished processing " + order);
-			} catch (Exception e) {
-				final int failureCount = order.getFailureCount() + 1;
-				order.setFailureCount(failureCount);
-				final long delay = retryIntervalls.getIntervall(failureCount);
-				if (delay == -1L) {
-					log.error("Give up to process " + order, e);
-				} else {
-					log.warn("Failed to process " + order
-							+ ". Scheduling retry.", e);
-					scheduleMoveOrder(order, System.currentTimeMillis() + delay);
-				}
-			}
-		} catch (Throwable e) {
-			log.error("unexpected error during processing message: " + message,
-					e);
-		}
-	}
+    public void setRetryIntervalls(String text) {
+        retryIntervalls = new RetryIntervalls(text);
+    }
 
-	private void process(MoveOrder order) throws SQLException,
-			UnkownAETException, IOException, DcmServiceException,
-			InterruptedException {
-		String aet = order.getRetrieveAET();
-		if (aet == null) {
-			aet = calledAET;
-		}
-		AEData aeData = new AECmd(aet).getAEData();
-		if (aeData == null) {
-			throw new UnkownAETException("Unkown Retrieve AET: " + aet);
-		}
-		AssociationFactory af = AssociationFactory.getInstance();
-		Association a = af.newRequestor(tlsConfig.createSocket(aeData));
-		a.setAcTimeout(acTimeout);
-		a.setDimseTimeout(dimseTimeout);
-		a.setSoCloseDelay(soCloseDelay);
-		AAssociateRQ rq = af.newAAssociateRQ();
-		rq.setCalledAET(aet);
-		rq.setCallingAET(callingAET);
+    public String getCallingAET() {
+        return callingAET;
+    }
+
+    public void setCallingAET(String aet) {
+        this.callingAET = aet;
+    }
+
+    public void scheduleMoveSeries(String retrieveAET, String destAET,
+            int priority, String pid, String studyIUID, String seriesIUID,
+            long scheduledTime) {
+        scheduleMoveOrder(new MoveOrder(retrieveAET, destAET, priority, pid,
+                studyIUID, seriesIUID), scheduledTime);
+    }
+
+    public void scheduleMove(String retrieveAET, String destAET, int priority,
+            String pid, String[] studyIUIDs, String[] seriesIUIDs,
+            String[] sopIUIDs, long scheduledTime) {
+        scheduleMoveOrder(new MoveOrder(retrieveAET, destAET, priority, pid,
+                studyIUIDs, seriesIUIDs, sopIUIDs), scheduledTime);
+    }
+
+    private void scheduleMoveOrder(MoveOrder order, long scheduledTime) {
+        try {
+            jmsDelegate.queue(queueName, order, JMSDelegate.toJMSPriority(order
+                    .getPriority()), scheduledTime);
+        } catch (Exception e) {
+            log.error("Failed to schedule order: " + order);
+        }
+    }
+
+    protected void startService() throws Exception {
+        jmsDelegate.startListening(queueName, this, concurrency);
+    }
+
+    protected void stopService() throws Exception {
+        jmsDelegate.stopListening(queueName);
+    }
+
+    public void onMessage(Message message) {
+        ObjectMessage om = (ObjectMessage) message;
+        try {
+            MoveOrder order = (MoveOrder) om.getObject();
+            log.info("Start processing " + order);
+            try {
+                process(order);
+                log.info("Finished processing " + order);
+            } catch (Exception e) {
+                final int failureCount = order.getFailureCount() + 1;
+                order.setFailureCount(failureCount);
+                final long delay = retryIntervalls.getIntervall(failureCount);
+                if (delay == -1L) {
+                    log.error("Give up to process " + order, e);
+                } else {
+                    log.warn("Failed to process " + order
+                            + ". Scheduling retry.", e);
+                    scheduleMoveOrder(order, System.currentTimeMillis() + delay);
+                }
+            }
+        } catch (Throwable e) {
+            log.error("unexpected error during processing message: " + message,
+                    e);
+        }
+    }
+
+    private void process(MoveOrder order) throws SQLException,
+            UnkownAETException, IOException, DcmServiceException,
+            InterruptedException {
+        String aet = order.getRetrieveAET();
+        if (aet == null) {
+            aet = calledAET;
+        }
+        AEData aeData = new AECmd(aet).getAEData();
+        if (aeData == null) {
+            throw new UnkownAETException("Unkown Retrieve AET: " + aet);
+        }
+        AssociationFactory af = AssociationFactory.getInstance();
+        Association a = af.newRequestor(tlsConfig.createSocket(aeData));
+        a.setAcTimeout(acTimeout);
+        a.setDimseTimeout(dimseTimeout);
+        a.setSoCloseDelay(soCloseDelay);
+        AAssociateRQ rq = af.newAAssociateRQ();
+        rq.setCalledAET(aet);
+        rq.setCallingAET(callingAET);
         rq.addPresContext(af.newPresContext(PCID_MOVE,
-                UIDs.PatientRootQueryRetrieveInformationModelMOVE,
-                NATIVE_TS));
-		PDU ac = a.connect(rq);
-		if (!(ac instanceof AAssociateAC)) {
-			throw new DcmServiceException(ERR_ASSOC_RJ,
-					"Association not accepted by " + aet + ": " + ac);
-		}
-		ActiveAssociation aa = af.newActiveAssociation(a, null);
-		aa.start();
-		try {
-	        if (a.getAcceptedTransferSyntaxUID(PCID_MOVE) == null)
-				throw new DcmServiceException(ERR_MOVE_RJ, 
-						"Patient Root Query Retrieve IM MOVE not supported by remote AE: " 
-						+ aet);
-			invokeDimse(aa, order);
-		} finally {
-			try {
-				aa.release(true);
+                UIDs.PatientRootQueryRetrieveInformationModelMOVE, NATIVE_TS));
+        PDU ac = a.connect(rq);
+        if (!(ac instanceof AAssociateAC)) {
+            throw new DcmServiceException(ERR_ASSOC_RJ,
+                    "Association not accepted by " + aet + ": " + ac);
+        }
+        ActiveAssociation aa = af.newActiveAssociation(a, null);
+        aa.start();
+        try {
+            if (a.getAcceptedTransferSyntaxUID(PCID_MOVE) == null)
+                throw new DcmServiceException(ERR_MOVE_RJ,
+                        "Patient Root Query Retrieve IM MOVE not supported by remote AE: "
+                                + aet);
+            invokeDimse(aa, order);
+        } finally {
+            try {
+                aa.release(true);
                 // workaround to ensure that the final MOVE-RSP is processed
-                // before to continue 
+                // before to continue
                 Thread.sleep(10);
-			} catch (Exception e) {
-				log.warn(
-						"Failed to release association " + aa.getAssociation(),
-						e);
-			}
-		}
-	}
+            } catch (Exception e) {
+                log.warn(
+                        "Failed to release association " + aa.getAssociation(),
+                        e);
+            }
+        }
+    }
 
-	private void invokeDimse(ActiveAssociation aa, MoveOrder order)
-			throws InterruptedException, IOException, DcmServiceException  {
-		AssociationFactory af = AssociationFactory.getInstance();
-		DcmObjectFactory dof = DcmObjectFactory.getInstance();
-		Command cmd = dof.newCommand();
-		cmd.initCMoveRQ(aa.getAssociation().nextMsgID(),
-                UIDs.PatientRootQueryRetrieveInformationModelMOVE,
-                order.getPriority(),
-                order.getMoveDestination());
+    private void invokeDimse(ActiveAssociation aa, MoveOrder order)
+            throws InterruptedException, IOException, DcmServiceException {
+        AssociationFactory af = AssociationFactory.getInstance();
+        DcmObjectFactory dof = DcmObjectFactory.getInstance();
+        Command cmd = dof.newCommand();
+        cmd.initCMoveRQ(aa.getAssociation().nextMsgID(),
+                UIDs.PatientRootQueryRetrieveInformationModelMOVE, order
+                        .getPriority(), order.getMoveDestination());
         Dataset ds = dof.newDataset();
         ds.putCS(Tags.QueryRetrieveLevel, order.getQueryRetrieveLevel());
         putLO(ds, Tags.PatientID, order.getPatientId());
         putUI(ds, Tags.StudyInstanceUID, order.getStudyIuids());
         putUI(ds, Tags.SeriesInstanceUID, order.getSeriesIuids());
         putUI(ds, Tags.SOPInstanceUID, order.getSopIuids());
-		log.debug("Move Identifier:\n");
-		log.debug(ds);
+        log.debug("Move Identifier:\n");
+        log.debug(ds);
         Dimse dimseRsp = aa.invoke(af.newDimse(PCID_MOVE, cmd, ds)).get();
         Command cmdRsp = dimseRsp.getCommand();
         int status = cmdRsp.getStatus();
-		if (status != 0) {
-	        if (status == Status.SubOpsOneOrMoreFailures 
-					&& order.getSopIuids() != null) {
-	            Dataset moveRspData = dimseRsp.getDataset();
-	            if (moveRspData != null) {
-	                String[] failedUIDs = 
-							ds.getStrings(Tags.FailedSOPInstanceUIDList);
-	                if (failedUIDs != null && failedUIDs.length != 0) {
-	                    order.setSopIuids(failedUIDs);
-	                }
-	            }
-	        }
-			throw new DcmServiceException(status, cmdRsp
-					.getString(Tags.ErrorComment));
-		}
-	}
+        if (status != 0) {
+            if (status == Status.SubOpsOneOrMoreFailures
+                    && order.getSopIuids() != null) {
+                Dataset moveRspData = dimseRsp.getDataset();
+                if (moveRspData != null) {
+                    String[] failedUIDs = ds
+                            .getStrings(Tags.FailedSOPInstanceUIDList);
+                    if (failedUIDs != null && failedUIDs.length != 0) {
+                        order.setSopIuids(failedUIDs);
+                    }
+                }
+            }
+            throw new DcmServiceException(status, cmdRsp
+                    .getString(Tags.ErrorComment));
+        }
+    }
 
-	private static void putLO(Dataset ds, int tag, String s) {
-		if (s != null)
-			ds.putLO(tag, s);
-	}
+    private static void putLO(Dataset ds, int tag, String s) {
+        if (s != null)
+            ds.putLO(tag, s);
+    }
 
-	private static void putUI(Dataset ds, int tag, String[] uids) {
-		if (uids != null)
-			ds.putUI(tag, uids);
-	}
+    private static void putUI(Dataset ds, int tag, String[] uids) {
+        if (uids != null)
+            ds.putUI(tag, uids);
+    }
 }
