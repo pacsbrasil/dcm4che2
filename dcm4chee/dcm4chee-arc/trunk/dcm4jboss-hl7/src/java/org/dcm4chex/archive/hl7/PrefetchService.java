efa6df75c27eee2120d0a010c9ec4ce0e0b1a93e
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2006-12-07 15:38:46 +0000

    Issue: DCMEE-185

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-hl7/src/java/org/dcm4chex/archive/hl7/PrefetchService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-hl7/src/java/org/dcm4chex/archive/hl7/PrefetchService.java
@@ -38,6 +38,18 @@
 
 package org.dcm4chex.archive.hl7;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -50,16 +62,34 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.sax.SAXResult;
 
+import org.dcm4che.data.Command;
 import org.dcm4che.data.Dataset;
 import org.dcm4che.data.DcmObjectFactory;
+import org.dcm4che.dict.Tags;
+import org.dcm4che.dict.UIDs;
+import org.dcm4che.net.AAssociateAC;
+import org.dcm4che.net.AAssociateRQ;
+import org.dcm4che.net.ActiveAssociation;
+import org.dcm4che.net.Association;
+import org.dcm4che.net.AssociationFactory;
+import org.dcm4che.net.Dimse;
+import org.dcm4che.net.FutureRSP;
+import org.dcm4che.net.PDU;
 import org.dcm4chex.archive.config.DicomPriority;
 import org.dcm4chex.archive.config.RetryIntervalls;
+import org.dcm4chex.archive.ejb.jdbc.AECmd;
+import org.dcm4chex.archive.ejb.jdbc.AEData;
 import org.dcm4chex.archive.mbean.JMSDelegate;
 import org.dcm4chex.archive.mbean.TLSConfigDelegate;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.io.DocumentSource;
+import org.dom4j.io.SAXContentHandler;
 import org.jboss.system.ServiceMBeanSupport;
 import org.regenstrief.xhl7.HL7XMLLiterate;
+import org.regenstrief.xhl7.HL7XMLReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
@@ -72,6 +102,7 @@ public class PrefetchService extends ServiceMBeanSupport implements
     private String prefetchSourceAET;
     private String destinationQueryAET;
     private String destinationStorageAET;
+    private String callingAET = "DCM4CHEE";
     private String stylesheetURL = "resource:dcm4chee-hl7/orm2prefetch.xsl";
     private ObjectName hl7ServerName;
     private ObjectName moveScuServiceName;
@@ -81,10 +112,17 @@ public class PrefetchService extends ServiceMBeanSupport implements
     private int destinationQueryPriority = 0;
     private int retrievePriority = 0;
 
+    private int acTimeout;
+    private int dimseTimeout;
+    private int maxPDUlen = 16352;
+    private int soCloseDelay;
+    
     private int concurrency = 1;
 
     private JMSDelegate jmsDelegate = new JMSDelegate(this);
     private TLSConfigDelegate tlsConfig = new TLSConfigDelegate(this);
+    
+    private static final String ONLINE = "ONLINE";
 
     public final String getPrefetchSourceAET() {
         return prefetchSourceAET != null ? prefetchSourceAET : "NONE";
@@ -133,6 +171,28 @@ public class PrefetchService extends ServiceMBeanSupport implements
     public final void setRetrievePriority(String retrievePriority) {
         this.retrievePriority = DicomPriority.toCode(retrievePriority);
     }
+    
+    /**
+     * @return the callingAET
+     */
+    public String getCallingAET() {
+        return callingAET;
+    }
+
+    /**
+     * @param callingAET the callingAET to set
+     */
+    public void setCallingAET(String callingAET) {
+        this.callingAET = callingAET;
+    }
+
+    public final int getMaxPDUlen() {
+        return maxPDUlen;
+    }
+    public final void setMaxPDUlen(int maxPDUlen) {
+        this.maxPDUlen = maxPDUlen;
+    }
+    
     
     public String getStylesheetURL() {
         return stylesheetURL;
@@ -208,6 +268,48 @@ public class PrefetchService extends ServiceMBeanSupport implements
             this.moveScuServiceName = moveScuServiceName;
     }
     
+    /**
+     * @return the acTimeout
+     */
+    public int getAcTimeout() {
+        return acTimeout;
+    }
+
+    /**
+     * @param acTimeout the acTimeout to set
+     */
+    public void setAcTimeout(int acTimeout) {
+        this.acTimeout = acTimeout;
+    }
+
+    /**
+     * @return the dimseTimeout
+     */
+    public int getDimseTimeout() {
+        return dimseTimeout;
+    }
+
+    /**
+     * @param dimseTimeout the dimseTimeout to set
+     */
+    public void setDimseTimeout(int dimseTimeout) {
+        this.dimseTimeout = dimseTimeout;
+    }
+
+    /**
+     * @return the soCloseDelay
+     */
+    public int getSoCloseDelay() {
+        return soCloseDelay;
+    }
+
+    /**
+     * @param soCloseDelay the soCloseDelay to set
+     */
+    public void setSoCloseDelay(int soCloseDelay) {
+        this.soCloseDelay = soCloseDelay;
+    }
+
     protected void startService() throws Exception {
         jmsDelegate.startListening(queueName, this, concurrency);
         server.addNotificationListener(hl7ServerName, this,
@@ -236,6 +338,7 @@ public class PrefetchService extends ServiceMBeanSupport implements
                 log.error("Failed to transform ORM into prefetch request", e);
                 return;
             }
+            prepareFindReqDS(findRQ);
             PrefetchOrder order = new PrefetchOrder(findRQ);
             try {
                 log.info("Scheduling " + order);
@@ -307,9 +410,201 @@ public class PrefetchService extends ServiceMBeanSupport implements
         }
     }
     
-    private void process(PrefetchOrder order) {
-        // TODO Auto-generated method stub
-        
+    private void process(PrefetchOrder order) throws Exception {
+        Dataset keys = order.getDataset();
+        log.debug("SearchDS from order:");log.debug(keys);
+        Map srcList = doCFIND(prefetchSourceAET, callingAET, keys, sourceQueryPriority);
+        Map destList = doCFIND(destinationQueryAET, callingAET, keys, destinationQueryPriority);
+        List notAvail = this.getListOfNotAvail(srcList, destList);
+        if (notAvail.size() > 0 ) {
+            log.debug("notAvail:"+notAvail);
+            log.info(notAvail.size()+" Series are not available on destination AE! Schedule for Pre-Fetch");
+            for ( Iterator iter = notAvail.iterator() ; iter.hasNext() ; ) {
+                scheduleMove( prefetchSourceAET, destinationStorageAET, retrievePriority, 
+                        (Dataset) iter.next(), 0l);
+            }
+        }
     }
 
+    /**
+     * @param keys
+     */
+    private void prepareFindReqDS(Dataset keys) {
+        String qrLevel = keys.getString(Tags.QueryRetrieveLevel);
+        if ( qrLevel != null && !qrLevel.equals("SERIES") ) {
+            log.warn("QueryRetrieveLevel of PrefetchOrder is "+qrLevel+"! Set to SERIES!");
+        }
+        keys.putCS(Tags.QueryRetrieveLevel,"SERIES");
+        if ( !keys.contains(Tags.PatientID) ) keys.putUI(Tags.PatientID);
+        if ( !keys.contains(Tags.StudyInstanceUID) ) keys.putUI(Tags.StudyInstanceUID);
+        if ( !keys.contains(Tags.SeriesInstanceUID) ) keys.putUI(Tags.SeriesInstanceUID);
+        keys.putIS(Tags.NumberOfSeriesRelatedInstances);
+        keys.putCS(Tags.InstanceAvailability);
+    }
+    
+    private Map doCFIND(String calledAET, String callingAET, Dataset keys, int priority ) throws Exception {
+        ActiveAssociation assoc = null;
+        try {
+            AEData aeData = new AECmd(calledAET).getAEData();
+            assoc = openAssoc(tlsConfig.createSocket(aeData),
+                    getCFINDAssocReq(calledAET, callingAET));
+            if (assoc == null) {
+                log.error("Couldnt open association to " + aeData);
+                return null;
+            }
+            Association as = assoc.getAssociation();
+            if (as.getAcceptedTransferSyntaxUID(1) == null) {
+                log.error(calledAET + " doesnt support CFIND request!", null);
+                return null;
+            }
+            Map result = new HashMap();
+            // send cfind request.
+            Command cmd = DcmObjectFactory.getInstance().newCommand();
+            cmd.initCFindRQ(1, UIDs.StudyRootQueryRetrieveInformationModelFIND,
+                    priority);
+            Dimse mcRQ = AssociationFactory.getInstance().newDimse(1, cmd,
+                    keys);
+            FutureRSP findRsp = assoc.invoke(mcRQ);
+            Dimse dimse = findRsp.get();
+            List pending = findRsp.listPending();
+            Iterator iter = pending.iterator();
+            Dataset ds;
+            while (iter.hasNext()) {
+                ds = ((Dimse) iter.next()).getDataset();
+                result.put(ds.getString(Tags.SeriesInstanceUID), ds);
+                log.debug(calledAET+": received Dataset:");log.debug(ds);
+            }
+            if (log.isDebugEnabled()) {
+                log.debug(calledAET+" : received final C-FIND RSP :"
+                        + dimse);
+            }
+            return result;
+        } finally {
+            if (assoc != null)
+                try {
+                    assoc.release(true);
+                } catch (Exception e1) {
+                    log.error(
+                            "Cant release association for CFIND"
+                                    + assoc.getAssociation(), e1);
+                }
+        }
+    }
+    
+    /**
+     * Return the association request for modality worklist C-FIND.
+     * <p>
+     * This association is used for sending modality worklist C-FIND request and action
+     * command.
+     * 
+     * @return Association for media creation.
+     */
+    private AAssociateRQ getCFINDAssocReq(String calledAET, String callingAET) {
+        AssociationFactory aFact = AssociationFactory.getInstance();
+        AAssociateRQ assocRQ = aFact.newAAssociateRQ();
+        assocRQ.setCalledAET(calledAET);
+        assocRQ.setCallingAET(callingAET);
+        assocRQ.setMaxPDULength(maxPDUlen);
+        assocRQ.addPresContext(aFact.newPresContext(1,
+                UIDs.StudyRootQueryRetrieveInformationModelFIND, new String[]{ UIDs.ExplicitVRLittleEndian,
+                    UIDs.ImplicitVRLittleEndian }));
+        return assocRQ;
+    }
+    
+    private ActiveAssociation openAssoc(Socket sock, AAssociateRQ assocRQ)
+        throws IOException, GeneralSecurityException {
+        AssociationFactory aFact = AssociationFactory.getInstance();
+        Association assoc = aFact.newRequestor(sock);
+        assoc.setAcTimeout(acTimeout);
+        assoc.setDimseTimeout(dimseTimeout);
+        assoc.setSoCloseDelay(soCloseDelay);
+        PDU assocAC = assoc.connect(assocRQ);
+        if (!(assocAC instanceof AAssociateAC)) {
+            return null;
+        }
+        ActiveAssociation retval = aFact.newActiveAssociation(assoc, null);
+        retval.start();
+        return retval;
+    }
+    
+    private List getListOfNotAvail(Map all, Map map ) {
+        ArrayList l = new ArrayList();
+        Dataset ds, dsAll;
+        Entry entry;
+        String seriesIUID;
+        StringBuffer sb = new StringBuffer();
+        for ( Iterator iter = all.entrySet().iterator() ; iter.hasNext() ; ) {
+            entry = (Entry) iter.next();
+            seriesIUID = (String) entry.getKey();
+            dsAll = (Dataset) entry.getValue();
+            ds = (Dataset) map.get(seriesIUID);
+            sb.setLength(0);
+            sb.append("Series ").append(seriesIUID).append(": ");
+            if ( ds == null ) {
+                log.debug(sb.append("Only known on source AE"));
+                l.add( dsAll ); 
+            } else if ( ! ONLINE.equals( ds.getString(Tags.InstanceAvailability)) ) {
+                log.debug(sb.append("Instances are not available (ONLINE) on destination AE!"));
+                l.add( dsAll ); 
+            } else {
+                int noi = ds.getInt(Tags.NumberOfSeriesRelatedInstances, -1);
+                int noi1 = dsAll.getInt(Tags.NumberOfSeriesRelatedInstances, -1);
+                sb.append("NumberOfSeriesRelatedInstances ");
+                if ( noi == -1 || noi1 == -1 ) {
+                    sb.append("is not available to check count of instances! dest:");
+                    log.warn(sb.append(noi).append(" src:").append(noi1));
+                } else if ( noi < noi1 ) {
+                    sb.append("on destination AE is less than on source AE! dest:");
+                    log.debug(sb.append(noi).append(" src:").append(noi1));
+                    l.add(dsAll);
+                }
+            }
+        }
+        return l;
+    }
+ 
+    private void scheduleMove(String retrieveAET, String destAET, int priority,
+            Dataset ds, long scheduledTime) {
+        try {
+            server.invoke(moveScuServiceName, "scheduleMove", new Object[] {
+                    retrieveAET, destAET, new Integer(priority), ds.getString(Tags.PatientID),
+                    ds.getString(Tags.StudyInstanceUID), 
+                    ds.getString(Tags.SeriesInstanceUID), null, new Long(scheduledTime) },
+                    new String[] { String.class.getName(),
+                            String.class.getName(), int.class.getName(),
+                            String.class.getName(), String.class.getName(),
+                            String.class.getName(), String[].class.getName(),
+                            long.class.getName() });
+        } catch (Exception e) {
+            log.error("Schedule Move failed:", e);
+        }
+    }
+    
+    
+    public void processFile(String filename) throws DocumentException, IOException, SAXException {
+        Dataset findRQ = DcmObjectFactory.getInstance().newDataset();
+        HL7XMLReader reader = new HL7XMLReader();
+        File file = new File(filename);
+        SAXContentHandler hl7in = new SAXContentHandler();
+        reader.setContentHandler(hl7in);
+        reader.parse(new InputSource( new FileInputStream(file)));
+        Document doc = hl7in.getDocument();
+        try {
+            Transformer t = getTemplates(stylesheetURL).newTransformer();
+            t.transform(new DocumentSource(doc), new SAXResult(findRQ.getSAXHandler2(null)));
+        } catch (TransformerException e) {
+            log.error("Failed to transform into prefetch request", e);
+            return;
+        }
+        this.prepareFindReqDS(findRQ);
+        PrefetchOrder order = new PrefetchOrder(findRQ);
+        try {
+            log.info("Scheduling Test PrefetchOrder:" + order);
+            jmsDelegate.queue(queueName, order, Message.DEFAULT_PRIORITY, 0L);
+        } catch (Exception e) {
+            log.error("Failed to schedule Test Order" + order, e);
+        }            
+     }
+    
+    
 }
