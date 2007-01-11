a34a33695867991715843ef34ad0cebb03b66ea3
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-01-11 15:19:35 +0000

    [#DCMEE-222] Emit Data Export Audit message on successful or failed Media Creation

--- dcm4chee/dcm4chee-audit/trunc/dcm4chee-audit-logger/src/main/java/org/dcm4chee/audit/logger/MediaExportLoggerMBean.java
+++ dcm4chee/dcm4chee-audit/trunc/dcm4chee-audit-logger/src/main/java/org/dcm4chee/audit/logger/MediaExportLoggerMBean.java
@@ -38,7 +38,11 @@
 
 package org.dcm4chee.audit.logger;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import javax.management.MBeanServer;
@@ -48,6 +52,17 @@ import javax.management.NotificationListener;
 import javax.management.ObjectName;
 
 import org.apache.log4j.Logger;
+import org.dcm4che.data.Dataset;
+import org.dcm4che.dict.Tags;
+import org.dcm4che.media.DirBuilderFactory;
+import org.dcm4che.media.DirReader;
+import org.dcm4che.media.DirRecord;
+import org.dcm4che2.audit.message.DataExportMessage;
+import org.dcm4che2.audit.message.DestinationMedia;
+import org.dcm4che2.audit.message.ParticipantObjectDescription;
+import org.dcm4che2.audit.message.Patient;
+import org.dcm4che2.audit.message.Study;
+import org.dcm4che2.audit.message.DataExportMessage.AuditEvent;
 import org.dcm4chex.cdw.common.MediaCreationRequest;
 import org.jboss.annotation.ejb.Management;
 import org.jboss.annotation.ejb.Service;
@@ -88,7 +103,7 @@ public class MediaExportLoggerMBean implements MediaExportLogger {
         NotificationFilterSupport f = new NotificationFilterSupport();
         f.enableType(MediaCreationRequest.class.getName());
         sources = server.queryNames(
-                new ObjectName("dcm4chee.cdw:service=MediaWriter"), null);
+                new ObjectName("dcm4chee.cdw:service=MediaWriter,*"), null);
         for (Iterator iter = sources.iterator(); iter.hasNext();) {
             server.addNotificationListener((ObjectName) iter.next(),
                     mediaCreationListener, f, null);
@@ -113,10 +128,85 @@ public class MediaExportLoggerMBean implements MediaExportLogger {
         try {
             MediaCreationRequest mcrq = 
                     (MediaCreationRequest) notif.getUserData();
-            //TODO
+            DirReader dirreader = DirBuilderFactory.getInstance().newDirReader(
+                    mcrq.getDicomDirFile());            
+            DirRecord patrec = dirreader.getFirstRecord();
+            DataExportMessage msg = new DataExportMessage(toEvent(mcrq) , 
+                    LoggerUtils.toSource(true, mcrq.getRequestAET(), 
+                            mcrq.getRequestIP()), toDestinationMedia(mcrq), 
+                            toPatient(patrec) );
+            addStudies(msg, patrec);
+            while ((patrec = patrec.getNextSibling()) != null) {
+                msg.addPatient(toPatient(patrec));
+                addStudies(msg, patrec);
+            }
+            LoggerUtils.log.info(msg);
         } catch (Throwable th) {
             log.warn("Failed to emit Data Export Audit Log message: ", th);
         }
     }
+
     };
+
+    private static Patient toPatient(DirRecord patrec) {
+        Dataset ds = patrec.getDataset();
+        return new Patient(ds.getString(Tags.PatientID))
+                .setPatientName(ds.getString(Tags.PatientName));
+    }
+
+    private static void addStudies(DataExportMessage msg, DirRecord patrec) 
+            throws IOException {
+        for (DirRecord styrec = patrec.getFirstChild(); styrec != null;
+                styrec = styrec.getNextSibling()) {
+            msg.addStudy(toStudy(styrec));
+        }
+    }
+
+    private static Study toStudy(DirRecord styrec) throws IOException {
+        Dataset styattrs = styrec.getDataset();
+        HashMap cuids = new HashMap();
+        for (DirRecord serrec = styrec.getFirstChild(); serrec != null;
+                serrec = serrec.getNextSibling()) {
+            for (DirRecord instrec = serrec.getFirstChild(); instrec != null;
+                    instrec = instrec.getNextSibling()) {
+                Dataset instattrs = instrec.getDataset();
+                String cuid = instattrs.getString(Tags.RefSOPClassUIDInFile);
+                ArrayList iuids = (ArrayList) cuids.get(cuid);
+                if (iuids == null) {
+                    cuids.put(cuid, iuids = new ArrayList());
+                }
+                iuids.add(instattrs.getString(Tags.RefSOPInstanceUIDInFile));
+            }           
+        }
+        Study sty = new Study(styattrs.getString(Tags.StudyInstanceUID));
+        ParticipantObjectDescription desc = new ParticipantObjectDescription();
+        for (Iterator iter = cuids.entrySet().iterator(); iter.hasNext();) {
+            Map.Entry el = (Map.Entry) iter.next();
+            ParticipantObjectDescription.SOPClass sopClass =
+                    new ParticipantObjectDescription.SOPClass(
+                            (String) el.getKey());
+            sopClass.setNumberOfInstances(((ArrayList) el.getValue()).size());
+            desc.addSOPClass(sopClass);
+        }
+        sty.addParticipantObjectDescription(desc);
+        return sty ;
+    }
+
+    private static DestinationMedia toDestinationMedia(MediaCreationRequest mcrq) {
+        String id = mcrq.getFilesetID();
+        String uid = mcrq.getFilesetUID();
+        DestinationMedia dest = new DestinationMedia(
+                id != null && id.length() > 0 ? id : uid);
+        dest.setAlternativeUserID(uid);
+        return dest;
+    }
+
+    private static DataExportMessage.AuditEvent toEvent(MediaCreationRequest mcrq) {
+        DataExportMessage.AuditEvent event = new DataExportMessage.AuditEvent();
+        if (mcrq.isFailed()) {
+            event.setEventOutcomeIndicator(AuditEvent.OutcomeIndicator.MINOR_FAILURE);
+        }
+        return event;
+    }
+
 }
