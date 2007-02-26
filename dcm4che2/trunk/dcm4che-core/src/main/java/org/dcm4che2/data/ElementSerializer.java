2d567809cf8c9b1455bb64d7a0942a716dd4d705
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-02-26 14:30:34 +0000

    pretty format

--- dcm4che2/trunk/dcm4che-core/src/main/java/org/dcm4che2/data/ElementSerializer.java
+++ dcm4che2/trunk/dcm4che-core/src/main/java/org/dcm4che2/data/ElementSerializer.java
@@ -48,58 +48,57 @@ import org.dcm4che2.data.DicomObject.Visitor;
 
 class ElementSerializer implements Serializable {
 
-	private static final long serialVersionUID = 4051046376018292793L;
+    private static final long serialVersionUID = 4051046376018292793L;
 
-	private static final DicomElement END_OF_SET = 
-		new SimpleDicomElement(Tag.ItemDelimitationItem, VR.UN, false, null, null);
+    private static final DicomElement END_OF_SET = new SimpleDicomElement(
+            Tag.ItemDelimitationItem, VR.UN, false, null, null);
 
-	private transient DicomObject attrs;
+    private transient DicomObject attrs;
 
-	public ElementSerializer(DicomObject attrs) {
-		this.attrs = attrs;
-	}
-		
-	private Object readResolve()
-			throws ObjectStreamException {
-		return attrs;
-	}
+    public ElementSerializer(DicomObject attrs) {
+        this.attrs = attrs;
+    }
 
-	private void writeObject(final ObjectOutputStream s)
-			throws IOException {
-		s.defaultWriteObject();
-		s.writeLong(attrs.getItemOffset());
-		try {
-			attrs.accept(new Visitor() {
-				public boolean visit(DicomElement attr) {
-					try {
-						s.writeObject(attr);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-					return true;
-				}
-			});
-		} catch (Exception e) {
-			if (e.getCause() instanceof IOException)
-				throw (IOException) e.getCause();
-		}
-		s.writeObject(END_OF_SET);
-	}
+    private Object readResolve() throws ObjectStreamException {
+        return attrs;
+    }
 
-	private void readObject(ObjectInputStream s)
-			throws IOException, ClassNotFoundException {
-		s.defaultReadObject();
-		attrs = new BasicDicomObject();
-		attrs.setItemOffset(s.readLong());
-		DicomElement attr = (DicomElement) s.readObject();
-		while (attr.tag() != Tag.ItemDelimitationItem) {
-			if (attr.vr() == VR.SQ && attr.hasItems()) {
-				for (int i = 0, n = attr.countItems(); i < n; ++i) {
-                    ((BasicDicomObject) attr.getDicomObject(i)).setParent(attrs);
-				}
-			}
-			((BasicDicomObject) attrs).addInternal(attr);
-			attr = (DicomElement) s.readObject();
-		}
-	}
+    private void writeObject(final ObjectOutputStream s) throws IOException {
+        s.defaultWriteObject();
+        s.writeLong(attrs.getItemOffset());
+        try {
+            attrs.accept(new Visitor() {
+                public boolean visit(DicomElement attr) {
+                    try {
+                        s.writeObject(attr);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            if (e.getCause() instanceof IOException)
+                throw (IOException) e.getCause();
+        }
+        s.writeObject(END_OF_SET);
+    }
+
+    private void readObject(ObjectInputStream s) throws IOException,
+            ClassNotFoundException {
+        s.defaultReadObject();
+        attrs = new BasicDicomObject();
+        attrs.setItemOffset(s.readLong());
+        DicomElement attr = (DicomElement) s.readObject();
+        while (attr.tag() != Tag.ItemDelimitationItem) {
+            if (attr.vr() == VR.SQ && attr.hasItems()) {
+                for (int i = 0, n = attr.countItems(); i < n; ++i) {
+                    ((BasicDicomObject) attr.getDicomObject(i))
+                            .setParent(attrs);
+                }
+            }
+            ((BasicDicomObject) attrs).addInternal(attr);
+            attr = (DicomElement) s.readObject();
+        }
+    }
 }
