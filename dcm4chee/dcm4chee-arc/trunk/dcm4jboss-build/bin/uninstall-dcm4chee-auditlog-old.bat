68d3a784aa1ad71110c0d623e9d87eb1629bb7aa
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-03-01 22:15:33 +0000

    [#DCMEE-287] Provide distribution package for upgrade to RFC-3881/DICOM Suppl95 ATNA Audit Logger

---
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/uninstall-dcm4chee-auditlog-old.bat
@@ -0,0 +1,8 @@
+rem -------------------------------------------------------------------------
+rem undeploy IHEYr4 Audit Logger Service v2.x
+rem required for installation of RFC-3881 Audit Logger Service v3.x
+rem -------------------------------------------------------------------------
+
+mkdir ..\dcm4chee-auditlog.old
+mkdir ..\dcm4chee-auditlog.old\deploy
+move ..\server\default\deploy\dcm4chee-auditlog-service.xml  ..\dcm4chee-auditlog.old\deploy\mkdir ..\dcm4chee-arr.old
