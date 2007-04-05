d00036a806dd4f5df90d599933251651101e5b58
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-04-05 11:29:22 +0000

    [#DCMEE-336] Set TAG field of Syslog Header in emitted Audit Message to ${app.name}[${app.pid}]

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/etc/bin/install_service.bat
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/etc/bin/install_service.bat
@@ -1,8 +1,5 @@
 @echo off
 setlocal
-rem Set maximum size of the memory allocation pool
-rem default value of 64MB may not be sufficient for dcm4chee
-set JAVA_OPTS=-Xmx100m
 set DIRNAME=%~dp0%
 set RUNJAR=%DIRNAME%\run.jar
 if exist "%RUNJAR%" goto found_runjar
@@ -16,9 +13,9 @@ if "%1" == "server" goto install
 if "%1" == "client" goto install
 echo "Usage: %0 server|client|uninstall"
 echo Options:
-echo   client    install "DICOM Archive" service, using client hotspot vm
-echo   server    install "DICOM Archive" service, using server hotspot vm
-echo   uninstall uninstall "DICOM Archive" service
+echo   client    install dcm4chee service, using client hotspot vm
+echo   server    install dcm4chee service, using server hotspot vm
+echo   uninstall uninstall dcm4chee service
 goto eof
 
 :install
@@ -42,11 +39,23 @@ echo Could not locate %TOOLS_JAR%. Unexpected results may occur.
 echo Make sure that JAVA_HOME points to a JDK and not a JRE.
 
 :install
-JavaService.exe -install "DICOM Archive" "%VM%" %JAVA_OPTS% "-Djava.class.path=%TOOLS_JAR%;%RUNJAR%"  -start org.jboss.Main -stop org.jboss.Main -method systemExit  -out "%DIRNAME%\out.txt" -current "%DIRNAME%"
+rem JVM memory allocation pool parameters. Modify as appropriate.
+set JAVA_OPTS=%JAVA_OPTS% -Xms128m -Xmx512m
+
+rem With Sun JVMs reduce the RMI GCs to once per hour
+set JAVA_OPTS=%JAVA_OPTS% -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
+
+rem Set java.library.path to find native jai-imageio components 
+JAVA_OPTS=%JAVA_OPTS% -Djava.library.path=%DIRNAME%
+
+rem Set app.name and app.pid used in emitted audit log messages
+JAVA_OPTS=%JAVA_OPTS% -Dapp.name=dcm4chee -Dapp.pid=%RANDOM%
+
+JavaService.exe -install dcm4chee "%VM%" %JAVA_OPTS% "-Djava.class.path=%TOOLS_JAR%;%RUNJAR%"  -start org.jboss.Main -stop org.jboss.Main -method systemExit  -out "%DIRNAME%\out.txt"
 goto eof
 
 :uninstall
-JavaService.exe -uninstall "DICOM Archive"
+JavaService.exe -uninstall dcm4chee
 goto eof
 
 :eof
