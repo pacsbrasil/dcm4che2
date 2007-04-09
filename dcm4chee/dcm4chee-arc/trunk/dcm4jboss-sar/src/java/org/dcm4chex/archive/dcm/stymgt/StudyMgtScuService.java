cfc7a817eda4b6f0ab9ed49b5964a234410fd7e8
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-04-09 20:07:35 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScuService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScuService.java
@@ -79,21 +79,21 @@ import org.jboss.system.ServiceMBeanSupport;
  * @author gunter.zeilinger@tiani.com
  * @version $Revision$ $Date$
  * @since 15.11.2004
- *
+ * 
  */
 public class StudyMgtScuService extends ServiceMBeanSupport implements
         MessageListener, NotificationListener {
 
-	private static final int ERR_STYMGT_RJ = -2;
+    private static final int ERR_STYMGT_RJ = -2;
 
-	private static final int ERR_ASSOC_RJ = -1;
+    private static final int ERR_ASSOC_RJ = -1;
 
-	private static final int MSG_ID = 1;
+    private static final int MSG_ID = 1;
 
-	private static final int PCID_STYMGT = 1;
+    private static final int PCID_STYMGT = 1;
 
-	private static final String[] NATIVE_TS = { UIDs.ExplicitVRLittleEndian,
-            UIDs.ImplicitVRLittleEndian};
+    private static final String[] NATIVE_TS = { UIDs.ExplicitVRLittleEndian,
+            UIDs.ImplicitVRLittleEndian };
 
     private TLSConfigDelegate tlsConfig = new TLSConfigDelegate(this);
 
@@ -104,7 +104,7 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
     private ForwardingRules forwardingRules = new ForwardingRules("");
 
     private ObjectName scpServiceName;
-    
+
     private String queueName;
 
     private int acTimeout;
@@ -112,39 +112,39 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
     private int dimseTimeout;
 
     private int soCloseDelay;
-	
-	private boolean retryIfNoSuchSOPInstance = false;
 
-	private int concurrency = 1;
+    private boolean retryIfNoSuchSOPInstance = false;
 
-        private JMSDelegate jmsDelegate = new JMSDelegate(this);
+    private int concurrency = 1;
 
-        public final ObjectName getJmsServiceName() {
-            return jmsDelegate.getJmsServiceName();
+    private JMSDelegate jmsDelegate = new JMSDelegate(this);
+
+    public final ObjectName getJmsServiceName() {
+        return jmsDelegate.getJmsServiceName();
+    }
+
+    public final void setJmsServiceName(ObjectName jmsServiceName) {
+        jmsDelegate.setJmsServiceName(jmsServiceName);
+    }
+
+    public final int getConcurrency() {
+        return concurrency;
+    }
+
+    public final void setConcurrency(int concurrency) throws Exception {
+        if (concurrency <= 0)
+            throw new IllegalArgumentException("Concurrency: " + concurrency);
+        if (this.concurrency != concurrency) {
+            final boolean restart = getState() == STARTED;
+            if (restart)
+                stop();
+            this.concurrency = concurrency;
+            if (restart)
+                start();
         }
+    }
 
-        public final void setJmsServiceName(ObjectName jmsServiceName) {
-            jmsDelegate.setJmsServiceName(jmsServiceName);
-        }
-        
-	public final int getConcurrency() {
-		return concurrency;
-	}
-
-	public final void setConcurrency(int concurrency) throws Exception {
-		if (concurrency <= 0)
-			throw new IllegalArgumentException("Concurrency: " + concurrency);
-		if (this.concurrency  != concurrency) {
-			final boolean restart = getState() == STARTED;
-			if (restart)
-				stop();
-			this.concurrency = concurrency;
-			if (restart)
-				start();
-		}
-	}
-
-	public final int getAcTimeout() {
+    public final int getAcTimeout() {
         return acTimeout;
     }
 
@@ -180,15 +180,15 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
         return scpServiceName;
     }
 
-	public boolean isRetryIfNoSuchSOPInstance() {
-		return retryIfNoSuchSOPInstance;
-	}
+    public boolean isRetryIfNoSuchSOPInstance() {
+        return retryIfNoSuchSOPInstance;
+    }
 
-	public void setRetryIfNoSuchSOPInstance(boolean retry) {
-		this.retryIfNoSuchSOPInstance = retry;
-	}
+    public void setRetryIfNoSuchSOPInstance(boolean retry) {
+        this.retryIfNoSuchSOPInstance = retry;
+    }
 
-	public final void setStudyMgtScpServiceName(ObjectName scpServiceName) {
+    public final void setStudyMgtScpServiceName(ObjectName scpServiceName) {
         this.scpServiceName = scpServiceName;
     }
 
@@ -201,21 +201,21 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
     }
 
     public final int getReceiveBufferSize() {
-        return tlsConfig.getReceiveBufferSize();        
+        return tlsConfig.getReceiveBufferSize();
     }
-    
+
     public final void setReceiveBufferSize(int size) {
         tlsConfig.setReceiveBufferSize(size);
     }
 
     public final int getSendBufferSize() {
-        return tlsConfig.getSendBufferSize();        
+        return tlsConfig.getSendBufferSize();
     }
-    
+
     public final void setSendBufferSize(int size) {
         tlsConfig.setSendBufferSize(size);
     }
-        
+
     public final boolean isTcpNoDelay() {
         return tlsConfig.isTcpNoDelay();
     }
@@ -223,15 +223,15 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
     public final void setTcpNoDelay(boolean on) {
         tlsConfig.setTcpNoDelay(on);
     }
-        
+
     public final String getQueueName() {
         return queueName;
     }
-    
+
     public final void setQueueName(String queueName) {
         this.queueName = queueName;
     }
-    
+
     public final String getCallingAET() {
         return callingAET;
     }
@@ -250,100 +250,94 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
 
     protected void startService() throws Exception {
         jmsDelegate.startListening(queueName, this, concurrency);
-        server.addNotificationListener(scpServiceName,
-                this,
-                StudyMgtScpService.NOTIF_FILTER,
-                null);
+        server.addNotificationListener(scpServiceName, this,
+                StudyMgtScpService.NOTIF_FILTER, null);
     }
 
     protected void stopService() throws Exception {
-        server.removeNotificationListener(scpServiceName,
-                this,
-				StudyMgtScpService.NOTIF_FILTER,
-                null);
+        server.removeNotificationListener(scpServiceName, this,
+                StudyMgtScpService.NOTIF_FILTER, null);
         jmsDelegate.stopListening(queueName);
     }
 
     public void handleNotification(Notification notif, Object handback) {
-		StudyMgtOrder order = (StudyMgtOrder) notif.getUserData();
-		forward(order.getCallingAET(), order.getCalledAET(),
-				order.getSOPInstanceUID(), order.getCommandField(),
-				order.getActionTypeID(), order.getDataset());
+        StudyMgtOrder order = (StudyMgtOrder) notif.getUserData();
+        forward(order.getCallingAET(), order.getCalledAET(), order
+                .getSOPInstanceUID(), order.getCommandField(), order
+                .getActionTypeID(), order.getDataset());
     }
 
- 	public int forward(String origCallingAET, String origCalledAET,
-			String iuid, int commandField, int actionTypeID, Dataset dataset) {
-		int count = 0;
-		Map keys = new HashMap();
-		keys.put("calling", new String[]{origCallingAET});
-		keys.put("called", new String[]{origCalledAET});
-		keys.put("command", new String[]{StudyMgtOrder.commandAsString(commandField, actionTypeID)});
-		String[] forwardAETs = forwardingRules.getForwardDestinationsFor(keys);
-		for (int i = 0; i < forwardAETs.length; i++) {
-			StudyMgtOrder order = new StudyMgtOrder(
-					origCallingAET, forwardAETs[i],
-					commandField, actionTypeID,
-					iuid, dataset);
+    public int forward(String origCallingAET, String origCalledAET,
+            String iuid, int commandField, int actionTypeID, Dataset dataset) {
+        int count = 0;
+        Map keys = new HashMap();
+        keys.put("calling", new String[] { origCallingAET });
+        keys.put("called", new String[] { origCalledAET });
+        keys.put("command", new String[] { StudyMgtOrder.commandAsString(
+                commandField, actionTypeID) });
+        String[] forwardAETs = forwardingRules.getForwardDestinationsFor(keys);
+        for (int i = 0; i < forwardAETs.length; i++) {
+            StudyMgtOrder order = new StudyMgtOrder(origCallingAET,
+                    forwardAETs[i], commandField, actionTypeID, iuid, dataset);
             try {
                 log.info("Scheduling " + order);
-                jmsDelegate.queue(queueName,
-                        order,
-                        Message.DEFAULT_PRIORITY,
+                jmsDelegate.queue(queueName, order, Message.DEFAULT_PRIORITY,
                         0L);
-				++count;
+                ++count;
             } catch (Exception e) {
                 log.error("Failed to schedule " + order, e);
             }
         }
-		return count;
-	}
-	
+        return count;
+    }
+
     public void onMessage(Message message) {
         ObjectMessage om = (ObjectMessage) message;
         try {
             StudyMgtOrder order = (StudyMgtOrder) om.getObject();
             log.info("Start processing " + order);
-			try {
-				try {
-					switch (order.getCommandField()) {
-					case Command.N_ACTION_RQ:
-						naction(order
-						.getCalledAET(), order.getSOPInstanceUID(), order
-										.getActionTypeID(), order.getDataset());
-						break;
-					case Command.N_CREATE_RQ:
-						ncreate(order.getCalledAET(), order.getSOPInstanceUID(),
-								order.getDataset());
-						break;
-					case Command.N_DELETE_RQ:
-						ndelete(order.getCalledAET(), order.getSOPInstanceUID());
-						break;
-					case Command.N_SET_RQ:
-						nset(order.getCalledAET(), order.getSOPInstanceUID(),
-								order.getDataset());
-						break;
-					}
-				} catch (DcmServiceException e) {
-					if (e.getStatus() != Status.NoSuchObjectInstance
-							|| retryIfNoSuchSOPInstance)
-						throw e;
-					log.info("No such SOP Instance for " + order);
-				}
-				order.setException(null);
-				log.info("Finished processing " + order);
-			} catch (Exception e) {
-	            final int failureCount = order.getFailureCount() + 1;
-	            order.setFailureCount(failureCount);
-				order.setException(e);
-	            final long delay = retryIntervalls.getIntervall(failureCount);
-	            if (delay == -1L) {
-	                log.error("Give up to process " + order);
-	            } else {
-	                log.warn("Failed to process " + order + ". Scheduling retry.");
-	                jmsDelegate.queue(queueName, order, 0, System
-	                        .currentTimeMillis()
-	                        + delay);
-				}
+            try {
+                try {
+                    switch (order.getCommandField()) {
+                    case Command.N_ACTION_RQ:
+                        naction(order.getCalledAET(),
+                                order.getSOPInstanceUID(), order
+                                        .getActionTypeID(), order.getDataset());
+                        break;
+                    case Command.N_CREATE_RQ:
+                        ncreate(order.getCalledAET(),
+                                order.getSOPInstanceUID(), order.getDataset());
+                        break;
+                    case Command.N_DELETE_RQ:
+                        ndelete(order.getCalledAET(), order.getSOPInstanceUID());
+                        break;
+                    case Command.N_SET_RQ:
+                        nset(order.getCalledAET(), order.getSOPInstanceUID(),
+                                order.getDataset());
+                        break;
+                    }
+                } catch (DcmServiceException e) {
+                    if (e.getStatus() != Status.NoSuchObjectInstance
+                            || retryIfNoSuchSOPInstance)
+                        throw e;
+                    log.info("No such SOP Instance for " + order);
+                }
+                order.setException(null);
+                log.info("Finished processing " + order);
+            } catch (Exception e) {
+                final int failureCount = order.getFailureCount() + 1;
+                order.setFailureCount(failureCount);
+                order.setException(e);
+                final long delay = retryIntervalls.getIntervall(failureCount);
+                if (delay == -1L) {
+                    log.error("Give up to process " + order);
+                } else {
+                    log.warn("Failed to process " + order
+                            + ". Scheduling retry.");
+                    jmsDelegate.queue(queueName, order, 0, System
+                            .currentTimeMillis()
+                            + delay);
+                }
             }
         } catch (JMSException e) {
             log.error("jms error during processing message: " + message, e);
@@ -353,47 +347,47 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
         }
     }
 
-	public void ncreate(String aet, String iuid, Dataset ds)
-			throws DcmServiceException, UnkownAETException, IOException, 
-					SQLException, InterruptedException {
-		Command cmd = DcmObjectFactory.getInstance().newCommand();
-		cmd.initNCreateRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
-		checkStatus(invoke(aet, cmd, ds).getCommand());
-	}
+    public void ncreate(String aet, String iuid, Dataset ds)
+            throws DcmServiceException, UnkownAETException, IOException,
+            SQLException, InterruptedException {
+        Command cmd = DcmObjectFactory.getInstance().newCommand();
+        cmd.initNCreateRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
+        checkStatus(invoke(aet, cmd, ds).getCommand());
+    }
 
-	public void nset(String aet, String iuid, Dataset ds)
-		throws DcmServiceException, UnkownAETException, IOException, 
-				SQLException, InterruptedException {
-		Command cmd = DcmObjectFactory.getInstance().newCommand();
-		cmd.initNSetRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
-		checkStatus(invoke(aet, cmd, ds).getCommand());
-	}
-	
-	public void naction(String aet, String iuid, int actionTypeID,
-			Dataset ds) 
-			throws DcmServiceException, UnkownAETException, IOException, 
-					SQLException, InterruptedException {
-		Command cmd = DcmObjectFactory.getInstance().newCommand();
-		cmd.initNActionRQ(MSG_ID, UIDs.TianiStudyManagement, iuid, actionTypeID);
-		checkStatus(invoke(aet, cmd, ds).getCommand());
-	}
+    public void nset(String aet, String iuid, Dataset ds)
+            throws DcmServiceException, UnkownAETException, IOException,
+            SQLException, InterruptedException {
+        Command cmd = DcmObjectFactory.getInstance().newCommand();
+        cmd.initNSetRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
+        checkStatus(invoke(aet, cmd, ds).getCommand());
+    }
 
-	public void ndelete(String aet, String iuid)
-		throws DcmServiceException, UnkownAETException, IOException, 
-				SQLException, InterruptedException {
-		Command cmd = DcmObjectFactory.getInstance().newCommand();
-		cmd.initNDeleteRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
-		checkStatus(invoke(aet, cmd, null).getCommand());
-	}
+    public void naction(String aet, String iuid, int actionTypeID, Dataset ds)
+            throws DcmServiceException, UnkownAETException, IOException,
+            SQLException, InterruptedException {
+        Command cmd = DcmObjectFactory.getInstance().newCommand();
+        cmd
+                .initNActionRQ(MSG_ID, UIDs.TianiStudyManagement, iuid,
+                        actionTypeID);
+        checkStatus(invoke(aet, cmd, ds).getCommand());
+    }
 
-	private Dimse invoke(String aet, Command cmd, Dataset ds)
-			throws DcmServiceException, UnkownAETException, IOException, 
-				SQLException, InterruptedException {
+    public void ndelete(String aet, String iuid) throws DcmServiceException,
+            UnkownAETException, IOException, SQLException, InterruptedException {
+        Command cmd = DcmObjectFactory.getInstance().newCommand();
+        cmd.initNDeleteRQ(MSG_ID, UIDs.TianiStudyManagement, iuid);
+        checkStatus(invoke(aet, cmd, null).getCommand());
+    }
+
+    private Dimse invoke(String aet, Command cmd, Dataset ds)
+            throws DcmServiceException, UnkownAETException, IOException,
+            SQLException, InterruptedException {
         AEData aeData = new AECmd(aet).getAEData();
         if (aeData == null) {
-			throw new UnkownAETException("Unkown Destination AET: " + aet);
+            throw new UnkownAETException("Unkown Destination AET: " + aet);
         }
-     	AssociationFactory af = AssociationFactory.getInstance();        
+        AssociationFactory af = AssociationFactory.getInstance();
         Association a = af.newRequestor(tlsConfig.createSocket(aeData));
         a.setAcTimeout(acTimeout);
         a.setDimseTimeout(dimseTimeout);
@@ -402,35 +396,36 @@ public class StudyMgtScuService extends ServiceMBeanSupport implements
         rq.setCalledAET(aet);
         rq.setCallingAET(callingAET);
         rq.addPresContext(af.newPresContext(PCID_STYMGT,
-                UIDs.TianiStudyManagement,
-                NATIVE_TS));
+                UIDs.TianiStudyManagement, NATIVE_TS));
         PDU ac = a.connect(rq);
         if (!(ac instanceof AAssociateAC)) {
-			throw new DcmServiceException(ERR_ASSOC_RJ, 
-					"Association not accepted by " + aet + ": " + ac);
+            throw new DcmServiceException(ERR_ASSOC_RJ,
+                    "Association not accepted by " + aet + ": " + ac);
         }
-		ActiveAssociation aa = af.newActiveAssociation(a, null);
+        ActiveAssociation aa = af.newActiveAssociation(a, null);
         aa.start();
-		try {
-	        if (a.getAcceptedTransferSyntaxUID(PCID_STYMGT) == null)
-				throw new DcmServiceException(ERR_STYMGT_RJ, 
-						"Tiani Study Mgt Service not supported by remote AE: " 
-						+ aet);
-			return aa.invoke(af.newDimse(PCID_STYMGT, cmd, ds)).get();
-		} finally {
-			try {
-				aa.release(true);
-			} catch (Exception e) {
-				log.warn("Failed to release association " + aa.getAssociation(), e);
-			}
-		}
-	}
+        try {
+            if (a.getAcceptedTransferSyntaxUID(PCID_STYMGT) == null)
+                throw new DcmServiceException(ERR_STYMGT_RJ,
+                        "Tiani Study Mgt Service not supported by remote AE: "
+                                + aet);
+            return aa.invoke(af.newDimse(PCID_STYMGT, cmd, ds)).get();
+        } finally {
+            try {
+                aa.release(true);
+            } catch (Exception e) {
+                log.warn(
+                        "Failed to release association " + aa.getAssociation(),
+                        e);
+            }
+        }
+    }
 
-	private void checkStatus(Command cmdRsp) throws DcmServiceException {
-		if (cmdRsp.getStatus() != 0) {
-			throw new DcmServiceException(cmdRsp.getStatus(), 
-					cmdRsp.getString(Tags.ErrorComment));
-		}
-	}
-	
+    private void checkStatus(Command cmdRsp) throws DcmServiceException {
+        if (cmdRsp.getStatus() != 0) {
+            throw new DcmServiceException(cmdRsp.getStatus(), cmdRsp
+                    .getString(Tags.ErrorComment));
+        }
+    }
+
 }
