e76f58c4882a8f076a060f14cb1c9f871ba2c47a
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-04-10 09:55:00 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/ae/AEDelegate.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/ae/AEDelegate.java
@@ -49,165 +49,173 @@ import javax.management.ReflectionException;
 import javax.servlet.ServletConfig;
 
 import org.apache.log4j.Logger;
-import org.dcm4chex.archive.ejb.jdbc.AEData;
+import org.dcm4chex.archive.ejb.interfaces.AEDTO;
 import org.jboss.mx.util.MBeanServerLocator;
 
 /**
  * @author franz.willer
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class AEDelegate {
-	   private static ObjectName echoServiceName = null;
-	   private static ObjectName aeServiceName = null;
-	   private static MBeanServer server;
-	   private List aes;
-		
-	    private static Logger log = Logger.getLogger( AEDelegate.class.getName() );
+    private static ObjectName echoServiceName = null;
 
-	    /** 
-	     * Iinitialize the Echo service delegator.
-	     * <p>
-	     * Set the name of the EchoService MBean with the servlet config param 'echoServiceName'.
-	     * 
-	     * @param config The ServletConfig object.
-	     */
-		public void init( ServletConfig config ) {
-	        if (server != null) return;
-	        server = MBeanServerLocator.locate();
-	        try {
-	        	echoServiceName = new ObjectName(config.getInitParameter("echoServiceName"));
-	        	aeServiceName = new ObjectName(config.getInitParameter("aeServiceName"));
-			} catch (Exception e) {
-				log.error( "Exception in init! ",e );
-			}
-	       
-	    }
+    private static ObjectName aeServiceName = null;
 
-		public Logger getLogger() {
-			return log;
-		}
-//AE Service	
-		/**
-		 * Return list of all configured AE
-		 */
-		public List getAEs() {
-			try {
-		        aes = (List) server.invoke(aeServiceName,
-		                "listAEs",
-		                null,
-		                null );
-		        return aes;
-			} catch ( Exception x ) {
-				log.error( "Exception occured in getAEs: "+x.getMessage(), x );
-				return null;
-			}
-		}
+    private static MBeanServer server;
 
-		/**
-		 * @param title
-		 * @return
-		 * @throws ReflectionException
-		 * @throws MBeanException
-		 * @throws InstanceNotFoundException
-		 */
-		public AEData getAE(String title) throws InstanceNotFoundException, MBeanException, ReflectionException {
-			return (AEData) server.invoke(aeServiceName,
-	                "getAE",
-	                new Object[] {title},
-	                new String[] { String.class.getName() } );
-		}
-		
-		/**
-		 * @param newAE
-		 * @throws ReflectionException
-		 * @throws MBeanException
-		 * @throws InstanceNotFoundException
-		 */
-		public void updateAE( AEData ae, boolean checkHost) throws InstanceNotFoundException, MBeanException, ReflectionException {
-	        server.invoke(aeServiceName,
-	                "updateAE",
-	                new Object[]{ new Long(ae.getPk()), ae.getTitle(), ae.getHostName(), 
-	        					new Integer(ae.getPort()),ae.getCipherSuitesAsString(), new Boolean(checkHost) },
-	                new String[]{ long.class.getName(), String.class.getName(), String.class.getName(),
-	        						int.class.getName(), String.class.getName(), boolean.class.getName()} );
-		}
-	
-		/**
-		 * @param title
-		 * @return
-		 * @throws ReflectionException
-		 * @throws MBeanException
-		 * @throws InstanceNotFoundException
-		 */
-		public AEData delAE(String title) throws InstanceNotFoundException, MBeanException, ReflectionException {
-			return (AEData) server.invoke(aeServiceName,
-	                "removeAE",
-	                new Object[] {title},
-	                new String[] { String.class.getName() } );
-		}
-		
-//ECHO Service		
-		/**
-		 * Makes the MBean call to echo an AE configuration.
-		 * 
-		 * 
-		 * @return An info string for status of echo.
-		 */
-		public String echo( AEData aeData, int nrOfTests ) {
-			if ( log.isDebugEnabled() ) log.debug("Send echo to "+aeData);
-			String resp = null;
-			try {
-		        Object o = server.invoke(echoServiceName,
-		                "echo",
-		                new Object[]{ aeData, new Integer( nrOfTests ) },
-		                new String[]{ AEData.class.getName(), Integer.class.getName() } );
-		        resp = (String) o;
-			} catch ( Exception x ) {
-				log.error( "Exception occured in echoAE: "+x.getMessage(), x );
-			}
-			if ( log.isDebugEnabled() ) log.debug("echo response for "+aeData+":"+resp);
-	        return resp;
-		}
+    private List aes;
 
-		/**
-		 * Makes the MBean call to echo an AET (AE config for given title).
-		 * 
-		 * 
-		 * @return An info string for status of echo.
-		 */
-		public String echo( String aet, int nrOfTests ) {
-			String resp = null;
-			try {
-		        Object o = server.invoke(echoServiceName,
-		                "echo",
-		                new Object[]{ aet, new Integer( nrOfTests ) },
-		                new String[]{ String.class.getName(), Integer.class.getName() } );
-		        resp = (String) o;
-			} catch ( Exception x ) {
-				log.error( "Exception occured in echo (AET="+aet+"): "+x.getMessage(), x );
-			}
-	        return resp;
-		}
+    private static Logger log = Logger.getLogger(AEDelegate.class.getName());
 
-		/**
-		 * Makes the MBean call to echoe an AE configuration.
-		 * 
-		 * 
-		 * @return An info string for status of echo.
-		 */
-		public String[] echoAll( AEData aeData ) {
-			String[] resp = null;
-			try {
-		        Object o = server.invoke(echoServiceName,
-		                "echoAll",
-		                null,
-		                null );
-		        resp = (String[]) o;
-			} catch ( Exception x ) {
-				log.error( "Exception occured in echo ("+aeData+"): "+x.getMessage(), x );
-			}
-	        return resp;
-		}
+    /**
+     * Iinitialize the Echo service delegator.
+     * <p>
+     * Set the name of the EchoService MBean with the servlet config param
+     * 'echoServiceName'.
+     * 
+     * @param config
+     *            The ServletConfig object.
+     */
+    public void init(ServletConfig config) {
+        if (server != null)
+            return;
+        server = MBeanServerLocator.locate();
+        try {
+            echoServiceName = new ObjectName(config
+                    .getInitParameter("echoServiceName"));
+            aeServiceName = new ObjectName(config
+                    .getInitParameter("aeServiceName"));
+        } catch (Exception e) {
+            log.error("Exception in init! ", e);
+        }
+
+    }
+
+    public Logger getLogger() {
+        return log;
+    }
+
+    // AE Service
+    /**
+     * Return list of all configured AE
+     */
+    public List getAEs() {
+        try {
+            aes = (List) server.invoke(aeServiceName, "listAEs", null, null);
+            return aes;
+        } catch (Exception x) {
+            log.error("Exception occured in getAEs: " + x.getMessage(), x);
+            return null;
+        }
+    }
+
+    /**
+     * @param title
+     * @return
+     * @throws ReflectionException
+     * @throws MBeanException
+     * @throws InstanceNotFoundException
+     */
+    public AEDTO getAE(String title) throws InstanceNotFoundException,
+            MBeanException, ReflectionException {
+        return (AEDTO) server
+                .invoke(aeServiceName, "getAE", new Object[] { title },
+                        new String[] { String.class.getName() });
+    }
+
+    /**
+     * @param newAE
+     * @throws ReflectionException
+     * @throws MBeanException
+     * @throws InstanceNotFoundException
+     */
+    public void updateAE(AEDTO ae, boolean checkHost)
+            throws InstanceNotFoundException, MBeanException,
+            ReflectionException {
+        server.invoke(aeServiceName, "updateAE", new Object[] {
+                new Long(ae.getPk()), ae.getTitle(), ae.getHostName(),
+                new Integer(ae.getPort()), ae.getCipherSuitesAsString(),
+                new Boolean(checkHost) }, new String[] { long.class.getName(),
+                String.class.getName(), String.class.getName(),
+                int.class.getName(), String.class.getName(),
+                boolean.class.getName() });
+    }
+
+    /**
+     * @param title
+     * @return
+     * @throws ReflectionException
+     * @throws MBeanException
+     * @throws InstanceNotFoundException
+     */
+    public AEDTO delAE(String title) throws InstanceNotFoundException,
+            MBeanException, ReflectionException {
+        return (AEDTO) server
+                .invoke(aeServiceName, "removeAE", new Object[] { title },
+                        new String[] { String.class.getName() });
+    }
+
+    // ECHO Service
+    /**
+     * Makes the MBean call to echo an AE configuration.
+     * 
+     * 
+     * @return An info string for status of echo.
+     */
+    public String echo(AEDTO aeData, int nrOfTests) {
+        if (log.isDebugEnabled())
+            log.debug("Send echo to " + aeData);
+        String resp = null;
+        try {
+            Object o = server.invoke(echoServiceName, "echo", new Object[] {
+                    aeData, new Integer(nrOfTests) }, new String[] {
+                    AEDTO.class.getName(), Integer.class.getName() });
+            resp = (String) o;
+        } catch (Exception x) {
+            log.error("Exception occured in echoAE: " + x.getMessage(), x);
+        }
+        if (log.isDebugEnabled())
+            log.debug("echo response for " + aeData + ":" + resp);
+        return resp;
+    }
+
+    /**
+     * Makes the MBean call to echo an AET (AE config for given title).
+     * 
+     * 
+     * @return An info string for status of echo.
+     */
+    public String echo(String aet, int nrOfTests) {
+        String resp = null;
+        try {
+            Object o = server.invoke(echoServiceName, "echo", new Object[] {
+                    aet, new Integer(nrOfTests) }, new String[] {
+                    String.class.getName(), Integer.class.getName() });
+            resp = (String) o;
+        } catch (Exception x) {
+            log.error("Exception occured in echo (AET=" + aet + "): "
+                    + x.getMessage(), x);
+        }
+        return resp;
+    }
+
+    /**
+     * Makes the MBean call to echoe an AE configuration.
+     * 
+     * 
+     * @return An info string for status of echo.
+     */
+    public String[] echoAll(AEDTO aeData) {
+        String[] resp = null;
+        try {
+            Object o = server.invoke(echoServiceName, "echoAll", null, null);
+            resp = (String[]) o;
+        } catch (Exception x) {
+            log.error("Exception occured in echo (" + aeData + "): "
+                    + x.getMessage(), x);
+        }
+        return resp;
+    }
 }
