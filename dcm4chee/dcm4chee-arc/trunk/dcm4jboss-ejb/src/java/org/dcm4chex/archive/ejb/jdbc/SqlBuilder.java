768ec66a328e444b10adb8cca823436890ce39bc
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-05-03 11:18:46 +0000

    [#DCMEE-176] C-Find by patient name with no delimiters yields matches on last name only

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/SqlBuilder.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/jdbc/SqlBuilder.java
@@ -288,13 +288,7 @@ class SqlBuilder {
             return;
         PersonName pn = DcmObjectFactory.getInstance().newPersonName(val);
         if (pn != null) {
-            String matchString = null;
-            if (val.indexOf('^') != -1 ) {
-                matchString = pn.toComponentGroupMatch();
-            } else {
-                matchString = pn.toComponentGroupString(true);
-            }
-            addWildCardMatch(null, nameFields[0], type2, toUpperCase(matchString));
+            addWildCardMatch(null, nameFields[0], type2, toUpperCase(pn.toComponentGroupMatch()));
         }
         PersonName ipn = pn.getIdeographic();
         if (ipn != null) {
