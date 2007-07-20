4ff93660108aaa6922fb83e3e3d3a04a556c50c2
jlvosters <jlvosters@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-07-20 21:42:32 +0000

    implement create methods in the DAOs

---
+++ dcm4chee/dcm4chee3/trunk/dcm4chee-common/src/main/java/org/dcm4che/archive/util/StringUtils.java
@@ -0,0 +1,9 @@
+package org.dcm4che.archive.util;
+
+public class StringUtils {
+
+    public static String toUpperCase(String s) {
+        return s != null ? s.toUpperCase() : null;
+    }
+    
+}
