7257e5044f3210b5c7fbe7724818bca6463a0566
damien_evans <damien_evans@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-07-25 21:01:57 +0000

    EE1: Fix a bad import that I had due to the SAAJ libraries not being on the Eclipse class path.  Eclipse was previously using an internal Sun JAXMString class which the build script could not find, therefore it worked in Eclipse but not in the build script.

--- dcm4chee/dcm4chee3/trunk/dcm4chee-archive/src/main/java/org/dcm4che/archive/xdsi/XDSIService.java
+++ dcm4chee/dcm4chee3/trunk/dcm4chee-archive/src/main/java/org/dcm4che/archive/xdsi/XDSIService.java
@@ -117,7 +117,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.sun.xml.internal.messaging.saaj.util.JAXMStreamSource;
+import com.sun.xml.messaging.saaj.util.JAXMStreamSource;
 
 /**
  * @author franz.willer@gwi-ag.com
