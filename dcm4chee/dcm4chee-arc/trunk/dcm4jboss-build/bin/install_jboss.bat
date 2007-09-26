c4d1c2e10891c019988dd1c05a039c4c5d08db3d
damien_evans <damien_evans@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-09-26 04:50:37 +0000

    Fix the copying of jar files from server/default/lib

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_jboss.bat
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_jboss.bat
@@ -54,7 +54,12 @@ copy "%JBOSS_CONF%\jndi.properties" "%DCM4CHEE_CONF%"
 xcopy /S "%JBOSS_CONF%\props" "%DCM4CHEE_CONF%\props\"
 xcopy /S "%JBOSS_CONF%\xmdesc" "%DCM4CHEE_CONF%\xmdesc\"
 
-xcopy /S "%JBOSS_SERV%\lib" "%DCM4CHEE_SERV%\lib\" /EXCLUDE:jboss-saaj.jar
+echo on
+echo jboss-saaj.jar > excludefile.txt
+echo excludefile.txt >> excludefile.txt
+xcopy /S /EXCLUDE:excludefile.txt "%JBOSS_SERV%\lib" "%DCM4CHEE_SERV%\lib\"
+del excludefile.txt
+echo off
 
 set JBOSS_DEPLOY=%JBOSS_SERV%\deploy
 set DCM4CHEE_DEPLOY=%DCM4CHEE_SERV%\deploy
