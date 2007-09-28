4c0f1a5c2fe837ec2809d22e3cd46fdcd0bf8ea8
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-09-28 09:42:00 +0000

    [#DCMEE-531] dcm4chee-arr(Seam) need deploy/jbossws.sar to run on JDK 5 => also use lib/jboss-saaj.jar instead lib/saaj-api.jar, but only provide additional lib/saaj-impl.jar for xds, as long we did not solve how to make use of saaj-impl provided by deploy/jbossws.sar

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_jboss.bat
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_jboss.bat
@@ -55,7 +55,6 @@ xcopy /S "%JBOSS_CONF%\props" "%DCM4CHEE_CONF%\props\"
 xcopy /S "%JBOSS_CONF%\xmdesc" "%DCM4CHEE_CONF%\xmdesc\"
 
 xcopy /S "%JBOSS_SERV%\lib" "%DCM4CHEE_SERV%\lib\"
-del "%DCM4CHEE_SERV%\lib\jboss-saaj.jar"
 
 set JBOSS_DEPLOY=%JBOSS_SERV%\deploy
 set DCM4CHEE_DEPLOY=%DCM4CHEE_SERV%\deploy
@@ -82,6 +81,7 @@ xcopy /S "%JBOSS_DEPLOY%\ejb3.deployer" "%DCM4CHEE_DEPLOY%\ejb3.deployer\"
 xcopy /S "%JBOSS_DEPLOY%\http-invoker.sar" "%DCM4CHEE_DEPLOY%\http-invoker.sar\"
 xcopy /S "%JBOSS_DEPLOY%\jboss-aop-jdk50.deployer" "%DCM4CHEE_DEPLOY%\jboss-aop-jdk50.deployer\"
 xcopy /S "%JBOSS_DEPLOY%\jboss-bean.deployer" "%DCM4CHEE_DEPLOY%\jboss-bean.deployer\"
+xcopy /S "%JBOSS_DEPLOY%\jbossws.sar" "%DCM4CHEE_DEPLOY%\jbossws.sar\"
 
 set JBOSS_WEB=%JBOSS_DEPLOY%\jboss-web.deployer
 set DCM4CHEE_WEB=%DCM4CHEE_DEPLOY%\jboss-web.deployer
