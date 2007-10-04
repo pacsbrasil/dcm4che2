d8f20dbc8bf172f39d34e52c77341df2e5fd6570
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2007-10-04 20:56:35 +0000

    [#DCM-122] Upgrade dependencies with newer available versions

--- dcm4che2/trunk/dcm4che-tool/dcm4che-tool-dcm2jpg/src/bin/dcm2jpg.bat
+++ dcm4che2/trunk/dcm4che-tool/dcm4che-tool-dcm2jpg/src/bin/dcm2jpg.bat
@@ -52,10 +52,10 @@ set CP=%CP%;%DCM4CHE_HOME%\lib\%MAIN_JAR%
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-2.0.12-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-image-2.0.12-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-imageio-2.0.12-SNAPSHOT.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.2.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.2.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.13.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.0.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.4.3.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.4.3.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.14.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.1.jar
 
 if "%JIO_LIB%" == "" goto :SKIP_SET_JIO_CLASSPATH
 
