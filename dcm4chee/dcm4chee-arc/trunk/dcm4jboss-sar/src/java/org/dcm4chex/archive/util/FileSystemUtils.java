dd6aeefd805e8fd7cbc295c32ead656b19256c9b
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-01-08 13:57:15 +0000

    [#DCMEE-680] showAvailableDiskSpace() failed on windows system when path contains a blank!

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/util/FileSystemUtils.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/util/FileSystemUtils.java
@@ -208,6 +208,8 @@ public class FileSystemUtils {
 //        }
 
         // build and run the 'dir' command
+    	if (! (path.charAt(0) == '\"') )
+    		path = "\""+path+"\"";
         String[] cmdAttrbs = new String[] {"cmd.exe", "/C", "dir /c " + path};
 
         // read in the output of the command to an ArrayList
