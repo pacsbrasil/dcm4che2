0021e7cff285248b2dd12708451d436a6e2215ee
damien_evans <damien_evans@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-01-14 19:04:50 +0000

    Pretty format

--- dcm4che14/trunk/src/java/org/dcm4cheri/net/ActiveAssociationImpl.java
+++ dcm4che14/trunk/src/java/org/dcm4cheri/net/ActiveAssociationImpl.java
@@ -93,274 +93,269 @@ import org.dcm4cheri.util.IntHashtable2;
 import org.dcm4cheri.util.LF_ThreadPool;
 
 /**
- * <description> 
- *
+ * <description>
+ * 
  * @see <related>
- * @author  <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
- * @author  <a href="mailto:umberto.cappellini@chello.at">Umberto Cappellini</a>
- * @version $Revision$ $Date$
- *   
+ * @author <a href="mailto:gunter@tiani.com">gunter zeilinger</a>
+ * @author <a href="mailto:umberto.cappellini@chello.at">Umberto Cappellini</a>
+ * @version $Revision$ $Date: 2008-01-14 13:04:17 -0600 (Mon, 14 Jan
+ *          2008) $
+ * 
  */
-final class ActiveAssociationImpl
-	implements ActiveAssociation, LF_ThreadPool.Handler, AssociationListener {
+final class ActiveAssociationImpl implements ActiveAssociation,
+        LF_ThreadPool.Handler, AssociationListener {
     private static Logger log = Logger.getLogger(ActiveAssociationImpl.class);
-	// Constants -----------------------------------------------------
 
-	// Attributes ----------------------------------------------------
+    // Constants -----------------------------------------------------
+
+    // Attributes ----------------------------------------------------
     private static int instCount = 0;
+
     private final String name = "ActiveAssoc-" + ++instCount;
-    
+
     // Static --------------------------------------------------------
-	private final AssociationImpl assoc;
-	private final DcmServiceRegistry services;
-	private final IntHashtable2 rspDispatcher = new IntHashtable2();
-	private final IntHashtable2 cancelDispatcher = new IntHashtable2();
-	private final LF_ThreadPool threadPool = new LF_ThreadPool(this, name);
-	private boolean running = false;
+    private final AssociationImpl assoc;
 
-	// Static --------------------------------------------------------
+    private final DcmServiceRegistry services;
 
-	// Constructors --------------------------------------------------
-	ActiveAssociationImpl(Association assoc, DcmServiceRegistry services) {
-		if (assoc.getState() != Association.ASSOCIATION_ESTABLISHED)
-			throw new IllegalStateException(
-				"Association not established - " + assoc.getState());
+    private final IntHashtable2 rspDispatcher = new IntHashtable2();
 
-		this.assoc = (AssociationImpl) assoc;
-		this.services = services;
-		((AssociationImpl) assoc).setThreadPool(threadPool);
-		assoc.addAssociationListener(this);
-	}
+    private final IntHashtable2 cancelDispatcher = new IntHashtable2();
 
-	// Public --------------------------------------------------------
-	public final void addCancelListener(int msgID, DimseListener l) {
-		cancelDispatcher.put(msgID, l);
-	}
-	
-	public String toString() {
-		return "Active-" + assoc;
-	}
+    private final LF_ThreadPool threadPool = new LF_ThreadPool(this, name);
 
-	public void run() {
-		if (running)
-			throw new IllegalStateException("Already running: " + threadPool);
+    private boolean running = false;
 
-		this.running = true;
-		threadPool.join();
-	}
+    // Static --------------------------------------------------------
 
-	public void start() {
-		if (running)
-			throw new IllegalStateException("Already running: " + threadPool);
+    // Constructors --------------------------------------------------
+    ActiveAssociationImpl(Association assoc, DcmServiceRegistry services) {
+        if (assoc.getState() != Association.ASSOCIATION_ESTABLISHED)
+            throw new IllegalStateException("Association not established - "
+                    + assoc.getState());
 
-		new Thread(this, name).start();
-	}
+        this.assoc = (AssociationImpl) assoc;
+        this.services = services;
+        ((AssociationImpl) assoc).setThreadPool(threadPool);
+        assoc.addAssociationListener(this);
+    }
 
-	public Association getAssociation() {
-		return assoc;
-	}
+    // Public --------------------------------------------------------
+    public final void addCancelListener(int msgID, DimseListener l) {
+        cancelDispatcher.put(msgID, l);
+    }
 
-	public void invoke(Dimse rq, DimseListener l)
-		throws InterruptedException, IOException {
-		//      checkRunning();
-		int msgID = rq.getCommand().getMessageID();
-		int maxOps = assoc.getMaxOpsInvoked();
-		if (maxOps == 0) {
-			rspDispatcher.put(msgID, l);
-		} else
-			synchronized (rspDispatcher) {
-				while (rspDispatcher.size() >= maxOps) {
-					rspDispatcher.wait();
-				}
-				rspDispatcher.put(msgID, l);
-			}
-		assoc.write(rq);
-	}
+    public String toString() {
+        return "Active-" + assoc;
+    }
 
-	public FutureRSP invoke(Dimse rq)
-		throws InterruptedException, IOException {
-		FutureRSPImpl retval = new FutureRSPImpl();
-		assoc.addAssociationListener(retval);
-		invoke(rq, retval);
-		return retval;
-	}
+    public void run() {
+        if (running)
+            throw new IllegalStateException("Already running: " + threadPool);
 
-	public void release(boolean waitOnRSP)
-		throws InterruptedException, IOException {
-		//      checkRunning();
-		if (waitOnRSP) {
-			synchronized (rspDispatcher) {
-				while (!rspDispatcher.isEmpty()) {
-					rspDispatcher.wait();
-				}
-			}
-		}
-		((AssociationImpl) assoc).writeReleaseRQ();
-	}
+        this.running = true;
+        threadPool.join();
+    }
 
-	// LF_ThreadPool.Handler implementation --------------------------
-	public void run(LF_ThreadPool pool) {
-	    Dimse dimse = null;
-		try {
-			dimse = assoc.read();
+    public void start() {
+        if (running)
+            throw new IllegalStateException("Already running: " + threadPool);
 
-			// if Association was released
-			if (dimse == null) {
-				pool.shutdown();
-				return;
-			}
-			assoc.initMDC();
-			Command cmd = dimse.getCommand();
-			switch (cmd.getCommandField()) {
-				case Command.C_STORE_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).c_store(
-						this,
-						dimse);
-					break;
-				case Command.C_GET_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).c_get(
-						this,
-						dimse);
-					break;
-				case Command.C_FIND_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).c_find(
-						this,
-						dimse);
-					break;
-				case Command.C_MOVE_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).c_move(
-						this,
-						dimse);
-					break;
-				case Command.C_ECHO_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).c_echo(
-						this,
-						dimse);
-					break;
-				case Command.N_EVENT_REPORT_RQ :
-					services.lookup(
-						cmd.getAffectedSOPClassUID()).n_event_report(
-						this,
-						dimse);
-					break;
-				case Command.N_GET_RQ :
-					services.lookup(cmd.getRequestedSOPClassUID()).n_get(
-						this,
-						dimse);
-					break;
-				case Command.N_SET_RQ :
-					services.lookup(cmd.getRequestedSOPClassUID()).n_set(
-						this,
-						dimse);
-					break;
-				case Command.N_ACTION_RQ :
-					services.lookup(cmd.getRequestedSOPClassUID()).n_action(
-						this,
-						dimse);
-					break;
-				case Command.N_CREATE_RQ :
-					services.lookup(cmd.getAffectedSOPClassUID()).n_create(
-						this,
-						dimse);
-					break;
-				case Command.N_DELETE_RQ :
-					services.lookup(cmd.getRequestedSOPClassUID()).n_delete(
-						this,
-						dimse);
-					break;
-				case Command.C_STORE_RSP :
-				case Command.C_GET_RSP :
-				case Command.C_FIND_RSP :
-				case Command.C_MOVE_RSP :
-				case Command.C_ECHO_RSP :
-				case Command.N_EVENT_REPORT_RSP :
-				case Command.N_GET_RSP :
-				case Command.N_SET_RSP :
-				case Command.N_ACTION_RSP :
-				case Command.N_CREATE_RSP :
-				case Command.N_DELETE_RSP :
-					handleResponse(dimse);
-					break;
-				case Command.C_CANCEL_RQ :
-					handleCancel(dimse);
-					break;
-				default :
-					throw new RuntimeException("Illegal Command: " + cmd);
-			}
-		} catch (IOException ioe) {
-			log.error(ioe);
-			pool.shutdown();
-		} finally {
-			// ensure readout of data PDVs
-		    if (dimse != null)
-		        dimse.closeDataStream();
-			assoc.clearMDC();		    
-		}
-	}
+        new Thread(this, name).start();
+    }
 
-	// Y overrides ---------------------------------------------------
+    public Association getAssociation() {
+        return assoc;
+    }
 
-	// Package protected ---------------------------------------------
+    public void invoke(Dimse rq, DimseListener l) throws InterruptedException,
+            IOException {
+        // checkRunning();
+        int msgID = rq.getCommand().getMessageID();
+        int maxOps = assoc.getMaxOpsInvoked();
+        if (maxOps == 0) {
+            rspDispatcher.put(msgID, l);
+        } else
+            synchronized (rspDispatcher) {
+                while (rspDispatcher.size() >= maxOps) {
+                    rspDispatcher.wait();
+                }
+                rspDispatcher.put(msgID, l);
+            }
+        assoc.write(rq);
+    }
 
-	// Protected -----------------------------------------------------
+    public FutureRSP invoke(Dimse rq) throws InterruptedException, IOException {
+        FutureRSPImpl retval = new FutureRSPImpl();
+        assoc.addAssociationListener(retval);
+        invoke(rq, retval);
+        return retval;
+    }
 
-	// Private -------------------------------------------------------
-	private void handleResponse(Dimse dimse) throws IOException {
-		DimseListener l = null;
-		synchronized (rspDispatcher) {
-			Command cmd = dimse.getCommand();
-			Dataset ds = dimse.getDataset(); // read out dataset, if any
-			int msgID = cmd.getMessageIDToBeingRespondedTo();
-			if (cmd.isPending()) {
-				l = (DimseListener) rspDispatcher.get(msgID);
-			} else {
-				l = (DimseListener) rspDispatcher.remove(msgID);
-				rspDispatcher.notify();
-			}
-			if (l != null)
-				l.dimseReceived(assoc, dimse);
-		}
-	}
+    public void release(boolean waitOnRSP) throws InterruptedException,
+            IOException {
+        // checkRunning();
+        if (waitOnRSP) {
+            synchronized (rspDispatcher) {
+                while (!rspDispatcher.isEmpty()) {
+                    rspDispatcher.wait();
+                }
+            }
+        }
+        ((AssociationImpl) assoc).writeReleaseRQ();
+    }
 
-	private void handleCancel(Dimse dimse) {
-		Command cmd = dimse.getCommand();
-		int msgID = cmd.getMessageIDToBeingRespondedTo();
-		DimseListener l = (DimseListener) cancelDispatcher.remove(msgID);
+    // LF_ThreadPool.Handler implementation --------------------------
+    public void run(LF_ThreadPool pool) {
+        Dimse dimse = null;
+        try {
+            dimse = assoc.read();
 
-		if (l != null)
-			l.dimseReceived(assoc, dimse);
-	}
+            // if Association was released
+            if (dimse == null) {
+                pool.shutdown();
+                return;
+            }
+            assoc.initMDC();
+            Command cmd = dimse.getCommand();
+            switch (cmd.getCommandField()) {
+            case Command.C_STORE_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).c_store(this,
+                        dimse);
+                break;
+            case Command.C_GET_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID())
+                        .c_get(this, dimse);
+                break;
+            case Command.C_FIND_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).c_find(this,
+                        dimse);
+                break;
+            case Command.C_MOVE_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).c_move(this,
+                        dimse);
+                break;
+            case Command.C_ECHO_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).c_echo(this,
+                        dimse);
+                break;
+            case Command.N_EVENT_REPORT_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).n_event_report(
+                        this, dimse);
+                break;
+            case Command.N_GET_RQ:
+                services.lookup(cmd.getRequestedSOPClassUID()).n_get(this,
+                        dimse);
+                break;
+            case Command.N_SET_RQ:
+                services.lookup(cmd.getRequestedSOPClassUID()).n_set(this,
+                        dimse);
+                break;
+            case Command.N_ACTION_RQ:
+                services.lookup(cmd.getRequestedSOPClassUID()).n_action(this,
+                        dimse);
+                break;
+            case Command.N_CREATE_RQ:
+                services.lookup(cmd.getAffectedSOPClassUID()).n_create(this,
+                        dimse);
+                break;
+            case Command.N_DELETE_RQ:
+                services.lookup(cmd.getRequestedSOPClassUID()).n_delete(this,
+                        dimse);
+                break;
+            case Command.C_STORE_RSP:
+            case Command.C_GET_RSP:
+            case Command.C_FIND_RSP:
+            case Command.C_MOVE_RSP:
+            case Command.C_ECHO_RSP:
+            case Command.N_EVENT_REPORT_RSP:
+            case Command.N_GET_RSP:
+            case Command.N_SET_RSP:
+            case Command.N_ACTION_RSP:
+            case Command.N_CREATE_RSP:
+            case Command.N_DELETE_RSP:
+                handleResponse(dimse);
+                break;
+            case Command.C_CANCEL_RQ:
+                handleCancel(dimse);
+                break;
+            default:
+                throw new RuntimeException("Illegal Command: " + cmd);
+            }
+        } catch (IOException ioe) {
+            log.error(ioe);
+            pool.shutdown();
+        } finally {
+            // ensure readout of data PDVs
+            if (dimse != null)
+                dimse.closeDataStream();
+            assoc.clearMDC();
+        }
+    }
 
-	private void checkRunning() {
-		if (!running)
-			throw new IllegalStateException("Not running: " + threadPool);
-	}
+    // Y overrides ---------------------------------------------------
 
-	   // AssociationListener implementation ----------------------------
-	   public void write(Association src, PDU pdu) {
-	   }
-	   
-	   public void received(Association src, Dimse dimse) {
-	   }
-	   
-	   public void error(Association src, IOException ioe) {
-	   }
-	   
-	   public void closing(Association src) {
-	   }
+    // Package protected ---------------------------------------------
 
-	   public void closed(Association src) {
-	       synchronized (rspDispatcher) {
-		       rspDispatcher.clear();
-		       rspDispatcher.notifyAll();
-	       }
-	       assoc.removeAssociationListener(this);
-	   }
-	   
-	   public void write(Association src, Dimse dimse) {
-	   }
-	   
-	   public void received(Association src, PDU pdu) {
-	   }
-	   
+    // Protected -----------------------------------------------------
+
+    // Private -------------------------------------------------------
+    private void handleResponse(Dimse dimse) throws IOException {
+        DimseListener l = null;
+        synchronized (rspDispatcher) {
+            Command cmd = dimse.getCommand();
+            Dataset ds = dimse.getDataset(); // read out dataset, if any
+            int msgID = cmd.getMessageIDToBeingRespondedTo();
+            if (cmd.isPending()) {
+                l = (DimseListener) rspDispatcher.get(msgID);
+            } else {
+                l = (DimseListener) rspDispatcher.remove(msgID);
+                rspDispatcher.notify();
+            }
+            if (l != null)
+                l.dimseReceived(assoc, dimse);
+        }
+    }
+
+    private void handleCancel(Dimse dimse) {
+        Command cmd = dimse.getCommand();
+        int msgID = cmd.getMessageIDToBeingRespondedTo();
+        DimseListener l = (DimseListener) cancelDispatcher.remove(msgID);
+
+        if (l != null)
+            l.dimseReceived(assoc, dimse);
+    }
+
+    private void checkRunning() {
+        if (!running)
+            throw new IllegalStateException("Not running: " + threadPool);
+    }
+
+    // AssociationListener implementation ----------------------------
+    public void write(Association src, PDU pdu) {
+    }
+
+    public void received(Association src, Dimse dimse) {
+    }
+
+    public void error(Association src, IOException ioe) {
+    }
+
+    public void closing(Association src) {
+    }
+
+    public void closed(Association src) {
+        synchronized (rspDispatcher) {
+            rspDispatcher.clear();
+            rspDispatcher.notifyAll();
+        }
+        assoc.removeAssociationListener(this);
+    }
+
+    public void write(Association src, Dimse dimse) {
+    }
+
+    public void received(Association src, PDU pdu) {
+    }
+
 }
