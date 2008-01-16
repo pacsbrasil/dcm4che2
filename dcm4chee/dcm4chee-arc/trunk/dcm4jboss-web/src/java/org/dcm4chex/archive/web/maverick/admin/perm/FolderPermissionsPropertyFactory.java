daa6b1f7a6f6be09d2e686c70632a83aae19c2e9
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-01-16 11:10:22 +0000

    [#DCMEE-683] Fetch User Role information by JAAS and not by direct query of DB user/roles tables.

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/admin/perm/FolderPermissionsPropertyFactory.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/admin/perm/FolderPermissionsPropertyFactory.java
@@ -41,8 +41,11 @@ package org.dcm4chex.archive.web.maverick.admin.perm;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.Principal;
+import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -51,11 +54,13 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import javax.security.auth.Subject;
+import javax.security.jacc.PolicyContext;
+
 import org.apache.log4j.Logger;
 import org.dcm4cheri.util.StringUtils;
 import org.dcm4chex.archive.util.FileUtils;
 import org.dcm4chex.archive.web.maverick.Dcm4cheeFormController;
-import org.dcm4chex.archive.web.maverick.admin.UserAdminDelegate;
 
 /**
  * 
@@ -64,120 +69,130 @@ import org.dcm4chex.archive.web.maverick.admin.UserAdminDelegate;
  * @since 13.04.2006
  */
 public class FolderPermissionsPropertyFactory extends FolderPermissionsFactory {
-	private static final String RAW_PROPERTIES = "RAW_PROPERTIES";
-	private Map mapPermissions = new HashMap();
+    private static final String RAW_PROPERTIES = "RAW_PROPERTIES";
+    private static final String SUBJECT_CONTEXT_KEY = "javax.security.auth.Subject.container";
 	
-	private static Logger log = Logger.getLogger(FolderPermissionsPropertyFactory.class.getName());
-	private UserAdminDelegate delegate = new UserAdminDelegate();
+    private Map mapPermissions = new HashMap();
 	
-	public void init() {
-		try {
-			if ( getInitParameter() == null) {
-				log.warn("No initString set. Use default");
-				setInitParameter( "conf/dcm4chee-web/folder.permissions" );
-			}
-			Properties props = new Properties();
-			File f = FileUtils.resolve(new File(getInitParameter()));
-			log.info("read permission properties from file: "+f);
-			props.load(new FileInputStream(f));
-			initMap(props);
-		} catch ( Exception x ) {
-			log.error("Cant initialize FolderPermissionsPropertyFactory!",x);
-		}
+    private static Logger log = Logger.getLogger(FolderPermissionsPropertyFactory.class.getName());
+	
+    public void init() {
+	try {
+	    if ( getInitParameter() == null) {
+		log.warn("No initString set. Use default");
+		setInitParameter( "conf/dcm4chee-web/folder.permissions" );
+	    }
+	    Properties props = new Properties();
+	    File f = FileUtils.resolve(new File(getInitParameter()));
+	    log.info("read permission properties from file: "+f);
+	    props.load(new FileInputStream(f));
+	    initMap(props);
+	} catch ( Exception x ) {
+	    log.error("Cant initialize FolderPermissionsPropertyFactory!",x);
 	}
+    }
 	
-	/**
-	 * @param props
-	 * @throws IOException
-	 */
-	private void initMap(Properties props) throws IOException {
-		mapPermissions.clear();
-		mapPermissions.put(RAW_PROPERTIES,props);
-		String[] apps = Dcm4cheeFormController.FOLDER_APPLICATIONS;
-		String perm, value, role, methods;
-		int pos;
-		Map map;
-		Set set;
-		for ( int i = 0, len = apps.length ; i < len ; i++ ) {
-			perm = props.getProperty(apps[i]);
-			if ( perm != null ) {
-				StringTokenizer st = new StringTokenizer(perm, ";");
-				while ( st.hasMoreTokens() ) {
-					value = st.nextToken();
-					pos = value.indexOf('(');
-					role = value.substring(0,pos);
-					map = (Map)mapPermissions.get(role);
-					if ( map == null ) {
-						map = new HashMap();
-						mapPermissions.put(role,map);
-					}
-					set = (Set) map.get(apps[i]);
-					if (set == null) {
-						set = new HashSet();
-						map.put(apps[i],set);
-					}
-					
-					methods = value.substring(++pos,value.length()-1);
-					addMethods(set, apps[i], methods, props);
-				}
-			}
-		}
-	}
+    /**
+     * @param props
+     * @throws IOException
+     */
+    private void initMap(Properties props) throws IOException {
+        mapPermissions.clear();
+	mapPermissions.put(RAW_PROPERTIES,props);
+	String[] apps = Dcm4cheeFormController.FOLDER_APPLICATIONS;
+	String perm, value, role, methods;
+	int pos;
+	Map map;
+	Set set;
+	for ( int i = 0, len = apps.length ; i < len ; i++ ) {
+	    perm = props.getProperty(apps[i]);
+	    if ( perm != null ) {
+		StringTokenizer st = new StringTokenizer(perm, ";");
+		while ( st.hasMoreTokens() ) {
+		    value = st.nextToken();
+		    pos = value.indexOf('(');
+		    role = value.substring(0,pos);
+		    map = (Map)mapPermissions.get(role);
+		    if ( map == null ) {
+		        map = new HashMap();
+		        mapPermissions.put(role,map);
+		    }
+		    set = (Set) map.get(apps[i]);
+		    if (set == null) {
+		        set = new HashSet();
+		        map.put(apps[i],set);
+		    }
 
-	private void addMethods(Set set, String app, String methods, Properties props ) {
-		if ( methods != null ) {
-			StringTokenizer st = new StringTokenizer(methods,",");
-			String method;
-			while ( st.hasMoreTokens() ) {
-				method = st.nextToken();
-				if (method.charAt(0)=='*') {
-					addMethods(set, app, props.getProperty(app+"."+method,"?"), props);
-				} else {
-					set.add(method);
-				}
-			}
-		}		
+		    methods = value.substring(++pos,value.length()-1);
+		    addMethods(set, apps[i], methods, props);
+		}
+	    }
 	}
+    }
 
-	public FolderPermissions getFolderPermissions(String userID) {
-		log.debug("getFolderPermission for user:"+userID);
-		FolderPermissions permissions = new FolderPermissions();
-		Collection roles;
-		try {
-			roles = delegate.getRolesOfUser(userID);
-		} catch ( Exception x ) {
-			log.error("Cant get roles for user "+userID+"! use role default",x);
-			roles = new ArrayList();
-			roles.add("default");
-			
-		}
-		log.debug("Roles:"+roles);
-		Map map;
-		Set set;
-		Map.Entry entry;
-		for ( Iterator iter = roles.iterator() ; iter.hasNext() ; ) {
-			map = (Map) mapPermissions.get(iter.next());//permission for a role (key=app,value=list of methods)
-			log.debug("permission for role:"+map);
-			if ( map != null ) {
-				for ( Iterator iter1 = map.entrySet().iterator() ; iter1.hasNext() ; ) {
-					entry = (Map.Entry) iter1.next();
-					set = (Set) entry.getValue();
-					permissions.addPermissions((String)entry.getKey(), (String[])set.toArray(new String[set.size()]));
-				}
-			}
-		}
-		if ( permissions.getNumberOfPrivilegedApps() == 0 ) {
-			log.warn("User "+userID+" has no Permissions for any Folder Application! Set to use folder only!");
-			permissions.addPermissions("folder",null);
-		}
+    private void addMethods(Set set, String app, String methods, Properties props ) {
+        if ( methods != null ) {
+            StringTokenizer st = new StringTokenizer(methods,",");
+            String method;
+            while ( st.hasMoreTokens() ) {
+                method = st.nextToken();
+                if (method.charAt(0)=='*') {
+                    addMethods(set, app, props.getProperty(app+"."+method,"?"), props);
+                } else {
+                    set.add(method);
+                }
+            }
+        }		
+    }
+
+    public FolderPermissions getFolderPermissions(String userID) {
+        log.debug("getFolderPermission for user:"+userID);
+        FolderPermissions permissions = new FolderPermissions();
+        Collection roles = new ArrayList();
+        try {
+            Subject subject = (Subject) PolicyContext.getContext(SUBJECT_CONTEXT_KEY);
+            Set groups = subject.getPrincipals(Group.class);
+            for (Iterator iter = groups.iterator(); iter.hasNext();) {
+                Group group = (Group) iter.next();
+                if ("Roles".equals(group.getName())) {
+                    for (Enumeration members = group.members();
+                    members.hasMoreElements();) {
+                        Principal member = (Principal) members.nextElement();
+                        roles.add(member.getName());
+                    }
+                }
+            }
+        } catch ( Exception x ) {
+            log.error("Cant get roles for user "+userID+"! use role default",x);
+            roles.add("default");
+        }
+        log.debug("Roles:"+roles);
+        Map map;
+        Set set;
+        Map.Entry entry;
+        for ( Iterator iter = roles.iterator() ; iter.hasNext() ; ) {
+            map = (Map) mapPermissions.get(iter.next());//permission for a role (key=app,value=list of methods)
+            log.debug("permission for role:"+map);
+            if ( map != null ) {
+                for ( Iterator iter1 = map.entrySet().iterator() ; iter1.hasNext() ; ) {
+                    entry = (Map.Entry) iter1.next();
+                    set = (Set) entry.getValue();
+                    permissions.addPermissions((String)entry.getKey(), (String[])set.toArray(new String[set.size()]));
+                }
+            }
+        }
+        if ( permissions.getNumberOfPrivilegedApps() == 0 ) {
+            log.warn("User "+userID+" has no Permissions for any Folder Application! Set to use folder only!");
+            permissions.addPermissions("folder",null);
+        }
         Properties rawProps = (Properties) mapPermissions.get(RAW_PROPERTIES);
-		String aeList = rawProps.getProperty(FolderPermissions.AEFILTER+"."+userID);
-		if ( aeList != null ) {
-			permissions.addPermissions(FolderPermissions.AEFILTER, StringUtils.split(aeList, ',') );
-		}
+        String aeList = rawProps.getProperty(FolderPermissions.AEFILTER+"."+userID);
+        if ( aeList != null ) {
+            permissions.addPermissions(FolderPermissions.AEFILTER, StringUtils.split(aeList, ',') );
+        }
         applyStationAet(permissions,rawProps, userID);
-		return permissions;
-	}
+        return permissions;
+    }
 
     private void applyStationAet(FolderPermissions permissions, Properties rawProps, String userID) {
         String stationAetGroups = rawProps.getProperty(FolderPermissions.STATION_AET_GROUP_LIST);
