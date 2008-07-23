8cbed6d2ddb0ef613c6a74b749f46d971bb7e436
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-07-23 15:26:21 +0000

    [maven-release-plugin]  copy for tag xero-0.4

---
+++ dcm4chee/xero/tags/xero-0.4/controller-xero/src/test/java/org/dcm4chee/xero/controller/RequestValidatorTest.java
@@ -0,0 +1,39 @@
+package org.dcm4chee.xero.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.dcm4chee.xero.metadata.MetaDataBean;
+import org.dcm4chee.xero.metadata.StaticMetaData;
+import org.dcm4chee.xero.metadata.access.MapWithDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class RequestValidatorTest {
+   private static final Logger log = LoggerFactory.getLogger(RequestValidatorTest.class);
+   MetaDataBean mdb = StaticMetaData.getMetaData("xero-model.metadata");
+   MetaDataBean model = mdb.get("model");
+
+   /**
+    * Tests that the default study query tests succeeds on valid data.
+    */
+   @SuppressWarnings("unchecked")
+   @Test
+   public void succeedQueryTest() {
+	  assert mdb!=null;
+	  assert model!=null;
+	  assert model.get("query")!=null;
+	  MapWithDefaults mwd = new MapWithDefaults(model);
+	  Map<String,Object> request = new HashMap<String,Object>();
+	  mwd.put(RequestValidator.PARAMETERS, request);
+	  request.put("ModalitiesInStudy", "CT");
+	  Map<String,Object> rv = (Map<String,Object>) mwd.get("query");
+	  assert rv!=null;
+	  assert "CT".equals(rv.get("ModalitiesInStudy"));
+	  String url = (String) rv.get("url");
+	  assert url!=null;
+	  log.info("URL for request validator="+url);
+	  assert "/wado2/study.xml?ModalitiesInStudy=CT".equals(url);
+   }
+}
---
+++ dcm4chee/xero/tags/xero-0.4/model-xero/src/main/java/org/dcm4chee/xero/model/QueryLayout.java
@@ -0,0 +1,17 @@
+package org.dcm4chee.xero.model;
+
+import org.dcm4chee.xero.metadata.access.LazyMap;
+
+/** A Query Box is a simple model object that is used to layout a query search criteria object. */
+public class QueryLayout extends Layout {
+   LazyMap query;
+
+   public QueryLayout(LazyMap query) {
+	  super("query/query");
+	  this.query = query;
+   }
+   
+   public LazyMap getQuery() {
+	  return this.query;
+   }
+}
