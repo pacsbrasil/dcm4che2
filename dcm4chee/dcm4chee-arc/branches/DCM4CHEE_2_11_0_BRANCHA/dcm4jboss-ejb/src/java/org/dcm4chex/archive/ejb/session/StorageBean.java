b5e6d7882c1f6a6caad37eddfcf84f7d7a8fe683
bubbag <bubbag@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-09-17 12:05:38 +0000

    Backported DCMEE 949

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_11_0_BRANCHA/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/session/StorageBean.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_11_0_BRANCHA/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/session/StorageBean.java
@@ -41,6 +41,7 @@ package org.dcm4chex.archive.ejb.session;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -221,10 +222,9 @@ public abstract class StorageBean implements SessionBean {
     /**
      * @ejb.interface-method
      */
-    public SeriesStored makeSeriesStored(String seriuid, Collection iuids)
+    public SeriesStored makeSeriesStored(String seriuid)
     throws FinderException {
-        SeriesLocal series = findBySeriesIuid(seriuid);
-        return makeSeriesStored(series, iuids);
+    	return makeSeriesStored(findBySeriesIuid(seriuid));
     }
 
     /**
@@ -275,7 +275,7 @@ public abstract class StorageBean implements SessionBean {
         ArrayList list = new ArrayList(c.size());
         SeriesStored seriesStored;
         for (Iterator iter = c.iterator(); iter.hasNext();) {
-            seriesStored = makeSeriesStored((SeriesLocal) iter.next(), null);
+            seriesStored = makeSeriesStored((SeriesLocal) iter.next());
             if (seriesStored != null) {
                 list.add(seriesStored);
             }
@@ -283,51 +283,70 @@ public abstract class StorageBean implements SessionBean {
         return (SeriesStored[]) list.toArray(new SeriesStored[list.size()]);
     }
     
-    private SeriesStored makeSeriesStored(SeriesLocal series, Collection iuids)
-            throws FinderException {
-        StudyLocal study = series.getStudy();
-        PatientLocal pat = study.getPatient();
-        Dataset seriesAttrs = series.getAttributes(true);
-        Dataset studyAttrs = study.getAttributes(true);
-        Dataset patAttrs = pat.getAttributes(true);
-        Dataset ian = DcmObjectFactory.getInstance().newDataset();
-        ian.putUI(Tags.StudyInstanceUID, study.getStudyIuid());
-        DcmElement refPPSSeq = ian.putSQ(Tags.RefPPSSeq);
-        Dataset pps = seriesAttrs.getItem(Tags.RefPPSSeq);
-        if (pps != null) {
-            if (!pps.contains(Tags.PerformedWorkitemCodeSeq)) {
-                pps.putSQ(Tags.PerformedWorkitemCodeSeq);
-            }
-            refPPSSeq.addItem(pps);
-        }
-        Dataset refSeries = ian.putSQ(Tags.RefSeriesSeq).addNewItem();
-        DcmElement refSOPs = refSeries.putSQ(Tags.RefSOPSeq);
-        refSeries.putUI(Tags.SeriesInstanceUID, series.getSeriesIuid());
-        Collection c = series.getInstances();
-        for (Iterator iter = c.iterator(); iter.hasNext();) {
-            InstanceLocal inst = (InstanceLocal) iter.next();
-            if (inst.getInstanceStatus() != RECEIVED 
-                    || (iuids != null && !iuids.contains(inst.getSopIuid()))) {
-                continue;
-            }
-            Dataset refSOP = refSOPs.addNewItem();           
-            refSOP.putUI(Tags.RefSOPClassUID, inst.getSopCuid());
-            refSOP.putUI(Tags.RefSOPInstanceUID, inst.getSopIuid());
-            refSOP.putAE(Tags.RetrieveAET, StringUtils.split(
-                    inst.getRetrieveAETs(), '\\')[0]);
-            refSOP.putCS(Tags.InstanceAvailability, "ONLINE");
-        }
-        if (refSOPs.countItems() == 0) {
-            return null;
-        }
-        SeriesStored seriesStored = new SeriesStored(patAttrs, studyAttrs,
-                seriesAttrs, ian);
-        return seriesStored;
-    }
+
+    private SeriesStored makeSeriesStored(SeriesLocal series)
+			throws FinderException {
+		StudyLocal study = series.getStudy();
+		Dataset ian = DcmObjectFactory.getInstance().newDataset();
+		ian.putUI(Tags.StudyInstanceUID, study.getStudyIuid());
+		Dataset refSeries = ian.putSQ(Tags.RefSeriesSeq).addNewItem();
+		DcmElement refSOPs = refSeries.putSQ(Tags.RefSOPSeq);
+		refSeries.putUI(Tags.SeriesInstanceUID, series.getSeriesIuid());
+		HashSet commonRetrieveAETs = null;
+		Collection c = series.getInstances();
+		for (Iterator iter = c.iterator(); iter.hasNext();) {
+			InstanceLocal inst = (InstanceLocal) iter.next();
+			if (inst.getInstanceStatus() != RECEIVED) {
+				continue;
+			}
+			String[] retrieveAETs = StringUtils.split(inst.getRetrieveAETs(),
+					'\\');
+			if (commonRetrieveAETs == null) {
+				commonRetrieveAETs = new HashSet();
+				commonRetrieveAETs.addAll(Arrays.asList(retrieveAETs));
+			} else {
+				commonRetrieveAETs.retainAll(Arrays.asList(retrieveAETs));
+			}
+			Dataset refSOP = refSOPs.addNewItem();
+			refSOP.putUI(Tags.RefSOPClassUID, inst.getSopCuid());
+			refSOP.putUI(Tags.RefSOPInstanceUID, inst.getSopIuid());
+			refSOP.putAE(Tags.RetrieveAET, retrieveAETs);
+			refSOP.putCS(Tags.InstanceAvailability, Availability.toString(inst
+					.getAvailabilitySafe()));
+		}
+		if (commonRetrieveAETs == null) {
+			return null;
+		}
+		PatientLocal pat = study.getPatient();
+		Dataset patAttrs = pat.getAttributes(false);
+		Dataset studyAttrs = study.getAttributes(false);
+		Dataset seriesAttrs = series.getAttributes(false);
+		Dataset pps = seriesAttrs.getItem(Tags.RefPPSSeq);
+		DcmElement refPPSSeq = ian.putSQ(Tags.RefPPSSeq);
+		if (pps != null) {
+			if (!pps.contains(Tags.PerformedWorkitemCodeSeq)) {
+				pps.putSQ(Tags.PerformedWorkitemCodeSeq);
+			}
+			refPPSSeq.addItem(pps);
+		}
+		SeriesStored seriesStored = new SeriesStored(patAttrs, studyAttrs, seriesAttrs, ian);
+		return seriesStored;
+	}
+
+	/**
+	 * @ejb.interface-method
+	 */
+	public void updateDerivedStudyAndSeriesFields(String seriuid)
+			throws FinderException {
+		SeriesLocal series = findBySeriesIuid(seriuid);
+		series.updateDerivedFields(true, true, false, true, true);
+		series.getStudy().updateDerivedFields(true, true, false, true, true,
+				true);
+	}
     
     /**
-     * @ejb.interface-method
-     */
+	 * @ejb.interface-method
+	 */
     public void storeFile(java.lang.String iuid, java.lang.String tsuid,
     		java.lang.String dirpath, java.lang.String fileid,
     		int size, byte[] md5, int status)
