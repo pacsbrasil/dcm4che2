fcb45443b663eb227dbe25880344893ecfeded26
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-09-25 12:44:42 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/mbean/ConsistenceCheckService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/mbean/ConsistenceCheckService.java
@@ -57,28 +57,30 @@ import org.jboss.system.ServiceMBeanSupport;
 
 /**
  * @author franz.willer@gwi-ag.com
- * @version $Revision$ $Date$
+ * @version $Revision$ $Date: 2006-10-24 20:34:19 +0200 (Tue, 24 Oct
+ *          2006) $
  * @since 35.03.2005
- *
+ * 
  */
 public class ConsistenceCheckService extends ServiceMBeanSupport {
 
     private final SchedulerDelegate scheduler = new SchedulerDelegate(this);
 
     private long taskInterval = 0L;
-	private long minStudyAge;
-	private long maxStudyAge;
-	private long maxCheckedBefore;
+    private long minStudyAge;
+    private long maxStudyAge;
+    private long maxCheckedBefore;
 
     private int disabledStartHour;
     private int disabledEndHour;
     private int limitNumberOfStudiesPerTask;
 
     private Integer listenerID;
-    
+
     private String timerIDCheckStudyConsistency;
 
-    private static final Logger log = Logger.getLogger(ConsistenceCheckService.class);
+    private static final Logger log = Logger
+            .getLogger(ConsistenceCheckService.class);
 
     private final NotificationListener consistentCheckListener = new NotificationListener() {
         public void handleNotification(Notification notif, Object handback) {
@@ -91,7 +93,7 @@ public class ConsistenceCheckService extends ServiceMBeanSupport {
                             + " !");
             } else {
                 try {
-                	check();
+                    check();
                 } catch (Exception e) {
                     log.error("Consistant check failed!", e);
                 }
@@ -106,7 +108,7 @@ public class ConsistenceCheckService extends ServiceMBeanSupport {
     public void setSchedulerServiceName(ObjectName schedulerServiceName) {
         scheduler.setSchedulerServiceName(schedulerServiceName);
     }
-    
+
     public final String getTaskInterval() {
         String s = RetryIntervalls.formatIntervalZeroAsNever(taskInterval);
         return (disabledEndHour == -1) ? s : s + "!" + disabledStartHour + "-"
@@ -129,12 +131,11 @@ public class ConsistenceCheckService extends ServiceMBeanSupport {
         }
         if (getState() == STARTED && oldInterval != taskInterval) {
             scheduler.stopScheduler(timerIDCheckStudyConsistency, listenerID,
-            		consistentCheckListener);
-            listenerID = scheduler.startScheduler(timerIDCheckStudyConsistency, taskInterval,
-            		consistentCheckListener);
+                    consistentCheckListener);
+            listenerID = scheduler.startScheduler(timerIDCheckStudyConsistency,
+                    taskInterval, consistentCheckListener);
         }
     }
-
 
     public int getLimitNumberOfStudiesPerTask() {
         return limitNumberOfStudiesPerTask;
@@ -144,127 +145,136 @@ public class ConsistenceCheckService extends ServiceMBeanSupport {
         this.limitNumberOfStudiesPerTask = limit;
     }
 
-	/**
-	 * Getter for minStudyAge. 
-	 * <p>
-	 * This value is used to ensure that consistent check is not performed 
-	 * on studies that are not completed (store is not completed).
-	 * 
-	 * @return Returns ##w (in weeks), ##d (in days), ##h (in hours).
-	 */
-	public String getMinStudyAge() {
-		return RetryIntervalls.formatInterval(minStudyAge);
-	}
-	
-	/**
-	 * Setter for minStudyAge. 
-	 * <p>
-	 * This value is used to ensure that consistent check is not performed 
-	 * on studies that are not completed (store is not completed).
-	 * 
-	 * @param age ##w (in weeks), ##d (in days), ##h (in hours).
-	 */
-	public void setMinStudyAge(String age) {
-		this.minStudyAge = RetryIntervalls.parseInterval(age);
-	}
-	
     /**
-	 * Getter for maxStudyAge. 
-	 * <p>
-	 * This value is used to limit consistent check to 'newer' studies.
+     * Getter for minStudyAge.
+     * <p>
+     * This value is used to ensure that consistent check is not performed on
+     * studies that are not completed (store is not completed).
+     * 
+     * @return Returns ##w (in weeks), ##d (in days), ##h (in hours).
+     */
+    public String getMinStudyAge() {
+        return RetryIntervalls.formatInterval(minStudyAge);
+    }
+
+    /**
+     * Setter for minStudyAge.
+     * <p>
+     * This value is used to ensure that consistent check is not performed on
+     * studies that are not completed (store is not completed).
+     * 
+     * @param age
+     *            ##w (in weeks), ##d (in days), ##h (in hours).
+     */
+    public void setMinStudyAge(String age) {
+        this.minStudyAge = RetryIntervalls.parseInterval(age);
+    }
+
+    /**
+     * Getter for maxStudyAge.
+     * <p>
+     * This value is used to limit consistent check to 'newer' studies.
      * 
      * @return ##w (in weeks), ##d (in days), ##h (in hours).
      */
     public String getMaxStudyAge() {
         return RetryIntervalls.formatInterval(maxStudyAge);
     }
-    
+
     /**
-	 * Setter for maxStudyAge. 
-	 * <p>
-	 * This value is used to limit consistent check to 'newer' studies.
-     *  
-     * @param maxStudyAge The maxStudyAge to set.
+     * Setter for maxStudyAge.
+     * <p>
+     * This value is used to limit consistent check to 'newer' studies.
+     * 
+     * @param maxStudyAge
+     *            The maxStudyAge to set.
      */
     public void setMaxStudyAge(String age) {
         this.maxStudyAge = RetryIntervalls.parseInterval(age);
     }
 
     /**
-	 * Getter for maxStudyAge. 
-	 * <p>
-	 * This value is used to limit consistent check to 'newer' studies.
+     * Getter for maxStudyAge.
+     * <p>
+     * This value is used to limit consistent check to 'newer' studies.
      * 
      * @return ##w (in weeks), ##d (in days), ##h (in hours).
      */
     public String getMaxCheckedBefore() {
         return RetryIntervalls.formatInterval(maxCheckedBefore);
     }
-    
+
     /**
-	 * Setter for maxStudyAge. 
-	 * <p>
-	 * This value is used to limit consistent check to 'newer' studies.
-     *  
-     * @param maxStudyAge The maxStudyAge to set.
+     * Setter for maxStudyAge.
+     * <p>
+     * This value is used to limit consistent check to 'newer' studies.
+     * 
+     * @param maxStudyAge
+     *            The maxStudyAge to set.
      */
     public void setMaxCheckedBefore(String maxCheckedBefore) {
         this.maxCheckedBefore = RetryIntervalls.parseInterval(maxCheckedBefore);
     }
-    
+
     public String check() throws RemoteException, FinderException {
-    	int updated = 0;
-    	long l = System.currentTimeMillis();
-    	Timestamp createdBefore = new Timestamp( l - this.minStudyAge );
-    	Timestamp createdAfter = new Timestamp( l - this.maxStudyAge );
-    	Timestamp checkedBefore = new Timestamp( l - this.maxCheckedBefore );
-    	ConsistencyCheck checker = newConsistencyCheck();
-    	if ( log.isDebugEnabled() ) log.debug("call findStudiesToCheck: createdAfter:"+createdAfter+" createdBefore:"+createdBefore+" checkedBefore:"+checkedBefore);
-    	long[] studyPks = checker.findStudiesToCheck( createdAfter, createdBefore, checkedBefore, this.limitNumberOfStudiesPerTask );
-    	for ( int i = 0, len = studyPks.length ; i < len ; i++) {
-    		if ( checker.updateStudy( studyPks[i]) ) {
-    			updated++;
-    		}
-    	}
-    	return updated + " of "+ studyPks.length + " studies updated!";
+        int updated = 0;
+        long l = System.currentTimeMillis();
+        Timestamp createdBefore = new Timestamp(l - this.minStudyAge);
+        Timestamp createdAfter = new Timestamp(l - this.maxStudyAge);
+        Timestamp checkedBefore = new Timestamp(l - this.maxCheckedBefore);
+        ConsistencyCheck checker = newConsistencyCheck();
+        if (log.isDebugEnabled())
+            log.debug("call findStudiesToCheck: createdAfter:" + createdAfter
+                    + " createdBefore:" + createdBefore + " checkedBefore:"
+                    + checkedBefore);
+        long[] studyPks = checker.findStudiesToCheck(createdAfter,
+                createdBefore, checkedBefore, this.limitNumberOfStudiesPerTask);
+        for (int i = 0, len = studyPks.length; i < len; i++) {
+            if (checker.updateStudy(studyPks[i])) {
+                updated++;
+            }
+        }
+        return updated + " of " + studyPks.length + " studies updated!";
     }
-    
+
     private boolean isDisabled(int hour) {
-        if (disabledEndHour == -1) return false;
+        if (disabledEndHour == -1)
+            return false;
         boolean sameday = disabledStartHour <= disabledEndHour;
-        boolean inside = hour >= disabledStartHour && hour < disabledEndHour; 
+        boolean inside = hour >= disabledStartHour && hour < disabledEndHour;
         return sameday ? inside : !inside;
     }
 
     protected void startService() throws Exception {
-        listenerID = scheduler.startScheduler(timerIDCheckStudyConsistency, taskInterval,
-        		consistentCheckListener);
+        listenerID = scheduler.startScheduler(timerIDCheckStudyConsistency,
+                taskInterval, consistentCheckListener);
     }
 
     protected void stopService() throws Exception {
         scheduler.stopScheduler(timerIDCheckStudyConsistency, listenerID,
-        		consistentCheckListener);
+                consistentCheckListener);
         super.stopService();
     }
 
     private ConsistencyCheck newConsistencyCheck() {
         try {
-        	ConsistencyCheckHome home = (ConsistencyCheckHome) EJBHomeFactory
+            ConsistencyCheckHome home = (ConsistencyCheckHome) EJBHomeFactory
                     .getFactory().lookup(ConsistencyCheckHome.class,
-                    		ConsistencyCheckHome.JNDI_NAME);
+                            ConsistencyCheckHome.JNDI_NAME);
             return home.create();
         } catch (Exception e) {
-            throw new RuntimeException("Failed to access ConsistencyCheck EJB:",
-                    e);
+            throw new RuntimeException(
+                    "Failed to access ConsistencyCheck EJB:", e);
         }
     }
 
-	public String getTimerIDCheckStudyConsistency() {
-		return timerIDCheckStudyConsistency;
-	}
+    public String getTimerIDCheckStudyConsistency() {
+        return timerIDCheckStudyConsistency;
+    }
 
-	public void setTimerIDCheckStudyConsistency(String timerIDCheckStudyConsistency) {
-		this.timerIDCheckStudyConsistency = timerIDCheckStudyConsistency;
-	}
+    public void setTimerIDCheckStudyConsistency(
+            String timerIDCheckStudyConsistency) {
+        this.timerIDCheckStudyConsistency = timerIDCheckStudyConsistency;
+    }
 
 }
