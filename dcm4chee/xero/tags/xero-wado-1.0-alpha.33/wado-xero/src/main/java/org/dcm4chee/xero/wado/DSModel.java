c85d12cfa1488f1fe6e43caaf6d0508accdb7443
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-09-26 20:10:32 +0000

    Releasing version 1.0-alpha.33

---
+++ dcm4chee/xero/tags/xero-wado-1.0-alpha.33/wado-xero/src/main/java/org/dcm4chee/xero/wado/DSModel.java
@@ -0,0 +1,26 @@
+package org.dcm4chee.xero.wado;
+
+import java.util.Map;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4chee.xero.metadata.filter.Filter;
+import org.dcm4chee.xero.metadata.filter.FilterItem;
+import org.dcm4chee.xero.metadata.servlet.ServletResponseItem;
+
+/**
+ * This class adds the dicom object to the model so that it can be rendered or
+ * otherwise used.
+ */
+public class DSModel implements Filter<ServletResponseItem> {
+
+	/** Add the ds (dicom object) to the model in the params */
+	@SuppressWarnings("unchecked")
+	public ServletResponseItem filter(FilterItem<ServletResponseItem> filterItem, Map<String, Object> params) {
+		DicomObject ds = DicomFilter.filterDicomObject(filterItem, params, null);
+		if (ds != null) {
+			((Map<String, Object>) params.get("model")).put("ds", new DicomObjectMap(ds));
+		}
+		return filterItem.callNextFilter(params);
+	}
+
+}
