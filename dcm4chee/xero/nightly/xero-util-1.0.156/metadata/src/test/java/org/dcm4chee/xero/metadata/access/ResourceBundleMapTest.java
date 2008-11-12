1a3c15083a7846329149d39e5c20b4f761b2f7f0
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-11-12 00:07:27 +0000

    Releasing version 1.0.156

---
+++ dcm4chee/xero/nightly/xero-util-1.0.156/metadata/src/test/java/org/dcm4chee/xero/metadata/access/ResourceBundleMapTest.java
@@ -0,0 +1,38 @@
+package org.dcm4chee.xero.metadata.access;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import org.testng.annotations.Test;
+
+/** Checks to see that resource bundle maps return the correct keys */
+public class ResourceBundleMapTest {
+
+	ClassLoader cl = Thread.currentThread().getContextClassLoader();
+	ResourceBundle rb = ResourceBundle.getBundle("i18n",new Locale("en"), cl);
+	static final Properties lazy = new Properties();
+	ResourceBundleMap rbm = new ResourceBundleMap(rb,lazy);
+	
+	static {
+		lazy.put("name.icon", "Icon Name");
+	}
+	
+	@Test
+	public void test_get_returns_value() {
+		assert rbm.get("name.label").equals("Name");
+		assert rbm.get("name.icon").equals("Icon Name"); 
+	}
+	
+	@Test
+	public void test_iteration() {
+		int count = 0;
+		for(@SuppressWarnings("unused") Map.Entry<String,Object> me : rbm.entrySet() ) {
+			count++;
+		}
+		System.out.println("Count="+count);
+		assert count==4;
+	}
+	
+}
