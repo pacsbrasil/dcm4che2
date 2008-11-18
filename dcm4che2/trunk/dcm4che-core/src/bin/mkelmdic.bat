490a40f05ca35ec1363a46da26ba0fd749020bed
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2008-11-18 15:17:03 +0000

    [#DCM-250] Upgrade to slf4j v1.5.0

--- dcm4che2/trunk/dcm4che-core/src/bin/mkelmdic.bat
+++ dcm4che2/trunk/dcm4che-core/src/bin/mkelmdic.bat
@@ -43,8 +43,8 @@ set JAVA=%JAVA_HOME%\bin\java
 :SKIP_SET_JAVA_HOME
 
 set CP=%DCM4CHE_HOME%\lib\%MAIN_JAR%
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.4.3.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.4.3.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.5.0.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.5.0.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.13.jar
 
 "%JAVA%" %JAVA_OPTS% -cp "%CP%" %MAIN_CLASS% %ARGS%
--- dcm4che2/trunk/dcm4che-tool/dcm4che-tool-dcm2jpg/src/bin/dcm2jpg.bat
+++ dcm4che2/trunk/dcm4che-tool/dcm4che-tool-dcm2jpg/src/bin/dcm2jpg.bat
@@ -53,8 +53,8 @@ set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-2.0.18-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-image-2.0.18-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-imageio-2.0.18-SNAPSHOT.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-imageio-rle-2.0.18-SNAPSHOT.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.4.3.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.4.3.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.5.0.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.5.0.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.13.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.1.jar
 
