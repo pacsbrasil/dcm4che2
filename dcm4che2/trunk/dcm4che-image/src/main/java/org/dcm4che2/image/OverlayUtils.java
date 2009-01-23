ee8d4385cb202f9d938723213f319d1e822287cd
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-01-23 12:51:29 +0000

    pretty format

--- dcm4che2/trunk/dcm4che-image/src/main/java/org/dcm4che2/image/OverlayUtils.java
+++ dcm4che2/trunk/dcm4che-image/src/main/java/org/dcm4che2/image/OverlayUtils.java
@@ -54,153 +54,173 @@ import org.dcm4che2.data.Tag;
 
 /**
  * Provides utility methods to extract overlay information from DICOM files.
+ * 
  * @author bwallace
  */
 public class OverlayUtils {
 
-	private static final int BITS_PER_BYTE = 8;
+    private static final int BITS_PER_BYTE = 8;
 
-	/** Returns true if the given frame number references an overlay - that is, is the form 0x60xx yyyy where
-	 * xx is the overlay number, and yyyy is the overlay frame number.   xx must be even.
-	 * @param imageIndex
-	 * @return true if this is an overlay frame.
-	 */
-	public static boolean isOverlay(int imageIndex) {
-		return ((imageIndex & 0x60000000) == 0x60000000)
-				&& (imageIndex & 0x9F010000) == 0;
-	}
+    /**
+     * Returns true if the given frame number references an overlay - that is,
+     * is the form 0x60xx yyyy where xx is the overlay number, and yyyy is the
+     * overlay frame number. xx must be even.
+     * 
+     * @param imageIndex
+     * @return true if this is an overlay frame.
+     */
+    public static boolean isOverlay(int imageIndex) {
+        return ((imageIndex & 0x60000000) == 0x60000000)
+                && (imageIndex & 0x9F010000) == 0;
+    }
 
+    /**
+     * Extra the frame number portion of the overlay number/imageIndex value.
+     * 
+     * @param imageIndex
+     * @return
+     */
+    public static int extractFrameNumber(int imageIndex) {
+        if (isOverlay(imageIndex))
+            return imageIndex & 0xFFFF;
+        throw new IllegalArgumentException(
+                "Only frame numbers of overlays can be extracted.");
+    }
 
-	/**
-	 * Extra the frame number portion of the overlay number/imageIndex value.
-	 * @param imageIndex
-	 * @return
-	 */
-	public static int extractFrameNumber(int imageIndex) {
-		if( isOverlay(imageIndex) ) return imageIndex & 0xFFFF;
-		throw new IllegalArgumentException("Only frame numbers of overlays can be extracted.");
-	}
-	
-	private static LookupTable reorderBytes;
-	static {
-		byte[] reorder = new byte[256];
-		for (int i = 0; i < 256; i++) {
-			for (int j = 0; j < 8; j++) {
-				int bitTest = 1 << j;
-				if ((i & bitTest) != 0)
-					reorder[i] |= (0x80 >> j);
-			}
-		}
-		reorderBytes = new ByteLookupTable(8, false, 0, 8, reorder);
-	}
+    private static LookupTable reorderBytes;
+    static {
+        byte[] reorder = new byte[256];
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 8; j++) {
+                int bitTest = 1 << j;
+                if ((i & bitTest) != 0)
+                    reorder[i] |= (0x80 >> j);
+            }
+        }
+        reorderBytes = new ByteLookupTable(8, false, 0, 8, reorder);
+    }
 
-	private static byte[] rgbArr = new byte[] { (byte) 0xFF, 0 };
+    private static byte[] rgbArr = new byte[] { (byte) 0xFF, 0 };
 
-	private static byte[] aArr = new byte[] { (byte) 0x00, (byte) 0xFF };
+    private static byte[] aArr = new byte[] { (byte) 0x00, (byte) 0xFF };
 
-	/**
-	 * Read an overlay image or region instead of a regular image.  Specify the overlayNumber by the 0x60xx 0000 number.
-	 * This will return a bitmap overlay, in the colour specified.  The image reader isn't required unless overlays encoded
-	 * in the high bits are being read.
-	 * 
-	 * @param ds is a DicomObject to read the overlay from.
-	 * @param overlayNumber - of the form 0x60xx yyyy where xx is the overlay index and yyyy is the frame index
-	 * @param reader is the image reader used to extract the raster for the high bits
-	 * @param rgb is the colour to apply, can be null to use black & white overlays.
-	 * @return A single bit buffered image, transparent except in the given colour where bits are 1.
-	 * @throws IOException only if an image from the image reader is attempted and throws an exception.
-	 */
-	public static BufferedImage extractOverlay(DicomObject ds, int overlayNumber, ImageReader reader, String rgbs)
-			throws IOException {
-		// We need the original overlay number.
-		if (!OverlayUtils.isOverlay(overlayNumber))
-			throw new IllegalArgumentException(
-					"Overlays must start with 0x60xx xxxx but it starts with "
-							+ Integer.toString(overlayNumber, 16));
-		int frameNumber = extractFrameNumber(overlayNumber); 
-		overlayNumber = overlayNumber & 0x60FE0000;
+    /**
+     * Read an overlay image or region instead of a regular image. Specify the
+     * overlayNumber by the 0x60xx 0000 number. This will return a bitmap
+     * overlay, in the colour specified. The image reader isn't required unless
+     * overlays encoded in the high bits are being read.
+     * 
+     * @param ds
+     *            is a DicomObject to read the overlay from.
+     * @param overlayNumber
+     *            - of the form 0x60xx yyyy where xx is the overlay index and
+     *            yyyy is the frame index
+     * @param reader
+     *            is the image reader used to extract the raster for the high
+     *            bits
+     * @param rgb
+     *            is the colour to apply, can be null to use black & white
+     *            overlays.
+     * @return A single bit buffered image, transparent except in the given
+     *         colour where bits are 1.
+     * @throws IOException
+     *             only if an image from the image reader is attempted and
+     *             throws an exception.
+     */
+    public static BufferedImage extractOverlay(DicomObject ds,
+            int overlayNumber, ImageReader reader, String rgbs)
+            throws IOException {
+        // We need the original overlay number.
+        if (!OverlayUtils.isOverlay(overlayNumber))
+            throw new IllegalArgumentException(
+                    "Overlays must start with 0x60xx xxxx but it starts with "
+                            + Integer.toString(overlayNumber, 16));
+        int frameNumber = extractFrameNumber(overlayNumber);
+        overlayNumber = overlayNumber & 0x60FE0000;
 
-		int rows = getOverlayHeight(ds, overlayNumber);
-		int cols = getOverlayWidth(ds, overlayNumber);
-		if (cols == 0 || rows == 0)
-			throw new IllegalArgumentException("No overlay found for "
-					+ Integer.toString(overlayNumber));
-		int position = ds.getInt(overlayNumber | Tag.OverlayBitPosition);
-		byte[] data;
-		if (position == 0) {
-			data = ds.getBytes(overlayNumber | Tag.OverlayData);
-			// Extract a sub-frame IF there is a sub-frame, and one is specified.  There must be at least 2 frames worth
-			// of data to even consider this operation.
-			if( frameNumber>0 && data.length>=rows*cols*2/BITS_PER_BYTE) {
-				byte[] frameData = new byte[rows*cols/BITS_PER_BYTE];
-				// TODO Replace with Array.copyOfRange once we are on 1.6
-				System.arraycopy(data, (frameNumber-1)*frameData.length, frameData, 0, frameData.length);
-				data = frameData;
-			}
-		} else {
-			Raster raw = reader.readRaster(frameNumber,null);
-			int rowLen = (cols+7)/8;
-			data = new byte[rows * rowLen];
-			int[] pixels = new int[cols];
-			int bit = (1 << position);
-			for(int y=0; y<rows; y++) {
-				pixels = raw.getPixels(0,y, cols, 1, pixels);
-				for(int x=0; x<cols; x++) {
-					if( (pixels[x] & bit)!=0 ) {
-						data[rowLen * y + x/8] |= (1 << (x % 8));
-					}
-				}
-			}
-		}
-		DataBuffer db = new DataBufferByte(data, data.length);
-		WritableRaster wr = Raster.createPackedRaster(db,
-				cols,rows, 1, new Point());
-		byte[] rArr = rgbArr;
-		byte[] gArr = rgbArr;
-		byte[] bArr = rgbArr;
-		if (rgbs != null && rgbs.length() > 0) {
-			if (rgbs.startsWith("#"))
-				rgbs = rgbs.substring(1);
-			int rgb = Integer.parseInt(rgbs, 16);
-			rArr = new byte[] { 0, (byte) ((rgb >> 16) & 0xFF) };
-			gArr = new byte[] { 0, (byte) ((rgb >> 8) & 0xFF) };
-			bArr = new byte[] { 0, (byte) (rgb & 0xFF) };
-		}
-		ColorModel cm = new IndexColorModel(1, 2, rArr, gArr, bArr, aArr);
-		BufferedImage bi = new BufferedImage(cm, wr, false, null);
-		reorderBytes.lookup(bi.getRaster().getDataBuffer(), bi.getRaster()
-				.getDataBuffer());
+        int rows = getOverlayHeight(ds, overlayNumber);
+        int cols = getOverlayWidth(ds, overlayNumber);
+        if (cols == 0 || rows == 0)
+            throw new IllegalArgumentException("No overlay found for "
+                    + Integer.toString(overlayNumber));
+        int position = ds.getInt(overlayNumber | Tag.OverlayBitPosition);
+        byte[] data;
+        if (position == 0) {
+            data = ds.getBytes(overlayNumber | Tag.OverlayData);
+            // Extract a sub-frame IF there is a sub-frame, and one is
+            // specified. There must be at least 2 frames worth
+            // of data to even consider this operation.
+            if (frameNumber > 0
+                    && data.length >= rows * cols * 2 / BITS_PER_BYTE) {
+                byte[] frameData = new byte[rows * cols / BITS_PER_BYTE];
+                // TODO Replace with Array.copyOfRange once we are on 1.6
+                System.arraycopy(data, (frameNumber - 1) * frameData.length,
+                        frameData, 0, frameData.length);
+                data = frameData;
+            }
+        } else {
+            Raster raw = reader.readRaster(frameNumber, null);
+            int rowLen = (cols + 7) / 8;
+            data = new byte[rows * rowLen];
+            int[] pixels = new int[cols];
+            int bit = (1 << position);
+            for (int y = 0; y < rows; y++) {
+                pixels = raw.getPixels(0, y, cols, 1, pixels);
+                for (int x = 0; x < cols; x++) {
+                    if ((pixels[x] & bit) != 0) {
+                        data[rowLen * y + x / 8] |= (1 << (x % 8));
+                    }
+                }
+            }
+        }
+        DataBuffer db = new DataBufferByte(data, data.length);
+        WritableRaster wr = Raster.createPackedRaster(db, cols, rows, 1,
+                new Point());
+        byte[] rArr = rgbArr;
+        byte[] gArr = rgbArr;
+        byte[] bArr = rgbArr;
+        if (rgbs != null && rgbs.length() > 0) {
+            if (rgbs.startsWith("#"))
+                rgbs = rgbs.substring(1);
+            int rgb = Integer.parseInt(rgbs, 16);
+            rArr = new byte[] { 0, (byte) ((rgb >> 16) & 0xFF) };
+            gArr = new byte[] { 0, (byte) ((rgb >> 8) & 0xFF) };
+            bArr = new byte[] { 0, (byte) (rgb & 0xFF) };
+        }
+        ColorModel cm = new IndexColorModel(1, 2, rArr, gArr, bArr, aArr);
+        BufferedImage bi = new BufferedImage(cm, wr, false, null);
+        reorderBytes.lookup(bi.getRaster().getDataBuffer(), bi.getRaster()
+                .getDataBuffer());
 
-		return bi;
-	}
+        return bi;
+    }
 
-	/**
-	 * Reads the width of the overlay - needs to be done separately from the
-	 * primary width even though they are supposed to be identical, as a stand
-	 * alone overlay won't have any width/height except in the overlay tags.
-	 * 
-	 * @param overlayNumber
-	 * @return The width in pixels of the given overlay.  
-	 * @throws IOException
-	 */
-	public static int getOverlayWidth(DicomObject ds, int overlayNumber) {
-		// Zero out the frame index first.
-		overlayNumber &= 0x60FF0000;
-		return ds.getInt(Tag.OverlayColumns | overlayNumber);
-	}
+    /**
+     * Reads the width of the overlay - needs to be done separately from the
+     * primary width even though they are supposed to be identical, as a stand
+     * alone overlay won't have any width/height except in the overlay tags.
+     * 
+     * @param overlayNumber
+     * @return The width in pixels of the given overlay.
+     * @throws IOException
+     */
+    public static int getOverlayWidth(DicomObject ds, int overlayNumber) {
+        // Zero out the frame index first.
+        overlayNumber &= 0x60FF0000;
+        return ds.getInt(Tag.OverlayColumns | overlayNumber);
+    }
 
-	/**
-	 * Reads the height of the overlay - needs to be done separately from the
-	 * primary width even though they are supposed to be identical, as a stand
-	 * alone overlay won't have any width/height except in the overlay tags.
-	 * 
-	 * @param overlayNumber
-	 * @return
-	 */
-	public static int getOverlayHeight(DicomObject ds, int overlayNumber) {
-		overlayNumber &= 0x60FF0000;
-		return ds.getInt(Tag.OverlayRows | overlayNumber);
-	}
-
+    /**
+     * Reads the height of the overlay - needs to be done separately from the
+     * primary width even though they are supposed to be identical, as a stand
+     * alone overlay won't have any width/height except in the overlay tags.
+     * 
+     * @param overlayNumber
+     * @return
+     */
+    public static int getOverlayHeight(DicomObject ds, int overlayNumber) {
+        overlayNumber &= 0x60FF0000;
+        return ds.getInt(Tag.OverlayRows | overlayNumber);
+    }
 
 }
