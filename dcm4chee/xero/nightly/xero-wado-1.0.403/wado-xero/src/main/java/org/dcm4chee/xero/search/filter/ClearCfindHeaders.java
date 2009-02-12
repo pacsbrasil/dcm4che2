7b2d7c36a8aa7314b74ea56c167f9be94e3be6ca
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-02-12 10:55:31 +0000

    Releasing version 1.0.403

---
+++ dcm4chee/xero/nightly/xero-wado-1.0.403/wado-xero/src/main/java/org/dcm4chee/xero/search/filter/ClearCfindHeaders.java
@@ -0,0 +1,74 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Sebastian Mohan, Agfa HealthCare Inc., 
+ * Portions created by the Initial Developer are Copyright (C) 2008
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Sebastian Mohan <sebastian.mnohan@agfa.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.dcm4chee.xero.search.filter;
+
+import java.util.Map;
+
+import org.dcm4chee.xero.metadata.filter.Filter;
+import org.dcm4chee.xero.metadata.filter.FilterItem;
+import org.dcm4chee.xero.search.study.DicomObjectType;
+import org.dcm4chee.xero.search.study.ImageBean;
+import org.dcm4chee.xero.search.study.PatientType;
+import org.dcm4chee.xero.search.study.ResultsBean;
+import org.dcm4chee.xero.search.study.SeriesType;
+import org.dcm4chee.xero.search.study.StudyType;
+
+public class ClearCfindHeaders  implements Filter<ResultsBean>{
+
+   /**
+    * Clear the CFind headers contained in the image, just before
+    * caching the data.
+    */
+   public ResultsBean filter(FilterItem<ResultsBean> filterItem, Map<String, Object> params) {
+      ResultsBean ret = filterItem.callNextFilter(params);
+      if( ret==null ) return null;
+      for(PatientType pt : ret.getPatient()) {
+         for(StudyType st : pt.getStudy()) {
+            for(SeriesType se : st.getSeries() ) {
+               for(DicomObjectType dot : se.getDicomObject()) {
+                  if( !(dot instanceof ImageBean) ) continue;
+                  ImageBean ib = (ImageBean) dot;
+                  ib.setCfindHeader(null);
+               }
+            }
+         }
+      }
+      return ret;
+   }
+
+}
