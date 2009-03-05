6546b3e5e141975faf407fa7f3f48508329824d9
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-03-05 10:22:49 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/AbstractCacheService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/AbstractCacheService.java
@@ -55,35 +55,36 @@ import org.jboss.system.ServiceMBeanSupport;
 
 /**
  * @author franz.willer
- *
- * The MBean to manage the WADO service.
- * <p>
- * This class use WADOSupport for the WADO methods and WADOCache for caching jpg images.
+ * 
+ *         The MBean to manage the WADO service.
+ *         <p>
+ *         This class use WADOSupport for the WADO methods and WADOCache for
+ *         caching jpg images.
  */
 public abstract class AbstractCacheService extends ServiceMBeanSupport {
 
     protected final SchedulerDelegate scheduler = new SchedulerDelegate(this);
-    
-	protected Logger log = Logger.getLogger( getClass().getName() );
-	protected WADOCache cache = null;
-	
-	private static final long MIN_FREE = 1 * FileUtils.MEGA;
-	private static final long MIN_PREF_FREE = 2 * FileUtils.MEGA;
-		
-	private String timerID;
-	
-    private final NotificationListener freeDiskSpaceListener = 
-        new NotificationListener(){
-            public void handleNotification(Notification notif, Object handback) {
-                try {
-                    cache.freeDiskSpace( true );
-                } catch (IOException e) {
-                    log.error("Failed to free disk space: ", e);
-                }
-            }};
-	private long freeDiskSpaceInterval;
-	private Integer freeDiskSpaceListenerID;
-	
+
+    protected Logger log = Logger.getLogger(getClass().getName());
+    protected WADOCache cache = null;
+
+    private static final long MIN_FREE = 1 * FileUtils.MEGA;
+    private static final long MIN_PREF_FREE = 2 * FileUtils.MEGA;
+
+    private String timerID;
+
+    private final NotificationListener freeDiskSpaceListener = new NotificationListener() {
+        public void handleNotification(Notification notif, Object handback) {
+            try {
+                cache.freeDiskSpace(true);
+            } catch (IOException e) {
+                log.error("Failed to free disk space: ", e);
+            }
+        }
+    };
+    private long freeDiskSpaceInterval;
+    private Integer freeDiskSpaceListenerID;
+
     public ObjectName getSchedulerServiceName() {
         return scheduler.getSchedulerServiceName();
     }
@@ -91,149 +92,158 @@ public abstract class AbstractCacheService extends ServiceMBeanSupport {
     public void setSchedulerServiceName(ObjectName schedulerServiceName) {
         scheduler.setSchedulerServiceName(schedulerServiceName);
     }
-            
-	public WADOCache getCache() {
-		return cache;
-	}
-	
-	public void setCacheRoot( String newRoot ) {
-		cache.setCacheRoot( newRoot );
-	}
-	
-	public String getCacheRoot() {
-		return cache.getCacheRoot();
-	}
-	
+
+    public WADOCache getCache() {
+        return cache;
+    }
+
+    public void setCacheRoot(String newRoot) {
+        cache.setCacheRoot(newRoot);
+    }
+
+    public String getCacheRoot() {
+        return cache.getCacheRoot();
+    }
+
     public String getDeleterThresholds() {
         return ((WADOCacheImpl) cache).getDeleterThresholds();
     }
 
     public void setDeleterThresholds(String s) {
-    	((WADOCacheImpl) cache).setDeleterThresholds(s);
+        ((WADOCacheImpl) cache).setDeleterThresholds(s);
     }
 
-	
-	public String showMinFreeSpace() {
-		return FileUtils.formatSize(cache.getMinFreeSpace());
-	}
+    public String showMinFreeSpace() {
+        return FileUtils.formatSize(cache.getMinFreeSpace());
+    }
 
-	public void setPreferredFreeSpace( String minFree ) {
-		((WADOCacheImpl) cache).setPreferredFreeSpace( FileUtils.parseSize(minFree, MIN_PREF_FREE) );
-	}
-	
-	public String getPreferredFreeSpace() {
-		return FileUtils.formatSize(cache.getPreferredFreeSpace());
-	}
-	
-	/**
-	 * Clears the cache.
-	 * <p>
-	 * Remove all files in the cache.
-	 * @throws IOException 
-	 *
-	 */
-	public String clearCache() throws IOException {
-		long before = cache.showFreeSpace();
-		cache.clearCache();
-		return getDeleteResult( before );
-	}
-	
-	private String getDeleteResult( long before ) throws IOException {
-		long after = cache.showFreeSpace();
-		long delta = after - before;
-		StringBuffer sb = new StringBuffer();
-		log.info("getDeleteResult: before:"+before+" , after:"+after+" , delta:"+delta);
-		sb.append(FileUtils.formatSize(delta)).append(" removed!");
-		if ( after < cache.getMinFreeSpace() ) {
-			sb.append(" WARNING: INSUFFICIANT Free disk space! Should be ").append(this.showMinFreeSpace());
-		} else if ( after < cache.getPreferredFreeSpace() ) {
-			sb.append(" WARNING: Free disk space is less than preferred free space (").append(this.getPreferredFreeSpace()).append(")");
-		}
-		return sb.toString();
-	}
+    public void setPreferredFreeSpace(String minFree) {
+        ((WADOCacheImpl) cache).setPreferredFreeSpace(FileUtils.parseSize(
+                minFree, MIN_PREF_FREE));
+    }
 
-	/**
-	 * Get the timer interval for scheduling freeDiskSpace.
-	 * <p>
-	 * The return value is either 'NEVER' or &lt;interval&gt;&lt;unit&gt; where interval is a 
-	 * natural number and unit one of 'w', 'd', 'h', 'm' or 's' representing week, day, hour,
-	 * minute or second.
-	 * 
-	 * @return A formatted String representing the timer interval.
-	 */
+    public String getPreferredFreeSpace() {
+        return FileUtils.formatSize(cache.getPreferredFreeSpace());
+    }
+
+    /**
+     * Clears the cache.
+     * <p>
+     * Remove all files in the cache.
+     * 
+     * @throws IOException
+     * 
+     */
+    public String clearCache() throws IOException {
+        long before = cache.showFreeSpace();
+        cache.clearCache();
+        return getDeleteResult(before);
+    }
+
+    private String getDeleteResult(long before) throws IOException {
+        long after = cache.showFreeSpace();
+        long delta = after - before;
+        StringBuffer sb = new StringBuffer();
+        log.info("getDeleteResult: before:" + before + " , after:" + after
+                + " , delta:" + delta);
+        sb.append(FileUtils.formatSize(delta)).append(" removed!");
+        if (after < cache.getMinFreeSpace()) {
+            sb.append(" WARNING: INSUFFICIANT Free disk space! Should be ")
+                    .append(this.showMinFreeSpace());
+        } else if (after < cache.getPreferredFreeSpace()) {
+            sb
+                    .append(
+                            " WARNING: Free disk space is less than preferred free space (")
+                    .append(this.getPreferredFreeSpace()).append(")");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Get the timer interval for scheduling freeDiskSpace.
+     * <p>
+     * The return value is either 'NEVER' or &lt;interval&gt;&lt;unit&gt; where
+     * interval is a natural number and unit one of 'w', 'd', 'h', 'm' or 's'
+     * representing week, day, hour, minute or second.
+     * 
+     * @return A formatted String representing the timer interval.
+     */
     public final String getFreeDiskSpaceInterval() {
         return RetryIntervalls.formatIntervalZeroAsNever(freeDiskSpaceInterval);
     }
-    
+
     /**
-	 * Set the timer interval for scheduling freeDiskSpace.
-	 * <p>
-	 * The parameter value is either 'NEVER' or &lt;interval&gt;&lt;unit&gt; where interval is a 
-	 * natural number and unit one of 'w', 'd', 'h', 'm' or 's' representing week, day, hour,
-	 * minute or second.
+     * Set the timer interval for scheduling freeDiskSpace.
+     * <p>
+     * The parameter value is either 'NEVER' or &lt;interval&gt;&lt;unit&gt;
+     * where interval is a natural number and unit one of 'w', 'd', 'h', 'm' or
+     * 's' representing week, day, hour, minute or second.
      * 
-     * @param interval Timer interval as formatted String.
-     * @throws Exception 
+     * @param interval
+     *            Timer interval as formatted String.
+     * @throws Exception
      */
     public void setFreeDiskSpaceInterval(String interval) throws Exception {
         freeDiskSpaceInterval = RetryIntervalls.parseIntervalOrNever(interval);
         if (getState() == STARTED) {
             scheduler.stopScheduler(timerID, freeDiskSpaceListenerID,
-            		freeDiskSpaceListener);
+                    freeDiskSpaceListener);
             freeDiskSpaceListenerID = scheduler.startScheduler(timerID,
-            		freeDiskSpaceInterval, freeDiskSpaceListener);
+                    freeDiskSpaceInterval, freeDiskSpaceListener);
         }
     }
-	
-	/**
-	 * Cleans the cache if necessary.
-	 * @throws IOException 
-	 *
-	 */
-	public String freeDiskSpace() throws IOException {
-		long before = cache.showFreeSpace();
-		cache.freeDiskSpace( false ); //cleans the cache and wait until clean process is done. (not in background)
-		return getDeleteResult( before );
-	}
-	
-	/**
-	 * Returns the available space of the cache drive.
-	 * <p>
-	 * 
-	 * @return
-	 * @throws IOException 
-	 */
-	public String showFreeSpace() throws IOException {
-		return FileUtils.formatSize( cache.showFreeSpace() );
-	}
-	
-	/**
-	 * Start scheduler for freeDiskSpace.
-	 * <p>
-	 * This queue is used to receive media creation request from scheduler or web interface.
-	 */
-    protected void startService() throws Exception {
-        freeDiskSpaceListenerID = scheduler.startScheduler(timerID, 
-        		freeDiskSpaceInterval, freeDiskSpaceListener);
+
+    /**
+     * Cleans the cache if necessary.
+     * 
+     * @throws IOException
+     * 
+     */
+    public String freeDiskSpace() throws IOException {
+        long before = cache.showFreeSpace();
+        cache.freeDiskSpace(false); // cleans the cache and wait until clean
+                                    // process is done. (not in background)
+        return getDeleteResult(before);
     }
 
-	/**
-	 * Stop scheduler for freeDiskSpace.
-	 * 
-	 */
+    /**
+     * Returns the available space of the cache drive.
+     * <p>
+     * 
+     * @return
+     * @throws IOException
+     */
+    public String showFreeSpace() throws IOException {
+        return FileUtils.formatSize(cache.showFreeSpace());
+    }
+
+    /**
+     * Start scheduler for freeDiskSpace.
+     * <p>
+     * This queue is used to receive media creation request from scheduler or
+     * web interface.
+     */
+    protected void startService() throws Exception {
+        freeDiskSpaceListenerID = scheduler.startScheduler(timerID,
+                freeDiskSpaceInterval, freeDiskSpaceListener);
+    }
+
+    /**
+     * Stop scheduler for freeDiskSpace.
+     * 
+     */
     protected void stopService() throws Exception {
         scheduler.stopScheduler(timerID, freeDiskSpaceListenerID,
-        		freeDiskSpaceListener);
+                freeDiskSpaceListener);
         super.stopService();
     }
 
-	public String getTimerID() {
-		return timerID;
-	}
+    public String getTimerID() {
+        return timerID;
+    }
 
-	public void setTimerID(String timerID) {
-		this.timerID = timerID;
-	}
-	
+    public void setTimerID(String timerID) {
+        this.timerID = timerID;
+    }
 
 }
