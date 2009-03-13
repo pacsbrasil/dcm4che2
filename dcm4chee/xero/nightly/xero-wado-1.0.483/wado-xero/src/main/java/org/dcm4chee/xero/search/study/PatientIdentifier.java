679acb55672cda4322be9ad61a6a85b0894d3ae0
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-03-13 00:12:00 +0000

    Releasing version 1.0.483

---
+++ dcm4chee/xero/nightly/xero-wado-1.0.483/wado-xero/src/main/java/org/dcm4chee/xero/search/study/PatientIdentifier.java
@@ -0,0 +1,100 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Bill Wallace, Agfa HealthCare Inc., 
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Bill Wallace <bill.wallace@agfa.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.dcm4chee.xero.search.study;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** This class handles identification of patients */
+public class PatientIdentifier {
+   private static final Logger log = LoggerFactory.getLogger(PatientIdentifier.class);
+	String patientID;
+
+	/** Identify the patient via the patient ID */
+	public PatientIdentifier(DicomObject data) {
+		// Read the patient ID and use it as the only comparison for now.
+		this.patientID = data.getString(Tag.PatientID);
+		if (this.patientID == null) {
+			//could be null for anonymized study, hashCode() won't like
+			this.patientID = data.getString(Tag.OtherPatientIDs);
+			if( this.patientID == null ) {
+			   this.patientID = PatientBean.sanitizeString(data.getString(Tag.PatientName));
+			}
+			if( this.patientID==null ) this.patientID="";
+			log.warn("Unable to find a patient ID - probably an anonymized study, choose ID={} as an alternate ID", this.patientID);
+		}
+		// Add the domain to the identifier as a URL type identifier.
+		String idDomain = data.getString(Tag.IssuerOfPatientID);
+		if( idDomain!=null && idDomain.length()>0 && !idDomain.equals(this.patientID) ) this.patientID = idDomain+":"+this.patientID;
+	}
+	
+	/** Empty constructor for use by JAXB - not actually used anywhere */
+	public PatientIdentifier() { patientID=""; }
+
+	/** Create an identification for a patient, from a string identifier */
+	public PatientIdentifier(String patientIdentifier) {
+		this.patientID = patientIdentifier;
+	}
+
+    /** Create an identification for a patient from the issuer and the ID */
+    public PatientIdentifier(String issuer, String patientID) {
+        this.patientID = issuer+":"+patientID;
+    }
+    
+	@Override
+	public boolean equals(Object obj) {
+		return (obj instanceof PatientIdentifier) && patientID.equals(((PatientIdentifier) obj).patientID);
+	}
+
+	@Override
+	public int hashCode() {
+		return patientID.hashCode();
+	}
+	
+	/** The string representation of the patient identifier is just the PatientID prefixed
+	 * by a pid:// url identifier, but in the future it can be more complex, and/or use different
+	 * syntaxes.
+	 */
+	public String toString() {
+		return this.patientID;
+	}
+	
+	
+}
