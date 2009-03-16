8280f9db5198f7247b752b8aceb813b170f09439
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-03-16 19:05:17 +0000

    [artf12258] Added JSON retrieval capability.

--- dcm4chee/xero-util/trunk/metadata/src/main/java/org/dcm4chee/xero/metadata/servlet/JaxbFilter.java
+++ dcm4chee/xero-util/trunk/metadata/src/main/java/org/dcm4chee/xero/metadata/servlet/JaxbFilter.java
@@ -39,6 +39,7 @@ package org.dcm4chee.xero.metadata.servlet;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -49,6 +50,9 @@ import javax.xml.bind.Marshaller;
 import org.dcm4chee.xero.metadata.MetaData;
 import org.dcm4chee.xero.metadata.filter.Filter;
 import org.dcm4chee.xero.metadata.filter.FilterItem;
+import org.dcm4chee.xero.metadata.filter.FilterUtil;
+import org.dcm4chee.xero.metadata.json.GroupedElementJSONEventWriter;
+import org.dcm4chee.xero.metadata.json.JSONPrettyPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -107,6 +111,44 @@ public class JaxbFilter implements Filter<ServletResponseItem>
 		}
 	}
 
+	/**
+	 * Output JSON to clients using the indicated JAXBProvider.
+	 * @author Andrew Cowan (andrew.cowan@agfa.com)
+	 */
+	static class JSONServletResponseItem implements ServletResponseItem
+	{
+	   private final Object data;
+      private final JAXBProvider provider;
+      private final boolean prettyPrint;
+
+      JSONServletResponseItem(Object data, JAXBProvider provider, boolean prettyPrint)
+      {
+         this.data = data;
+         this.provider = provider;
+         this.prettyPrint = prettyPrint;
+      }
+      
+      public void writeResponse(HttpServletRequest request, HttpServletResponse response) throws IOException
+      {
+         try {
+            Marshaller m = provider.createMarshaller();
+            response.setContentType("text/javascript");
+            
+            Writer writer = response.getWriter();
+            if(prettyPrint)
+               writer = new JSONPrettyPrinter(writer);
+            
+            GroupedElementJSONEventWriter jsonWriter = new GroupedElementJSONEventWriter(writer);
+            m.marshal(data, jsonWriter);
+         } catch (JAXBException e) {
+            throw new RuntimeException(e);
+         } catch (IOException e) {
+            throw new RuntimeException(e);
+         }
+      }
+	}
+	
+
 	/** Convert the object returned by the source filter (if any) into a servlet response item that 
 	 * serializes it as XML.
 	 * @param nextFilter is called to get the item to convert to XML.
@@ -116,7 +158,16 @@ public class JaxbFilter implements Filter<ServletResponseItem>
 	public ServletResponseItem filter(FilterItem<ServletResponseItem> nextFilter, Map<String, Object> params) {
 		Object data = sourceFilter.filter(null, params);
 		if( data==null ) return nextFilter.callNextFilter(params);
+		
+		String contentType = FilterUtil.getString(params, "contentType");
+		boolean pretty = FilterUtil.getBoolean(params, "pretty", false);
+		
+		if(contentType == null || contentType.equalsIgnoreCase("text/xml"))
 		return new JaxbServletResponseItem(data,provider);
+		else if(contentType.contains("javascript"))
+		   return new JSONServletResponseItem(data,provider,pretty);
+		else
+		   throw new RuntimeException("Unknown content type '"+contentType+"'");
 	}
 
 	/** Returns the source filter used to get the object data to render */
