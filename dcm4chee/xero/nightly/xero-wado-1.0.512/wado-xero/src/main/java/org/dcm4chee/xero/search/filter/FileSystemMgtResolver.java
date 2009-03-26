a51d56c90416ed4845ac364e9d4139caa367851b
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-03-26 00:13:40 +0000

    Releasing version 1.0.512

---
+++ dcm4chee/xero/nightly/xero-wado-1.0.512/wado-xero/src/main/java/org/dcm4chee/xero/search/filter/FileSystemMgtResolver.java
@@ -0,0 +1,84 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * David Smith, Laura Peters, Agfa HealthCare Inc., 
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * David Smith <david.smith@agfa.com>
+ * Laura Peters <laura.peters@agfa.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.dcm4chee.xero.search.filter;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBHome;
+import javax.ejb.EJBObject;
+import javax.ejb.FinderException;
+import javax.naming.NameNotFoundException;
+
+import org.dcm4chex.archive.ejb.interfaces.FileDTO;
+import org.dcm4chex.archive.ejb.interfaces.FileSystemMgt2;
+import org.dcm4chex.archive.ejb.interfaces.FileSystemMgt2Home;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class is used to get the file path(s) from the correct FileSystemMgt
+ * EJB.
+ * 
+ * @author dsmith1547
+ * 
+ */
+public class FileSystemMgtResolver {
+    private static final Logger log = LoggerFactory.getLogger(FileSystemMgtResolver.class);
+
+    public static FileDTO[] getDTOs(String host, String port, String instanceUID) throws Exception, CreateException,
+            RemoteException, FinderException {
+         try {
+             EJBHome home = (EJBHome) EJBServiceLocator.getInstance().getRemoteHome(host, port, "ejb/FileSystemMgt2",
+                     FileSystemMgt2Home.class);
+
+             EJBObject fileMgt = ((FileSystemMgt2Home) home).create();
+             FileDTO[] dtos = ((FileSystemMgt2) fileMgt).getFilesOfInstance(instanceUID);
+             return dtos;
+         } catch (NoClassDefFoundError e) {
+             log.warn("Using old file system management.");
+         } catch(ClassNotFoundException e) {
+             log.warn("Using old file system management.");
+         } catch(NameNotFoundException e) {
+             log.warn("Using old file system management.");
+         }
+            
+        return null;
+    }
+}
