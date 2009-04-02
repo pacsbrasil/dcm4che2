30083e0ef618153907d4c1571a7a3eab83e62a26
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-04-02 08:44:26 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-rid/src/java/org/dcm4chex/rid/web/RequestObjectFactory.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-rid/src/java/org/dcm4chex/rid/web/RequestObjectFactory.java
@@ -45,36 +45,36 @@ import org.dcm4chex.rid.common.BasicRequestObject;
 
 /**
  * @author franz.willer
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
  */
 public class RequestObjectFactory {
 
-	/**
-	 * Returns an request object for given hjttp request.
-	 * <p>
-	 * Returns null, if the request is not á IHE RID request.
-	 * 
-	 * @param request The http request.
-	 * 
-	 * @return
-	 */
-	public static BasicRequestObject getRequestObject( HttpServletRequest request ) {
-		BasicRequestObject reqObj = null; 
-		String reqType = request.getParameter("requestType");
-		if ( reqType == null ) {
-			reqType = request.getParameter("RT");
-		}
-		if ( reqType == null ) return null; //wrong URL
-		if ( "DOCUMENT".equals( reqType )) {
-			return new RIDDocumentRequestObject( request );
-		} else if ( reqType.startsWith( "SUMMARY" ) ) {
-			return new RIDInfoRequestObject( request );
-		} else if ( reqType.startsWith( "LIST" ) ) { 
-			return new RIDInfoRequestObject( request );
-		} else {
-			return null; //wrong URL
-		}
-	}
+    /**
+     * Returns an request object for given hjttp request.
+     * <p>
+     * Returns null, if the request is not IHE RID request.
+     * 
+     * @param request
+     *            The http request.
+     * 
+     * @return
+     */
+    public static BasicRequestObject getRequestObject(HttpServletRequest request) {
+        BasicRequestObject reqObj = null;
+        String reqType = request.getParameter("requestType");
+        if (reqType == null) {
+            reqType = request.getParameter("RT");
+        }
+        if (reqType == null)
+            return null; // wrong URL
+        if ("DOCUMENT".equals(reqType)) {
+            return new RIDDocumentRequestObject(request);
+        } else if (reqType.startsWith("SUMMARY")) {
+            return new RIDInfoRequestObject(request);
+        } else if (reqType.startsWith("LIST")) {
+            return new RIDInfoRequestObject(request);
+        } else {
+            return null; // wrong URL
+        }
+    }
 }
