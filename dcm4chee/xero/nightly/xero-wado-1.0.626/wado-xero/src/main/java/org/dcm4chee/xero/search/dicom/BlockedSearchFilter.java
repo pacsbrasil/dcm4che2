1e3ff4c31d4679c067fb70b0d532329d5a9de97e
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-05-11 13:13:29 +0000

    Releasing version 1.0.626

---
+++ dcm4chee/xero/nightly/xero-wado-1.0.626/wado-xero/src/main/java/org/dcm4chee/xero/search/dicom/BlockedSearchFilter.java
@@ -0,0 +1,95 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Bill Wallace, Agfa HealthCare Inc., 
+ * Portions created by the Initial Developer are Copyright (C) 2008
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Bill Wallace <bill.wallace@agfa.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.dcm4chee.xero.search.dicom;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.dcm4chee.xero.metadata.MetaDataBean;
+import org.dcm4chee.xero.metadata.MetaDataUser;
+import org.dcm4chee.xero.metadata.filter.Filter;
+import org.dcm4chee.xero.metadata.filter.FilterItem;
+import org.dcm4chee.xero.metadata.filter.FilterUtil;
+import org.dcm4chee.xero.metadata.servlet.ServletResponseItem;
+import org.dcm4chee.xero.search.ResultFromDicom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Returns a directory record sequence blocked return DICOM object.
+ * Be careful of the return data - it may contain nested file header information, and that may mean that
+ * it is in turn encoded individually, eg deflated on a individual return item basis, and with custom
+ * specific character set per block.  
+ * 
+ * @author bwallace
+ *
+ */
+public class BlockedSearchFilter implements Filter<ServletResponseItem>, MetaDataUser {
+   private static final Logger log = LoggerFactory.getLogger(BlockedSearchFilter.class);
+   public static final String QUERY_LEVEL="level";
+   
+   Map<String,Filter<ResultFromDicom>> childFilters = new HashMap<String,Filter<ResultFromDicom>>(6);
+   
+   /**
+    * Returns a DICOM Directory 
+    */
+   public ServletResponseItem filter(FilterItem<ServletResponseItem> filterItem, Map<String, Object> params) {
+	  log.debug("Doing a blocked servlet response to stream the C-Find data to the client.");
+	  String level = FilterUtil.getString(params,QUERY_LEVEL,"study");
+
+	  Filter<ResultFromDicom> filter = childFilters.get(level);
+	  if( filter==null ) {
+	     log.warn("Unable to find CFIND query results - likely the level is undefined:{}",level);
+	     return null;
+	  }
+	  BlockedServletResponseItem ret = new BlockedServletResponseItem(filter, params);
+	  ret.setFilename("cfind"+level+".dcm");
+	  return ret;
+   }
+
+   /** Read all the available child filters */
+	@SuppressWarnings("unchecked")
+   public void setMetaData(MetaDataBean metaDataBean) {
+		for(Map.Entry<String,Object> me : metaDataBean.entrySet()) {
+			Object v = ((MetaDataBean) me.getValue()).getValue();
+			if( v instanceof Filter ) {
+				childFilters.put(me.getKey(), (Filter<ResultFromDicom>) v);
+			}
+		}
+   }
+}
