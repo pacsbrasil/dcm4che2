5da808debe1699826f1a5a39b5a2ad00099f14dc
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-05-12 14:55:25 +0000

    Releasing version 1.0.630

---
+++ dcm4chee/xero/nightly/xero-wado-1.0.630/wado-xero/src/main/java/org/dcm4chee/xero/search/filter/SortImageFilter.java
@@ -0,0 +1,148 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Bill Wallace, Agfa HealthCare Inc., 
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Bill Wallace <bill.wallace@agfa.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.dcm4chee.xero.search.filter;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.dcm4chee.xero.metadata.filter.Filter;
+import org.dcm4chee.xero.metadata.filter.FilterItem;
+import org.dcm4chee.xero.search.study.DicomObjectComparator;
+import org.dcm4chee.xero.search.study.DicomObjectType;
+import org.dcm4chee.xero.search.study.ImageBean;
+import org.dcm4chee.xero.search.study.ImageBeanMultiFrame;
+import org.dcm4chee.xero.search.study.PatientType;
+import org.dcm4chee.xero.search.study.ResultsBean;
+import org.dcm4chee.xero.search.study.SeriesComparator;
+import org.dcm4chee.xero.search.study.SeriesType;
+import org.dcm4chee.xero.search.study.StudyType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class sorts the images by instance number, and adds the position
+ * information into the image level data.
+ * 
+ * @author bwallace
+ * 
+ */
+public class SortImageFilter implements Filter<ResultsBean> {
+   private static final Logger log = LoggerFactory.getLogger(SortImageFilter.class);
+
+   /**
+     * Sort the returned results, by series number and by image number.
+     * 
+     * @param filterItem
+     *            contains information about the other filters, including the
+     *            next filter.
+     * @param params
+     *            are the parameters to use to determine the series.
+     * @return a patient object with sorted series and images. Only
+     *         series/images matching the params queries will be included.
+     */
+   public ResultsBean filter(FilterItem<ResultsBean> filterItem, Map<String, Object> params) {
+	  ResultsBean results = filterItem.callNextFilter(params);
+	  if (results == null)
+		 return null;
+	  log.debug("Sorting results.");
+	  return sortSeriesAndImages(results);
+   }
+
+   /**
+     * Sorts the series and images, and adds in positional information. TODO:
+     * This is supposed to take into account multi-frame ordering and grouping,
+     * but some of the enhanced CT/MR ordering may not be supported yet.
+     * 
+     * @param results
+     * @return
+     */
+   public static ResultsBean sortSeriesAndImages(ResultsBean results) {
+	  List<PatientType> patients = results.getPatient();
+	  for (PatientType patient : patients) {
+		 List<StudyType> studies = patient.getStudy();
+		 for (StudyType study : studies) {
+			sortSeries(study.getSeries());
+		 }
+	  }
+	  return results;
+   }
+
+   /**
+     * Sorts the series items, and then calls the sort on the image items.
+     * 
+     * @param series
+     *            are a list of series objects to sort by series number
+     */
+   public static void sortSeries(List<SeriesType> series) {
+	  Collections.sort(series, new SeriesComparator());
+	  for (SeriesType aSeries : series) {
+		 List<DicomObjectType> dicomObjects = aSeries.getDicomObject();
+		 if (dicomObjects != null && dicomObjects.size() > 0) {
+			sortImages(dicomObjects);
+			DicomObjectType lastDot = dicomObjects.get(dicomObjects.size() - 1);
+			if (lastDot instanceof ImageBean) {
+			   ImageBean image = (ImageBean) lastDot;
+			   aSeries.setViewable(lastDot.getPosition() + image.getNumberOfFrames());
+			}
+		 }
+	  }
+   }
+
+   /**
+     * Sorts the image items TODO Currently assumes that multi-frame images are
+     * fully sequential.
+     * 
+     * @param images
+     *            are the dicom objects to sort
+     */
+   public static void sortImages(List<DicomObjectType> images) {
+	  Collections.sort(images, new DicomObjectComparator());
+	  int position = 0;
+	  for (DicomObjectType dot : images) {
+		 if (dot instanceof ImageBeanMultiFrame) {
+			dot.setPosition(position);
+			ImageBeanMultiFrame image = (ImageBeanMultiFrame) dot;
+			position += image.getNumberOfFrames();
+		 } else if (dot instanceof ImageBean) {
+			dot.setPosition(position++);
+		 }
+	  }
+   }
+
+}
---
+++ dcm4chee/xero/nightly/xero-wado-1.0.630/wado-xero/src/main/java/org/dcm4chee/xero/search/study/GspsBean.java
@@ -0,0 +1,188 @@
+package org.dcm4chee.xero.search.study;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlAnyAttribute;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.namespace.QName;
+
+import static org.dcm4chee.xero.search.study.PatientBean.sanitizeString;
+
+import org.dcm4che2.data.DicomElement;
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.dcm4chee.xero.search.LocalModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Represents a GSPS (Presentation State) DICOM Object, potentially including the child markups and text annotations.
+ * @author bwallace
+ */
+public class GspsBean extends GspsType implements LocalModel<String>, DicomObjectInterface {
+	static Logger log = LoggerFactory.getLogger(GspsBean.class.getName());
+
+	Set<String> referencedSOPInstance;
+	
+	
+	@XmlTransient
+	SeriesBean series;
+	
+	@XmlTransient
+	MacroItems macroItems;
+	
+	/** Constructor for JAXB 
+	 * @deprecated
+	 */
+	public GspsBean() {
+	   
+	}
+	
+    public GspsBean(SeriesBean series) {
+        this.series = series;
+    }
+
+        /** Create a GSPS Bean object */
+	public GspsBean(SeriesBean series, DicomObject dobj) {
+	    this.series = series;
+		addResult(dobj);
+	}
+
+	/**
+	 * Initialize the image level attributes by copying the DicomObject's image
+	 * level data for Columns, Rows, SOP Instance UID and Instance Number.
+	 * 
+	 * @param dcmobj
+	 *            to copy image level data into this from.
+	 */
+	public void addResult(DicomObject dcmobj) {
+	    setObjectUID(dcmobj.getString(Tag.SOPInstanceUID));
+		setContentLabel(sanitizeString(dcmobj.getString(Tag.ContentLabel)));
+		Date date = null;
+		try {
+			date = dcmobj.getDate(Tag.PresentationCreationDate,
+					Tag.PresentationCreationTime);
+		} catch (NumberFormatException nfe) {
+			log.warn("Illegal presentation date or time:" + nfe);
+		}
+		if (date == null) {
+			try {
+				date = dcmobj.getDate(Tag.ContentDate, Tag.ContentTime);
+			} catch (NumberFormatException nfe) {
+				log.warn("Illegal presentation date or time:" + nfe);
+			}
+		}
+		if (date == null) {
+			log.warn("GSPS presentation and content dates both null for "
+					+ getObjectUID());
+			date = new Date(0);
+		}
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		setPresentationDateTime(PatientBean.datatypeFactory
+				.newXMLGregorianCalendar(cal));
+		initReferencedImages(dcmobj);
+	}
+
+	private void initReferencedImages(DicomObject dcmobj) {
+		// Initialize the referenced instances.
+		DicomElement sqSeries = dcmobj.get(Tag.ReferencedSeriesSequence);
+		if (sqSeries != null && sqSeries.hasItems()) {
+			referencedSOPInstance = new HashSet<String>();
+			int serCount = sqSeries.countItems();
+			for (int i = 0; i < serCount; i++) {
+				DicomObject seriesDcm = sqSeries.getDicomObject(i);
+				DicomElement sqImage = seriesDcm
+						.get(Tag.ReferencedImageSequence);
+				if (sqImage == null || !sqImage.hasItems()) {
+					log
+							.warn("GSPS object "
+									+ getObjectUID()
+									+ " has no referenced images for one of the series.");
+					continue;
+				}
+				int imgCount = sqImage.countItems();
+				for (int j = 0; j < imgCount; j++) {
+					DicomObject imageDcm = sqImage.getDicomObject(j);
+					String uid = imageDcm
+							.getString(Tag.ReferencedSOPInstanceUID);
+					if (uid == null) {
+						log.warn("GSPS object " + getObjectUID()
+								+ " has a null SOP instance reference.");
+						continue;
+					}
+					referencedSOPInstance.add(uid);
+				}
+			}
+		} else {
+			log.warn("GSPS object " + getObjectUID()
+					+ " has no referenced series.");
+		}
+	}
+
+	/**
+	 * GspsBean's are always considered empty - they don't have any
+	 * customizations yet.
+	 */
+	public boolean clearEmpty() {
+		return true;
+	}
+
+	/** The SOP Instance is the ID for a GSPS object */
+	public String getId() {
+		return getObjectUID();
+	}
+
+	/** Get the set of object uid's that this GSPS references */
+	public Set<String> getReferencedSOPInstance() {
+		return referencedSOPInstance;
+	}
+
+   public SeriesBean getSeriesBean() {
+      return series;
+   }
+
+   /** Gets additional attributes and child elements defined in other objects */
+   public MacroItems getMacroItems() {
+      if (macroItems == null)
+         macroItems = new MacroItems();
+      return macroItems;
+   }
+   
+   /** Get the attributes from the macro items that are included in this object. */
+   @XmlAnyAttribute
+   public Map<QName, String> getOtherAttributes() {
+      Map<QName, String> ret = null;
+      if (macroItems != null)
+         ret = macroItems.getAnyAttributes();
+      if( log.isDebugEnabled() ) log.debug("Getting other attributes="+ret);
+      return ret;
+   }
+   
+   @XmlAnyElement(lax=true)
+   public List<Object> getOtherElements() {
+      if( this.macroItems==null ) return null;
+      return this.macroItems.getOtherElements();
+   }
+
+   /** Adds the given macro to this objects set of macro items */
+   public void addMacro(Macro m) {
+      clearMacro(m.getClass());
+      getMacroItems().addMacro(m);
+   }
+   
+   /** Clears the macro from this class, and any children of this class.
+    * This uses getMacroItems rather than direct access to the variable in order to work with both
+    * frame and non-framed images.  It still needs extra work for the multi-frame single object, however. */
+   public void clearMacro(Class<? extends Macro> clazz) {
+      Macro item = getMacroItems().findMacro(clazz);
+      if (item != null)
+         getMacroItems().removeMacro(item);
+   }
+}
