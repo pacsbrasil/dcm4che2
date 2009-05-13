9dd5325d12f5e54f8025b2e36751055103bd5ecd
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-05-13 09:10:41 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScp.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScp.java
@@ -60,213 +60,218 @@ import org.jboss.logging.Logger;
 
 class StudyMgtScp extends DcmServiceBase {
 
-	final StudyMgtScpService service;
-	private Logger log;
-	private boolean ignoreDeleteFailed;
+    final StudyMgtScpService service;
+    private Logger log;
+    private boolean ignoreDeleteFailed;
 
-	public StudyMgtScp(StudyMgtScpService service) {
-		this.service = service;
-		this.log = service.getLog();
-	}
-
-	/**
-	 * @return Returns the ignoreDeleteFailed.
-	 */
-	public boolean isIgnoreDeleteFailed() {
-		return ignoreDeleteFailed;
-	}
-	/**
-	 * @param ignoreDeleteFailed The ignoreDeleteFailed to set.
-	 */
-	public void setIgnoreDeleteFailed(boolean ignoreDeleteFailed) {
-		this.ignoreDeleteFailed = ignoreDeleteFailed;
-	}
-	
-    private StudyMgtHome getStudyMgtHome() throws HomeFactoryException {
-        return (StudyMgtHome) EJBHomeFactory.getFactory().lookup(
-				StudyMgtHome.class, StudyMgtHome.JNDI_NAME);
+    public StudyMgtScp(StudyMgtScpService service) {
+        this.service = service;
+        this.log = service.getLog();
     }
 
-	
-	protected Dataset doNAction(ActiveAssociation assoc, Dimse rq, 
-			Command rspCmd) throws IOException, DcmServiceException {
-		Command cmd = rq.getCommand();
-		int actionTypeID = cmd.getInt(Tags.ActionTypeID, -1);
-		String iuid = cmd.getRequestedSOPInstanceUID();
-		Dataset ds = rq.getDataset();
-		if ( log.isDebugEnabled() ) {
-			log.debug("Received N-Action cmd with ds:");log.debug(ds);
-		}
-		checkStudyIuid(iuid, ds);
-		try {
-			StudyMgt stymgt = getStudyMgtHome().create();
-			try {
-				switch (actionTypeID) {
-				case 1:
-					stymgt.deleteSeries(toSeriesIuids(ds));
-					break;
-				case 2:
-					stymgt.deleteInstances(toSopIuids(ds));
-					break;
-				}
-			} finally {
-				try {
-					stymgt.remove();
-				} catch (Exception e) {
-					log.warn("Failed to remove StudyMgt Session Bean", e);
-				}
-			}
-		} catch (DcmServiceException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new DcmServiceException(Status.ProcessingFailure, e);
-		}
-		service.sendStudyMgtNotification(assoc, Command.N_ACTION_RQ, 
-				actionTypeID, iuid, ds);
-		return null;
-	}
+    /**
+     * @return Returns the ignoreDeleteFailed.
+     */
+    public boolean isIgnoreDeleteFailed() {
+        return ignoreDeleteFailed;
+    }
 
-	private String[] toSeriesIuids(Dataset ds) throws DcmServiceException {
-		return toIuids(ds, true);
-	}
-	
-	private String[] toSopIuids(Dataset ds) throws DcmServiceException {
-		return toIuids(ds, false);
-	}
-	
-	private String[] toIuids(Dataset ds, boolean seriesIuids)
-			throws DcmServiceException {
-		List iuids = new ArrayList();
-		DcmElement sersq = ds.get(Tags.RefSeriesSeq);
-		if (sersq == null) {
-			throw new DcmServiceException(Status.MissingAttribute,
-					"Missing Referenced Series Seq.");
-		}
-		for (int i = 0, n = sersq.countItems(); i < n; ++i) {
-			Dataset ser = sersq.getItem(i);
-			String siuid = ser.getString(Tags.SeriesInstanceUID);
-			if (siuid == null) {
-				throw new DcmServiceException(Status.MissingAttribute,
-						"Missing Series Instance UID");
-			}
-			if (seriesIuids) {
-				iuids.add(siuid);				
-			} else {
-				DcmElement sops = ser.get(Tags.RefSOPSeq);
-				if (sops == null) {
-					throw new DcmServiceException(Status.MissingAttribute,
-							"Missing Referenced SOP Seq.");
-				}
-				for (int j = 0, m = sops.countItems(); j < m; ++j) {
-					Dataset sop = sops.getItem(i);
-					String iuid = sop.getString(Tags.RefSOPInstanceUID);
-					if (iuid == null) {
-						throw new DcmServiceException(Status.MissingAttribute,
-								"Missing Referenced SOP Instance UID");
-					}
-					iuids.add(iuid);
-				}
-			}
-		}
-		return (String[]) iuids.toArray(new String[iuids.size()]);
-	}
+    /**
+     * @param ignoreDeleteFailed
+     *            The ignoreDeleteFailed to set.
+     */
+    public void setIgnoreDeleteFailed(boolean ignoreDeleteFailed) {
+        this.ignoreDeleteFailed = ignoreDeleteFailed;
+    }
 
-	protected Dataset doNCreate(ActiveAssociation assoc, Dimse rq, 
-			Command rspCmd) throws IOException, DcmServiceException {
-		Command cmd = rq.getCommand();
-		String iuid = cmd.getAffectedSOPInstanceUID();
-		Dataset ds = rq.getDataset();
-		String suid = ds.getString(Tags.StudyInstanceUID);
-		if (suid == null) {
-			throw new DcmServiceException(Status.MissingAttribute,
-					"Missing Study Instance UID");
-		}
-		if (iuid == null) {
-			rspCmd.putUI(Tags.AffectedSOPInstanceUID, iuid = suid);
-		} else {
-			checkStudyIuid(iuid, ds);
-		}
-		try {
-			StudyMgt stymgt = getStudyMgtHome().create();
-			try {
-				stymgt.createStudy(ds, service.patientMatching());
-			} finally {
-				try {
-					stymgt.remove();
-				} catch (Exception e) {
-					log.warn("Failed to remove StudyMgt Session Bean", e);
-				}
-			}
-		} catch (DcmServiceException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new DcmServiceException(Status.ProcessingFailure, e);
-		}
-		service.sendStudyMgtNotification(assoc, Command.N_CREATE_RQ, 0, iuid, ds);
-		return null;
-	}
+    private StudyMgtHome getStudyMgtHome() throws HomeFactoryException {
+        return (StudyMgtHome) EJBHomeFactory.getFactory().lookup(
+                StudyMgtHome.class, StudyMgtHome.JNDI_NAME);
+    }
 
-	protected Dataset doNDelete(ActiveAssociation assoc, Dimse rq, 
-			Command rspCmd) throws IOException, DcmServiceException {
-		Command cmd = rq.getCommand();
-		String iuid = cmd.getRequestedSOPInstanceUID();
-		Dataset ds = rq.getDataset(); // should be null
-		try {
-			StudyMgt stymgt = getStudyMgtHome().create();
-			try {
-				stymgt.deleteStudy(iuid);
-			} catch ( DcmServiceException e ) {
-				if ( ! (ignoreDeleteFailed && e.getStatus() == Status.NoSuchSOPClass ) ) {
-					throw e;
-				}
-			} finally {
-				try {
-					stymgt.remove();
-				} catch (Exception e) {
-					log.warn("Failed to remove StudyMgt Session Bean", e);
-				}
-			}
-		} catch (DcmServiceException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new DcmServiceException(Status.ProcessingFailure, e);
-		}
-		service.sendStudyMgtNotification(assoc, Command.N_DELETE_RQ, 0, iuid, null);
-		return null;
-	}
+    protected Dataset doNAction(ActiveAssociation assoc, Dimse rq,
+            Command rspCmd) throws IOException, DcmServiceException {
+        Command cmd = rq.getCommand();
+        int actionTypeID = cmd.getInt(Tags.ActionTypeID, -1);
+        String iuid = cmd.getRequestedSOPInstanceUID();
+        Dataset ds = rq.getDataset();
+        if (log.isDebugEnabled()) {
+            log.debug("Received N-Action cmd with ds:");
+            log.debug(ds);
+        }
+        checkStudyIuid(iuid, ds);
+        try {
+            StudyMgt stymgt = getStudyMgtHome().create();
+            try {
+                switch (actionTypeID) {
+                case 1:
+                    stymgt.deleteSeries(toSeriesIuids(ds));
+                    break;
+                case 2:
+                    stymgt.deleteInstances(toSopIuids(ds));
+                    break;
+                }
+            } finally {
+                try {
+                    stymgt.remove();
+                } catch (Exception e) {
+                    log.warn("Failed to remove StudyMgt Session Bean", e);
+                }
+            }
+        } catch (DcmServiceException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DcmServiceException(Status.ProcessingFailure, e);
+        }
+        service.sendStudyMgtNotification(assoc, Command.N_ACTION_RQ,
+                actionTypeID, iuid, ds);
+        return null;
+    }
 
-	protected Dataset doNSet(ActiveAssociation assoc, Dimse rq, 
-			Command rspCmd) throws IOException, DcmServiceException {
-		Command cmd = rq.getCommand();
-		String iuid = cmd.getRequestedSOPInstanceUID();
-		Dataset ds = rq.getDataset();
-		checkStudyIuid(iuid, ds);
-		try {
-			StudyMgt stymgt = getStudyMgtHome().create();
-			try {
-				stymgt.updateStudy(iuid, ds);
-			} finally {
-				try {
-					stymgt.remove();
-				} catch (Exception e) {
-					log.warn("Failed to remove StudyMgt Session Bean", e);
-				}
-			}
-		} catch (DcmServiceException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new DcmServiceException(Status.ProcessingFailure, e);
-		}
-		service.sendStudyMgtNotification(assoc, Command.N_SET_RQ, 0, iuid, ds);
-		return null;
-	}
+    private String[] toSeriesIuids(Dataset ds) throws DcmServiceException {
+        return toIuids(ds, true);
+    }
 
-	private void checkStudyIuid(String iuid, Dataset ds) throws DcmServiceException {
-		String suid = ds.getString(Tags.StudyInstanceUID);
-		if (suid != null && !suid.equals(iuid)) {
-			throw new DcmServiceException(Status.InvalidAttributeValue,
-				"Study Instance UID must match SOP Instance UID");
-		}
-	}
+    private String[] toSopIuids(Dataset ds) throws DcmServiceException {
+        return toIuids(ds, false);
+    }
+
+    private String[] toIuids(Dataset ds, boolean seriesIuids)
+            throws DcmServiceException {
+        List iuids = new ArrayList();
+        DcmElement sersq = ds.get(Tags.RefSeriesSeq);
+        if (sersq == null) {
+            throw new DcmServiceException(Status.MissingAttribute,
+                    "Missing Referenced Series Seq.");
+        }
+        for (int i = 0, n = sersq.countItems(); i < n; ++i) {
+            Dataset ser = sersq.getItem(i);
+            String siuid = ser.getString(Tags.SeriesInstanceUID);
+            if (siuid == null) {
+                throw new DcmServiceException(Status.MissingAttribute,
+                        "Missing Series Instance UID");
+            }
+            if (seriesIuids) {
+                iuids.add(siuid);
+            } else {
+                DcmElement sops = ser.get(Tags.RefSOPSeq);
+                if (sops == null) {
+                    throw new DcmServiceException(Status.MissingAttribute,
+                            "Missing Referenced SOP Seq.");
+                }
+                for (int j = 0, m = sops.countItems(); j < m; ++j) {
+                    Dataset sop = sops.getItem(i);
+                    String iuid = sop.getString(Tags.RefSOPInstanceUID);
+                    if (iuid == null) {
+                        throw new DcmServiceException(Status.MissingAttribute,
+                                "Missing Referenced SOP Instance UID");
+                    }
+                    iuids.add(iuid);
+                }
+            }
+        }
+        return (String[]) iuids.toArray(new String[iuids.size()]);
+    }
+
+    protected Dataset doNCreate(ActiveAssociation assoc, Dimse rq,
+            Command rspCmd) throws IOException, DcmServiceException {
+        Command cmd = rq.getCommand();
+        String iuid = cmd.getAffectedSOPInstanceUID();
+        Dataset ds = rq.getDataset();
+        String suid = ds.getString(Tags.StudyInstanceUID);
+        if (suid == null) {
+            throw new DcmServiceException(Status.MissingAttribute,
+                    "Missing Study Instance UID");
+        }
+        if (iuid == null) {
+            rspCmd.putUI(Tags.AffectedSOPInstanceUID, iuid = suid);
+        } else {
+            checkStudyIuid(iuid, ds);
+        }
+        try {
+            StudyMgt stymgt = getStudyMgtHome().create();
+            try {
+                stymgt.createStudy(ds, service.patientMatching());
+            } finally {
+                try {
+                    stymgt.remove();
+                } catch (Exception e) {
+                    log.warn("Failed to remove StudyMgt Session Bean", e);
+                }
+            }
+        } catch (DcmServiceException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DcmServiceException(Status.ProcessingFailure, e);
+        }
+        service.sendStudyMgtNotification(assoc, Command.N_CREATE_RQ, 0, iuid,
+                ds);
+        return null;
+    }
+
+    protected Dataset doNDelete(ActiveAssociation assoc, Dimse rq,
+            Command rspCmd) throws IOException, DcmServiceException {
+        Command cmd = rq.getCommand();
+        String iuid = cmd.getRequestedSOPInstanceUID();
+        Dataset ds = rq.getDataset(); // should be null
+        try {
+            StudyMgt stymgt = getStudyMgtHome().create();
+            try {
+                stymgt.deleteStudy(iuid);
+            } catch (DcmServiceException e) {
+                if (!(ignoreDeleteFailed && e.getStatus() == Status.NoSuchSOPClass)) {
+                    throw e;
+                }
+            } finally {
+                try {
+                    stymgt.remove();
+                } catch (Exception e) {
+                    log.warn("Failed to remove StudyMgt Session Bean", e);
+                }
+            }
+        } catch (DcmServiceException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DcmServiceException(Status.ProcessingFailure, e);
+        }
+        service.sendStudyMgtNotification(assoc, Command.N_DELETE_RQ, 0, iuid,
+                null);
+        return null;
+    }
+
+    protected Dataset doNSet(ActiveAssociation assoc, Dimse rq, Command rspCmd)
+            throws IOException, DcmServiceException {
+        Command cmd = rq.getCommand();
+        String iuid = cmd.getRequestedSOPInstanceUID();
+        Dataset ds = rq.getDataset();
+        checkStudyIuid(iuid, ds);
+        try {
+            StudyMgt stymgt = getStudyMgtHome().create();
+            try {
+                stymgt.updateStudy(iuid, ds);
+            } finally {
+                try {
+                    stymgt.remove();
+                } catch (Exception e) {
+                    log.warn("Failed to remove StudyMgt Session Bean", e);
+                }
+            }
+        } catch (DcmServiceException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DcmServiceException(Status.ProcessingFailure, e);
+        }
+        service.sendStudyMgtNotification(assoc, Command.N_SET_RQ, 0, iuid, ds);
+        return null;
+    }
+
+    private void checkStudyIuid(String iuid, Dataset ds)
+            throws DcmServiceException {
+        String suid = ds.getString(Tags.StudyInstanceUID);
+        if (suid != null && !suid.equals(iuid)) {
+            throw new DcmServiceException(Status.InvalidAttributeValue,
+                    "Study Instance UID must match SOP Instance UID");
+        }
+    }
 
 }
