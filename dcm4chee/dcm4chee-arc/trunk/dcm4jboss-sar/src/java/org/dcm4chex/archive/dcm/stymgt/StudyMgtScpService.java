cd558a751731c3f7014c69ad46c24f4787845b61
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-05-13 09:29:31 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScpService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/stymgt/StudyMgtScpService.java
@@ -56,35 +56,37 @@ public class StudyMgtScpService extends AbstractScpService {
 
     public static final NotificationFilter NOTIF_FILTER = new NotificationFilter() {
 
-		private static final long serialVersionUID = 3257281448414097465L;
+        private static final long serialVersionUID = 3257281448414097465L;
 
-		public boolean isNotificationEnabled(Notification notif) {
+        public boolean isNotificationEnabled(Notification notif) {
             return EVENT_TYPE.equals(notif.getType());
         }
     };
 
     private StudyMgtScp stymgtScp = new StudyMgtScp(this);
-    
-	/**
-	 * @return Returns the ignoreDeleteFailed.
-	 */
-	public boolean isIgnoreDeleteFailed() {
-		return stymgtScp.isIgnoreDeleteFailed();
-	}
-	/**
-	 * @param ignoreDeleteFailed The ignoreDeleteFailed to set.
-	 */
-	public void setIgnoreDeleteFailed(boolean ignoreDeleteFailed) {
-		stymgtScp.setIgnoreDeleteFailed( ignoreDeleteFailed );
-	}
 
-	protected void bindDcmServices(DcmServiceRegistry services) {
+    /**
+     * @return Returns the ignoreDeleteFailed.
+     */
+    public boolean isIgnoreDeleteFailed() {
+        return stymgtScp.isIgnoreDeleteFailed();
+    }
+
+    /**
+     * @param ignoreDeleteFailed
+     *            The ignoreDeleteFailed to set.
+     */
+    public void setIgnoreDeleteFailed(boolean ignoreDeleteFailed) {
+        stymgtScp.setIgnoreDeleteFailed(ignoreDeleteFailed);
+    }
+
+    protected void bindDcmServices(DcmServiceRegistry services) {
         services.bind(UIDs.Dcm4cheStudyManagement, stymgtScp);
-	}
+    }
 
-	protected void unbindDcmServices(DcmServiceRegistry services) {
-		services.unbind(UIDs.Dcm4cheStudyManagement);		
-	}
+    protected void unbindDcmServices(DcmServiceRegistry services) {
+        services.unbind(UIDs.Dcm4cheStudyManagement);
+    }
 
     protected void enablePresContexts(AcceptorPolicy policy) {
         policy.putPresContext(UIDs.Dcm4cheStudyManagement,
@@ -96,13 +98,13 @@ public class StudyMgtScpService extends AbstractScpService {
     }
 
     void sendStudyMgtNotification(ActiveAssociation assoc, int cmdField,
-			int actionTypeID, String iuid, Dataset ds) {
-		Association a = assoc.getAssociation();
-		long eventID = super.getNextNotificationSequenceNumber();
-		Notification notif = new Notification(EVENT_TYPE, this, eventID);
-		notif.setUserData(new StudyMgtOrder(a.getCallingAET(), a
-				.getCalledAET(), cmdField, actionTypeID, iuid, ds));
-		super.sendNotification(notif);
-	}
+            int actionTypeID, String iuid, Dataset ds) {
+        Association a = assoc.getAssociation();
+        long eventID = super.getNextNotificationSequenceNumber();
+        Notification notif = new Notification(EVENT_TYPE, this, eventID);
+        notif.setUserData(new StudyMgtOrder(a.getCallingAET(),
+                a.getCalledAET(), cmdField, actionTypeID, iuid, ds));
+        super.sendNotification(notif);
+    }
 
 }
