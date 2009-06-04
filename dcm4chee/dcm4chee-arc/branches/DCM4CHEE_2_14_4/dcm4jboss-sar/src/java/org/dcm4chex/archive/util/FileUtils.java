440170a7d233d8dfaa811f2e7db86fd1181e8cb3
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-06-04 09:06:21 +0000

    pretty format

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/util/FileUtils.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/util/FileUtils.java
@@ -56,133 +56,135 @@ import org.dcm4che.data.FileFormat;
 import org.dcm4che.dict.Tags;
 import org.jboss.system.server.ServerConfigLocator;
 
-
 /**
  * @author gunter.zeilinger@tiani.com
  * @version $Revision$ $Date$
  * @since 19.09.2004
- *
+ * 
  */
 public class FileUtils {
 
     protected static final Logger log = Logger.getLogger(FileUtils.class);
 
     private static final int BUFFER_SIZE = 512;
-    
-	public static final long MEGA = 1000000L;
+
+    public static final long MEGA = 1000000L;
 
     public static final long GIGA = 1000000000L;
 
     public static final long MAX_TIMES_CREATE_FILE = 10;
-    
-    public static final long INTERVAL_CREATE_FILE = 250; //ms
+
+    public static final long INTERVAL_CREATE_FILE = 250; // ms
 
     private static char[] HEX_DIGIT = { '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-	public static String toHex(int val) {
-	    char[] ch8 = new char[8];
-	    for (int i = 8; --i >= 0; val >>= 4) {
-	        ch8[i] = HEX_DIGIT[val & 0xf];
-	    }
-	    return String.valueOf(ch8);
-	}
-    
-	/**
-	 * Create a new file based on hash code, including the directory structure if necessary. 
-	 * In case of collision, we will reset hashcode and try again.
-	 * 
-	 * @param dir the directory where the new file will be created
-	 * @param hash the initial hash value
-	 * @return the file created successfully
-	 * @throws Exception
-	 */
-    public static File createNewFile(File dir, int hash) throws Exception {
-		File f = null;
-		boolean success = false;
-		Exception lastException = null;
-		for(int i = 0; i < MAX_TIMES_CREATE_FILE && !success; i++) {					
-			// In UNIX, mkdirs and createNewFile can throw IOException 
-			// instead of nicely returning false
-			try
-			{				
-		        if (!dir.exists()) {
-		            success = dir.mkdirs();
-		            if(!success)
-		            	throw new IOException("Directory creation failed: " + dir.getCanonicalPath());
-		        }
-
-				// Try to construct a new file name everytime
-				f = new File(dir, toHex(hash++));
-
-				success = f.createNewFile();	
-				
-				if(i > 0)
-					log.info("file: " + dir.getCanonicalPath() 
-							+ " created successfully after retries: " + i);
-				
-				if(success)
-					return f;
-				else
-					i--; // if not success, we should not increment i which is for exceptional retry
-			}
-			catch(Exception e)
-			{
-	        	if(lastException == null )
-	        		log.warn("failed to create file: " + dir.getCanonicalPath()
-	        				+ " - retry: " + (i+1) + " of " + MAX_TIMES_CREATE_FILE 
-	        				+ ". Will retry again.", e);
-	        	else
-	        		log.warn("failed to create file: " + dir.getCanonicalPath() 
-	        				+ " - got the same exception as above - retry: "
-	        				+ (i+1) + " of " + MAX_TIMES_CREATE_FILE + ". Will retry again" );	        	
-	        	lastException = e;
-
-				success = false;
-								
-				// Maybe other thread is trying to do the same thing
-				// Let's wait for INTERVAL_CREATE_FILE ms - this rarely happens
-				try {
-					Thread.sleep(INTERVAL_CREATE_FILE);
-				} catch (InterruptedException e1) {
-				} 
-			}
-		}
-		// Run out of retries, throw original exception
-		throw lastException;
+    public static String toHex(int val) {
+        char[] ch8 = new char[8];
+        for (int i = 8; --i >= 0; val >>= 4) {
+            ch8[i] = HEX_DIGIT[val & 0xf];
+        }
+        return String.valueOf(ch8);
     }
-	
+
+    /**
+     * Create a new file based on hash code, including the directory structure
+     * if necessary. In case of collision, we will reset hashcode and try again.
+     * 
+     * @param dir
+     *            the directory where the new file will be created
+     * @param hash
+     *            the initial hash value
+     * @return the file created successfully
+     * @throws Exception
+     */
+    public static File createNewFile(File dir, int hash) throws Exception {
+        File f = null;
+        boolean success = false;
+        Exception lastException = null;
+        for (int i = 0; i < MAX_TIMES_CREATE_FILE && !success; i++) {
+            // In UNIX, mkdirs and createNewFile can throw IOException
+            // instead of nicely returning false
+            try {
+                if (!dir.exists()) {
+                    success = dir.mkdirs();
+                    if (!success)
+                        throw new IOException("Directory creation failed: "
+                                + dir.getCanonicalPath());
+                }
+
+                // Try to construct a new file name everytime
+                f = new File(dir, toHex(hash++));
+
+                success = f.createNewFile();
+
+                if (i > 0)
+                    log.info("file: " + dir.getCanonicalPath()
+                            + " created successfully after retries: " + i);
+
+                if (success)
+                    return f;
+                else
+                    i--; // if not success, we should not increment i which is
+                         // for exceptional retry
+            } catch (Exception e) {
+                if (lastException == null)
+                    log.warn("failed to create file: " + dir.getCanonicalPath()
+                            + " - retry: " + (i + 1) + " of "
+                            + MAX_TIMES_CREATE_FILE + ". Will retry again.", e);
+                else
+                    log.warn("failed to create file: " + dir.getCanonicalPath()
+                            + " - got the same exception as above - retry: "
+                            + (i + 1) + " of " + MAX_TIMES_CREATE_FILE
+                            + ". Will retry again");
+                lastException = e;
+
+                success = false;
+
+                // Maybe other thread is trying to do the same thing
+                // Let's wait for INTERVAL_CREATE_FILE ms - this rarely happens
+                try {
+                    Thread.sleep(INTERVAL_CREATE_FILE);
+                } catch (InterruptedException e1) {
+                }
+            }
+        }
+        // Run out of retries, throw original exception
+        throw lastException;
+    }
+
     public static String slashify(File f) {
         return f.getPath().replace(File.separatorChar, '/');
     }
-    
+
     public static File resolve(File f) {
-        if (f.isAbsolute()) return f;
+        if (f.isAbsolute())
+            return f;
         File serverHomeDir = ServerConfigLocator.locate().getServerHomeDir();
         return new File(serverHomeDir, f.getPath());
     }
-    
+
     public static File toFile(String unixPath) {
         return resolve(new File(unixPath.replace('/', File.separatorChar)));
     }
 
-    public static File toExistingFile(String unixPath) throws FileNotFoundException {
+    public static File toExistingFile(String unixPath)
+            throws FileNotFoundException {
         File f = toFile(unixPath);
         if (!f.isFile()) {
             throw new FileNotFoundException(f.toString());
         }
         return f;
     }
-    
+
     public static File toFile(String unixDirPath, String unixFilePath) {
         return resolve(new File(unixDirPath.replace('/', File.separatorChar),
                 unixFilePath.replace('/', File.separatorChar)));
     }
-    
+
     public static String formatSize(long size) {
-        return (size == -1) ? "UNKOWN"
-                : (size < GIGA) ? ((float) size / MEGA) + "MB"
-                                : ((float) size / GIGA) + "GB";
+        return (size == -1) ? "UNKOWN" : (size < GIGA) ? ((float) size / MEGA)
+                + "MB" : ((float) size / GIGA) + "GB";
     }
 
     public static long parseSize(String s, long minSize) {
@@ -194,27 +196,27 @@ public class FileUtils {
         else
             throw new IllegalArgumentException(s);
         try {
-            long size = (long) (Float.parseFloat(s.substring(0, s.length() - 2)) * u);
+            long size = (long) (Float
+                    .parseFloat(s.substring(0, s.length() - 2)) * u);
             if (size >= minSize)
                 return size;
         } catch (IllegalArgumentException e) {
         }
         throw new IllegalArgumentException(s);
     }
-    
-    public static boolean equalsPixelData(File f1, File f2)
-    		throws IOException {
-    	InputStream in1 = new BufferedInputStream(new FileInputStream(f1));
-    	try {
-    		InputStream in2 = new BufferedInputStream(new FileInputStream(f2));
-    		try {    			
+
+    public static boolean equalsPixelData(File f1, File f2) throws IOException {
+        InputStream in1 = new BufferedInputStream(new FileInputStream(f1));
+        try {
+            InputStream in2 = new BufferedInputStream(new FileInputStream(f2));
+            try {
                 Dataset attrs = DcmObjectFactory.getInstance().newDataset();
-    	    	DcmParserFactory pf = DcmParserFactory.getInstance();
-				DcmParser p1 = pf.newDcmParser(in1);
-    	    	DcmParser p2 = pf.newDcmParser(in2);
+                DcmParserFactory pf = DcmParserFactory.getInstance();
+                DcmParser p1 = pf.newDcmParser(in1);
+                DcmParser p2 = pf.newDcmParser(in2);
                 p1.setDcmHandler(attrs.getDcmHandler());
-    			p1.parseDcmFile(FileFormat.DICOM_FILE, Tags.PixelData);
-    			p2.parseDcmFile(FileFormat.DICOM_FILE, Tags.PixelData);
+                p1.parseDcmFile(FileFormat.DICOM_FILE, Tags.PixelData);
+                p2.parseDcmFile(FileFormat.DICOM_FILE, Tags.PixelData);
                 int samples = attrs.getInt(Tags.SamplesPerPixel, 1);
                 int frames = attrs.getInt(Tags.NumberOfFrames, 1);
                 int rows = attrs.getInt(Tags.Rows, 1);
@@ -232,12 +234,13 @@ public class FileUtils {
                 int[] mask = { 0xff, 0xff };
                 int len, len2;
                 if (bitsAlloc == 16 && bitsStored < 16) {
-                    mask[p1.getDcmDecodeParam().byteOrder == ByteOrder.LITTLE_ENDIAN ? 1 : 0]
-                            = 0xff >>> (16 - bitsStored);
-                } 
+                    mask[p1.getDcmDecodeParam().byteOrder == ByteOrder.LITTLE_ENDIAN ? 1
+                            : 0] = 0xff >>> (16 - bitsStored);
+                }
                 int pos = 0;
                 while (pos < pixelDataLength) {
-                    len = in1.read(b1, 0, Math.min(pixelDataLength - pos, BUFFER_SIZE));
+                    len = in1.read(b1, 0, Math.min(pixelDataLength - pos,
+                            BUFFER_SIZE));
                     if (len < 0) // EOF
                         return false;
                     int off = 0;
@@ -246,17 +249,17 @@ public class FileUtils {
                         if (len2 < 0) // EOF
                             return false;
                     }
-                    for (int i=0; i<len; i++, pos++)
+                    for (int i = 0; i < len; i++, pos++)
                         if (((b1[i] - b2[i]) & mask[pos & 1]) != 0)
                             return false;
                 }
                 return true;
-    		} finally {
-    			in2.close();
-    		}
-    	} finally {
-    		in1.close();
-    	}
+            } finally {
+                in2.close();
+            }
+        } finally {
+            in1.close();
+        }
     }
 
     public static boolean delete(File file, boolean deleteEmptyParents) {
