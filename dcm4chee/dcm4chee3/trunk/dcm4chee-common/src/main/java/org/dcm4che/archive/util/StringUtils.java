cc500e9b70fb2136a144f9d5f1adaca8c5e40d3a
damien_evans <damien_evans@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-06-18 19:11:19 +0000

    Remove old dcm4chee3.  dcm4chee-arc3 is the new naming convention going forward.

--- dcm4chee/dcm4chee3/trunk/dcm4chee-common/src/main/java/org/dcm4che/archive/util/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package org.dcm4che.archive.util;
-
-public class StringUtils {
-
-    public static String toUpperCase(String s) {
-        return s != null ? s.toUpperCase() : null;
-    }
-    
-}
