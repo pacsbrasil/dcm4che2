3688b85b5ec91422501ef1c681dc3e6a4d20395c
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-07-07 17:38:48 +0000

    Updated for JBoss 5.1.0

--- dcm4chee/dcm4chee-xero/trunk/installer/src/main/resources/bin/run.bat
+++ dcm4chee/dcm4chee-xero/trunk/installer/src/main/resources/bin/run.bat
@@ -1,6 +1,6 @@
 @echo off
 rem -------------------------------------------------------------------------
-rem JBoss Bootstrap Script for Win32
+rem JBoss Bootstrap Script for Windows
 rem -------------------------------------------------------------------------
 
 rem $Id: run.bat 5689 2008-01-15 20:52:00Z gunterze $
@@ -8,92 +8,116 @@ rem $Id: run.bat 5689 2008-01-15 20:52:00Z gunterze $
 @if not "%ECHO%" == ""  echo %ECHO%
 @if "%OS%" == "Windows_NT"  setlocal
 
+if "%OS%" == "Windows_NT" (
+  set "DIRNAME=%~dp0%"
+) else (
 set DIRNAME=.\
-if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
-set PROGNAME=run.bat
-if "%OS%" == "Windows_NT" set PROGNAME=%~nx0%
+)
+
+rem Read an optional configuration file.
+if "x%RUN_CONF%" == "x" (   
+   set "RUN_CONF=%DIRNAME%run.conf.bat"
+)
+if exist "%RUN_CONF%" (
+   echo Calling %RUN_CONF%
+   call "%RUN_CONF%" %*
+) else (
+   echo Config file not found %RUN_CONF%
+)
 
 pushd %DIRNAME%..
-set JBOSS_HOME=%CD%
+if "x%JBOSS_HOME%" == "x" (
+  set "JBOSS_HOME=%CD%"
+)
 popd
 
-REM Add bin/native to the PATH if present
-if exist "%JBOSS_HOME%\bin\native" set PATH=%JBOSS_HOME%\bin\native;%PATH%
-if exist "%JBOSS_HOME%\bin\native" set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path="%PATH%"
+set DIRNAME=
 
-rem Read all command line arguments
+if "%OS%" == "Windows_NT" (
+  set "PROGNAME=%~nx0%"
+) else (
+  set "PROGNAME=run.bat"
+)
 
-REM
-REM The %ARGS% env variable commented out in favor of using %* to include
-REM all args in java command line. See bug #840239. [jpl]
-REM
-REM set ARGS=
-REM :loop
-REM if [%1] == [] goto endloop
-REM         set ARGS=%ARGS% %1
-REM         shift
-REM         goto loop
-REM :endloop
+if "x%JAVA_OPTS%" == "x" (
+  set "JAVA_OPTS=-Dprogram.name=Xero"
+) else (
+  set "JAVA_OPTS=-Dprogram.name=Xero %JAVA_OPTS%"
+)
+
+if "x%JAVA_HOME%" == "x" (
+set JAVA=java
+echo JAVA_HOME is not set.  Unexpected results may occur.
+echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+  if exist "%JAVA_HOME%\lib\tools.jar" (
+    set "JAVAC_JAR=%JAVA_HOME%\lib\tools.jar"
+  )
+)
+
+rem Add -server to the JVM options, if supported
+"%JAVA%" -server -version 2>&1 | findstr /I hotspot > nul
+if not errorlevel == 1 (
+  set "JAVA_OPTS=%JAVA_OPTS% -server"
+)
+
+rem Set Java platform if 64-Bit JVM used
+set JAVA_PLATFORM=
+"%JAVA%" -version 2>&1 | findstr /I "64-Bit ^| x86_64" > nul
+if not errorlevel == 1 (
+  if /I "%PROCESSOR_ARCHITECTURE%"=="IA64"  (set JAVA_PLATFORM=i64
+  ) else if /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set JAVA_PLATFORM=x64
+  ) else if /I "%PROCESSOR_ARCHITECTURE%"=="x64"   (set JAVA_PLATFORM=x64
+  ) else if /I "%PROCESSOR_ARCHITEW6432%"=="IA64"  (set JAVA_PLATFORM=i64
+  ) else if /I "%PROCESSOR_ARCHITEW6432%"=="AMD64" (set JAVA_PLATFORM=x64
+  ) else (
+    echo PROCESSOR_ARCHITECTURE is not set. Unexpected results may occur.
+    echo Set PROCESSOR_ARCHITECTURE according to the 64-Bit JVM used.
+  )
+)
+
+if "x%JAVA_PLATFORM%" == "x" set JAVA_PLATFORM=x86
+
+rem Add native to the PATH if present
+set JBOSS_NATIVE_LOC=
+if exist "%JBOSS_HOME%\bin\META-INF\lib\windows-%JAVA_PLATFORM%" (
+  set "JBOSS_NATIVE_HOME=%JBOSS_HOME%\bin\META-INF\lib\windows-%JAVA_PLATFORM%"
+) else if exist "%JBOSS_HOME%\bin\native" (
+  set "JBOSS_NATIVE_HOME=%JBOSS_HOME%\bin\native"
+)
+
+if not "x%JBOSS_NATIVE_HOME%" == "x" (
+  set "PATH=%JBOSS_NATIVE_HOME%;%PATH%;%JBOSS_HOME%\bin"
+  set JAVA_OPTS=%JAVA_OPTS% "-Djava.library.path=%JBOSS_NATIVE_HOME%;%PATH%;%JBOSS_HOME%\bin"
+)
 
 rem Find run.jar, or we can't continue
 
-set RUNJAR=%JBOSS_HOME%\bin\run.jar
-if exist "%RUNJAR%" goto FOUND_RUN_JAR
-echo Could not locate %RUNJAR%. Please check that you are in the
-echo bin directory when running this script.
-goto END
+if exist "%JBOSS_HOME%\bin\run.jar" (
+  if "x%JAVAC_JAR%" == "x" (
+    set "RUNJAR=%JAVAC_JAR%;%JBOSS_HOME%\bin\run.jar"
+  ) else (
+    set "RUNJAR=%JBOSS_HOME%\bin\run.jar"
+  )
+) else (
+  echo Could not locate "%JBOSS_HOME%\bin\run.jar".
+  echo Please check that you are in the bin directory when running this script.
+  goto END
+)
 
-:FOUND_RUN_JAR
-
-if not "%JAVA_HOME%" == "" goto ADD_TOOLS
-
-set JAVA=java
-
-echo JAVA_HOME is not set.  Unexpected results may occur.
-echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
-goto SKIP_TOOLS
-
-:ADD_TOOLS
-
-set JAVA=%JAVA_HOME%\bin\java
-
-rem A full JDK with toos.jar is not required anymore since jboss web packages
-rem the eclipse jdt compiler and javassist has its own internal compiler.
-if not exist "%JAVA_HOME%\lib\tools.jar" goto SKIP_TOOLS
-
-rem If exists, point to the JDK javac compiler in case the user wants to
-rem later override the eclipse jdt compiler for compiling JSP pages.
-set JAVAC_JAR=%JAVA_HOME%\lib\tools.jar
-
-:SKIP_TOOLS
-
-rem If JBOSS_CLASSPATH or JAVAC_JAR is empty, don't include it, as this will 
+rem If JBOSS_CLASSPATH empty, don't include it, as this will
 rem result in including the local directory in the classpath, which makes
 rem error tracking harder.
-if not "%JAVAC_JAR%" == "" set RUNJAR=%JAVAC_JAR%;%RUNJAR%
-if "%JBOSS_CLASSPATH%" == "" set RUN_CLASSPATH=%RUNJAR%
-if "%RUN_CLASSPATH%" == "" set RUN_CLASSPATH=%JBOSS_CLASSPATH%;%RUNJAR%
+if "x%JBOSS_CLASSPATH%" == "x" (
+  set "RUN_CLASSPATH=%RUNJAR%"
+) else (
+  set "RUN_CLASSPATH=%JBOSS_CLASSPATH%;%RUNJAR%"
+)
 
 set JBOSS_CLASSPATH=%RUN_CLASSPATH%
 
 rem Setup JBoss specific properties
-set JAVA_OPTS=%JAVA_OPTS% -Dprogram.name=%PROGNAME%
-
-rem Add -server to the JVM options, if supported
-"%JAVA%" -version 2>&1 | findstr /I hotspot > nul
-if not errorlevel == 1 (set JAVA_OPTS=%JAVA_OPTS% -server)
-
-rem JVM memory allocation pool parameters. Modify as appropriate.
-set JAVA_OPTS=%JAVA_OPTS% -Xms128m -Xmx512m
-
-rem With Sun JVMs reduce the RMI GCs to once per hour
-set JAVA_OPTS=%JAVA_OPTS% -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
-
-rem Set app.name  used in emitted audit log messages
-set JAVA_OPTS=%JAVA_OPTS% -Dapp.name=dcm4jboss-xero
-
-rem JPDA options. Uncomment and modify as appropriate to enable remote debugging.
-rem set JAVA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y %JAVA_OPTS%
 
 rem Setup the java endorsed dirs
 set JBOSS_ENDORSED_DIRS=%JBOSS_HOME%\lib\endorsed
@@ -114,10 +138,14 @@ echo ===========================================================================
 echo.
 
 :RESTART
-"%JAVA%" %JAVA_OPTS% "-Djava.endorsed.dirs=%JBOSS_ENDORSED_DIRS%" -classpath "%JBOSS_CLASSPATH%" org.jboss.Main %*
+"%JAVA%" %JAVA_OPTS% ^
+   -Djava.endorsed.dirs="%JBOSS_ENDORSED_DIRS%" ^
+   -classpath "%JBOSS_CLASSPATH%" ^
+   org.jboss.Main %*
+
 if ERRORLEVEL 10 goto RESTART
 
 :END
-if "%NOPAUSE%" == "" pause
+if "x%NOPAUSE%" == "x" pause
 
 :END_NO_PAUSE
