49d42c78eb819183def3ed03d69963256c0f83ce
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-08-27 07:22:44 +0000

    [#DCMEE-1261] Allow multiple ReferencedDirectoryPath in StoreSCP. fix uncaught Exception!

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/storescp/StoreScp.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/storescp/StoreScp.java
@@ -711,13 +711,17 @@ public class StoreScp extends DcmServiceBase implements AssociationListener {
 
     private String[] selectReferencedDirectoryURI(String uri) {
         if ( referencedDirectoryPath == null ) {
-            FileSystemDTO[] fsDTOs = getFileSystemMgt().getFileSystemsOfGroup(this.refFileSystemGroupID);
-            String uriPath;
-            for ( FileSystemDTO dto : fsDTOs ) {
-                uriPath = FileUtils.toFile(dto.getDirectoryPath()).toURI().toString();
-                if (uri.startsWith(uriPath)) {
-                    return new String[]{ dto.getDirectoryPath(), uriPath};
+            try {
+                FileSystemDTO[] fsDTOs = getFileSystemMgt().getFileSystemsOfGroup(this.refFileSystemGroupID);
+                String uriPath;
+                for ( FileSystemDTO dto : fsDTOs ) {
+                    uriPath = FileUtils.toFile(dto.getDirectoryPath()).toURI().toString();
+                    if (uri.startsWith(uriPath)) {
+                        return new String[]{ dto.getDirectoryPath(), uriPath};
+                    }
                 }
+            } catch (Exception x ) {
+                log.error("Can't get FilesystemMgt Bean!",x);
             }
         } else {
             for ( int i = 0; i< referencedDirectoryURI.length ; i++) {
