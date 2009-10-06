c9a2ea25eb388b1cabd3e1a2c63239e2db5faddb
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-10-06 21:00:36 +0000

    [#DCMEE-1299] MPPSEmulator failed with TransactionRollbackException.

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/mppsscu/MPPSEmulatorService.java
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/mppsscu/MPPSEmulatorService.java
@@ -94,6 +94,7 @@ public class MPPSEmulatorService extends ServiceMBeanSupport implements
 
     private String[] stationAETs = {};
     private long[] delays;
+    private int limitNrOfSeriesPerTask;
     
     private ObjectName mppsScuServiceName;
     
@@ -171,6 +172,14 @@ public class MPPSEmulatorService extends ServiceMBeanSupport implements
         return isRunning;
     }
     
+    public int getLimitNrOfSeriesPerTask() {
+        return limitNrOfSeriesPerTask;
+    }
+
+    public void setLimitNrOfSeriesPerTask(int limit) {
+        this.limitNrOfSeriesPerTask = limit;
+    }
+
     public void handleNotification(Notification notification, Object handback) {
         if (stationAETs.length > 0) {
             new Thread(new Runnable(){
@@ -204,7 +213,7 @@ public class MPPSEmulatorService extends ServiceMBeanSupport implements
                 Collection<Long> studyPks;
                 try {
                     studyPks = (Collection<Long>) mppsEmulator.getStudiesWithMissingMPPS(stationAETs[i],
-                            delays[i]);
+                            delays[i], limitNrOfSeriesPerTask);
                 } catch (Exception e) {
                     log.error("Failed to emulate MPPS for series received from " + 
                             stationAETs[i] + " failed:", e);
