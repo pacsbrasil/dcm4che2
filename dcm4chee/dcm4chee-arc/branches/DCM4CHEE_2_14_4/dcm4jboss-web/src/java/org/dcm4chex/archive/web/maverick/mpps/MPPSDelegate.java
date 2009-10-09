5aa578d9852655b5189b92f33b2ac2b060a17178
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-10-09 13:47:10 +0000

    [#DCMEE-1304] reactivate and improve 'Delete' functionality in MPPS console

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/mpps/MPPSDelegate.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-web/src/java/org/dcm4chex/archive/web/maverick/mpps/MPPSDelegate.java
@@ -122,6 +122,23 @@ public class MPPSDelegate {
     }
 
     /**
+     * Deletes MPPS entries specified by an array of MPPS IUIDs.
+     * <p>
+     *
+     * @param iuids  The List of Instance UIDs of the MPPS Entries to delete.
+     * @return
+     */
+    public boolean deleteMPPSEntries(String[] iuids) {
+        try {
+            Object o = server.invoke(mppsScpServiceName, "deleteMPPSEntries",
+                    new Object[] { iuids }, new String[] { String[].class.getName() });
+            return ((Boolean) o).booleanValue();
+        } catch (Exception x) {
+            log.error("Exception occured in deleteMPPSEntries: " + x.getMessage(), x);
+        }
+        return false;
+    }
+    /**
      * @param mppsIUIDs
      */
     public boolean unlinkMPPS(String[] mppsIUIDs) {
