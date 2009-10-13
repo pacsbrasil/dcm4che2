19c0a5e574616e7210a638741f431581cbf59f86
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-10-13 13:39:23 +0000

    [#DCMEE-1309] Use TAR File ID assigned by HSM instead derived from File ID in online file system

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/hsm/FileCopyService.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/hsm/FileCopyService.java
@@ -45,7 +45,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.security.MessageDigest;
 import java.sql.SQLException;
 import java.util.Arrays;
