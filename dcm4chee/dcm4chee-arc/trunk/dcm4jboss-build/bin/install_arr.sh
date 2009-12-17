2503ee816d7784b75be63bbbb0685d385c623b61
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2009-12-17 10:05:53 +0000

    [#DCMEE-1353] Update install_arr.* scripts to deploy dcm4chee-arr-2.0.9

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_arr.sh
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_arr.sh
@@ -15,32 +15,33 @@ fi
 ARR_HOME="$1"
 ARR_SERV="$ARR_HOME"/server/default
 
-if [ -f "$ARR_SERV"/deploy/dcm4chee-arr-db2-3.0.8.ear ]; then
+if [ -f "$ARR_SERV"/deploy/dcm4chee-arr-db2-3.0.9.ear ]; then
   ARR_DB=db2
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-firebird-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-firebird-3.0.9.ear ]; then
   ARR_DB=firebird
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-hsql-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-hsql-3.0.9.ear ]; then
   ARR_DB=hsql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mssql-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mssql-3.0.9.ear ]; then
   ARR_DB=mssql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mysql-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mysql-3.0.9.ear ]; then
   ARR_DB=mysql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-oracle-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-oracle-3.0.9.ear ]; then
   ARR_DB=oracle
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-psql-3.0.8.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-psql-3.0.9.ear ]; then
   ARR_DB=psql
 else
   echo Could not locate dcm4chee-arr in "$ARR_HOME"
   exit 1
 fi
 
-cp -v "$ARR_SERV"/conf/dcm4chee-auditlog/arr-udplistener-xmbean.xml \
+cp -v "$ARR_SERV"/conf/dcm4chee-auditlog/arr-tcplistener-xmbean.xml \
+   "$ARR_SERV"/conf/dcm4chee-auditlog/arr-udplistener-xmbean.xml \
   "$DCM4CHEE_SERV"/conf/dcm4chee-auditlog
 
-cp -v "$ARR_SERV"/deploy/dcm4chee-arr-${ARR_DB}-3.0.8.ear \
+cp -v "$ARR_SERV"/deploy/dcm4chee-arr-${ARR_DB}-3.0.9.ear \
   "$ARR_SERV"/deploy/arr-${ARR_DB}-ds.xml \
   "$DCM4CHEE_HOME"/doc/dcm4chee-auditlog-service.xml \
   "$DCM4CHEE_SERV"/deploy
 
-cp -v "$ARR_SERV"/lib/dcm4che-core-2.0.19.jar \
+cp -v "$ARR_SERV"/lib/dcm4che-core-2.0.21.jar \
   "$DCM4CHEE_SERV"/lib
