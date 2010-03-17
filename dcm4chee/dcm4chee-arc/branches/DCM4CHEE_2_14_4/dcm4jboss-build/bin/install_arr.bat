a971c856ed44a35c6d4cf734e844aa34c0dd3583
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-03-17 14:31:08 +0000

    [#ARR-109] Use Maven classifier to distinguish DB specific versions

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-build/bin/install_arr.bat
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-build/bin/install_arr.bat
@@ -9,6 +9,7 @@ if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
 
 set DCM4CHEE_HOME=%DIRNAME%..
 set DCM4CHEE_SERV=%DCM4CHEE_HOME%\server\default
+set VERS=3.0.10
 
 if exist "%DCM4CHEE_SERV%" goto found_dcm4chee
 echo Could not locate %DCM4CHEE_SERV%. Please check that you are in the
@@ -24,13 +25,13 @@ goto end
 set ARR_HOME=%1
 set ARR_SERV=%ARR_HOME%\server\default
 
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-db2-3.0.10.ear" set ARR_DB=db2
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-firebird-3.0.10.ear" set ARR_DB=firebird
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-hsql-3.0.10.ear" set ARR_DB=hsql
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-mssql-3.0.10.ear" set ARR_DB=mssql
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-mysql-3.0.10.ear" set ARR_DB=mysql
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-oracle-3.0.10.ear" set ARR_DB=oracle
-if exist "%ARR_SERV%\deploy\dcm4chee-arr-psql-3.0.10.ear" set ARR_DB=psql
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-db2.ear" set ARR_DB=db2
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-firebird.ear" set ARR_DB=firebird
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-hsql.ear" set ARR_DB=hsql
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-mssql.ear" set ARR_DB=mssql
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-mysql.ear" set ARR_DB=mysql
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-oracle.ear" set ARR_DB=oracle
+if exist "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-psql.ear" set ARR_DB=psql
 if not [%ARR_DB%] == [] goto found_arr
 echo Could not locate dcm4chee-arr in %ARR_HOME%.
 goto end
@@ -39,7 +40,7 @@ goto end
 copy "%ARR_SERV%\conf\dcm4chee-auditlog\arr-tcplistener-xmbean.xml" "%DCM4CHEE_SERV%\conf\dcm4chee-auditlog"
 copy "%ARR_SERV%\conf\dcm4chee-auditlog\arr-udplistener-xmbean.xml" "%DCM4CHEE_SERV%\conf\dcm4chee-auditlog"
 
-copy "%ARR_SERV%\deploy\dcm4chee-arr-%ARR_DB%-3.0.10.ear" "%DCM4CHEE_SERV%\deploy"
+copy "%ARR_SERV%\deploy\dcm4chee-arr-%VERS%-%ARR_DB%.ear" "%DCM4CHEE_SERV%\deploy"
 copy "%ARR_SERV%\deploy\arr-%ARR_DB%-ds.xml" "%DCM4CHEE_SERV%\deploy"
 copy "%DCM4CHEE_HOME%\doc\dcm4chee-auditlog-service.xml" "%DCM4CHEE_SERV%\deploy"
 
