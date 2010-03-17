577e4ca16f9c8e0b434d420a44ce81c032a39035
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-03-17 14:29:05 +0000

    [#ARR-109] Use Maven classifier to distinguish DB specific versions

--- dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_arr.sh
+++ dcm4chee/dcm4chee-arc/trunk/dcm4jboss-build/bin/install_arr.sh
@@ -6,6 +6,7 @@
 DIRNAME=`dirname $0`
 DCM4CHEE_HOME="$DIRNAME"/..
 DCM4CHEE_SERV="$DCM4CHEE_HOME"/server/default
+VERS=3.0.10
 
 if [ x$1 = x ]; then
   echo "Usage: $0 <path-to-dcm4chee-arr-installation-directory>"
@@ -15,19 +16,19 @@ fi
 ARR_HOME="$1"
 ARR_SERV="$ARR_HOME"/server/default
 
-if [ -f "$ARR_SERV"/deploy/dcm4chee-arr-db2-3.0.10.ear ]; then
+if [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-db2.ear ]; then
   ARR_DB=db2
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-firebird-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-firebird.ear ]; then
   ARR_DB=firebird
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-hsql-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-hsql.ear ]; then
   ARR_DB=hsql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mssql-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-mssql.ear ]; then
   ARR_DB=mssql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-mysql-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-mysql.ear ]; then
   ARR_DB=mysql
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-oracle-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-oracle.ear ]; then
   ARR_DB=oracle
-elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-psql-3.0.10.ear ]; then
+elif [ -f "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-psql.ear ]; then
   ARR_DB=psql
 else
   echo Could not locate dcm4chee-arr in "$ARR_HOME"
@@ -38,7 +39,7 @@ cp -v "$ARR_SERV"/conf/dcm4chee-auditlog/arr-tcplistener-xmbean.xml \
    "$ARR_SERV"/conf/dcm4chee-auditlog/arr-udplistener-xmbean.xml \
   "$DCM4CHEE_SERV"/conf/dcm4chee-auditlog
 
-cp -v "$ARR_SERV"/deploy/dcm4chee-arr-${ARR_DB}-3.0.10.ear \
+cp -v "$ARR_SERV"/deploy/dcm4chee-arr-${VERS}-${ARR_DB}.ear \
   "$ARR_SERV"/deploy/arr-${ARR_DB}-ds.xml \
   "$DCM4CHEE_HOME"/doc/dcm4chee-auditlog-service.xml \
   "$DCM4CHEE_SERV"/deploy
