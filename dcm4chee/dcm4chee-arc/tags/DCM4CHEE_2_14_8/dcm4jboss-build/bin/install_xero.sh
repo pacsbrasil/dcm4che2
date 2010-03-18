aa6cb92ceaee23d94508722924cb6077ec45acb1
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-03-18 14:06:52 +0000

    [#DCMEE-1349] Release dcm4chee-2.14.8

---
+++ dcm4chee/dcm4chee-arc/tags/DCM4CHEE_2_14_8/dcm4jboss-build/bin/install_xero.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# -----------------------------------------------------------------
+# copy XERO and WADO2 components into DCM4CHEE Archive installation
+# -----------------------------------------------------------------
+
+DIRNAME=`dirname $0`
+DCM4CHEE_HOME="$DIRNAME"/..
+DCM4CHEE_SERV="$DCM4CHEE_HOME"/server/default
+
+if [ x$1 = x ]; then
+  echo "Usage: $0 <path-to-dcm4chee-xero-installation-directory>"
+  exit 1
+fi
+
+XER_HOME="$1"
+XER_SERV="$XER_HOME"/server/default
+
+if [ ! -f "$XER_SERV"/deploy/xero.war ]; then
+  echo Could not locate dcm4chee-xero in "$XER_HOME"
+  exit 1
+fi
+
+cp -v "$XER_SERV"/conf/ae-local.properties "$DCM4CHEE_SERV"/conf/
+cp -v "$XER_SERV"/deploy/wado2.war "$DCM4CHEE_SERV"/deploy/
+cp -v "$XER_SERV"/deploy/xero.war "$DCM4CHEE_SERV"/deploy/
+cp -v "$XER_SERV"/deploy/jboss-web.deployer/server.xml \
+  "$DCM4CHEE_SERV"/deploy/jboss-web.deployer/
