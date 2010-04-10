2ed8e87dce6508121f63579929e46d3f8c1c0fd4
kianusch <kianusch@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-04-10 19:03:11 +0000

    Branche 2.1

---
+++ dcm4chee/dcm4chee-jpdbi/branches/2.1/src/java/com/agfa/db/tools/DBTypes.java
@@ -0,0 +1,11 @@
+// $Id$
+
+package com.agfa.db.tools;
+
+public class DBTypes {
+   public static final int UNKNOWN = 0;
+
+   public static final int ORACLE = 1;
+
+   public static final int MYSQL = 2;
+}
