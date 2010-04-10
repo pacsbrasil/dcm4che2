43affd49a9758dd7bbb3dd5acef41adce348c338
kianusch <kianusch@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-04-10 13:16:32 +0000

    Version 1.x

---
+++ dcm4chee/dcm4chee-jpdbi/tags/1.x/src/java/com/agfa/db/tools/Query.java
@@ -0,0 +1,21 @@
+/*
+$Id$
+$Log: Query.java,v $
+
+*/
+
+package com.agfa.db.tools;
+
+public class Query {
+    public String select = null;
+    public String from = null;
+    public String where = null;
+    public String links = null;
+    
+    public Query (String a, String b, String c, String d) {
+        select = a;
+        from = b;
+        where = c;
+        links = d;
+    }
+}
