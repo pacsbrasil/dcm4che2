b1d718466a417cb0f429a5c8cb8520337ad435ca
wayfarer3130 <wayfarer3130@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-05-21 23:05:37 +0000

    Releasing version 1.0-rc.722

---
+++ dcm4chee/wado2/tags/xero-wado-1.0-rc.722/wado2-core/src/main/java/org/dcm4chee/xero/dicom/SimpleKeyStoreLoader.java
@@ -0,0 +1,81 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1/GPL 2.0/LGPL 2.1
+// 
+// The contents of this file are subject to the Mozilla Public License Version 
+// 1.1 (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+// 
+// The Original Code is part of dcm4che, an implementation of DICOM(TM) in Java(TM), hosted at http://sourceforge.net/projects/dcm4che
+//  
+// The Initial Developer of the Original Code is Agfa Healthcare.
+// Portions created by the Initial Developer are Copyright (C) 2009 the Initial Developer. All Rights Reserved.
+// 
+// Contributor(s):
+// Andrew Cowan <andrew.cowan@agfa.com>
+// 
+// Alternatively, the contents of this file may be used under the terms of
+// either the GNU General Public License Version 2 or later (the "GPL"), or
+// the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+// in which case the provisions of the GPL or the LGPL are applicable instead
+// of those above. If you wish to allow use of your version of this file only
+// under the terms of either the GPL or the LGPL, and not to allow others to
+// use your version of this file under the terms of the MPL, indicate your
+// decision by deleting the provisions above and replace them with the notice
+// and other provisions required by the GPL or the LGPL. If you do not delete
+// the provisions above, a recipient may use your version of this file under
+// the terms of any one of the MPL, the GPL or the LGPL.
+// 
+// ***** END LICENSE BLOCK *****
+package org.dcm4chee.xero.dicom;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+/**
+ * Basic Keystore selector that will choose a keystore based on the 
+ * name of the file.
+ * @author Andrew Cowan (andrew.cowan@agfa.com)
+ */
+public class SimpleKeyStoreLoader implements KeyStoreLoader
+{
+   // TODO: Support URLs as well so that resource extraction can be used for unit testing...
+   
+   @Override
+   public KeyStore loadKeyStore(String fileName, String password) 
+      throws GeneralSecurityException, IOException
+   {
+      if(fileName == null) 
+         return null;
+      if(password == null)
+         throw new IllegalArgumentException("Password for keystore '"+fileName+"' is undefined.");
+      
+      KeyStore key = KeyStore.getInstance(toKeyStoreType(fileName));
+      InputStream in = new FileInputStream(fileName);
+      try
+      {
+         key.load(in, password.toCharArray());
+      }
+      finally
+      {
+         in.close();
+      }
+      return key;
+   }
+   
+   private static String toKeyStoreType(String fname)
+   {
+      return fname.endsWith(".p12") || fname.endsWith(".P12")
+               ? "PKCS12" : "JKS";
+  }
+   
+   
+
+}
