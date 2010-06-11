430361a1758bfa5ee3bee3aceab5743dcacc0f72
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-06-11 12:05:49 +0000

    [#WEB-115] Add 'SimpleDicomEditor' as a user friendly Dicom Attribute editor.

--- dcm4chee/dcm4chee-web/trunk/dcm4chee-web-common/src/main/java/org/dcm4chee/web/common/markup/BaseForm.java
+++ dcm4chee/dcm4chee-web/trunk/dcm4chee-web-common/src/main/java/org/dcm4chee/web/common/markup/BaseForm.java
@@ -52,20 +52,38 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.IBehavior;
 import org.apache.wicket.datetime.StyleDateConverter;
 import org.apache.wicket.datetime.markup.html.form.DateTextField;
+import org.apache.wicket.extensions.yui.calendar.DateField;
 import org.apache.wicket.extensions.yui.calendar.DateTimeField;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.AbstractTextComponent.ITextFormatProvider;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.validator.AbstractValidator;
+import org.apache.wicket.validation.validator.StringValidator;
+import org.dcm4che2.data.DicomElement;
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.ElementDictionary;
+import org.dcm4che2.data.SpecificCharacterSet;
+import org.dcm4che2.data.Tag;
+import org.dcm4che2.data.VR;
+import org.dcm4che2.data.VRMap;
+import org.dcm4che2.util.UIDUtils;
 import org.dcm4chee.web.common.behaviours.MarkInvalidBehaviour;
 import org.dcm4chee.web.common.behaviours.TooltipBehaviour;
+import org.dcm4chee.web.common.model.DicomElementModel;
 import org.dcm4chee.web.common.util.DateUtils;
+import org.dcm4chee.web.common.validators.UIDValidator;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -87,7 +105,7 @@ public class BaseForm extends Form<Object> {
     private IVisitor<Component> visitor = new FormVisitor();
     
     MarkInvalidBehaviour markInvalidBehaviour = new MarkInvalidBehaviour();
-    
+        
     public BaseForm(String id) {
         super(id);
     }
@@ -147,48 +165,20 @@ public class BaseForm extends Form<Object> {
         return tf;
     }
 
-    public DateTimeField addLabeledDateTimeField(String id, IModel<Date> model, final IModel<Boolean> enabledModel, final boolean max) {
-        DateTimeField dtf = addDateTimeField(id, model, enabledModel, max);
+    public SimpleDateTimeField addLabeledDateTimeField(String id, IModel<Date> model, final IModel<Boolean> enabledModel, final boolean max) {
+        SimpleDateTimeField dtf = getSimpleDateTimeField(id, model, enabledModel, max);
         addInternalLabel(id);
         return dtf;
     }
 
-    public DateTimeField addDateTimeField(String id, IModel<Date> model,
+    public SimpleDateTimeField getSimpleDateTimeField(String id, IModel<Date> model,
             final IModel<Boolean> enabledModel, final boolean max) {
-        final DateTimeField dtf = new DateTimeField(id, model) {
+        final SimpleDateTimeField dtf = new SimpleDateTimeField(id, model) {
             private static final long serialVersionUID = 1L;
 
             @Override
-            protected boolean use12HourFormat() {
-                return false;
-            }
-
-            @Override
-            protected void convertInput() {
-                super.convertInput();
-                Date d = this.getConvertedInput();
-                if (d != null) {
-                    GregorianCalendar cal = new GregorianCalendar();
-                    cal.setTime(d);
-                    if (max && cal.get(Calendar.HOUR_OF_DAY) == 0 &&
-                        cal.get(Calendar.MINUTE) == 0 && cal.get(Calendar.SECOND) == 0) {
-                        cal.set(Calendar.HOUR_OF_DAY, 23);
-                        cal.set(Calendar.MINUTE, 59);
-                        cal.set(Calendar.SECOND, 59);
-                        cal.set(Calendar.MILLISECOND, 999);
-                        this.setConvertedInput(cal.getTime());
-                    }
-                }
-            }
-            @SuppressWarnings("unchecked")
-            @Override
-            protected DateTextField newDateTextField(String id, PropertyModel dateFieldModel) {
-                return getDateTextField(id, dateFieldModel, false);
-            }
-
-            @Override
             public boolean isEnabled() {
-                return enabledModel.getObject();
+                return enabledModel == null ? true : enabledModel.getObject();
             }
         };
         dtf.add(new TooltipBehaviour(tooltipBehaviour == null ? null : tooltipBehaviour.getPrefix(), 
@@ -217,6 +207,31 @@ public class BaseForm extends Form<Object> {
                                         .withPivotYear(2000);
                     }
             });
+        if (addTooltip) {
+            dt.add(new TooltipBehaviour(tooltipBehaviour == null ? null : tooltipBehaviour.getPrefix(), 
+                    id, new PropertyModel<Object>(dt,"textFormat")));
+        }
+        dt.add(markInvalidBehaviour);
+        return dt;
+    }
+    
+    public DateField getDateField(String id, IModel<Date> model, boolean addTooltip) {
+        DateField dt = new DateField(id, model){
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onComponentTag(ComponentTag tag) {
+                super.onComponentTag(tag);
+                if ( tag.getAttribute("class") == null )
+                    tag.put("class", "dateField");
+            }
+            
+            @SuppressWarnings("unchecked")
+            @Override
+            protected DateTextField newDateTextField(String id, PropertyModel dateFieldModel) {
+                return getDateTextField(id, dateFieldModel, false);
+            }            
+        };
         if (addTooltip) {
             dt.add(new TooltipBehaviour(tooltipBehaviour == null ? null : tooltipBehaviour.getPrefix(), 
                     id, new PropertyModel<Object>(dt,"textFormat")));
@@ -319,6 +334,56 @@ public class BaseForm extends Form<Object> {
                 return false;
         }
         return true;
+    }
+
+    public FormComponent<?> getDicomObjectField(String id, DicomObject dcmObj, int[] tagPath) {
+        VR vr = DicomElementModel.getVRof(dcmObj, tagPath);
+        FormComponent<?> fc;
+        if (vr==VR.DA) {
+            fc = this.getDateField(id, DicomElementModel.newDateModel(dcmObj, tagPath), false);
+        } else if (vr==VR.DT) {
+            fc = this.getSimpleDateTimeField(id, DicomElementModel.newDateModel(dcmObj, tagPath), null, false);
+        } else if (vr==VR.TM) {
+            fc = new TimeField(id, DicomElementModel.newDateModel(dcmObj, tagPath));
+        } else {
+            final String tfClass = ( vr == VR.IS || vr == VR.AE || vr == VR.SH || vr == VR.CS) ?
+                   "textFieldSH" : "textFieldLO";
+            FormComponent<String> fct = new TextField<String>(id, DicomElementModel.newStringModel(dcmObj, tagPath)){
+                 private static final long serialVersionUID = 1L;
+
+                @Override
+                public void onComponentTag(ComponentTag tag) {
+                    super.onComponentTag(tag);
+                    tag.put("class", tfClass);
+                }
+            };
+            fct.add(getDicomTextValidator(vr));
+            fc = fct;
+        }
+        if (fc instanceof ITextFormatProvider) {
+            fc.add(new TooltipBehaviour("dcmfield.", "format", new PropertyModel<Object>(fc,"textFormat")));
+        }
+        return fc;
+    }
+
+    @SuppressWarnings("unchecked")
+    private IValidator<String> getDicomTextValidator(VR vr) {
+        if (vr == VR.AE || vr == VR.SH || vr == VR.CS) {
+            return new StringValidator.MaximumLengthValidator(16);
+        } else if (vr == VR.UI) {
+            return new UIDValidator();
+        } else if (vr == VR.LO || vr == VR.PN) {
+            return new StringValidator.MaximumLengthValidator(64);
+        } else if (vr == VR.ST) {
+            return new StringValidator.MaximumLengthValidator(1024);
+        } else if (vr == VR.LT) {
+            return new StringValidator.MaximumLengthValidator(10240);
+        } else if (vr == VR.IS) {
+            return new StringValidator.MaximumLengthValidator(12);
+        } else if (vr == VR.UT) {
+            return new StringValidator.MaximumLengthValidator(Integer.MAX_VALUE);
+        }
+        return new StringValidator.MaximumLengthValidator(64);
     }
 
 }
