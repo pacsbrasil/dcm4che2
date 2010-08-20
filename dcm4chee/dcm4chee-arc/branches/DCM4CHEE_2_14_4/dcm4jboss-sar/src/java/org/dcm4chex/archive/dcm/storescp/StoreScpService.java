9350910f363550f5d04ad8ec1d2dc8339e108be8
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-08-20 14:16:35 +0000

    [#DCMEE-1460] StoreSCP importFile of a tar file sets filesize to 0.

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/storescp/StoreScpService.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-sar/src/java/org/dcm4chex/archive/dcm/storescp/StoreScpService.java
@@ -847,10 +847,8 @@ public class StoreScpService extends AbstractScpService {
         FileMetaInfo fmi = DcmObjectFactory.getInstance().newFileMetaInfo(cuid,
                 iuid, fileDTO.getFileTsuid());
         ds.setFileMetaInfo(fmi);
-        String fsPath = fileDTO.getDirectoryPath();
         String filePath = fileDTO.getFilePath();
-        File f = FileUtils.toFile(fsPath, filePath);
-        scp.updateDB(store, ds, fileDTO.getFileSystemPk(), filePath, f.length(),
+        scp.updateDB(store, ds, fileDTO.getFileSystemPk(), filePath, fileDTO.getFileSize(),
                 fileDTO.getFileMd5(), true);
         if (last) {
             logInstancesStoredAndSendSeriesStoredNotification(store, seriuid);
