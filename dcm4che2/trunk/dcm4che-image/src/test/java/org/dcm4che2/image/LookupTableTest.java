d276855e88fa835d3920e8aa1a5869f479fe944d
laura_peters <laura_peters@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-10-21 15:54:48 +0000

    [#DCM-440] Added pixel padding functionality to the LookupTables that handle VOILUT.

--- dcm4che2/trunk/dcm4che-image/src/test/java/org/dcm4che2/image/LookupTableTest.java
+++ dcm4che2/trunk/dcm4che-image/src/test/java/org/dcm4che2/image/LookupTableTest.java
@@ -9,7 +9,7 @@ public class LookupTableTest {
     public void testCreateRampLutSigned16C2048W4096Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 3
         LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 2048.0f, 4096.0f,
-                "LINEAR", false, null);
+                "LINEAR", false, null, null, null);
         assertEquals(0, lut.lookup(0));
         assertEquals(0, lut.lookup(-1));
         assertEquals(33, lut.lookup(527));
@@ -22,7 +22,7 @@ public class LookupTableTest {
     public void testCreateRampLutSigned16C2048W1Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 3
         LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 2048.0f, 1.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(0));
         assertEquals(0, lut.lookup(-1));
         assertEquals(0, lut.lookup(527));
@@ -36,7 +36,7 @@ public class LookupTableTest {
     public void testCreateRampLutSigned16C0W100Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 3
         LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 0.0f, 100.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(-51));
         assertEquals(0, lut.lookup(-50));
         assertEquals(3, lut.lookup(-49));
@@ -49,7 +49,7 @@ public class LookupTableTest {
     public void testCreateRampLutSigned16C0W1Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 3
         LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 0.0f, 1.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(-1));
         assertEquals(255, lut.lookup(0));
         assertEquals(255, lut.lookup(1));
@@ -59,7 +59,7 @@ public class LookupTableTest {
     public void testCreateRampLutIdentityUnsigned8C128W256Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 4
         LookupTable lut = LookupTable.createLut(8, false, 8, 1.0f, 0.0f, 128.0f, 256.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(0));
         assertEquals(33, lut.lookup(33));
         assertEquals(128, lut.lookup(128));
@@ -70,7 +70,7 @@ public class LookupTableTest {
     public void testCreateRampLutSigned16C2048W2Output8() {
         // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 5
         LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 2048.0f, 2.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(0));
         assertEquals(0, lut.lookup(-1));
         assertEquals(0, lut.lookup(527));
@@ -86,8 +86,22 @@ public class LookupTableTest {
         // the center and width to 0.5 and 1.0 respectively. The black pixels should render as
         // black.
         LookupTable lut = LookupTable.createLut(12, false, 8, 1.0f, 0.0f, 0.5f, 1.0f, "LINEAR",
-                false, null);
+                false, null, null, null);
         assertEquals(0, lut.lookup(0));
         assertEquals(255, lut.lookup(1));
+    }
+    
+    @Test
+    public void testCreateRampLutSigned16C2048W4096WithPixelPaddingValueOutput8() {
+        // from DICOM PS3.3 - 2009, chapter C.11.2.1.2, note 3
+        LookupTable lut = LookupTable.createLut(16, true, 8, 1.0f, 0.0f, 2048.0f, 4096.0f,
+                "LINEAR", false, null, 4090, null);
+        assertEquals(0, lut.lookup(0));
+        assertEquals(0, lut.lookup(-1));
+        assertEquals(22, lut.lookup(350));
+        assertEquals(33, lut.lookup(527));
+        assertEquals(128, lut.lookup(2048));
+        assertEquals(0, lut.lookup(4090));
+        assertEquals(255, lut.lookup(4095));
     }
 }
