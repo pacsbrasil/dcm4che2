38354581c55bd91207cd2bff0342f05cc4756043
gmeinerj <gmeinerj@a605150a-d416-0410-a114-93ca8beb8bc8>
2010-12-10 14:55:27 +0000

    [#DCM4CHEE-1565] WADO Service does not use autowindowing for icon/jpeg creation

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/WADOSupport.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_4/dcm4jboss-wado/src/java/org/dcm4chex/wado/mbean/WADOSupport.java
@@ -1191,17 +1191,6 @@ public class WADOSupport {
                 }
 
                 DcmImageReadParamImpl dcmParam = (DcmImageReadParamImpl) param;
-                if ((data.get(Tags.WindowCenter) == null)
-                    && (data.get(Tags.WindowCenter) == null)
-                    && (data.get(Tags.SharedFunctionalGroupsSeq) == null)
-                    && (data.get(Tags.RescaleIntercept) == null)
-                    && (data.get(Tags.RescaleSlope) == null)) {
-                    // these result in full white image
-                    // param.setMaskPixelData(false);
-                    log.warn("Possible full white image, investigate!");
-                    dcmParam.setAutoWindowing(false);
-                }
-
                 bi = reader.read(frame, dcmParam);
             } catch (Exception x) {
                 log.error("Can't read image:", x);
