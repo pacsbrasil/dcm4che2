34966afcd2c0ded366c64ced4bdee5a2e127dc2d
garyxu <garyxu@a605150a-d416-0410-a114-93ca8beb8bc8>
2011-01-13 20:12:02 +0000

    Created branch for asb.

---
+++ dcm4chee/dcm4chee-xds/branches/AGILITY_DEV/dcm4chee-xdsb-repo/dcm4chee-xdsb-repo-ws/src/main/java/org/dcm4chee/xds/repo/ws/WSMtomRspHandler.java
@@ -0,0 +1,53 @@
+package org.dcm4chee.xds.repo.ws;
+
+import javax.activation.DataHandler;
+import javax.mail.util.ByteArrayDataSource;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.MessageContext.Scope;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+import org.dcm4chee.xds.common.XDSConstants;
+import org.jboss.ws.core.CommonMessageContext;
+import org.jboss.ws.core.jaxws.handler.GenericSOAPHandler;
+import org.jboss.ws.core.soap.SOAPMessageImpl;
+import org.jboss.ws.extensions.xop.XOPContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WSMtomRspHandler extends GenericSOAPHandler
+{
+    private Logger log = LoggerFactory.getLogger(WSMtomRspHandler.class);
+
+    protected boolean handleInbound(MessageContext msgContext) {
+        return true;
+    }
+    
+    protected boolean handleOutbound(MessageContext msgContext) {
+        try {
+            if ( "true".equals(msgContext.get("DISABLE_FORCE_MTOM_RESPONSE") ) ) {
+                log.info("FORCE MTOM/XOP Response Message is disabled! Return with Simple SOAP!");
+                return true;
+            }
+            
+            SOAPMessage msg = ((SOAPMessageContext)msgContext).getMessage();
+            //CommonMessageContext cmsgContext = MessageContextAssociation.peekMessageContext();
+            CommonMessageContext cmsgContext = (CommonMessageContext) msgContext;
+            if ( msg instanceof SOAPMessageImpl ) {
+                log.info("FORCE MTOM/XOP Response Message!");
+                ByteArrayDataSource DUMMY_PLAIN_DATA_SOURCE = new ByteArrayDataSource("DUMMY", "text/plain");
+                msg.addAttachmentPart(msg.createAttachmentPart(new DataHandler(DUMMY_PLAIN_DATA_SOURCE)));
+                ((SOAPMessageImpl)msg).setXOPMessage(true);
+                Scope currScope=cmsgContext.getCurrentScope();
+                cmsgContext.setCurrentScope(Scope.APPLICATION);
+                XOPContext.setMTOMEnabled(true);
+                cmsgContext.setCurrentScope(currScope);
+            } else {
+                log.warn("Can't force MTOM/XOP Response Message! msg is NOT SOAPMessageImpl!");
+            }
+        } catch (Exception e) {
+            log.error("Can't force MTOM/XOP Response Message! Error:"+e,e);
+        }
+        return true;
+    }
+}
