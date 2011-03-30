85d5be4f4de33bfc35ac54472c0f2940a74a2e1d
mdgmelvin <mdgmelvin@a605150a-d416-0410-a114-93ca8beb8bc8>
2011-03-30 15:51:16 +0000

    [artf60620] Adding behaviour to update attributes from work done on RC branch.

--- dcm4chee/dcm4chee-arc/branches/AGILITY_DEV/dcm4jboss-sar/src/java/org/dcm4chex/archive/mbean/UpdateAttributesService.java
+++ dcm4chee/dcm4chee-arc/branches/AGILITY_DEV/dcm4jboss-sar/src/java/org/dcm4chex/archive/mbean/UpdateAttributesService.java
@@ -197,7 +197,7 @@ public class UpdateAttributesService extends ServiceMBeanSupport {
         return updateSeries(seriesIuid, updateAttributes());
     }
 
-    private int updateSeries(String seriesIuid,
+    protected int updateSeries(String seriesIuid,
             UpdateAttributes updateAttributes) throws Exception {
         Dataset keys = DcmObjectFactory.getInstance().newDataset();
         keys.putUI(Tags.SeriesInstanceUID, seriesIuid);
