b36a1374346f1ba761163237b80807fe079d4796
gunterze <gunterze@a605150a-d416-0410-a114-93ca8beb8bc8>
2011-04-05 12:53:53 +0000

    replace tabs by spaces

--- dcm4che14/trunk/src/java/org/dcm4cheri/image/ImageReaderFactory.java
+++ dcm4che14/trunk/src/java/org/dcm4cheri/image/ImageReaderFactory.java
@@ -73,10 +73,10 @@ public class ImageReaderFactory {
         final String formatName = s.substring(0, delim);
         final String className = s.substring(delim+1);
         for (Iterator it = ImageIO.getImageReadersByFormatName(formatName);
-        		it.hasNext();) {
-		    ImageReader r = (ImageReader) it.next();
-		    if (className.equals(r.getClass().getName()))
-		            return r;
+                it.hasNext();) {
+            ImageReader r = (ImageReader) it.next();
+            if (className.equals(r.getClass().getName()))
+                return r;
         }
         throw new ConfigurationException("No Image Reader of class " + className
                 + " available for format:" + formatName); 
