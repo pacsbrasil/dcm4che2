22d8b855680cfce0de3b63c85ec639c7f2c6b7de
nroduit <nroduit@a605150a-d416-0410-a114-93ca8beb8bc8>
2011-05-08 18:14:54 +0000

    [maven-release-plugin]  copy for tag weasis-pacs-connector-0.5.0

---
+++ weasis/weasis_dcm4chee/tags/weasis-pacs-connector-0.5.0/src/main/java/org/weasis/servlet/Weasis_Launcher.java
@@ -0,0 +1,267 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Weasis Team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Nicolas Roduit - initial API and implementation
+ ******************************************************************************/
+
+package org.weasis.servlet;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.weasis.dcm4che.dicom.DicomNode;
+import org.weasis.dcm4che.dicom.Manifest;
+import org.weasis.launcher.wado.Patient;
+import org.weasis.launcher.wado.WadoParameters;
+import org.weasis.launcher.wado.WadoQuery;
+import org.weasis.launcher.wado.WadoQueryException;
+
+public class Weasis_Launcher extends HttpServlet {
+    private static final long serialVersionUID = 8946852726380985736L;
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Weasis_Launcher.class);
+
+    static final String DEFAULT_JNLP_TEMPLATE_NAME = "launcher.jnlp";
+    static final String JNLP_EXTENSION = ".jnlp";
+    static final String JNLP_MIME_TYPE = "application/x-java-jnlp-file";
+
+    static final String PatientID = "patientID";
+    static final String StudyUID = "studyUID";
+    static final String SeriesUID = "seriesUID";
+    static final String ObjectUID = "objectUID";
+
+    static final Properties pacsProperties = new Properties();
+
+    /**
+     * Constructor of the object.
+     */
+    public Weasis_Launcher() {
+        super();
+    }
+
+    /**
+     * Initialization of the servlet. <br>
+     * 
+     * @throws ServletException
+     *             if an error occurs
+     */
+    @Override
+    public void init() throws ServletException {
+        logger.debug("init() - getServletContext : {} ", getServletConfig().getServletContext());
+        logger.debug("init() - getRealPath : {}", getServletConfig().getServletContext().getRealPath("/"));
+        try {
+            URL config = this.getClass().getResource("/weasis-pacs-connector.properties");
+            logger.info("External configuration file : {}", config);
+            if (config == null) {
+                config = this.getClass().getResource("/weasis-connector-default.properties");
+                logger.info("Default configuration file : {}", config);
+            }
+            if (config != null) {
+                pacsProperties.load(config.openStream());
+            } else {
+                logger.error("Cannot find  a configuration file for weasis-pacs-connector");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * The doGet method of the servlet. <br>
+     * 
+     * This method is called when a form has its tag value method equals to get.
+     * 
+     * @param request
+     *            the request send by the client to the server
+     * @param response
+     *            the response send by the server to the client
+     * @throws ServletErrorException
+     * @throws IOException
+     * @throws ServletException
+     * @throws ServletException
+     *             if an error occurred
+     * @throws IOException
+     *             if an error occurred
+     */
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        try {
+            logRequestInfo(request);
+            response.setContentType(JNLP_MIME_TYPE);
+
+            String wadoQueriesURL = pacsProperties.getProperty("pacs.wado.url", "http://localhost:8080/wado");
+            String pacsAET = pacsProperties.getProperty("pacs.aet", "DCM4CHEE");
+            String pacsHost = pacsProperties.getProperty("pacs.host", "localhost");
+            int pacsPort = Integer.parseInt(pacsProperties.getProperty("pacs.port", "11112"));
+            DicomNode dicomSource = new DicomNode(pacsAET, pacsHost, pacsPort);
+            String componentAET = pacsProperties.getProperty("aet", "WEASIS");
+            List<Patient> patients = getPatientList(request, dicomSource, componentAET);
+
+            String wadoQueryFile = "";
+
+            if (patients == null || patients.size() < 1) {
+                logger.warn("No data has been found!");
+                response.sendError(HttpServletResponse.SC_NO_CONTENT, "No data has been found!");
+                return;
+            }
+            try {
+                // If the web server requires an authentication (pacs.web.login=user:pwd)
+                String webLogin = pacsProperties.getProperty("pacs.web.login", null);
+                if (webLogin != null) {
+                    webLogin =
+                        new String(org.apache.commons.codec.binary.Base64.encodeBase64(webLogin.trim().getBytes()));
+                }
+                boolean onlysopuid = Boolean.valueOf(pacsProperties.getProperty("wado.onlysopuid"));
+                String addparams = pacsProperties.getProperty("wado.addparams", "");
+                String overrideTags = pacsProperties.getProperty("wado.override.tags", null);
+
+                WadoParameters wado = new WadoParameters(wadoQueriesURL, onlysopuid, addparams, overrideTags, webLogin);
+                WadoQuery wadoQuery =
+                    new WadoQuery(patients, wado, pacsProperties.getProperty("pacs.db.encoding", "utf-8"));
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                WadoQuery.gzipCompress(new ByteArrayInputStream(wadoQuery.toString().getBytes()), outStream);
+                wadoQueryFile = Base64.encodeBase64String(outStream.toByteArray());
+
+            } catch (WadoQueryException e) {
+                logger.error(e.getMessage());
+                return;
+            }
+
+            InputStream is = null;
+            try {
+                is = getClass().getResourceAsStream("/launcher.xml");
+                BufferedReader dis = new BufferedReader(new InputStreamReader(is));
+
+                // response.setContentLength(launcherStr.length());
+
+                PrintWriter outWriter = response.getWriter();
+                String s;
+                while ((s = dis.readLine()) != null) {
+                    if (s.startsWith("**arg-xml**")) {
+                        outWriter.print("<argument>$dicom:get -i ");
+                        outWriter.print(wadoQueryFile);
+                        outWriter.print("</argument>");
+                    } else {
+                        // TODO set in properties
+                        s = s.replaceAll("--cdb--", "http://localhost:8080/weasis");
+                        outWriter.println(s);
+                    }
+                }
+                outWriter.close();
+
+            } catch (MalformedURLException mue) {
+                mue.printStackTrace();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException ioe) {
+                    // just going to ignore this
+                }
+
+            }
+        } catch (Exception e) {
+            logger.error("doGet(HttpServletRequest, HttpServletResponse)", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * The doPost method of the servlet. <br>
+     * 
+     * This method is called when a form has its tag value method equals to post.
+     * 
+     * @param request
+     *            the request send by the client to the server
+     * @param response
+     *            the response send by the server to the client
+     * @throws ServletException
+     *             if an error occurred
+     * @throws IOException
+     *             if an error occurred
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            response.setContentType(JNLP_MIME_TYPE);
+
+        } catch (Exception e) {
+            logger.error("doHead(HttpServletRequest, HttpServletResponse)", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Destruction of the servlet. <br>
+     */
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    /**
+     * @param request
+     */
+    protected void logRequestInfo(HttpServletRequest request) {
+        logger.debug("logRequestInfo(HttpServletRequest) - getRequestQueryURL : {}{}", request.getRequestURL()
+            .toString(), request.getQueryString() != null ? ("?" + request.getQueryString().trim()) : "");
+        logger.debug("logRequestInfo(HttpServletRequest) - getContextPath : {}", request.getContextPath());
+        logger.debug("logRequestInfo(HttpServletRequest) - getRequestURI : {}", request.getRequestURI());
+        logger.debug("logRequestInfo(HttpServletRequest) - getServletPath : {}", request.getServletPath());
+    }
+
+    private List<Patient> getPatientList(HttpServletRequest request, DicomNode dicomSource, String componentAET) {
+        String pat = request.getParameter(PatientID);
+        String stu = request.getParameter(StudyUID);
+        String ser = request.getParameter(SeriesUID);
+        String obj = request.getParameter(ObjectUID);
+        List<Patient> patients = null;
+        try {
+            if (obj != null) {
+                patients = Manifest.buildFromSopInstanceUID(dicomSource, componentAET, obj);
+            } else if (ser != null) {
+                patients = Manifest.buildFromSeriesInstanceUID(dicomSource, componentAET, ser);
+            } else if (stu != null) {
+                patients = Manifest.buildFromStudyInstanceUID(dicomSource, componentAET, stu);
+            } else if (pat != null) {
+                patients = Manifest.buildFromPatientID(dicomSource, componentAET, pat);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return patients;
+    }
+}
