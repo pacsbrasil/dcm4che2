3c5546782cfabc4fcb5612cd3cbda440d38c966e
robertdavid <robertdavid@a605150a-d416-0410-a114-93ca8beb8bc8>
2011-11-16 15:29:55 +0000

    [#WEB-612] On empty ae group dropdown only the ae titles without group are shown, should be all

--- dcm4chee/dcm4chee-web/trunk/dcm4chee-web-ejb/src/main/java/org/dcm4chee/web/dao/ae/AEHomeBean.java
+++ dcm4chee/dcm4chee-web/trunk/dcm4chee-web-ejb/src/main/java/org/dcm4chee/web/dao/ae/AEHomeBean.java
@@ -69,10 +69,10 @@ public class AEHomeBean implements AEHomeLocal {
 
     @SuppressWarnings("unchecked")
     public List<AE> findAll(String filter) {
-        String filterQuery = filter == null ? "WHERE aeGroup IS NULL" : 
-            "*".equals(filter) ? "" : "WHERE aeGroup = :filter";
+        String filterQuery = filter == null ? "" : 
+            "-".equals(filter) ? "WHERE aeGroup IS NULL" : "WHERE aeGroup = :filter";
         Query query = em.createQuery("FROM AE ae " + filterQuery + " ORDER BY ae.title, ae.aeGroup");
-        if (filter != null) query.setParameter("filter", filter);
+        if (filter != null && !"-".equals(filter)) query.setParameter("filter", filter);
         List<AE> l = query.getResultList();
         em.clear();
         return l;
