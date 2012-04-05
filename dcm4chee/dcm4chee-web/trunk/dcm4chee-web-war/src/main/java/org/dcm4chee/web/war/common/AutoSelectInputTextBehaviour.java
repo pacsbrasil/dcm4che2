d2002c005084538caa02834ea4656d8101f3fc14
awsjn <awsjn@a605150a-d416-0410-a114-93ca8beb8bc8>
2012-04-05 12:33:41 +0000

    [#WEB-741] Automatically select text within TC search text-based input components when getting the focus

---
+++ dcm4chee/dcm4chee-web/trunk/dcm4chee-web-war/src/main/java/org/dcm4chee/web/war/common/AutoSelectInputTextBehaviour.java
@@ -0,0 +1,19 @@
+package org.dcm4chee.web.war.common;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.markup.ComponentTag;
+
+/**
+ * @author Bernhard Ableitinger <bernhard.ableitinger@agfa.com>
+ * @since Apr 04, 2012
+ */
+public class AutoSelectInputTextBehaviour extends AbstractBehavior 
+{
+    @Override
+    public void onComponentTag(final Component component, final ComponentTag tag)
+    {
+        tag.put("onblur","this.selectionStart=this.selectionEnd=-1");
+        tag.put("onfocus","this.select()");
+    }
+}
