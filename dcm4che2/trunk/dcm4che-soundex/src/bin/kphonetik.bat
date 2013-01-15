fa282b396f1656b19437078f9b9db68e52191b12
laura_peters <laura_peters@a605150a-d416-0410-a114-93ca8beb8bc8>
2013-01-15 19:30:57 +0000

    [#DCM-549] Changing the version to 2.0.28-SNAPSHOT as 2.0.27 has been released. checked by dsmith1547

--- dcm4che2/trunk/dcm4che-soundex/src/bin/kphonetik.bat
+++ dcm4che2/trunk/dcm4che-soundex/src/bin/kphonetik.bat
@@ -9,7 +9,7 @@ if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
 set MAIN_CLASS=org.dcm4che2.soundex.KPhonetik
-set MAIN_JAR=dcm4che-soundex-2.0.27.jar
+set MAIN_JAR=dcm4che-soundex-2.0.28-SNAPSHOT.jar
 
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
