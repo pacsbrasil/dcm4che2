67e7794970f1529bf6f75c3b59f69271c2272dbd
waveform <waveform@a605150a-d416-0410-a114-93ca8beb8bc8>
2013-05-29 19:49:21 +0000

    [#DCMEE-1967] Add file-system-related properties to Study, Series, and Instance entities

--- dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_2_BRANCHA/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/entity/SeriesBean.java
+++ dcm4chee/dcm4chee-arc/branches/DCM4CHEE_2_14_2_BRANCHA/dcm4jboss-ejb/src/java/org/dcm4chex/archive/ejb/entity/SeriesBean.java
@@ -995,6 +995,20 @@ public abstract class SeriesBean implements EntityBean {
     }
 
     /**
+     * @ejb.select query="SELECT OBJECT(f) FROM File f WHERE f.instance.series.pk = ?1 AND f.fileSystem.pk = ?2"
+     *             transaction-type="Supports"
+     */
+    public abstract Collection ejbSelectFiles(java.lang.Long series_fk, java.lang.Long filesystem_fk)
+            throws FinderException;
+
+    /**    
+     * @ejb.interface-method
+     */
+    public Collection getFiles(Long fsPk) throws FinderException {      
+        return ejbSelectFiles(getPk(), fsPk);
+    }
+
+    /**
      * @ejb.select query="SELECT COUNT(f) FROM File f WHERE f.instance.series.pk = ?1 AND f.fileSystem.pk = ?2"
      *             transaction-type="Supports"
      */
@@ -1021,4 +1035,18 @@ public abstract class SeriesBean implements EntityBean {
     public Collection getInstances(Long fsPk) throws FinderException {      
         return ejbSelectInstances(getPk(), fsPk);
     }
+
+    /**
+     * @ejb.select query="SELECT OBJECT(f) FROM File f WHERE f.instance.series.pk = ?1"
+     *             transaction-type="Supports"
+     */
+    public abstract Collection ejbSelectAllFiles(java.lang.Long series_fk)
+            throws FinderException;
+
+    /**    
+     * @ejb.interface-method
+     */
+    public Collection getAllFiles() throws FinderException {      
+        return ejbSelectAllFiles(getPk());
+    }    
 }
