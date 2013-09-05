fed2ad4e302764d6badc0ddfb8fab16543f74445
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2013-09-05 13:01:51 +0000

    [#WEB-1037] Fix usage of DatePicker

--- dcm4chee/dcm4chee-web-common/trunk/src/main/java/org/dcm4chee/web/common/markup/SimpleDateTimeField.java
+++ dcm4chee/dcm4chee-web-common/trunk/src/main/java/org/dcm4chee/web/common/markup/SimpleDateTimeField.java
@@ -41,6 +41,7 @@ package org.dcm4chee.web.common.markup;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.IBehavior;
@@ -85,7 +86,15 @@ public class SimpleDateTimeField extends FormComponentPanel<Date> implements ITe
                     protected DateTimeFormatter getFormat() {
                     	return DateTimeFormat.forPattern(DateUtils.getDatePattern(dateField));
                     }                    
-            });
+                    @Override
+                    public Date convertToObject(String value, Locale locale) {
+                        if (value != null && value.length()==1 && Character.isDigit(value.charAt(0))) {
+                            long t = System.currentTimeMillis() - 86400000*(value.charAt(0)-0x30);
+                            return new Date(t);
+                        }
+                        return super.convertToObject(value, locale);
+                    }
+           });
         dateField.add(new DatePicker() {
         	
             private static final long serialVersionUID = 1L;
