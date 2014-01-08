8a6bfa692f1fc65695f4ad4aeb3aee1dc3ca1b1c
awsjn <awsjn@a605150a-d416-0410-a114-93ca8beb8bc8>
2014-01-08 15:23:13 +0000

    [#WEB-1092] Teaching-Files: Fixed removal of keywords

--- dcm4chee/dcm4chee-web/branches/DCM4CHEE_WEB_3_0_4-RC/dcm4chee-web-war/src/main/java/org/dcm4chee/web/war/tc/TCEditableObject.java
+++ dcm4chee/dcm4chee-web/branches/DCM4CHEE_WEB_3_0_4-RC/dcm4chee-web-war/src/main/java/org/dcm4chee/web/war/tc/TCEditableObject.java
@@ -205,11 +205,17 @@ public class TCEditableObject extends TCObject {
     }
     
     public void addKeyword(ITextOrCode keyword) {
-        addKeywordImpl(keyword);
+        if (addKeywordImpl(keyword))
+        {
+        	modified = true;
+        }
     }
 
     public void removeKeyword(ITextOrCode keyword) {
-        removeKeywordImpl(keyword);
+        if (removeKeywordImpl(keyword))
+        {
+        	modified = true;
+        }
     }
 
     public void setLevel(Level level) {
