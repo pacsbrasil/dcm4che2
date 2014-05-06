770cf2ab39a835bd103d2a3e9703f7db96209c19
javawilli <javawilli@a605150a-d416-0410-a114-93ca8beb8bc8>
2014-05-06 14:08:39 +0000

    [#DCM-584] FileCache.free() throws NPE if directory is not readable.

--- dcm4che2/trunk/dcm4che-filecache/src/test/java/org/dcm4che2/filecache/FileCacheTest.java
+++ dcm4che2/trunk/dcm4che-filecache/src/test/java/org/dcm4che2/filecache/FileCacheTest.java
@@ -10,25 +10,28 @@ import org.junit.Test;
 
 public class FileCacheTest {
 
-	@Test
-	public void testJournalDate() throws IOException {
-		FileCache fc = new FileCache();
-		fc.setCacheRootDir(new File("/wado2cache"));
-		fc.setJournalRootDir(new File("/wado2cache/Journal"));
-		File f1 = new File("/wado2cache/Journal/2013/01/05/15");
-		f1.mkdirs();
-		Date d = fc.getJournalDate(f1);
-		assertEquals("Dates should be the same", new Date(1357416000000l), d);
-	}
+    @SuppressWarnings("deprecation")
+    private static final Date DATE = new Date(113,0,5,15,0);
+    
+    @Test
+    public void testJournalDate() throws IOException {
+        FileCache fc = new FileCache();
+        fc.setCacheRootDir(new File("/wado2cache"));
+        fc.setJournalRootDir(new File("/wado2cache/Journal"));
+        File f1 = new File("/wado2cache/Journal/2013/01/05/15");
+        f1.mkdirs();
+        Date d = fc.getJournalDate(f1);
+        assertEquals("Dates should be the same", DATE, d);
+    }
 
-	@Test
-	public void testJournalOldestDate() throws IOException {
-		FileCache fc = new FileCache();
-		fc.setCacheRootDir(new File("/wado2cache"));
-		fc.setJournalRootDir(new File("/wado2cache/Journal"));
-		File f1 = new File("/wado2cache/Journal/2013/01/05/15");
-		f1.mkdirs();
-		Date d = fc.findOldestJournalDate();
-		assertTrue("Should find a date no newer than the test directory date of 2013-01-05 15", d.compareTo(new Date(1357416000000l))<=0);
-	}
+    @Test
+    public void testJournalOldestDate() throws IOException {
+        FileCache fc = new FileCache();
+        fc.setCacheRootDir(new File("/wado2cache"));
+        fc.setJournalRootDir(new File("/wado2cache/Journal"));
+        File f1 = new File("/wado2cache/Journal/2013/01/05/15");
+        f1.mkdirs();
+        Date d = fc.findOldestJournalDate();
+        assertTrue("Should find a date no newer than the test directory date of 2013-01-05 15", d.compareTo(DATE)<=0);
+    }
 }
